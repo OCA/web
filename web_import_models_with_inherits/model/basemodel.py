4c46339fbe766d6e969131ba47065544c6e4d67e
Stefan Rijnhart <stefan@therp.nl>
2013-02-04 21:32:51 +0100

    [ADD] Module to allow the import of models with _inherits 	(workaround for lp:930318)

---
+++ web_import_models_with_inherits/model/basemodel.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Therp BV (<http://therp.nl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.osv.orm import BaseModel
+
+def get_fields_inherits(self, cr, uid, context=None):
+    """
+    Pass the values of the _inherits dictionary
+    """
+    if not self._inherits:
+        return []
+    return self._inherits.values()
+
+BaseModel.get_fields_inherits = get_fields_inherits
