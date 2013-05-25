7b47f091099b0b9e757d59ff37314873fe6ef09b
Nhomar Hernandez <nhomar@gmail.com>
2013-05-25 00:03:02 -0530

    [FIX] Licence was not right in the __openerp__.py

--- web_nocreatedb/__openerp__.py
+++ web_nocreatedb/__openerp__.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#
 #    Financed and Planified by Vauxoo
 #    developed by: nhomar@vauxoo.com
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
+#    it under the terms of the Licence: 
+#    
+#    WTFPL – Do What the Fuck You Want to Public License 
+#    
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -16,7 +15,6 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 ##############################################################################
 
 {
@@ -24,6 +22,7 @@
     'author': "Vauxoo",
     'category': "Web",
     'website': "http://vauxoo.com",
+    'licence': "WTFPL",
     'description': """
 After install this module, you will not see anymore the
 "Manage Databases" link in login screen.
