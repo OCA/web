56f8490fe5598ec16500b0ab2f78ee1ee3f5ac43
Holger Brunn <hbrunn@therp.nl>
2013-05-26 07:12:55 +0200

    [ADD] web_treeview_text_newlines

---
+++ web_treeview_text_newlines/static/src/js/web_treeview_text_newlines.js
@@ -0,0 +1,35 @@
+/* -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Therp BV (<http://therp.nl>)
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+############################################################################*/
+
+openerp.web_treeview_text_newlines = function(openerp)
+{
+    var format_cell_original = openerp.web.format_cell;
+    openerp.web.format_cell = function(row_data, column, options)
+    {
+        result = format_cell_original(row_data, column, options);
+        if(column.type == 'text')
+        {
+            result = result.replace(/\n/g, '<br/>');
+        }
+        return result;
+    }
+}
