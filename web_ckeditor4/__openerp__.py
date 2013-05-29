db9239cec8fed17c5c5e9a0e7548feb42cf711cb
Holger Brunn <hbrunn@therp.nl>
2013-05-29 21:31:33 +0200

    [IMP] mention which widget names when to use

--- web_ckeditor4/__openerp__.py
+++ web_ckeditor4/__openerp__.py
@@ -25,6 +25,9 @@
     'version': '1.0',
     'description': """
     This addon provides a widget for editing html fields via CKEditor 4.x
+
+    Use widget="text_html" if you need just html display. In the unlikely case
+    you need specific features of ckeditor, use widget="text_ckeditor4".
     """,
     'author': 'Therp BV',
     'website': 'http://www.therp.nl',
