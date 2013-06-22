0ec76a2c2cd6cbadbb1ebee347bca2e89c3100fa
Nhomar Hernandez <nhomar@gmail.com>
2013-06-22 09:15:39 -0530

    [FIX] Licence without bad words.

--- web_nocreatedb/__openerp__.py
+++ web_nocreatedb/__openerp__.py
@@ -3,14 +3,20 @@
 #    Financed and Planified by Vauxoo
 #    developed by: tulio@vauxoo.com
 #
+
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the Licence: 
-#    
-#    WTFPL – Do What the Fuck You Want to Public License 
-#    
+#    it under the terms of the GNU General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+#
 ##############################################################################
 
 {
@@ -18,7 +24,6 @@
     'author': "Vauxoo",
     'category': "Web",
     'website': "http://vauxoo.com",
-    'licence': "WTFPL",
     'description': """
 After install this module, you will not see anymore the
 "Manage Databases" link in login screen.
