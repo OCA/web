d5e4988657e9336e908fbf112cf584336ec55761
Augustin Cisterne-Kaas <augustin.cisternekaas@elico-corp.com>
2014-03-13 22:58:40 +0800

    [FIX] polymorphic widget is set to readonly if linked model value is none

--- web_many2one_polymorphic/static/src/js/view_form.js
+++ web_many2one_polymorphic/static/src/js/view_form.js
@@ -41,6 +41,23 @@ openerp.web_polymorphic = function (instance) {
                 self.field.relation = self.field_manager.get_field_value(self.polymorphic);              
             });
             this._super();
+            this.set_polymorphic_event();
+            this.set({
+                readonly: true
+            });
+
+        },
+        set_polymorphic_event: function() {
+            self = this;
+            this.field_manager.fields[this.polymorphic].$el.on(
+                'change', function(){
+                    field_value = self.field_manager.get_field_value(self.polymorphic);
+                    if(field_value !== false)
+                        self.set("effective_readonly", false);
+                    else
+                        self.set("effective_readonly", true);
+                }
+            );
         }
     });
     instance.web.form.widgets.add('polymorphic', 'instance.web.form.FieldPolymorphic')
