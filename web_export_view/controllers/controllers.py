01872a582b26f63f467db67b48a64549f5dc59eb
Adil Houmadi <ah@taktik.be>
2014-08-02 11:05:20 +0200

    [FIX] : see => https://github.com/OCA/web/pull/27#discussion-diff-15447231

--- web_export_view/controllers/controllers.py
+++ web_export_view/controllers/controllers.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2012 Agile Business Group sagl (<http://www.agilebg.com>)
 #    Copyright (C) 2012 Domsense srl (<http://www.domsense.com>)
+#    Copyright (C) 2012-2013 Agile Business Group sagl (<http://www.agilebg.com>)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
