376c2901ca55e9c77c9cefccb51b50cedc084c91
Karim Omran <karim.m.omran@gmail.com>
2014-08-27 01:15:36 +0300

    fixed flake8 errors

--- color_theme/__openerp__.py
+++ color_theme/__openerp__.py
@@ -1,28 +1,29 @@
 {
-	'name':'Color Theme',
-	'version': '0.1',
-	'category': 'Tools',
-	'author': 'K-Omran',
-	'description': """
+    'name': 'Color Theme',
+    'version': '0.1',
+    'category': 'Tools',
+    'author': 'K-Omran',
+    'description': """
 This module allows user to easily change OpenERP color theme.
 =============================================================
 This module allows user To define new color theme and save it for future use.\n
-Each user can select any stored color theme as his theme and also can make his own theme.\n
+Each user can select any stored color theme as his theme\n
+and also can make his own theme.\n
 special thanks to "Zesty Beanz Technologies" for their contributions.
 """,
 
-	'depends': ['base', 'web'],
-	'data': ['security/ir.model.access.csv',
-			'views/color_theme_view.xml',],
-	'js': ['static/lib/colorpicker/js/colorpicker.js',
-			'static/lib/colorpicker/js/eye.js',
-			'static/lib/colorpicker/js/utils.js',
-			'static/lib/colorpicker/js/layout.js?ver=1.0.2',
-			'static/src/js/color_theme.js',],
+    'depends': ['base', 'web', ],
+    'data': ['security/ir.model.access.csv',
+            'views/color_theme_view.xml', ],
+    'js': ['static/lib/colorpicker/js/colorpicker.js',
+            'static/lib/colorpicker/js/eye.js',
+            'static/lib/colorpicker/js/utils.js',
+            'static/lib/colorpicker/js/layout.js?ver=1.0.2',
+            'static/src/js/color_theme.js', ],
 
-	'qweb': [],
-	'css': ['static/lib/colorpicker/css/colorpicker.css',
-			'static/lib/colorpicker/css/layout.css',],
+    'qweb': [],
+    'css': ['static/lib/colorpicker/css/colorpicker.css',
+            'static/lib/colorpicker/css/layout.css', ],
 
-	'web_preload': False,
+    'web_preload': False,
 }
--- color_theme/color_theme.py
+++ color_theme/color_theme.py
@@ -1,42 +1,41 @@
 from openerp.osv import orm, fields
+
+
 # ============================Color Theme Model==============================
 
+
 class color_theme(orm.Model):
-	"""This Model To Store Color Themes To be Selectd Later And Applied To System Users"""
+    """This Model To Store Color Themes To be Selectd Later"""
 
+    _name = 'color.theme'
 
-	_name = 'color.theme'
+    _rec_name = 'theme_name'
 
-	_rec_name = 'theme_name'
+    _columns = {'theme_name': fields.char('Theme Name', size=128, required=True, ),
 
-	_columns = {
+                'top_bar_gradient_1': fields.char('Top Bar Gradient First Color', size=6, ),
 
-				'theme_name': fields.char('Theme Name', size=128, required=True, ),
+                'top_bar_gradient_2': fields.char('Top Bar Gradient Second Color', size=6, ),
 
-				'top_bar_gradient_1': fields.char('Top Bar Gradient First Color', size=6, ),
+                'button_gradient_1': fields.char('Button Background Gradient First Color', size=6, ),
 
-				'top_bar_gradient_2': fields.char('Top Bar Gradient Second Color', size=6, ),
+                'button_gradient_2': fields.char('Button Background Gradient Second Color', size=6, ),
 
-				'button_gradient_1': fields.char('Button Background Gradient First Color', size=6, ),
+                'left_bar': fields.char('Left Bar Color', size=6, ),
 
-				'button_gradient_2': fields.char('Button Background Gradient Second Color', size=6, ),
+                'body_font': fields.char('Body Font Color', size=6, ),
 
-				'left_bar': fields.char('Left Bar Color', size=6, ),
+                'main_menu_font': fields.char('Main Menu Font Color', size=6, ),
 
-				'body_font': fields.char('Body Font Color', size=6, ),
+                'sub_menu_font': fields.char('Sub Menu Font Color', size=6, ),
 
-				'main_menu_font': fields.char('Main Menu Font Color', size=6, ),
+                'top_bar_menu_font': fields.char('Top Bar Menu Font Color', size=6, ),
 
-				'sub_menu_font': fields.char('Sub Menu Font Color', size=6, ),
+                'tab_string': fields.char('Tab String Font Color', size=6, ),
 
-				'top_bar_menu_font': fields.char('Top Bar Menu Font Color', size=6, ),
-
-				'tab_string': fields.char('Tab String Font Color', size=6, ),
-
-				'many2one_font': fields.char('Link Color', size=6, ),
-
+                'many2one_font': fields.char('Link Color', size=6, ),
                 }
 
-	_sql_constraints = [('unique_theme_name', 'unique(theme_name)', 'Theme With The Same Name Already Exists'), ]
+    _sql_constraints = [('unique_theme_name', 'unique(theme_name)', 'Theme With The Same Name Already Exists'), ]
 
 # ==============================================================================================
