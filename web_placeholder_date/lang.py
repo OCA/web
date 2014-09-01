5d08796955d424f2900169731ba15806d00cc01c
Cristian Salamea <cristian@ayni.io>
2014-09-01 16:21:16 -0500

    PEP8

--- web_placeholder_date/lang.py
+++ web_placeholder_date/lang.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from openerp.osv import fields, osv
+from openerp.osv import fields, orm
 
-class ResLang(osv.osv):
+class ResLang(orm.Model):
     _inherit = 'res.lang'
 
     def get_placeholder(self, cr, uid, lang_code='en_US'):
@@ -11,10 +11,10 @@ class ResLang(osv.osv):
         formatting years ? 
         """
         to_replace = {"%d": 'dd', "%m": 'mm', "%Y": 'YYYY'}
-        ids = self.search(cr, uid, [('code','=',lang_code)])
+        ids = self.search(cr, uid, [('code', '=', lang_code)])
         lang_data = self.read(cr, uid, ids, ['date_format'])
         date_format = lang_data[0]['date_format']
-        for k,v in to_replace.items():
-            date_format = date_format.replace(k,v)
+        for k, v in to_replace.items():
+            date_format = date_format.replace(k, v)
         return date_format
         
