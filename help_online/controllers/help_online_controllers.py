8064eab318e25b0cc33ea4ea3b81c59fcdb9abe0
Laurent Mignon <laurent.mignon@acsone.eu>
2014-09-19 08:38:06 +0200

    [ADD] Module  help_online
    This module allows the creation of an online help available from the
    lists and forms in Odoo.
    
    When loading a view, the module generates a button allowing access to
    an help page for the related model if the page exists and the user is
    member of the group 'Help reader'. If the page doesn't exist and the
    user is member of the group 'Help writer', the module generate a button
    allowing the creation an help page.
    
    The help pages are created and managed via the website Module.

---
+++ help_online/controllers/help_online_controllers.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Authors: Mignon Laurent
+#    Copyright (c) 2014 Acsone SA/NV (http://www.acsone.eu)
+#    All Rights Reserved
+#
+#    WARNING: This program as such is intended to be used by professional
+#    programmers who take the whole responsibility of assessing all potential
+#    consequences resulting from its eventual inadequacies and bugs.
+#    End users who are looking for a ready-to-use solution with commercial
+#    guarantees and support are strongly advised to contact a Free Software
+#    Service Company.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import openerp.http as http
+from openerp.http import request
+
+
+class HelpOnlineController(http.Controller):
+
+    @http.route('/help_online/build_url', type='json', auth='user')
+    def build_url(self, model, view_type, domain=None, context=None):
+        help_online_model = request.env['help.online']
+        return help_online_model.get_page_url(
+            model, view_type, domain=None, context=None)
