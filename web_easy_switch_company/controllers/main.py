5b02c327025b35aca24156e4c8d60594a4bb9159
Sylvain LE GAL <sylvain.legal@grap.coop>
2014-10-15 00:46:07 +0200

    [FIX] 'web_easy_switch_company' flake8

--- web_easy_switch_company/controllers/main.py
+++ web_easy_switch_company/controllers/main.py
@@ -25,10 +25,14 @@ from openerp.http import request
 
 
 class WebEasySwitchCompanyController(http.Controller):
-    @http.route('/web_easy_switch_company/switch/change_current_company', type='json', auth='none')
+    @http.route(
+        '/web_easy_switch_company/switch/change_current_company',
+        type='json', auth='none')
     def change_current_company(self, company_id):
-        registry = openerp.modules.registry.RegistryManager.get(request.session.db)
+        registry = openerp.modules.registry.RegistryManager.get(
+            request.session.db)
         uid = request.session.uid
         with registry.cursor() as cr:
-            res = registry.get("res.users").change_current_company(cr, uid, company_id)
+            res = registry.get("res.users").change_current_company(
+                cr, uid, company_id)
             return res
