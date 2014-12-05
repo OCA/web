cd89ceef9aed766df8f8e180ddac488d8e59dd3a
Stefan Rijnhart <stefan@therp.nl>
2014-12-05 19:02:10 +0100

    web_tree_widget

---
+++ web_tree_image/__openerp__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2014 Therp BV (<http://therp.nl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Show images in tree views",
+    "version": "1.0",
+    "author": "Therp BV",
+    "description": """\
+This module defines a tree image widget, to be used with function fields of
+type character. Use widget='tree_image' in your view definition. Optionally, set
+a 'height' tag. Default height is 16px.
+
+The content of the field can be any of the following:
+
+* the absolute or relative location of an image. For example, \
+"/<module>/static/src/img/youricon.png"
+
+* a standard icon from the web distribution, without path or extension, For \
+example, 'gtk-open'
+
+* A dynamic image in a data url base 64 format. To show a regular image field \
+from the model, use a function field wrapper that retrieves the image with \
+bin_size=False in the context, and prefix with 'data:image/png;base64,'
+    """,
+    'depends': [
+        'web',
+    ],
+    'qweb': [
+        'static/src/xml/widget.xml',
+    ],
+    'data': [
+        'view/assets.xml',
+    ],
+}
