e497db67805f6be1ee733c06c171022ec39600fa
Stefan Rijnhart <stefan@therp.nl>
2014-12-08 10:28:21 +0100

    [RFR] Move module description to README [FIX] rst line endings [IMP] Comment on arbitrary media subtype

--- web_tree_image/__openerp__.py
+++ web_tree_image/__openerp__.py
@@ -25,23 +25,6 @@
     "name": "Show images in tree views",
     "version": "1.0",
     "author": "Therp BV",
-    "description": """\
-This module defines a tree image widget, to be used with either binary fields
-or (function) fields of type character. Use widget='tree_image' in your view
-definition. Optionally, set a 'height' tag. Default height is 16px.
-
-If you use the widget with a character field, the content of the field can be
-any of the following:
-
-* the absolute or relative location of an image. For example, \
-"/<module>/static/src/img/youricon.png"
-
-* a standard icon from the web distribution, without path or extension, For \
-example, 'gtk-open'
-
-* A dynamic image in a data url base 64 format. Prefix with \
-'data:image/png;base64,'
-    """,
     'url': 'https://github.com/OCA/Web',
     'depends': [
         'web',
