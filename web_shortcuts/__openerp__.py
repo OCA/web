6aafa521004ded513b9536876e285afdffdc26f5
Adrien Peiffer <adrien.peiffer@acsone.eu>
2014-12-14 22:16:28 +0100

    [IMP] Move description in readme.rst

--- web_shortcuts/__openerp__.py
+++ web_shortcuts/__openerp__.py
@@ -22,15 +22,6 @@
     'name': 'Web Shortcuts',
     'version': '1.0',
     'category': 'Tools',
-    'description': """
-Enable shortcuts feature in the web client.
-===========================================
-
-Add a Shortcut icon in the systray in order to access the user's shortcuts
-(if any).
-
-Add a Shortcut icon besides the views title in order to add/remove a shortcut.
-    """,
     'author': 'OpenERP SA',
     'website': 'http://openerp.com',
     'depends': ['base'],
