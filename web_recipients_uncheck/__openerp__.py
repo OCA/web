d1481309111fee499af01e56bfd199d0d9239355
luc-demeyer <luc.demeyer@noviat.com>
2014-12-20 22:32:51 +0100

    V8 port of web_recipients_uncheck

---
+++ web_recipients_uncheck/__openerp__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2010-2013 OpenERP s.a. (<http://openerp.com>).
+#    Copyright (C) 2013 initOS GmbH & Co. KG (<http://www.initos.com>).
+#    Author Katja Matthes <katja.matthes at initos.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Uncheck recipients on res.partner',
+    'version': '0.3',
+    'author': 'initOS GmbH & Co. KG',
+    'category': 'Web',
+    'description': """Uncheck additional recipients and followers when
+sending a message at the customer view.
+    """,
+    'website': 'http://www.initos.com',
+    'license': 'AGPL-3',
+    'images': [],
+    'depends': ['mail'],
+    'data': [
+        'view/web_recipients_uncheck.xml',
+    ],
+}
