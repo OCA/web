ee4510383eb7565825b32cac5b502d65d04f9922
Nicolas JEUDY <nicolas@sudokeys.com>
2015-01-02 17:43:58 +0100

    fix: rename module to web_widget_text_markdown

---
+++ web_widget_text_markdown/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'web_widget_text_markdown',
+    'version': '1.0',
+    'author': 'Sudokeys',
+    'maintainer': 'Sudokeys',
+    'category': '',
+    'license': 'AGPL-3',
+    'depends': ['base','web'],
+    'website': 'http://www.sudokey.com',
+    'data': ['views/main.xml',],
+    "qweb": ["static/src/xml/bootstrap_markdown.xml",
+             ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False
+}
