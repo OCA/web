2f939bbb03b7e59a92f45bdd22c4039219d762bf
Nicolas JEUDY <nicolas@sudokeys.com>
2015-01-05 12:24:37 +0100

    fix: obscur wrong path on last commit. recreate portage from scratch.

---
+++ web_m2x_options/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": 'web_m2x_options',
+    "version": "8.0.0.2",
+    "depends": [
+        'base',
+        'web',
+    ],
+    'data': ['views/view.xml'],
+    "author": "0k.io,Odoo Community Association (OCA)",
+    "installable": True,
+    "active": False,
+}
