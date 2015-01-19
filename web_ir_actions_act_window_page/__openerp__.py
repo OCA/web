79b98813496051e06089a1d0c19da2998602bce0
Stefan Rijnhart <stefan@therp.nl>
2015-01-19 11:02:27 +0100

    [PRT] Port 8.0 of web_ir_actions_act_window_page

---
+++ web_ir_actions_act_window_page/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013-2015 Therp BV (<http://therp.nl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Window actions for client side paging",
+    "summary": ("Allows a developer to trigger a pager to show the previous "
+                "or next next record in the form view by returning the "
+                "appropriate window action"),
+    "version": "1.0",
+    "author": "Therp BV",
+    "category": "Dependency",
+    "depends": ['web'],
+    'data': ['view/qweb.xml'],
+    'installable': True,
+}
