4f637157c80472dd0631e5da63239167bf5a2fb4
Holger Brunn <hbrunn@therp.nl>
2015-01-27 09:23:24 +0100

    [FIX] change category to 'Hidden/Dependency'

--- web_context_in_colors/__openerp__.py
+++ web_context_in_colors/__openerp__.py
@@ -24,7 +24,7 @@
     "version" : "1.1",
     "author" : "Therp BV",
     "complexity": "normal",
-    "category" : "Dependency",
+    "category" : "Hidden/Dependency",
     "depends" : [
         'web',
     ],
