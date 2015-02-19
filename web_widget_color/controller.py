b8d7a5cbb9a47c22f773571480985e6d5c9d888d
Adil Houmadi <ah@taktik.be>
2015-02-19 13:37:34 +0100

    [IMP] : - Rename the module to web_widget_color - Remove special type (Color) and use (Char) instead of.

---
+++ web_widget_color/controller.py
@@ -0,0 +1,61 @@
+# -*- encoding: utf-8 -*-
+############################################################################
+#
+# OpenERP, Open Source Web Color
+# Copyright (C) 2012 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+# Copyright (C) 2014 Anybox <http://anybox.fr>
+# Copyright (C) 2015 Taktik SA <http://taktik.be>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# @author Étienne Beaudry Auger <etienne.b.auger@savoirfairelinux.com>
+# @author Adil Houmadi <ah@taktik.be>
+#
+##############################################################################
+import openerp.http as http
+from openerp.http import request
+from os.path import join, abspath, exists
+import mimetypes
+
+
+class JsColor(http.Controller):
+    @http.route("/jscolor/<string:image>", type="http", auth="user")
+    def jscolor(self, image):
+        addons_path = http.addons_manifest['web_widget_color']['addons_path']
+        path = join(
+            addons_path,
+            'web_widget_color',
+            'static',
+            'lib',
+            'jscolor',
+            image
+        )
+        if not exists(path):
+            return request.not_found()
+        try:
+            image_file = open(abspath(path))
+            image_data = image_file.read()
+            image_file.close()
+            mime_type = mimetypes.guess_type(path)
+            if len(mime_type) > 1:
+                mime_type = mime_type[0]
+            else:
+                return request.not_found()
+        except:
+            return request.not_found()
+        headers = [
+            ('Content-Type', '%s' % mime_type),
+            ('Content-Length', len(image_data)),
+        ]
+        return request.make_response(image_data, headers)
---
+++ web_widget_color/static/src/js/widget.js
@@ -0,0 +1,50 @@
+openerp.web_widget_color = function (instance) {
+
+    instance.web.form.widgets.add('color', 'instance.web.form.FieldColor');
+
+    instance.web.search.fields.add('color', 'instance.web.search.CharField');
+
+    instance.web.form.FieldColor = instance.web.form.FieldChar.extend({
+        template: 'FieldColor',
+        widget_class: 'oe_form_field_color',
+        is_syntax_valid: function () {
+            var $input = this.$('input');
+            if (!this.get("effective_readonly") && $input.size() > 0) {
+                var val = $input.val();
+                var isOk = /^#[0-9A-F]{6}$/i.test(val);
+                if (!isOk) {
+                    return false;
+                }
+                try {
+                    this.parse_value(this.$('input').val(), '');
+                    return true;
+                } catch (e) {
+                    return false;
+                }
+            }
+            return true;
+        },
+        render_value: function () {
+            var show_value = this.format_value(this.get('value'), '');
+            if (!this.get("effective_readonly")) {
+                var $input = this.$el.find('input');
+                $input.val(show_value);
+                $input.css("background-color", show_value)
+                jscolor.init(this.$el[0]);
+            } else {
+                this.$(".oe_form_char_content").text(show_value);
+                this.$('div').css("background-color", show_value)
+            }
+        }
+    });
+
+    /*
+     * Init jscolor for each editable mode on view form
+     */
+    instance.web.FormView.include({
+        to_edit_mode: function () {
+            this._super();
+            jscolor.init(this.$el[0]);
+        }
+    });
+};
