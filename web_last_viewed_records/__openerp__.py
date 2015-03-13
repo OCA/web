9045f88a46aab13d29c6486252dd48c7fd007fde
Ivan Yelizariev <ivann.exe@gmail.com>
2015-03-13 17:41:40 +0200

    whitespace cleanup

--- web_last_viewed_records/__openerp__.py
+++ web_last_viewed_records/__openerp__.py
@@ -1,14 +1,14 @@
 {
-    'name' : 'Last viewed records',
-    'version' : '1.0.0',
-    'author' : 'Ivan Yelizariev, Odoo Community Association (OCA)',
-    'category' : 'Base',
-    'website' : 'https://yelizariev.github.io',
-    'depends' : ['web', 'mail'],
-    'data':[
+    'name': 'Last viewed records',
+    'version': '1.0.0',
+    'author': 'Ivan Yelizariev, Odoo Community Association (OCA)',
+    'category': 'Base',
+    'website': 'https://yelizariev.github.io',
+    'depends': ['web', 'mail'],
+    'data': [
         'views.xml',
         ],
-    'qweb' : [
+    'qweb': [
         "static/src/xml/*.xml",
     ],
     'installable': True
