97332bcd6406c177bb6a1f627fb38ae055a26829
Ivan Yelizariev <ivann.exe@gmail.com>
2015-03-16 12:57:49 +0200

    Check existence of a localStorage variable.
    In browsers without localStorage variable links to viewed records are
    stored only for current browser session (i.e. until user close browser tab)

--- web_last_viewed_records/static/src/js/main.js
+++ web_last_viewed_records/static/src/js/main.js
@@ -1,4 +1,9 @@
 openerp.web_last_viewed_records = function(instance){
+    var localStorage = {};
+    if (typeof window.localStorage !== "undefined"){
+        localStorage = window.localStorage;
+    }
+
     instance.web.ActionManager.include({
         last_viewed_history_var: 'odoo_last_viewed',
         last_viewed_history_size: 8,
