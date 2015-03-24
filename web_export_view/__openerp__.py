ed234d5478c34100613694b5343321aea11f5335
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2015-03-24 08:34:51 +0100

    [ADD] icon and README

--- web_export_view/__openerp__.py
+++ web_export_view/__openerp__.py
@@ -23,26 +23,6 @@
     'name': 'Export Current View',
     'version': '1.2',
     'category': 'Web',
-    'description': """
-WEB EXPORT VIEW
-===============
-
-One of the best OpenERP’s features is exporting custom data to CSV/XLS. You can
-do it by clicking on the export link in the sidebar. The export action allows
-use to configure what to be exported by selecting fields, etc, and allows you
-to save your export as a template so that you can export it once again without
-having to configure it again.
-
-That feature is as great and advanced as limited for an everyday experience.
-A lot of customers want simply to export the tree view they are looking to.
-
-If you miss this feature as us, probably you’ll find an answer into our
-web_export_view module.
-
-After you installed it, you’ll find an additional link ‘Export current view’
-right below the ‘Export’ one. By clicking on it you’ll get a XLS file contains
-the same data of the tree view you are looking at, headers included.
-""",
     'author': "Agile Business Group,Odoo Community Association (OCA)",
     'website': 'http://www.agilebg.com',
     'license': 'AGPL-3',
