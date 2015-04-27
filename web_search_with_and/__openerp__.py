e47ca7ed6fb9bed289c596ee2f695224586ffc15
Andrius Preimantas <andrius@versada.lt>
2015-04-27 17:56:12 +0300

    Squash of 8 commits
    Add README.rst
    
    Too short underline for module title in README.rst
    
    Improving module meta information
    
    Version 1.0
    
    W391 blank line at end of file
    
    Remove module description because README.rst is there
    
    web_search_with_and: Latest OCA conventions

---
+++ web_search_with_and/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2015 Andrius Preimantas <andrius@versada.lt>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "Use AND conditions on omnibar search",
+    'version': '8.0.1.0.0',
+    'author': 'Versada UAB, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'category': 'web',
+    'website': 'http://www.versada.lt',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'data/data.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
