dcd96786fde0b407a209251609fcd202b2ce8491
Holger Brunn <hbrunn@therp.nl>
2015-05-01 12:18:16 +0200

    add web_searchbar_full_width

---
+++ web_searchbar_full_width/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2015 Therp BV <http://therp.nl>.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Full width searchbar",
+    "version": "8.0.1.0.0",
+    "author": "Therp BV, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "summary": "Show search bar in full screen width",
+    "depends": [
+        'web',
+    ],
+    "data": [
+        'views/templates.xml',
+    ],
+    "qweb": [
+        'static/src/xml/web_searchbar_full_width.xml',
+    ],
+    "test": [
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+    "external_dependencies": {
+        'python': [],
+    },
+}
