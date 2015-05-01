30636b65eb9ad042bc6f8ce065e6d9984a36660f
Holger Brunn <hbrunn@therp.nl>
2015-05-01 17:11:21 +0200

    [ADD] web_hide_menu

---
+++ web_hide_menu/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2015 Therp BV <http://therp.nl>.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Hide menus",
+    "version": "1.0",
+    "author": "Therp BV",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "summary": "Hide top and left menu bar",
+    "depends": [
+        'web',
+    ],
+    "data": [
+        "data/ir_config_parameter.xml",
+        'views/templates.xml',
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+    "external_dependencies": {
+        'python': [],
+    },
+}
---
+++ web_hide_menu/static/src/js/web_hide_menu.js
@@ -0,0 +1,160 @@
+//-*- coding: utf-8 -*-
+//############################################################################
+//
+//   OpenERP, Open Source Management Solution
+//   This module copyright (C) 2015 Therp BV <http://therp.nl>.
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU Affero General Public License as
+//   published by the Free Software Foundation, either version 3 of the
+//   License, or (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU Affero General Public License for more details.
+//
+//   You should have received a copy of the GNU Affero General Public License
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//############################################################################
+
+openerp.web_hide_menu = function(instance)
+{
+    instance.web.WebClient.include({
+        show_bar_treshold: 10,
+        hide_delay: 10000,
+        leftbar_hide_timeout_id: null,
+        main_menu_hide_timeout_id: null,
+        start: function()
+        {
+            var self = this;
+            return this._super.apply(this, arguments)
+            .then(function()
+            {
+                var addon_name = 'web_hide_menu',
+                    parameters = _.map(
+                        ['show_bar_treshold', 'hide_delay'],
+                        function(a) { return addon_name + '.' + a });
+                return (new openerp.web.Model('ir.config_parameter'))
+                .query(['key', 'value'])
+                .filter([['key', 'in', parameters]])
+                .all()
+                .then(function(params)
+                {
+                    _.each(params, function(param)
+                    {
+                        self[param.key.replace(addon_name + '.', '')] =
+                            parseInt(param.value);
+                    });
+                });
+            })
+            .then(function()
+            {
+                self.$el
+                .bind('mousemove', _.bind(self.on_mousemove, self));
+            })
+        },
+        show_application: function()
+        {
+            this._super.apply(this, arguments);
+            openerp.client.toggle_main_menu(false, this.hide_delay);
+        },
+        toggle_bars: function(hide)
+        {
+            this.toggle_main_menu(hide);
+            this.toggle_left_bar(hide);
+        },
+        toggle_menu_element(selector, timeout_id, show, delay)
+        {
+            if(this[timeout_id])
+            {
+                clearTimeout(this[timeout_id]);
+                this[timeout_id] = null;
+            }
+            if(delay)
+            {
+                this[timeout_id] = setTimeout(
+                    _.bind(
+                        this.toggle_menu_element, this,
+                        selector, timeout_id, show),
+                    delay);
+            }
+            else
+            {
+                this.$(selector).toggle(show);
+            }
+        },
+        toggle_main_menu: function(show, delay)
+        {
+            this.toggle_menu_element(
+                '#oe_main_menu_navbar', 'main_menu_hide_timeout_id', show,
+                delay);
+        },
+        toggle_left_bar: function(show, delay)
+        {
+            this.toggle_menu_element(
+                '.oe_leftbar', 'leftbar_hide_timeout_id', show, delay);
+        },
+        on_mousemove: function(e)
+        {
+            var on_main_menu = jQuery(e.srcElement)
+                .parents('#oe_main_menu_navbar').length > 0,
+                on_left_bar = jQuery(e.srcElement)
+                .parents('.oe_leftbar').length > 0;
+            if(on_left_bar && openerp.client.leftbar_hide_timeout_id)
+            {
+                clearTimeout(openerp.client.leftbar_hide_timeout_id);
+                openerp.client.leftbar_hide_timeout_id = null;
+            }
+            if(on_main_menu && openerp.client.main_menu_hide_timeout_id)
+            {
+                clearTimeout(openerp.client.main_menu_hide_timeout_id);
+                openerp.client.main_menu_hide_timeout_id = null;
+            }
+            if(!on_left_bar && !openerp.client.leftbar_hide_timeout_id)
+            {
+                this.toggle_left_bar(false, openerp.client.hide_delay);
+            }
+            if(!on_main_menu && !openerp.client.main_menu_hide_timeout_id)
+            {
+                this.toggle_main_menu(false);
+            }
+            if(e.pageX < this.show_bar_treshold)
+            {
+                this.toggle_left_bar(true);
+            }
+            if(e.pageY < this.show_bar_treshold)
+            {
+                this.toggle_main_menu(true);
+            }
+        },
+    });
+    instance.web.Menu.include({
+        start: function()
+        {
+            this.on('menu_click', this, this.on_menu_click_with_action);
+            openerp.client.toggle_left_bar(false, openerp.client.hide_delay);
+            return this._super.apply(this, arguments);
+        },
+        on_top_menu_click: function()
+        {
+            this.top_menu_click = true;
+            return this._super.apply(this, arguments);
+        },
+        on_menu_click_with_action: function()
+        {
+            this.close_menu = !this.top_menu_click;
+            this.top_menu_click = false;
+        },
+        open_menu: function()
+        {
+            this._super.apply(this, arguments);
+            if(this.close_menu)
+            {
+                openerp.client.toggle_left_bar(false);
+            }
+            this.close_menu = false;
+        },
+    });
+}
