2d6d3707f7af3fefba72573d0123e655c2da8563
Holger Brunn <hbrunn@therp.nl>
2015-05-06 17:44:45 +0200

    [REN] to web_advanced_search_x2x
    and replace company icon

---
+++ web_advanced_search_x2x/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2015 Therp BV <http://therp.nl>.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Search x2x fields",
+    "version": "1.0",
+    "author": "Therp BV",
+    "license": "AGPL-3",
+    "category": "Usability",
+    "summary": "Use a search widget in advanced search for x2x fields",
+    "depends": [
+        'web',
+    ],
+    "data": [
+        'views/templates.xml',
+    ],
+    "qweb": [
+        'static/src/xml/web_advanced_search_x2x.xml',
+    ],
+    "test": [
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+    "external_dependencies": {
+        'python': [],
+    },
+}
