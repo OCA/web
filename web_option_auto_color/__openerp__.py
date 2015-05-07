e57c4bd930837d1072ca06bb395e8de5936f91b4
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2015-05-07 18:41:04 +0200

    [IMP] Rename web_widget_auto_color to web_option_auto_color

--- web_option_auto_color/__openerp__.py
+++ web_option_auto_color/__openerp__.py
@@ -1,29 +1,29 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#     This file is part of web_widget_auto_color,
+#     This file is part of web_option_auto_color,
 #     an Odoo module.
 #
 #     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
 #
-#     web_widget_auto_color is free software:
+#     web_option_auto_color is free software:
 #     you can redistribute it and/or modify it under the terms of the GNU
 #     Affero General Public License as published by the Free Software
 #     Foundation,either version 3 of the License, or (at your option) any
 #     later version.
 #
-#     web_widget_auto_color is distributed
+#     web_option_auto_color is distributed
 #     in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
 #     even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 #     PURPOSE.  See the GNU Affero General Public License for more details.
 #
 #     You should have received a copy of the GNU Affero General Public License
-#     along with web_widget_auto_color.
+#     along with web_option_auto_color.
 #     If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 {
-    'name': "Web Widget Auto Color",
+    'name': "Web Option Auto Color",
     'author': "ACSONE SA/NV,Odoo Community Association (OCA)",
     'website': "http://acsone.eu",
     'category': 'web',
@@ -33,7 +33,7 @@
         'web',
     ],
     'data': [
-        'views/web_widget_auto_color.xml',
+        'views/web_option_auto_color.xml',
     ],
     'qweb': [
         'static/src/xml/templates.xml',
