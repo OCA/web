69af1776d7e5025b59a2120e7fc31c57bfc359af
Andrius Preimantas <andrius@versada.lt>
2015-05-14 17:18:14 +0300

    [FIX] Remove module description because README.rst is there

--- web_search_with_and/__openerp__.py
+++ web_search_with_and/__openerp__.py
@@ -8,25 +8,7 @@
     'author': 'Versada UAB, Odoo Community Association (OCA)',
     'category': 'web',
     'website': 'http://www.versada.lt',
-    'description': """
-When searching for records on same field Odoo joins multiple queries with OR.
-For example:
-
-* Perform a search for customer "John" on field Name
-* Odoo displays customers containing "John"
-* Search for "Smith" on same field Name
-* Odoo displays customers containing "John" OR "Smith"
-
-With this module installed you can press Shift key before searching for "Smith"
-and Odoo finds customers containing "John" AND "Smith"
-
-Usage
-=====
-
-* Enter your value in Search Input field
-* Press and hold Shift key
-* Select field with mouse or keyboard to perform search on
-    """,
+    'description': "",
     'depends': [
         'web',
     ],
