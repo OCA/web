7a48445c182cd3f230046ad3e2688cf3ae8a171a
Damien Crier <damien.crier@camptocamp.com>
2015-06-05 07:56:44 +0200

    [FIX] pep8 [FIX] remove unuseful comments

--- web_warning_on_save/static/src/js/web_warning_on_save.js
+++ web_warning_on_save/static/src/js/web_warning_on_save.js
@@ -10,17 +10,12 @@ openerp.web_warning_on_save = function (instance) {
 				var model = self.model;
 				var param = {'model': model, 
 						'id': self.datarecord.id ? self.datarecord.id : id};
-//				try {
 				self.rpc('/web/warning_on_save/check_warning_on_save', param)
 				.done(function(res) {
 					if (res != false){
 						alert(res);
 					}
 				});
-//				}
-//				catch(err){
-//					// nothing
-//				}
 			})
 	    },
 	});
