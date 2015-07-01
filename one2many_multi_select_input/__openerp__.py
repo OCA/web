42cd764b91868907aa37745fda385d71ffee8e9f
szufisher <szufisher@gmail.com>
2015-07-01 16:43:18 +0800

    Create __openerp__.py

---
+++ one2many_multi_select_input/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'paste_from_clipboard',
+    'category': 'Hidden',
+    'version': '1.0',
+    'description':
+        """
+
+
+        """,
+    'depends': ['web'],
+    'auto_install': False,
+    'data': [
+        'views/one2many_multi_select_input.xml',
+    ],
+    'qweb' : [
+        
+    ],
+}
