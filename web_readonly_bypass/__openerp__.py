b8e22636429f7c1e3fb0ce44aab7bf4f8a35abed
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-07-02 10:27:05 +0200

    [IMP] web_readonly_bypass: port to OCA from acsone-addons

--- web_readonly_bypass/__openerp__.py
+++ web_readonly_bypass/__openerp__.py
@@ -3,14 +3,6 @@
 #
 #    Authors: Nemry Jonathan & Laetitia Gangloff
 #    Copyright (c) 2014 Acsone SA/NV (http://www.acsone.eu)
-#    All Rights Reserved
-#
-#    WARNING: This program as such is intended to be used by professional
-#    programmers who take the whole responsibility of assessing all potential
-#    consequences resulting from its eventual inadequacies and bugs.
-#    End users who are looking for a ready-to-use solution with commercial
-#    guarantees and support are strongly advised to contact a Free Software
-#    Service Company.
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -30,32 +22,16 @@
     'name': 'Read Only ByPass',
     'version': '1.0',
     "author": "ACSONE SA/NV",
-    "maintainer": "ACSONE SA/NV",
+    "maintainer": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "http://www.acsone.eu",
     'category': 'Technical Settings',
     'depends': [
-        'base',
         'web',
     ],
-    'description': """
-Read Only ByPass
-================
-This Module provides a solution to the problem of the interaction between
-'readonly' attribute and 'on_change' attribute when used together
-
-Behavior: add `readonly_fields` changed by `on_change` into the fields passing
-into an update or create. If `filter_out_readonly` is into the context and set
-True then apply native behavior.
-    """,
-    'images': [],
+    'summary': 'Allow to save onchange modifications to readonly fields',
     'data': [
         'views/readonly_bypass.xml',
     ],
-    'qweb': [
-    ],
-    'css': [],
-    'demo': [],
-    'test': [],
     'installable': True,
     'auto_install': False,
 }
