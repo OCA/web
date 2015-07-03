5eb3f759d073f205f1126dca3cff11d7326112a9
Pierre Verkest <pverkest@anybox.fr>
2015-07-03 09:48:35 +0200

    Add README.rst, fix formating

--- web_readonly_bypass/__openerp__.py
+++ web_readonly_bypass/__openerp__.py
@@ -77,8 +77,10 @@ Bug Tracker
 
 Bugs are tracked on `GitHub Issues <https://github.com/OCA/web/issues>`_.
 In case of trouble, please check there if your issue has already been reported.
-If you spotted it first, help us smashing it by providing a detailed and welcomed feedback
-`here <https://github.com/OCA/web/issues/new?body=module:%20web_readonly_bypass%0Aversion:%208.0.1.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+If you spotted it first, help us smashing it by providing a detailed and
+welcomed feedback `here <https://github.com/OCA/web/issues/new?body=module:%20
+web_readonly_bypass%0Aversion:%208.0.1.0%0A%0A**Steps%20to%20reproduce**%0A-%20
+...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
 
 
 Credits
