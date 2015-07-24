366cb8325b70a06fb6e7dc4585a0255101aabc6d
Damien Crier <damien.crier@camptocamp.com>
2015-07-24 08:59:42 +0200

    [ADD] add module 'web_widget_color_tree_field'

---
+++ web_widget_color_tree_field/__openerp__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Damien Crier
+#    Copyright 2015 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'web_widget_color_tree_field',
+    'category': 'Hidden',
+    'version': '1.0',
+    'depends': ['web'],
+    'author': "Camptocamp,Odoo Community Association (OCA)",
+    'license': 'AGPL-3',
+    'website': 'http://www.camptocamp.com',
+    'data': [
+        'views/web_widget_color_tree_field.xml',
+    ],
+    'qweb': [
+        'static/xml/*.xml',
+    ],
+    'auto_install': False
+}
