edaf79cb7d4630db206243daff33e2af0e6d16fc
oihane <oihanecruce@gmail.com>
2015-08-11 12:21:21 +0200

    [IMP] <web_group_expand> Make it work for v8 Adapt to OCA

--- web_group_expand/__openerp__.py
+++ web_group_expand/__openerp__.py
@@ -1,19 +1,19 @@
 {
-    "name" : "Group Expand Buttons",
-    "category": "Hidden",
-    "version" : '1.0',
-    "author" : 'OpenERP SA',
-    "description":
-        """
-A group by list can be expanded and collapased with buttons
-===============================================================
-You'll see two buttons appear on top right corner of the list when you perform a group by with which you can expand and collapse grouped records by level.
-        """,
-    "depends" : ["web"],
-    "js": ["static/src/js/web_group_expand.js"],
-     'qweb' : ["static/src/xml/expand_buttons.xml"],
-     'css' : ["static/src/css/expand_buttons.css"],
-    'installable': True,
-    'auto_install': False,
+    "name": "Group Expand Buttons",
+    "category": "Web",
+    "version": "1.0",
+    "author": "OpenERP SA, "
+              "AvanzOSC, "
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "Odoo Community Association (OCA)",
+    "depends": [
+        "web"
+    ],
+    "qweb": [
+        "static/src/xml/expand_buttons.xml",
+    ],
+    "data": [
+        "views/templates.xml",
+    ],
+    "installable": True,
 }
-
