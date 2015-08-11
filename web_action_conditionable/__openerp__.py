0f3dbe67442d433a1dcfe4b4e00bbe1d6eac2ce7
Cristian Salamea <ovnicraft@gmail.com>
2015-08-10 19:22:41 -0500

    Web addon
    This module implements condition for actions in tree view.
    do allows: edit=“false” create=“false” delete=“false”
    This module allows too: delete=“state==‘draft’”, simple expression.

---
+++ web_action_conditionable/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": 'web_action_conditionable',
+    "version": "0.1",
+    "depends": [
+        'base',
+        'web',
+    ],
+    'data': ['views/view.xml'],
+    "author": "Cristian Salamea,Odoo Community Association (OCA)",
+    "installable": True,
+    "active": False,
+}
