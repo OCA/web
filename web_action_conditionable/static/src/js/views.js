2a7ef6c985735196910ebf91807f9fb448bf3f5c
Cristian Salamea <ovnicraft@gmail.com>
2015-08-11 10:42:13 -0500

    FIX
    testing in all cases is better catch errno in JSON.parse.
    attrs[action] is always “string” type.

--- web_action_conditionable/static/src/js/views.js
+++ web_action_conditionable/static/src/js/views.js
@@ -5,9 +5,10 @@ openerp.web_action_conditionable = function (instance) {
     is_action_enabled: function(action) {
       var attrs = this.fields_view.arch.attrs;
       if (action in attrs) {
-	if ($.type(attrs[action]) == 'boolean') {
-	  return JSON.parse(attrs[action])
-	} else {
+	try {
+	  data = JSON.parse(attrs[action]);
+	  return data;
+	} catch(error) {
 	  var expr = attrs[action];
 	  var expression = py.parse(py.tokenize(expr));
 	  var cxt = this.dataset.get_context().__eval_context.__contexts[1];
