e71ca6dfe1d8365e7a451bab8d8810a6c5182e01
Cristian Salamea <ovnicraft@gmail.com>
2015-08-10 19:22:41 -0500

    [add] web_action_conditionable

---
+++ web_action_conditionable/__openerp__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": 'web_action_conditionable',
+    "version": "0.1",
+    "depends": [
+        'base',
+        'web',
+    ],
+    'data': ['views/view.xml'],
+    "author": "Cristian Salamea,Odoo Community Association (OCA)",
+    "installable": True,
+}
