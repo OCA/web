ee10aaf1e76965a539f940e6ab168f4b92f0b490
Holger Brunn <hbrunn@therp.nl>
2015-08-31 14:01:23 +0200

    [FIX] simplify code

--- web_menu_autohide/static/src/js/web_menu_autohide.js
+++ web_menu_autohide/static/src/js/web_menu_autohide.js
@@ -145,10 +145,8 @@ openerp.web_menu_autohide = function(instance)
                 .parents(this.navbar_query).length > 0,
                 on_left_bar = jQuery(e.target)
                 .parents(this.leftbar_query).length > 0;
-            if(!on_left_bar && !on_main_menu && openerp.client.leftbar_hide_timeout_id)
+            if(!on_left_bar && !on_main_menu)
             {
-                clearTimeout(openerp.client.leftbar_hide_timeout_id);
-                openerp.client.leftbar_hide_timeout_id = null;
                 this.toggle_left_bar(false);
             }
         },
