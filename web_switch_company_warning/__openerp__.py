a582b8c68ec66eeffa99f9cb34acd73950b20634
hparfr <raph@hpar.fr>
2015-09-30 09:54:08 +0200

    [FIX] web_switch_company_warning
    Fix travis build stuff.

--- web_switch_company_warning/__openerp__.py
+++ web_switch_company_warning/__openerp__.py
@@ -3,10 +3,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Multicompany - Switch Company Warning",
-    "summary": "Shows a warning if current company has been switched in another tab or window."
+    "summary": "Show a warning if current company has been switched"
+    " in another tab or window."
     "version": "0.1",
     "category": "web",
-    "website": "https://github.com/OCA/web/blob/8.0/web_switch_company_warning",
+    "website": "http://akretion.com",
     "license": "AGPL-3",
     "author": "Akretion / Odoo Community Association (OCA)",
     "depends": [
