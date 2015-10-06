efb28067065b0a374422d1f68c13f080fc0203ad
AIM Systems <phillips@aimsystems.ca>
2015-10-06 17:02:19 -0400

    Oct6_2015_01

---
+++ web_toggle/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'AIM Bootstrap Toggle Widget',
+    'version': '1.0',
+    'summary': 'Odoo Checkbox Widget',
+    'category': 'Tools',
+    'description':
+        """
+AIM Checkbox Toggle Widget
+=================
+        """,
+    'data': [
+        "toggle_widget.xml",
+    ],
+    'depends' : ['base'],
+    'qweb': ['static/src/xml/*.xml'],
+    'application': True,
+}
