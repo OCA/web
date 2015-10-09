26a4091dc73eb855a508e56b5d17f64f29d18040
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:03:39 +0200

    [UPD] prefix versions with 8.0

--- web_advanced_search_x2x/__openerp__.py
+++ web_advanced_search_x2x/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Search x2x fields",
-    "version": "1.0",
+    "version": "8.0.1.0.0",
     "author": "Therp BV, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
