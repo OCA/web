9b36ae7ca921403d266af1cffdec8a96517f7b80
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-10-14 02:57:05 +0200

    [MIG] Make modules uninstallable

--- web_widget_x2many_2d_matrix/__openerp__.py
+++ web_widget_x2many_2d_matrix/__openerp__.py
@@ -38,7 +38,7 @@
     "test": [
     ],
     "auto_install": False,
-    "installable": True,
+    'installable': False,
     "application": False,
     "external_dependencies": {
         'python': [],
