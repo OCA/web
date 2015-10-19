df2caca8ee16fb234cc0d47d3a3796ec41dcc788
Pierre Verkest <pverkest@anybox.fr>
2015-10-19 18:24:18 +0200

    attribute type

--- web_widget_many2many_tags_multi_selection/__openerp__.py
+++ web_widget_many2many_tags_multi_selection/__openerp__.py
@@ -23,7 +23,7 @@
     'name': 'Tags multiple selection',
     'version': '7.0.1.0',
     'author': 'Akretion, Anybox, Odoo Community Association (OCA)',
-    'descriptions': """
+    'description': """
 .. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
    :alt: License: AGPL-3
