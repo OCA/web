79254f1f2daadf0841f8de83b814b27a40848d1b
Nikolina Todorova <nikolina.todorova@initos.com>
2015-10-23 09:03:13 +0200

    Chnage company name

--- web_text_widget/__openerp__.py
+++ web_text_widget/__openerp__.py
@@ -3,7 +3,7 @@
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2010-2015 OpenERP s.a. (<http://openerp.com>).
-#    Copyright (C) 2015 initOS GmbH & Co. KG (<http://www.initos.com>).
+#    Copyright (C) 2015 initOS GmbH (<http://www.initos.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
