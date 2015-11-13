1419fdb98cce994ad74a264a6b9281675d8c8847
oihane <oihanecruce@gmail.com>
2015-11-13 09:18:09 +0100

    web_decimal_numpad_dot: now changes . for , in floating numbers

--- web_decimal_numpad_dot/static/src/js/numpad_dot.js
+++ web_decimal_numpad_dot/static/src/js/numpad_dot.js
@@ -1,23 +1,20 @@
 (function() {
 
-var instance = openerp;
+    var instance = openerp;
 
-instance.web.form.FieldFloat = instance.web.form.FieldChar.extend({
-    is_field_number: true,
-    widget_class: 'oe_form_field_float',
-    events: {
-        "keyup": "floatKeyup",
-    },
-    floatKeyup: function(e){
-        var code = e.which ? e.which : e.keyCode;
-
-        if (code === 110){
-            var input = this.$("input").val();
-            input = input.substr(0, input.length -1);
-            input += instance.web._t.database.parameters.decimal_point;
-            this.set("value", input);
-        }
-    },
-});
-
+    instance.web.form.FieldFloat = instance.web.form.FieldChar.extend({
+        is_field_number: true,
+        widget_class: 'oe_form_field_float',
+        events: {
+            "keypress": "floatKeypress",
+        },
+        floatKeypress: function(e){
+            if(e.keyCode == '46' || e.charCode == '46'){
+                //Cancel the keypress
+                e.preventDefault();
+                // Add the comma to the value of the input field
+                 this.$("input").val(this.$("input").val() + ',');
+            }
+        },
+    });
 })();
