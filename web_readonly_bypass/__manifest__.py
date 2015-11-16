c39506fcac87a2530a0690a7749a63d80db9141f
Jonathan Nemry (ACSONE) <jonathan.nemry@acsone.eu>
2015-11-16 14:54:57 +0100

    [FIX] include one2many popup into the readonly_by_pass feature

--- web_readonly_bypass/__manifest__.py
+++ web_readonly_bypass/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Read Only ByPass',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "maintainer": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "http://www.acsone.eu",
