18ea28898a895d7f63885c0bc494657db39a5074
fossoult <thomas.fossoul@noviat.com>
2015-11-25 14:32:00 +0100

    OCA new guidelines remove readme and add description on openerp.py remove demo images and add new demo v7.0 image

--- web_tree_date_search/__openerp__.py
+++ web_tree_date_search/__openerp__.py
@@ -1,30 +1,72 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (c) 2015 Noviat nv/sa (www.noviat.com)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Thomas Fossoul, Noviat <thomas.fossoul@noviat.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Tree dates search',
-    'version': '8.0.3.0.1',
+    'version': '7.0.3.0.1',
     'author': 'Noviat, Odoo Community Association (OCA)',
     'website': 'http://www.noviat.com',
     'license': 'AGPL-3',
     'category': 'Web',
+    'description': """
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License
+Tree Date Search
+===============================
+This module allows you to easily add dates range search fields on top of
+tree View rows.
+
+These dates fields can be used in combination with the Search window.
+This module requires developer skills to be used.
+
+Usage
+=====
+
+Add a new 'dates_filter' key and a list of fields name in the action context:
+{'dates_filter': ['start']}
+or several dates:
+{'dates_filter': ['start','stop',..]}
+
+.. image:: web_tree_date_search/static/src/img/demo.png
+
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/162/7.0
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/OCA/web/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed `feedback
+<https://github.com/OCA/
+web/issues/new?body=module:%20
+web_tree_date_search%0Aversion:%20
+7.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+
+Credits
+=======
+
+Author
+------
+* Thomas Fossoul, Noviat <thomas.fossoul@noviat.com>
+
+Maintainer
+----------
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
+    """,
     'depends': [
         'web',
     ],
