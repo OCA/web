fcc13436ec185b8e422e30f91b1ae86de1acb0ac
eLBati <lorenzo.battistini@agilebg.com>
2015-11-26 14:35:27 +0100

    ADD web_widget_digital_signature_sale_order

---
+++ web_widget_digital_signature_sale_order/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright 2015 Lorenzo Battistini - Agile Business Group
+#
+#    About License, see __openerp__.py
+#
+##############################################################################
+
+from . import sale_order
