71210c651b3ea6d674f18b118c8f92511cd66ce6
Holger Brunn <hbrunn@therp.nl>
2015-12-09 21:30:36 +0100

    [ADD] web_search_autocomplete_prefetch

---
+++ web_search_autocomplete_prefetch/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2015 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Prefetch autocomplete offers",
+    "version": "8.0.1.0.0",
+    "author": "Therp BV,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Usability",
+    "summary": "Offer only items on autocompletion that will yield results",
+    "depends": [
+        'web',
+    ],
+    "images": [
+        'images/web_search_autocomplete_prefetch.png',
+    ],
+    "data": [
+        'views/templates.xml',
+    ],
+    "installable": True,
+}
---
+++ web_search_autocomplete_prefetch/static/src/js/web_search_autocomplete_prefetch.js
@@ -0,0 +1,87 @@
+//-*- coding: utf-8 -*-
+//© 2015 Therp BV <http://therp.nl>
+//License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+openerp.web_search_autocomplete_prefetch = function(instance)
+{
+    // overwrite this or use it to recycle the functionality for your own field
+    openerp.web_search_autocomplete_prefetch.complete = function(self, value, data)
+    {
+        if(self.options['web_search_autocomplete_prefetch.disable'])
+        {
+            return data;
+        }
+        var facet = {
+            get: function(name)
+            {
+                switch(name)
+                {
+                    case 'value': return value;
+                    case 'operator': return 'ilike';
+                }
+            },
+        },
+        domain = new instance.web.CompoundDomain(
+            self.get_domain({values: [facet]}),
+            self.view.dataset.domain);
+        domain.set_eval_context(self.view.dataset.get_context());
+        return self.view.dataset._model.call(
+            'search_count', [domain.eval()])
+            .then(function(count)
+            {
+                if(count)
+                {
+                    _.each(data, function(obj)
+                    {
+                        obj.label += _.str.sprintf(' (%s)', count);
+                    });
+                    return data;
+                }
+                else
+                {
+                    return null;
+                }
+            });
+    }
+
+    instance.web.search.CharField.include({
+        init: function()
+        {
+            var result = this._super.apply(this, arguments);
+            this.options = instance.web.py_eval(this.attrs.options || '{}');
+            return result;
+        },
+        complete: function(value)
+        {
+            var self = this;
+            return this._super.apply(this, arguments).then(function(data)
+            {
+                return openerp.web_search_autocomplete_prefetch.complete(
+                    self, value, data);
+            });
+        }
+    });
+
+    instance.web.search.ManyToOneField.include({
+        complete: function(value)
+        {
+            var self = this;
+            return this._super.apply(this, arguments).then(function(data)
+            {
+                return openerp.web_search_autocomplete_prefetch.complete(
+                    self, value, data);
+            });
+        }
+    });
+
+    instance.web.search.AutoComplete.include({
+        select_item: function()
+        {
+            if(!this.current_result)
+            {
+                return;
+            }
+            return this._super.apply(this, arguments);
+        },
+    });
+}
