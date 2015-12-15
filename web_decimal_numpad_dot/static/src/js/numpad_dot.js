1a03fd4273834ea5b3698f5c2443e50156d7ab39
Omar Castiñeira Saavedra <omar@comunitea.com>
2015-12-15 11:53:05 +0100

    [IMP] web_decimal_numpad_dot: fixed float widget not working properly

--- web_decimal_numpad_dot/static/src/js/numpad_dot.js
+++ web_decimal_numpad_dot/static/src/js/numpad_dot.js
@@ -2,11 +2,13 @@
 
     var instance = openerp;
 
-    instance.web.form.FieldFloat = instance.web.form.FieldChar.extend({
-        is_field_number: true,
-        widget_class: 'oe_form_field_float',
-        events: {
-            "keypress": "floatKeypress",
+    instance.web.form.FieldFloat = instance.web.form.FieldFloat.extend({
+        render_value: function() {
+            var self = this;
+            this._super();
+            if (!this.get('readonly')){
+                this.$el.find('input').on('keypress', this.floatKeypress.bind(this));
+            }
         },
         floatKeypress: function(e){
             if(e.keyCode == '46' || e.charCode == '46'){
@@ -14,7 +16,7 @@
                 e.preventDefault();
                 // Add the comma to the value of the input field
                  this.$("input").val(this.$("input").val() + ',');
-            }
+             }
         },
     });
 })();
