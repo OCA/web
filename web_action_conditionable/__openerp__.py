a0080bbd8e46d65e0219a94d12d7ff3adccd965a
Clovis NZOUENDJOU <clovis@anybox.fr>
2015-12-16 15:50:19 +0100

    Init module web_action_conditionable in version 7.0

---
+++ web_action_conditionable/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": 'web_action_conditionable',
+    "version": "8.0.0.1.0",
+    "depends": [
+        'base',
+        'web',
+    ],
+    'data': [],
+    'js': ['static/src/js/views.js'],
+    "author": "Cristian Salamea,Odoo Community Association (OCA)",
+    "installable": True,
+}
