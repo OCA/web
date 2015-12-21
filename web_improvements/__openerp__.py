a3656fa9adc7fef9f7d983d907eb1c9fbc148fd9
Marcelo Pickler <loxamir@gmail.com>
2015-12-21 13:50:59 -0300

    Try make travis-ci green

--- web_improvements/__openerp__.py
+++ web_improvements/__openerp__.py
@@ -2,27 +2,30 @@
 # © 2015 Marcelo Pickler & Rossa S.A.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-	"name": "Web Improvements",
+    "name": "Web Improvements",
     "summary": "This modules aims to be a compilation of community improvements for the standard web module",
     "version": "8.0.1.0.0",
-	"category": "web",
+    "category": "web",
     "website": "http://www.rossa.com.py",
     "author": "Rossa S.A., Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-	"installable": True,
-	"auto_install": False,
+    "installable": True,
+    "auto_install": False,
     "external_dependencies": {
         "python": [],
         "bin": [],
     },
-	"depends": [
-		"web",
-	],
-	"data" : [
-		"theme.xml",
-		"views/webclient_templates.xml"],
-	"qweb" : [
-		"static/src/xml/*.xml",
-	],
+    "depends": [
+        "web",
+    ],
+    "data" : [
+        "theme.xml",
+        "views/webclient_templates.xml"],
+    "qweb" : [
+        "static/src/xml/*.xml",
+    ],
+    "images": [
+        "static/description/icon.png",
+    ],
 }
