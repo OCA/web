ac3215b192beb98d232d7cae71ec7d1d2e4878c4
Peter Hahn <Peter Hahn peter.hahn@initos.com>
2015-12-22 14:28:20 +0100

    [WIP] Send mail using internal mail wizard

---
+++ web_widget_mail_send_odoo/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2015 initOS GmbH (<http://www.initos.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Mail open internally',
+    'summary': 'Open mail in new window',
+    'version': '8.0.1.0.0',
+    "category": "Social Network",
+    'website': 'https://odoo-community.org',
+    'author': 'initOS GmbH, Odoo Community Association (OCA)',
+    "license": "AGPL-3",
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'depends': [
+        'web',
+        'mail',
+    ],
+    'data': [
+        'assets.xml',
+    ],
+    'qweb': [
+        'static/src/xml/web_widget_mail_send_odoo.xml',
+    ],
+}
