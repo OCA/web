fe1848db2d72078bc0af40b680814d95c4ae503f
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-01-04 16:23:01 +0100

    Add web_access_rule_buttons
    Disable buttons which lead to action disallowed by record rules.

---
+++ web_access_rule_buttons/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{'name': 'Web Access Rules Buttons',
+ 'summary': 'Disable Edit button if access rules prevent this action',
+ 'version': '9.0.1.0.0',
+ 'author': 'Camptocamp,Odoo Community Association (OCA)',
+ 'license': 'AGPL-3',
+ 'category': 'Web',
+ 'depends': ['web',
+             ],
+ 'website': 'http://www.camptocamp.com',
+ 'data': ['views/web_access_rule_buttons.xml',
+          ],
+ 'installable': True,
+ }
