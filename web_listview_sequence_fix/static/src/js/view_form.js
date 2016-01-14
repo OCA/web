d164e204e126a17e7cc07329e41ce916c1766030
Andrius Preimantas <andrius@versada.lt>
2016-01-14 10:46:01 +0200

    [IMP][web_listview_sequence_fix] More robust resequencing logic

--- web_listview_sequence_fix/static/src/js/view_form.js
+++ web_listview_sequence_fix/static/src/js/view_form.js
@@ -1,36 +1,20 @@
 openerp.web_listview_sequence_fix = function (instance) {
 
     instance.web.FormView.include({
-        load_defaults: function () {
+        _process_save: function(save_obj) {
             var self = this;
-            var keys = _.keys(this.fields_view.fields);
-            if (_.contains(keys, 'sequence') && this.fields_view.fields['sequence'].type == 'integer') {
-                var parent = this.ViewManager.getParent();
-                if (parent !== undefined &&
-                        parent.records !== undefined &&
-                        parent.records.records !== undefined) {
-                    var records = parent.dataset.cache;
-                    var default_sequence = 1;
-                    if (records.length > 0) {
-                        var fnct = _.max;
-                        var incremental = 1;
-                        if (parent.editable() == 'top' && records.length > 1) {
-                            fnct = _.min;
-                            incremental = -1;
-                        }
-                        var element = fnct(records, function(o){
-                            return o.values.sequence;
-                        });
-                        default_sequence = element.values.sequence + incremental;
-                    }
-                    var options = {};
-                    options['context'] = {'default_sequence': default_sequence}
-                    return this.dataset.default_get(keys, options).then(function(r) {
-                        self.trigger('load_record', r);
-                    });
-                }
+            var to_ret = this._super(save_obj);
+            if (self.fields_view.fields.sequence &&
+                    self.fields_view.fields.sequence.type == 'integer' &&
+                    self.dataset.cache) {
+                _.each(self.dataset.cache, function(o, index) {
+                    save_deferral = self.dataset.write(
+                        o.id, {'sequence': index}).then(function(r) {
+                            return self.record_saved(r);
+                    }, null);
+                });
             }
-            return this._super();
+            return to_ret;
         }
     });
 };
