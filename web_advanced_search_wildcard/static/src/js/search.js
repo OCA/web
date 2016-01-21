67ff31a2377391032be1fe1c41393b991bc14ed7
lfreeke <lfreeke@therp.nl>
2016-01-21 11:21:48 +0100

    [FIX] Adept javascript to module name

--- web_advanced_search_wildcard/static/src/js/search.js
+++ web_advanced_search_wildcard/static/src/js/search.js
@@ -1,4 +1,4 @@
-openerp.search_enhanced_operators = function(instance){
+openerp.web_advanced_search_wildcard = function(instance){
     var _lt = instance.web._lt;
     instance.web.search.ExtendedSearchProposition.Char.prototype.operators.push(
         {value: '=ilike', text: _lt("matches")}
