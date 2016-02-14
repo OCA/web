ee2d0906257dc54ff8a332e76754134845d4fad8
Tony Galmiche <tony.galmiche@infosaone.com>
2016-02-14 15:53:01 +0100

    add web_widget_tolocalestring

---
+++ web_widget_tolocalestring/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# (c) 2016 Tony Galmiche / InfoSaône
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Web Widget toLocaleString",
+    "summary": "This module add 'tolocalestring' widget in tree view for formatting numbers or not to display the values 0",
+    "version": "8.0.1.0.0",
+    "category": "web",
+    "website": "https://odoo-community.org/",
+    "author": "Tony Galmiche/InfoSaône, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "base", "web",
+    ],
+    "data": [
+        "views/assets.xml",
+    ],
+    "demo": [
+    ],
+    "qweb": [
+        "static/src/xml/widget.xml",
+    ]
+}
