662e2e6e0d8fa4c1d1b220e36dd880b996980a3e
Peter <codingforfun@users.noreply.github.com>
2016-02-18 09:30:01 +0100

    Fix according to reviewers comments
    Fix version number scheme and remove unnecessary dependency.

--- web_tree_no_open/__openerp__.py
+++ web_tree_no_open/__openerp__.py
@@ -3,7 +3,7 @@
 {
     'name': 'web_tree_no_open',
     'summary': 'Disable actions for click onto Tree- or ListView rows.',
-    'version': '8.0.0.1.0',
+    'version': '8.0.1.0.0',
     'author': 'initOS GmbH, Odoo Community Association (OCA)',
     'installable': True,
     'license': 'AGPL-3',
@@ -11,7 +11,6 @@
     'installable': True,
     'auto_install': False,
     'depends': [
-        'base',
         'web',
     ],
     'data': [
