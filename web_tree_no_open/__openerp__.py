b4a2f7db46775f47c9b40d6ad030c3e74a4ae8cd
Peter Hahn <peter.hahn@initos.com>
2016-02-18 16:01:39 +0100

    Fix travis: 'Duplicate key 'installable' in dictionary'

--- web_tree_no_open/__openerp__.py
+++ web_tree_no_open/__openerp__.py
@@ -5,7 +5,6 @@
     'summary': 'Disable actions for click onto Tree- or ListView rows.',
     'version': '8.0.1.0.0',
     'author': 'initOS GmbH, Odoo Community Association (OCA)',
-    'installable': True,
     'license': 'AGPL-3',
     'application': False,
     'installable': True,
