bc2661fd73166be23b8372c744972116a92edb2a
Jamin Shah <jaiminshah2009@gmail.com>
2016-02-20 12:37:36 +0530

    [MIG]Migrated web widget many2many_tags multi selection in v9

--- web_widget_many2many_tags_multi_selection/__openerp__.py
+++ web_widget_many2many_tags_multi_selection/__openerp__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Tags multiple selection',
-    'version': '8.0.0.1.0',
-    'author': 'Akretion, Odoo Community Association (OCA)',
+    'version': '9.0.1.1.0',
+    'author': 'Akretion, Odoo Community Association (OCA), Jamin Shah',
     'depends': [
         'web',
     ],
