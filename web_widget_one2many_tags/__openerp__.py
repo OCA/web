8450fe3781efa47264d1b00a3840c596531e9dd8
Holger Brunn <hbrunn@therp.nl>
2016-03-03 11:20:53 +0100

    web_widget_one2many_tags

---
+++ web_widget_one2many_tags/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# ?? 2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Tags widget for one2many fields",
+    "version": "8.0.1.0.0",
+    "author": "Therp BV,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Hidden/Dependency",
+    "summary": "Provides a widget similar to many2many_tags for one2many "
+    "fields",
+    "depends": [
+        'web',
+    ],
+    "data": [
+        'views/templates.xml',
+    ],
+}
