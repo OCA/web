14153cab44e1713b2ff020ee59d9d78aaebaf2d9
astirpe <a.stirpe@onestein.nl>
2016-03-11 15:50:03 +0100

    [ADD] Module menu_collapsible 8.0

---
+++ menu_collapsible/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2015 ONESTEiN BV (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Collapsible menu',
+    'license': 'AGPL-3',
+    'images': ['static/description/main_screenshot.png'],
+    'summary': 'Foldable second level Odoo menu',
+    'author': 'ONESTEiN BV,Odoo Community Association (OCA)',
+    'website': 'http://www.onestein.eu',
+    'category': 'Web',
+    'version': '8.0.1.0.0',
+    'depends': ['web'],
+    'data': [
+        'templates/menu_collapsible.xml',
+    ],
+}
---
+++ menu_collapsible/static/src/js/menu_collapsible.js
@@ -0,0 +1,16 @@
+openerp.menu_collapsible = function(instance) {
+    instance.web.Menu.include({
+        self: this,
+        start: function() {
+            //Secondary menu
+            $(".oe_secondary_submenu").hide();
+            $(".oe_secondary_menu_section").unbind("click");
+            $(".oe_secondary_menu_section").click(this.section_clicked);
+
+            return this._super.apply(this, arguments);
+        },
+        section_clicked: function() {
+            $(this).next().toggle(100);
+        }
+    });
+};
