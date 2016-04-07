3c7632628f4ab50bffd0a2185bc422e9e95cf5c1
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-04-07 14:30:57 +0200

    [IMP] web_favicon: Nicer icon + README imp + support files

--- web_favicon/models/res_company.py
+++ web_favicon/models/res_company.py
@@ -1,22 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    This module copyright (C) 2015 Therp BV (<http://therp.nl>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Therp BV <http://therp.nl>
+# © 2016 Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from openerp import models, fields
 
 
@@ -24,6 +10,8 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     favicon_backend = fields.Binary()
-    favicon_backend_mimetype = fields.Char(
-        help='Set the mimetype of your file. Usually, that\'s one of\n'
-        'image/x-icon, image/gif, image/png')
+    favicon_backend_mimetype = fields.Selection(
+        selection=[('image/x-icon', 'image/x-icon'),
+                   ('image/gif', 'image/gif'),
+                   ('image/png', 'image/png')],
+        help='Set the mimetype of your file.')
