320651f84dc8330b1620c7f00659551e108b5891
Rodney Vargas <rodney.vargas@clearcorp.co.cr>
2016-04-18 10:51:39 -0600

    [FIX]-web_export_view: change contributors information

--- web_export_view/__init__.py
+++ web_export_view/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2016 Agile Business Group
+# © 2012 Agile Business Group
+# © 2012 Domsense srl
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import controllers
