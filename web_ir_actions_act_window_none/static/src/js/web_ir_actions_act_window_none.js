1260b7d38fbdceb2c578c2f31350c76dad5281c2
Holger Brunn <hbrunn@therp.nl>
2016-04-26 18:33:25 +0200

    web_ir_actions_act_window_none

---
+++ web_ir_actions_act_window_none/static/src/js/web_ir_actions_act_window_none.js
@@ -0,0 +1,19 @@
+//-*- coding: utf-8 -*-
+//?? 2016 Therp BV <http://therp.nl>
+//License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+openerp.web_ir_actions_act_window_none = function(instance)
+{
+    instance.web.ActionManager.include({
+        ir_actions_act_window_none: function(action)
+        {
+            if(action.reload === undefined || action.reload)
+            {
+                if(this.dialog_widget && this.dialog_widget.views.form)
+                {
+                    return this.dialog_widget.views.form.controller.reload();
+                }
+            }
+        },
+    });
+}
