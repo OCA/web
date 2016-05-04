79d4312548b3c9b229bcec728bb01120c33e0e4f
Cesar Lage <kaerdsar@gmail.com>
2016-05-04 04:32:11 -0400

    [FIX] some updates after first revision

--- web_widget_radio_tree/__openerp__.py
+++ web_widget_radio_tree/__openerp__.py
@@ -1,26 +1,9 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-# Odoo, an open source suite of business apps
-# This module copyright (C) 2015 bloopark systems (<http://bloopark.de>).
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2016 Cesar Lage (bloopark systems GmbH)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': "Web Widget Radio Tree",
-    'summary': """Add radio buttons for records in tree.""",
+    'summary': "Add radio buttons for records in tree.",
     'author': "bloopark systems GmbH & Co. KG",
     'website': "http://www.bloopark.de",
     'category': 'web',
@@ -33,7 +16,5 @@
     ],
     'qweb': [
         'static/src/xml/widget.xml',
-    ],
-    'installable': True,
-    'auto_install': False,
+    ]
 }
