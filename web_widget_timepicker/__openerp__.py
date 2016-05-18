55b9b1e4a27705096ce577da7a54ff958bca1d26
docmfried <funkyapes_n_bees@web.de>
2016-05-18 17:44:21 +0200

    [FIX]: Formal license and Travis LINT errors [FIX]: Renaming files according to OCA standards [FIX]: Change internal persistance form char to float [FIX]: Reorder jquery plugin to lib directory [ADD]: Form field record defintion can override timepicker plugin options

--- web_widget_timepicker/__openerp__.py
+++ web_widget_timepicker/__openerp__.py
@@ -1,49 +1,34 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2016 Michael Fried @ Vividlab (<http://www.vividlab.de>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2016 Michael Fried @ Vividlab (<http://www.vividlab.de>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': '',
-    'version': '0.1',
-    'author': 'Vividlab, Odoo Community Association (OCA)',
+    'name': 'Web Timepicker Widget',
+    'version': '9.0.1.0.0',
+    'author': 'Michael Fried@Vividlab, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'category': 'Web',
     'website': 'https://github.com/OCA/Web',
 
     # any module necessary for this one to work correctly
     'depends': [
-     'web'
+        'web'
     ],
-
-    'css': [ 'static/src/css/jquery.timepicker.css',
-			 'static/src/css/timepicker.css',
-			],
-    'js': [  'static/src/js/timepicker_widget.js',
-			 'static/src/js/jquery.timepicker.js',
-			],
-    'qweb' : [ 'static/src/xml/time_picker.xml', ],
+    'css': [
+        'static/src/lib/jquery.timerpicker/jquery.timepicker.css',
+		'static/src/css/web_widget_timepicker.css'
+    ],
+    'js': [
+        'static/src/lib/jquery.timerpicker/jquery.timepicker.js',
+        'static/src/js/web_widget_timepicker.js',
+    ],
+    'qweb' : [ 
+        'static/src/xml/web_widget_timepicker.xml'
+    ],
 
     # always loaded
     'data': [
-        'views/assets.xml',
+        'views/web_widget_timepicker_assets.xml'
     ],
 
     #Installation options
