8c6f2bb6d582a105b1fc332317a4a7d6ced128e6
Siddharth Bhalgami <siddharth.bhalgami@gmail.com>
2016-06-12 16:32:31 +0530

    [MIG] web_dialog_siza: Migrate to v9.0

--- web_dialog_size/static/src/js/web_dialog_size.js
+++ web_dialog_size/static/src/js/web_dialog_size.js
@@ -1,56 +1,41 @@
-openerp.web_dialog_size= function (instance) {
+odoo.define('web_dialog_size.web_dialog_size', function (require) {
+'use strict';
 
-    instance.web.Dialog =  instance.web.Dialog.extend({
-        init_dialog: function () {
-            var self = this,
-                result = this._super();
-            self.$dialog_box.find('.dialog_button_extend')
-                .on('click', self.proxy('_extending'));
-            self.$dialog_box.find('.dialog_button_restore')
-                .on('click', self.proxy('_restore'));
-            return jQuery.when(result).then(function()
-            {
-                var deferred = null;
-                if(openerp.web_dialog_size.default_maximize === undefined)
-                {
-                    deferred = (new openerp.web.Model('ir.config_parameter'))
-                        .call('get_param',
-                              ['web_dialog_size.default_maximize'])
-                        .then(function(default_maximize)
-                        {
-                            openerp.web_dialog_size.default_maximize =
-                                default_maximize;
-                        });
-                }
-                return jQuery.when(deferred).then(function()
-                {
-                    if(openerp.web_dialog_size.default_maximize)
-                    {
-                        self._extending();
-                    }
-                    else
-                    {
-                        self._restore();
-                    }
-                });
-            });
-        },
+var Model = require('web.DataModel');
+var Dialog = require('web.Dialog');
 
-        _extending: function(e) {
-            var dialog = this.$el.parents('.modal-dialog');
-            dialog.addClass('dialog_full_screen');
-            dialog.find('.dialog_button_extend').hide();
-            dialog.find('.dialog_button_restore').show();
-        },
+Dialog.include({
 
-        _restore: function(e) {
-            var dialog = this.$el.parents('.modal-dialog');
-            dialog.removeClass('dialog_full_screen');
-            dialog.find('.dialog_button_restore').hide();
-            dialog.find('.dialog_button_extend').show();
-        },
+    init: function (parent, options) {
+        var self = this;
+        this._super.apply(this, arguments);
+        self.$modal.find('.dialog_button_extend').on('click', self.proxy('_extending'));
+        self.$modal.find('.dialog_button_restore').on('click', self.proxy('_restore'));
 
-    });
+        new Model('ir.config_parameter').query(['key', 'value']).
+        filter([['key', '=', 'web_dialog_size.default_maximize']]).all().then(function(default_maximize) {
+            if (default_maximize.length && default_maximize[0]['value'] == 1) {
+                self._extending();
+            } else {
+                self._restore();
+            }
+        });
+    },
 
-};
+    _extending: function() {
+        var dialog = this.$el.parents('.modal-dialog');
+        dialog.addClass('dialog_full_screen');
+        dialog.find('.dialog_button_extend').hide();
+        dialog.find('.dialog_button_restore').show();
+    },
 
+    _restore: function() {
+        var dialog = this.$el.parents('.modal-dialog');
+        dialog.removeClass('dialog_full_screen');
+        dialog.find('.dialog_button_restore').hide();
+        dialog.find('.dialog_button_extend').show();
+    },
+
+});
+
+});
