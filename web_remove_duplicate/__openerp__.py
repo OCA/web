7df0e7ee77c3c27657e0c0d32c1ac8671ca8f771
Denis Roussel <denis.roussel@acsone.eu>
2016-07-05 17:01:14 +0200

    [ADD] Add new module to allow the removal of the Duplicate button on every form

---
+++ web_remove_duplicate/__openerp__.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2016 Acsone SA (<http://acsone.eu>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Remove Duplicate Action",
+    "version": "1.0",
+    "author": "Acsone SA,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "complexity": "normal",
+    "description": """
+Introduction
+============
+
+This addon provide a form option to allow to hide 'Duplicate' action
+on objects
+
+Usage
+=====
+
+Simply depend on web_remove_duplicate and add the attribute 'remove_dependency'
+ on the form view
+    """,
+    "category": "Dependency",
+    "depends": [
+        'web',
+    ],
+    "data": [
+    ],
+    "js": [
+        'static/src/js/web_remove_duplicate.js'
+    ],
+    "css": [
+    ],
+    "qweb": [
+    ],
+    "test": [
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+    "external_dependencies": {
+        'python': [],
+    },
+}
