f8c6fadc8609634e154bd8b2f7e9c6bc79aa4f3d
Denis Roussel <denis.roussel@acsone.eu>
2016-07-06 12:01:47 +0200

    [FIX] Add a description to manifest

--- web_duplicate_visibility/__openerp__.py
+++ web_duplicate_visibility/__openerp__.py
@@ -13,6 +13,16 @@
     'website': 'https://acsone.eu',
     'application': False,
     'installable': True,
+    "description": """
+Web Duplicate Visibility
+========================
+Features:
+---------
+
+Allows to add a 'duplicate' attribute to a form and to set it at '0'
+to remove the duplicate function
+
+    """,
     'depends': ['web'],
     'js': ['static/src/js/web_duplicate_visibility.js'],
     'data': [
