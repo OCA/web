7f17576bdacfc0521c174eb1b0a553f9672a5931
Denis Roussel <denis.roussel@acsone.eu>
2016-07-06 10:26:36 +0200

    [ADD] Add module to manage Duplicate button visibility (backport v9 module)

---
+++ web_duplicate_visibility/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Acsone SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Web Duplicate Visibility',
+    'summary': """
+        This module allows to manage the visibility of duplicate button from
+        the form view declaration.""",
+    'version': '7.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Acsone SA,Odoo Community Association (OCA)',
+    'website': 'https://acsone.eu',
+    'application': False,
+    'installable': True,
+    'depends': ['web'],
+    'js': ['static/src/js/web_duplicate_visibility.js'],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
