dd9d985eb2384b5b754f9fc80bd4dd117e120cfa
Antonio Espinosa <antespi@gmail.com>
2016-07-20 14:50:34 +0200

    [MIG] web_environment_ribbon

--- web_environment_ribbon/__openerp__.py
+++ web_environment_ribbon/__openerp__.py
@@ -1,29 +1,15 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2015 Francesco OpenCode Apruzzese (<cescoap@gmail.com>)
-#    All Rights Reserved
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2015 Francesco OpenCode Apruzzese <cescoap@gmail.com>
+# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': "Web Environment Ribbon",
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'category': 'Web',
-    'author': 'Francesco OpenCode Apruzzese,Odoo Community Association (OCA)',
+    'author': 'Francesco OpenCode Apruzzese, '
+              'Tecnativa, '
+              'Odoo Community Association (OCA)',
     'website': 'https://it.linkedin.com/in/francescoapruzzese',
     'license': 'AGPL-3',
     "depends": [
@@ -36,5 +22,5 @@
     "update_xml": [],
     "demo_xml": [],
     "auto_install": False,
-    'installable': False
+    "installable": True
 }
