b5c175820bffd317a02a8a90234de3790775dac6
Antonio Espinosa <antespi@gmail.com>
2016-07-21 09:37:53 +0200

    [MIG] web_translate_dialog

--- web_translate_dialog/README.rst
+++ web_translate_dialog/README.rst
@@ -1,7 +1,15 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+====================
 Web Translate Dialog
 ====================
 
-This module replaces the standard translation view by an easy-to-use pop-up view where you can translate all the fields of the object in all the installed languages (for long-time users, it may remind them the translation pop-up of OpenERP 6.1).
+This module replaces the standard translation view by an easy-to-use pop-up
+view where you can translate all the fields of the object in all the installed
+languages (for long-time users, it may remind them the translation pop-up of
+OpenERP 6.1).
 
 This module also features:
 
@@ -12,25 +20,53 @@ This module also features:
 Usage
 =====
 
-Go to an object that has translatable fields (*Products* for example) and select *More > Translate* (or click on *Edit* and then click on the flag at the top-right of one of the translatable fields): the translation view will pop-up on your screen. This translation view contains all the translatable fields of the object.
+Go to an object that has translatable fields (*Products* for example) and
+select *More > Translate* (or click on *Edit* and then click on the flag at
+the top-right of one of the translatable fields): the translation view will
+pop-up on your screen. This translation view contains all the translatable
+fields of the object.
+
+If you click in the standard translate icon then the translation view will
+pop-up with this field only.
+
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/162/9.0
+
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/OCA/web/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed feedback.
 
 Credits
 =======
+
+Images
+------
+
+* Odoo Community Association: `Icon <https://github.com/OCA/maintainer-tools/blob/master/template/module/static/description/icon.svg>`_.
 
 Contributors
 ------------
 
 * Guewen Baconnier (Camptocamp)
+* Antonio Espinosa <antonio.espinosa@tecnativa.com>
 
 Maintainer
 ----------
 
-.. image:: http://odoo-community.org/logo.png
+.. image:: https://odoo-community.org/logo.png
    :alt: Odoo Community Association
-   :target: http://odoo-community.org
+   :target: https://odoo-community.org
 
 This module is maintained by the OCA.
 
-OCA, or the Odoo Community Association, is a nonprofit organization whose mission is to support the collaborative development of Odoo features and promote its widespread use.
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
 
-To contribute to this module, please visit http://odoo-community.org.
+To contribute to this module, please visit https://odoo-community.org.
--- web_translate_dialog/static/src/js/web_translate_dialog.js
+++ web_translate_dialog/static/src/js/web_translate_dialog.js
@@ -1,209 +1,228 @@
-openerp.web_translate_dialog = function (instance) {
+/* Copyright 2012 Guewen Baconnier (Camptocamp SA)
+   Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+ * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
 
-    "use strict";
+odoo.define('web_translate_dialog.translate_dialog', function(require){
+"use strict";
 
-    var QWeb = instance.web.qweb,
-        _t  = instance.web._t,
-        _lt = instance.web._lt;
+var _ = require('_');
+var $ = require('$');
 
-    instance.web.FormView.include({
-        load_form: function(data) {
-            var self = this;
-            this._super(data);
-            if (this.sidebar) {
-                this.sidebar.add_items('other', _.compact([
-                    self.is_action_enabled('edit') && { label: _t('Translate'), callback: self.on_button_translate },
-                ]));
-            }
-        },
-        on_button_translate: function() {
-            var self = this;
-            $.when(this.has_been_loaded).then(function() {
-                self.open_translate_dialog(this);
-            });
-        },
-    });
+var core = require('web.core');
+var data = require('web.data');
+var common = require('web.form_common');
 
-    instance.web.View.include({
-        open_translate_dialog: function() {
-            new instance.web_translate_dialog.TranslateDialog(this).open();
+var FormView = require('web.FormView');
+var View = require('web.View');
+var Dialog = require('web.Dialog');
+
+var _t = core._t;
+var QWeb = core.qweb;
+
+var translateDialog = Dialog.extend({
+    template: "TranslateDialog",
+    init: function(parent, field, content) {
+        this._super(parent,
+                    {title: _t("Translations"),
+                     width: '90%',
+                     height: '80%'},
+                    content);
+        this.view_language = this.session.user_context.lang;
+        this.view = parent;
+        this.view_type = parent.fields_view.type || '';
+        this.$view_form = null;
+        this.$sidebar_form = null;
+        if (!!field) {
+            this.translatable_fields_keys = [field];
+            this.translatable_fields = _.filter(
+                this.view.translatable_fields || [],
+                function(i) {return i.name == field;}
+            );
+        } else {
+            this.translatable_fields_keys = _.map(
+                this.view.translatable_fields || [],
+                function(i) {return i.name;}
+            );
+            this.translatable_fields = this.view.translatable_fields.slice(0);
         }
-    });
+        this.languages = null;
+        this.languages_loaded = $.Deferred();
+        (new data.DataSetSearch(this, 'res.lang', this.view.dataset.get_context(),
+                                [['translatable', '=', '1']]))
+            .read_slice(['code', 'name'], { sort: 'id' })
+            .then(this.on_languages_loaded);
+    },
+    on_languages_loaded: function(langs) {
+        this.languages = langs;
+        this.languages_loaded.resolve();
+    },
+    open: function() {
+        var self = this,
+            sup = this._super;
+        // the template needs the languages
+        $.when(this.languages_loaded).then(function() {
+            return sup.call(self);
+        });
+    },
+    start: function() {
+        var self = this;
+        this.$el.find('.oe_translation_field').change(function() {
+            $(this).toggleClass('touched', ($(this).val() != $(this).attr('data-value')));
+        });
+        this.$footer.html(QWeb.render("TranslateDialog.buttons"));
+        this.$footer.find(".oe_form_translate_dialog_save_button").click(function(){
+            self.on_button_save();
+            self.on_button_close();
+        });
+        this.$footer.find(".oe_form_translate_dialog_cancel_button").click(function(){
+            self.on_button_close();
+        });
 
-    instance.web_translate_dialog.TranslateDialog = instance.web.Dialog.extend({
-        template: "TranslateDialog",
-        init: function(parent, options, content) {
-            this._super(parent,
-                        {title: _t("Translations"),
-                         width: '90%',
-                         height: '80%'},
-                        content);
-            this.view_language = this.session.user_context.lang;
-            this.view = parent;
-            this.view_type = parent.fields_view.type || '';
-            this.$view_form = null;
-            this.$sidebar_form = null;
-            this.translatable_fields_keys = _.map(this.view.translatable_fields || [], function(i) { return i.name;});
-            this.languages = null;
-            this.languages_loaded = $.Deferred();
-            (new instance.web.DataSetSearch(this,
-                                            'res.lang',
-                                            this.view.dataset.get_context(),
-                                            [['translatable', '=', '1']]))
-                .read_slice(['code', 'name'], { sort: 'id' })
-                .then(this.on_languages_loaded);
-        },
-        on_languages_loaded: function(langs) {
-            this.languages = langs;
-            this.languages_loaded.resolve();
-        },
-        open: function() {
-            var self = this,
-                sup = this._super;
-            // the template needs the languages
-            $.when(this.languages_loaded).then(function() {
-                return sup.call(self);
-            });
-        },
-        start: function() {
-            var self = this;
-            this.$el.find('.oe_translation_field').change(function() {
-                $(this).toggleClass('touched', ($(this).val() != $(this).attr('data-value')));
-            });
-            this.$buttons.html(QWeb.render("TranslateDialog.buttons"));
-            this.$buttons.find(".oe_form_translate_dialog_save_button").click(function(){
-                self.on_button_save();
-                self.on_button_close();
-            });
-            this.$buttons.find(".oe_form_translate_dialog_cancel_button").click(function(){
-                self.on_button_close();
-            });
-            this.initialize_html_fields();
-
-            this.do_load_fields_values();
-        },
-        initialize_html_fields: function() {
-            this.$el.find('.oe_form_field_html textarea').each(function() {
-                var $textarea = $(this);
-                var width = 100;  // forced to fixed size on initialization
-                                  // will be changed to percentage right after
-                                  // the creation
-                var height = 250;
-                $textarea.cleditor({
-                    width:      width, // width not including margins, borders or padding
-                    height:     height, // height not including margins, borders or padding
-                    controls:   // controls to add to the toolbar
-                                "bold italic underline strikethrough " +
-                                "| removeformat | bullets numbering | outdent " +
-                                "indent | link unlink | source",
-                    bodyStyle:  // style to assign to document body contained within the editor
-                                "margin:4px; color:#4c4c4c; font-size:13px; font-family:'Lucida Grande',Helvetica,Verdana,Arial,sans-serif; cursor:text"
-                });
-
-                var $cleditor = $textarea.cleditor()[0];
-                // Down to -- end, this is a workaround for the bug
-                // https://bugs.launchpad.net/openerp-web/+bug/1258463
-                // The editor is initially created with a fixed size so
-                // the buggy event is not bound to $(window), then we restore
-                // a percentage width and bind the "normal" event without the
-                // CHM's buggy change.
-                $cleditor.$main.width('95%');
-                $cleditor.options.width = '95%';
-                $(window).resize(function() {
-                    //Forcefully blurred iframe contentWindow, chrome, IE, safari doesn't trigger blur on window resize and due to which text disappears
-                    var contentWindow = $cleditor.$frame[0].contentWindow;
-                    if(!$.browser.mozilla && contentWindow){
-                        $(contentWindow).trigger('blur');
+        this.do_load_fields_values();
+    },
+    initialize_html_fields: function(lang) {
+        var self = this;
+        _.each(this.translatable_fields_keys, function(f) {
+            // Initialize summernote if HTML field
+            self.$el.find('.oe_form_field_html .oe_translation_field[name="' + lang.code + '-' + f + '"]').each(function() {
+                var $parent = $(this).summernote({
+                    'focus': false,
+                    'toolbar': [
+                        ['style', ['style']],
+                        ['font', ['bold', 'italic', 'underline', 'clear']],
+                        ['fontsize', ['fontsize']],
+                        ['color', ['color']],
+                        ['para', ['ul', 'ol', 'paragraph']],
+                        ['table', ['table']],
+                        ['insert', ['link', 'picture']],
+                        ['history', ['undo', 'redo']]
+                    ],
+                    'prettifyHtml': false,
+                    'styleWithSpan': false,
+                    'inlinemedia': ['p'],
+                    'lang': "odoo",
+                    'onChange': function (value) {
+                        $(this).toggleClass('touched', (value != $(this).attr('data-value')));
                     }
-                });
-                $cleditor.refresh();
-                // -- end
-
-                $cleditor.change(function() {
-                    this.updateTextArea();
-                    this.$area.toggleClass('touched',
-                                        (this.$area.val() != this.$area.attr('data-value')));
+                }).parent();
+                // Triggers a mouseup to refresh the editor toolbar
+                $parent.find('.note-editable').trigger('mouseup');
+                $parent.find('.note-editing-area').css({
+                    minHeight:'100px',
+                    minWidth:'260px',
                 });
             });
-        },
-        set_fields_values: function(lang, values) {
-            var self = this;
-            _.each(this.translatable_fields_keys, function(f) {
-                self.$el.find('.oe_translation_field[name="' + lang.code + '-' + f + '"]')
-                    .val(values[f] || '')
-                    .attr('data-value', values[f] || '');
+        });
+    },
+    set_fields_values: function(lang, values) {
+        var self = this;
+        _.each(this.translatable_fields_keys, function(f) {
+            self.$el.find('.oe_translation_field[name="' + lang.code + '-' + f + '"]')
+                .val(values[f] || '')
+                .attr('data-value', values[f] || '');
+        });
+        this.$el.find('textarea.oe_translation_field').css({
+            minHeight:'100px',
+        });
+        $(window).resize();  // triggers the autosize
+        this.initialize_html_fields(lang);
+    },
+    do_load_fields_values: function() {
+        var self = this,
+            deferred = [];
 
-                var $tarea = self.$el.find('.oe_form_field_html .oe_translation_field[name="' + lang.code + '-' + f + '"]');
-                if ($tarea.length) {
-                    $tarea.cleditor()[0].updateFrame();
-                }
-            });
-            var $textarea = this.$el.find('textarea.oe_translation_field');
-            $textarea.css({minHeight:'100px'});
-            $textarea.autosize();
-            $(window).resize();  // triggers the autosize
-        },
-        do_load_fields_values: function() {
-            var self = this,
-                deferred = [];
-
-            this.$el.find('.oe_translation_field').val('').removeClass('touched');
-            _.each(self.languages, function(lg) {
-                var deff = $.Deferred();
-                deferred.push(deff);
-                if (lg.code === self.view_language) {
-                    var values = {};
-                    _.each(self.translatable_fields_keys, function(field) {
-                        values[field] = self.view.fields[field].get_value();
+        this.$el.find('.oe_translation_field').val('').removeClass('touched');
+        _.each(self.languages, function(lg) {
+            var deff = $.Deferred();
+            deferred.push(deff);
+            if (lg.code === self.view_language) {
+                var values = {};
+                _.each(self.translatable_fields_keys, function(field) {
+                    values[field] = self.view.fields[field].get_value();
+                });
+                self.set_fields_values(lg, values);
+                deff.resolve();
+            } else {
+                self.view.dataset.call(
+                    'read',
+                    [[self.view.datarecord.id],
+                    self.translatable_fields_keys,
+                    self.view.dataset.get_context({
+                        'lang': lg.code
+                    })]).done(function (rows) {
+                        self.set_fields_values(lg, rows[0]);
+                        deff.resolve();
                     });
-                    self.set_fields_values(lg, values);
-                    deff.resolve();
-                } else {
-                    self.view.dataset.call(
-                        'read',
-                        [[self.view.datarecord.id],
-                        self.translatable_fields_keys,
-                        self.view.dataset.get_context({
-                            'lang': lg.code
-                        })]).done(function (rows) {
-                            self.set_fields_values(lg, rows[0]);
-                            deff.resolve();
-                        });
-                };
+            }
+        });
+        return deferred;
+    },
+    on_button_save: function() {
+        var translations = {},
+            self = this,
+            translation_mutex = new $.Mutex();
+        self.$el.find('.oe_translation_field.touched').each(function() {
+            var field = $(this).attr('name').split('-');
+            if (!translations[field[0]]) {
+                translations[field[0]] = {};
+            }
+            translations[field[0]][field[1]] = $(this).val();
+        });
+        _.each(translations, function(text, code) {
+            if (code === self.view_language) {
+                self.view.set_values(text);
+            }
+            translation_mutex.exec(function() {
+                return new data.DataSet(self, self.view.dataset.model,
+                                        self.view.dataset.get_context())
+                    .write(self.view.datarecord.id, text,
+                           { context : { 'lang': code }});
             });
-            return deferred;
-        },
-        on_button_save: function() {
-            var translations = {},
-                self = this,
-                translation_mutex = new $.Mutex();
-            self.$el.find('.oe_translation_field.touched').each(function() {
-                var field = $(this).attr('name').split('-');
-                if (!translations[field[0]]) {
-                    translations[field[0]] = {};
-                }
-                translations[field[0]][field[1]] = $(this).val();
-            });
-            _.each(translations, function(data, code) {
-                if (code === self.view_language) {
-                    self.view.set_values(data);
-                }
-                translation_mutex.exec(function() {
-                    return new instance.web.DataSet(self, self.view.dataset.model, self.view.dataset.get_context()).write(self.view.datarecord.id, data, { context : { 'lang': code }});
-                });
-            });
-            this.close();
-        },
-        on_button_close: function() {
-            this.close();
-        },
+        });
+        this.close();
+    },
+    on_button_close: function() {
+        this.close();
+    },
 
-    });
+});
 
-    instance.web.form.AbstractField.include({
-        on_translate: function() {
-            // the image next to the fields opens the translate dialog
-            this.view.open_translate_dialog();
-        },
-    });
+FormView.include({
+    render_sidebar: function($node) {
+        this._super($node);
+        if (this.sidebar) {
+            this.sidebar.add_items('other', _.compact([
+                this.is_action_enabled('edit') &&
+                this.translatable_fields.length > 0 && {
+                    label: _t('Translate'),
+                    callback: this.on_button_translate
+                },
+            ]));
+        }
+    },
+    on_button_translate: function() {
+        var self = this;
+        $.when(this.has_been_loaded).then(function() {
+            self.open_translate_dialog();
+        });
+    },
+});
+
+View.include({
+    open_translate_dialog: function(field) {
+        new translateDialog(this, field).open();
+    }
+});
+
+common.AbstractField.include({
+    on_translate: function() {
+        // the image next to the fields opens the translate dialog
+        this.view.open_translate_dialog(this.name);
+    },
+});
+
+return {
+    translateDialog: translateDialog,
 };
+
+}); // odoo.define
