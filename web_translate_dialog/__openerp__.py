03ba7cc69eca47e7f324039aa848f87dc2028ac7
Antonio Espinosa <antespi@gmail.com>
2016-07-21 09:37:53 +0200

    [MIG] web_translate_dialog

--- web_translate_dialog/__openerp__.py
+++ web_translate_dialog/__openerp__.py
@@ -1,32 +1,26 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Guewen Baconnier
-#    Copyright 2012 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
-{"name": "Web Translate Dialog",
- "category": "Web",
- "summary": "Easy-to-use pop-up to translate fields in several languages",
- "license": "AGPL-3",
- "author": "Camptocamp,Odoo Community Association (OCA)",
- "version": "8.0.1.0.0",
- "depends": ['web'],
- 'data': ['view/web_translate.xml'],
- 'qweb': ["static/src/xml/base.xml"],
- 'installable': False,
- }
+# Copyright 2012 Guewen Baconnier (Camptocamp SA)
+# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Web Translate Dialog",
+    "summary": "Easy-to-use pop-up to translate fields in several languages",
+    "version": "9.0.1.0.0",
+    "category": "Web",
+    "website": "https://odoo-community.org/",
+    "author": "Camptocamp, "
+              "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "view/web_translate.xml",
+    ],
+    "qweb": [
+        "static/src/xml/base.xml",
+    ]
+}
