4d57cf41ff069d0c7ab5952f75197d45f077ff95
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-08-02 00:59:05 +0200

    [MIG] web_timeline: Migration to 9.0

--- web_timeline/__openerp__.py
+++ web_timeline/__openerp__.py
@@ -4,16 +4,15 @@
 
 {
     'name': "Web timeline",
-    'summary': """
-        Interactive visualization chart to visualize events in time
-    """,
-    "version": "8.0.1.0.0",
+    'summary': "Interactive visualization chart to show events in time",
+    "version": "9.0.1.0.0",
     'author': 'ACSONE SA/NV,'
+              'Tecnativa,'
               'Odoo Community Association (OCA)',
-    "category": "Tools",
+    "category": "web",
     "website": "http://acsone.eu",
     'depends': [
-        'web'
+        'web',
     ],
     'qweb': [
         'static/src/xml/web_timeline.xml',
