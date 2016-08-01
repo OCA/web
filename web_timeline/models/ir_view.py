ab4fabdfd97b6ca37d592df4a3a898c76e4a4dc8
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-08-02 00:59:05 +0200

    [MIG] web_timeline: Migration to 9.0

--- web_timeline/models/ir_view.py
+++ web_timeline/models/ir_view.py
@@ -2,8 +2,7 @@
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models
-from openerp import api
+from openerp import fields, models
 
 
 TIMELINE_VIEW = ('timeline', 'Timeline')
@@ -12,14 +11,4 @@ TIMELINE_VIEW = ('timeline', 'Timeline')
 class IrUIView(models.Model):
     _inherit = 'ir.ui.view'
 
-    @api.model
-    def _setup_fields(self):
-        """Hack due since the field 'type' is not defined with the new api.
-        """
-        cls = type(self)
-        type_selection = cls._fields['type'].selection
-        if TIMELINE_VIEW not in type_selection:
-            tmp = list(type_selection)
-            tmp.append(TIMELINE_VIEW)
-            cls._fields['type'].selection = tuple(set(tmp))
-        super(IrUIView, self)._setup_fields()
+    type = fields.Selection(selection_add=[TIMELINE_VIEW])
