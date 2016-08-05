de00943ec60dba83ac09ae88bef4e04625acef96
Dave Lasley <dave@laslabs.com>
2016-08-04 19:29:01 -0700

    [ADD] web_responsive: Create new module * Create App Drawer for primary menu * Move secondary menu to top bar * Add keyboard shortcuts for primary menu navigation * Make web client slightly more mobile compatible (no widgets)

---
+++ web_app_drawer/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Web App Drawer",
+    "summary": "It moves the sidebar to a main nav and adds an app drawer",
+    "version": "9.0.1.0.0",
+    "category": "Website",
+    "website": "https://laslabs.com/",
+    "author": "LasLabs, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        'web',
+    ],
+    "data": [
+        'views/assets.xml',
+        'views/web.xml',
+    ],
+}
