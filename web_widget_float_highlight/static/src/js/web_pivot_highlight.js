7eb68dbb509a8e4f8ae86b2d14a06da413a36cd1
Antonio Esposito <a.esposito@onestein.nl>
2016-08-26 16:54:00 +0200

    [FIX] Solved compatibility issue in highlight functionality on pivot view

--- web_widget_float_highlight/static/src/js/web_pivot_highlight.js
+++ web_widget_float_highlight/static/src/js/web_pivot_highlight.js
@@ -7,8 +7,15 @@ odoo.define("web.pivot.float.highlight", function (require) {
 
         init: function () {
             this._super.apply(this, arguments);
+            this.to_highlight = [];
+            this.highlight_lookup = {};
+        },
+        view_loading: function (fvg)  {
+            var res = this._super.apply(this, arguments);
+            var self = this;
+
             try{
-                var my_options = JSON.parse(this.fields_view.arch.attrs.options.split("'").join('"')) || false;
+                var my_options = JSON.parse(fvg.arch.attrs.options.split("'").join('"')) || false;
             }
             catch(err){
                 var my_options = false;
@@ -19,24 +26,18 @@ odoo.define("web.pivot.float.highlight", function (require) {
             var options = {
                 lower_threshold: (my_options && my_options.lower_threshold) || 0,
                 upper_threshold: (my_options && my_options.upper_threshold) || 0,
-                lower_bg_color: (my_options && my_options.lower_bg_color) || load_defaults && "red" || "white",
-                middle_bg_color: (my_options && my_options.middle_bg_color) || "white",
-                upper_bg_color: (my_options && my_options.upper_bg_color) || load_defaults && "green" || "white",
-                lower_font_color: (my_options && my_options.lower_font_color) || load_defaults && "white" || "#666666",
-                middle_font_color: (my_options && my_options.middle_font_color) || "#666666",
-                upper_font_color: (my_options && my_options.upper_font_color) || load_defaults && "white" || "#666666",
+                lower_bg_color: (my_options && my_options.lower_bg_color) || load_defaults && "red" || false,
+                middle_bg_color: (my_options && my_options.middle_bg_color) || false,
+                upper_bg_color: (my_options && my_options.upper_bg_color) || load_defaults && "green" || false,
+                lower_font_color: (my_options && my_options.lower_font_color) || load_defaults && "white" || false,
+                middle_font_color: (my_options && my_options.middle_font_color) || false,
+                upper_font_color: (my_options && my_options.upper_font_color) || load_defaults && "white" || false,
                 load_defaults: load_defaults
             };
 
             this.parsed_options = options;
-            this.to_highlight = [];
-            this.highlight_lookup = {};
-        },
-        willStart: function () {
-            var res = this._super.apply(this, arguments);
-            var self = this;
 
-            this.fields_view.arch.children.forEach(function (field) {
+            fvg.arch.children.forEach(function (field) {
                 switch (field.attrs.type) {
                 case 'measure':
                     if (field.attrs.highlight && field.attrs.highlight.toLowerCase() == 'true'){
@@ -65,27 +66,23 @@ odoo.define("web.pivot.float.highlight", function (require) {
             var measure_types = this.active_measures.map(function (name) {
                 return self.measures[name].type;
             });
-            var widgets = this.widgets;
-
             var my_options = this.parsed_options;
-
             for (i = 0; i < rows.length; i++) {
                 $row = $('<tr>');
                 $header = $('<td>')
                     .text(rows[i].title)
                     .data('id', rows[i].id)
                     .css('padding-left', (5 + rows[i].indent * 30) + 'px')
-                    .addClass(rows[i].expanded ? 'o_pivot_header_cell_opened' : 'o_pivot_header_cell_closed');
+                    .addClass(rows[i].expanded ? 'oe-opened' : 'oe-closed');
                 if (rows[i].indent > 0) $header.attr('title', groupby_labels[rows[i].indent - 1]);
                 $header.appendTo($row);
                 for (j = 0; j < length; j++) {
-                    value = formats.format_value(rows[i].values[j], {type: measure_types[j % nbr_measures], widget: widgets[j % nbr_measures]});
+                    value = formats.format_value(rows[i].values[j], {type: measure_types[j % nbr_measures]});
                     $cell = $('<td>')
                                 .data('id', rows[i].id)
                                 .data('col_id', rows[i].col_ids[Math.floor(j / nbr_measures)])
-                                .toggleClass('o_empty', !value)
-                                .text(value)
-                                .addClass('o_pivot_cell_value text-right');
+                                .toggleClass('oe-empty', !value)
+                                .text(value);
                     if (((j >= length - this.active_measures.length) && display_total) || i === 0){
                         $cell.css('font-weight', 'bold');
                     }
@@ -111,7 +108,6 @@ odoo.define("web.pivot.float.highlight", function (require) {
                         }
                     }
                     $row.append($cell);
-                    $cell.toggleClass('hidden-xs', j < length - this.active_measures.length);
                 }
                 $tbody.append($row);
             }
