b539d50518d05c00a35358ba93547c85a98e245c
Zakaria Makrelouf <z.makrelouf@gmail.com>
2016-09-07 09:19:57 +0100

    [9.0][MIG] Migration of module web_m2x_options Adapt JS file to the new JS API and make the module installable [MIG] adapt form.js to the new API. [FIX] Fix bug mentioned in pull #262. [MIG] Make the module installable.

--- web_m2x_options/__openerp__.py
+++ web_m2x_options/__openerp__.py
@@ -2,13 +2,12 @@
 
 {
     "name": 'web_m2x_options',
-    "version": "8.0.0.2.0",
+    "version": "9.0.0.1.0",
     "depends": [
         'base',
         'web',
     ],
     'data': ['views/view.xml'],
     "author": "0k.io,Odoo Community Association (OCA)",
-    'installable': False,
-    "active": False,
+    'installable': True,
 }
