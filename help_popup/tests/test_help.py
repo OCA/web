29f3f7c0b19ac09c278a8361beb66587eefb3b02
David Beal <david.beal@akretion.com>
2016-10-04 15:55:28 +0200

    [FIX] upd tests

--- help_popup/tests/test_help.py
+++ help_popup/tests/test_help.py
@@ -14,22 +14,6 @@ class TestHelpPopup(common.TransactionCase):
         partner_act = self.env.ref('base.action_partner_form')
         return partner_act.button_open_help_popup()
 
-    # def test_update_partially_field(self):
-    #     partner_act = self.env.ref('base.action_partner_form')
-    #     import pdb; pdb.set_trace()
-    #     partner_act.with_context(install_mode=True).write(
-    #         {'advanced_help': 'new_content'})
-    #     partner_act.with_context(install_mode=True).write(
-    #         {'advanced_help': 'toto'})
-    #     # import pdb; pdb.set_trace()
-    #     # vals = {'advanced_help': 'toto'}
-    #     # partner_act._update_help_field(vals, 'advanced_help', 'help_popup')
-    #     self.assertEqual(
-    #         partner_act.advanced_help,
-    #         u'\nnew_content<help_base>toto</help_base>',
-    #         "Partially updating of 'ir.model'.advanced_help is broken")
-    #     # partner_act.remove_obsolete_help('base')
-
     def test_advanced_user_model_help(self):
         """ test if advanced_help_model field in 'ir.actions.act_window'
             propagate changes in 'ir.model' advanced_help field
