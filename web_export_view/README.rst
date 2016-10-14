42650f1445438fe540a7593cc1f0b1f290fb7027
Henry Zhou <zhouhenry@live.com>
2016-10-14 19:29:43 +0800

    [MIG 10.0] web_export_view
    Based on Rodney's pull request '9.0 web export view rv #346'.
    Avoided to export the sequence column with handle widget.
    Adapted to css class name of odoo 10.0.

--- web_export_view/README.rst
+++ web_export_view/README.rst
@@ -4,7 +4,7 @@
 Export Current View
 ===================
 
-One of the best OpenERP's features is exporting custom data to CSV/XLS. You can
+One of the best Odoo's features is exporting custom data to CSV/XLS. You can
 do it by clicking on the export link in the sidebar. The export action allows
 us to configure what to be exported by selecting fields, etc, and allows you
 to save your export as a template so that you can export it once again without
@@ -21,7 +21,7 @@ Usage
 =====
 
 After you installed it, you'll find an additional link 'Export current view'
-right below the 'Export' one. By clicking on it you'll get a XLS file contains
+right on the sidebar. By clicking on it you'll get a XLS file contains
 the same data of the tree view you are looking at, headers included.
 
 
@@ -29,11 +29,10 @@ the same data of the tree view you are looking at, headers included.
 Bug Tracker
 ===========
 
-Bugs are tracked on `GitHub Issues <https://github.com/OCA/web/issues>`_.
-In case of trouble, please check there if your issue has already been reported.
-If you spotted it first, help us smashing it by providing a detailed and welcomed feedback
-`here <https://github.com/OCA/web/issues/new?body=module:%20web_export_view%0Aversion:%208.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
-
+Bugs are tracked on `GitHub Issues
+<https://github.com/OCA/web/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed feedback.
 
 Credits
 =======
@@ -41,6 +40,8 @@ Credits
 Contributors
 ------------
 
+ * Henry Zhou (MAXodoo) <zhouhenry@live.com>
+ * Rodney <https://github.com/rv-clearcorp>
  * Simone Orsi <simahawk@gmail.com>
  * Lorenzo Battistini <lorenzo.battistini@agilebg.com>
  * Stefan Rijnhart <stefan@therp.nl>
--- web_export_view/static/src/js/web_export_view.js
+++ web_export_view/static/src/js/web_export_view.js
@@ -1,38 +1,24 @@
-//  @@@ web_export_view custom JS @@@
-//#############################################################################
-//    
-//    Copyright (C) 2012 Agile Business Group sagl (<http://www.agilebg.com>)
-//    Copyright (C) 2012 Therp BV (<http://therp.nl>)
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU Affero General Public License as published
-//    by the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU Affero General Public License for more details.
-//
-//    You should have received a copy of the GNU Affero General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-//#############################################################################
-openerp.web_export_view = function (instance) {
+odoo.define('web_export_view.web_export_view', function (require) {
+"use strict";
 
-    var _t = instance.web._t, QWeb = instance.web.qweb;
+    var core = require('web.core');
+    var Sidebar = require('web.Sidebar');
+    var QWeb = core.qweb;
 
-    instance.web.Sidebar.include({
+    var _t = core._t;
+
+    Sidebar.include({
+
         redraw: function () {
             var self = this;
             this._super.apply(this, arguments);
-            if (self.getParent().ViewManager.active_view == 'list') {
-                self.$el.find('.oe_sidebar').append(QWeb.render('AddExportViewMain', {widget: self}));
-                self.$el.find('.oe_sidebar_export_view_xls').on('click', self.on_sidebar_export_view_xls);
+            if (self.getParent().ViewManager.active_view.type == 'list') {
+                self.$el.find('.o_dropdown').last().append(QWeb.render('AddExportViewMain', {widget: self}));
+                self.$el.find('.export_treeview_xls').on('click', self.on_sidebar_export_treeview_xls);
             }
         },
 
-        on_sidebar_export_view_xls: function () {
+        on_sidebar_export_treeview_xls: function () {
             // Select the first list of the current (form) view
             // or assume the main view is a list view and use that
             var self = this,
@@ -51,50 +37,51 @@ openerp.web_export_view = function (instance) {
                     return true;
                 });
             }
-            export_columns_keys = [];
-            export_columns_names = [];
+            var export_columns_keys = [];
+            var export_columns_names = [];
             $.each(view.visible_columns, function () {
-                if (this.tag == 'field') {
+                if (this.tag == 'field' && (this.widget === undefined || this.widget != 'handle')) {
                     // non-fields like `_group` or buttons
                     export_columns_keys.push(this.id);
                     export_columns_names.push(this.string);
                 }
             });
-            rows = view.$el.find('.oe_list_content > tbody > tr');
-            export_rows = [];
+            var rows = view.$el.find('.o_list_view > tbody > tr');
+            var export_rows = [];
             $.each(rows, function () {
-                $row = $(this);
+                var $row = $(this);
                 // find only rows with data
                 if ($row.attr('data-id')) {
-                    export_row = [];
-                    checked = $row.find('th input[type=checkbox]').attr("checked");
-                    if (children && checked === "checked") {
+                    var export_row = [];
+                    var checked = $row.find('.o_list_record_selector input[type=checkbox]').is(':checked');
+                    if (children && checked === true) {
                         $.each(export_columns_keys, function () {
-                            cell = $row.find('td[data-field="' + this + '"]').get(0);
-                            text = cell.text || cell.textContent || cell.innerHTML || "";
-                            if (cell.classList.contains("oe_list_field_float")) {
-                                export_row.push(instance.web.parse_value(text, {'type': "float"}));
-                            }
-                            else if (cell.classList.contains("oe_list_field_boolean")) {
-                                var data_id = $('<div>' + cell.innerHTML + '</div>');
-                                if (data_id.find('input').get(0).checked) {
+                            var $cell = $row.find('td[data-field="' + this + '"]')
+                            var $cellcheckbox = $cell.find('.o_checkbox input[type=checkbox]');
+                            if ($cellcheckbox.length) {
+                                if ($cellcheckbox.is(':checked')) {
                                     export_row.push(_t("True"));
                                 }
                                 else {
                                     export_row.push(_t("False"));
                                 }
                             }
-                            else if (cell.classList.contains("oe_list_field_integer")) {
-                                var tmp2 = text;
-                                do {
-                                    tmp = tmp2;
-                                    tmp2 = tmp.replace(instance.web._t.database.parameters.thousands_sep, "");
-                                } while (tmp !== tmp2);
-
-                                export_row.push(parseInt(tmp2));
-                            }
                             else {
-                                export_row.push(text.trim());
+                                var cell = $cell.get(0);
+                                var text = cell.text || cell.textContent || cell.innerHTML || "";
+
+                                if (cell.classList.contains("o_list_number")) {
+                                    var tmp2 = text;
+                                    do {
+                                        var tmp = tmp2;
+                                        tmp2 = tmp.replace(core._t.database.parameters.thousands_sep, "");
+                                    } while (tmp !== tmp2);
+
+                                    export_row.push(parseFloat(tmp2));
+                                }
+                                else {
+                                    export_row.push(text.trim());
+                                }
                             }
                         });
                         export_rows.push(export_row);
@@ -112,6 +99,6 @@ openerp.web_export_view = function (instance) {
                 complete: $.unblockUI
             });
         }
-    });
 
-};
+    });
+});
