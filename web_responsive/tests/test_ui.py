e5095f6802d9a0fba4ec72898b78d3296346900d
Dave Lasley <dave@laslabs.com>
2016-10-14 04:08:10 -0700

    [MIG] web_responsive: Migrate to v10 * Bump version * Change all openerp to odoo * Fix qunit suite injection * Remove jQuery require * Change to new selectors:   * `oe_leftbar` to `o_sub_menu`   * `oe_logo` to `o_sub_menu_logo`   * `oe_footer` to `o_sub_menu_footer`   * `oe_secondary_menus_container` to `o_sub_menu_content` * Add style to hide oe_footer instead of remove to not break `support_branding` * Add note in ReadMe explaining override of `support_branding` * Set top margin of app drawer title to 0 to fit v10 proportions * Fix notification badge positioning * Add o_web_client class to body to fix overlaps * Scroll control panel with page * Change navbar z-index to not be overlapped by buttons * Raise z-index on header to raise over buttons * Handle layered notifications via z-index * Remove `#` from navigation links HREF to prevent history littering

--- web_responsive/tests/test_ui.py
+++ web_responsive/tests/test_ui.py
@@ -2,7 +2,7 @@
 # Copyright 2016 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from openerp.tests import HttpCase
+from odoo.tests import HttpCase
 
 
 class TestUi(HttpCase):
