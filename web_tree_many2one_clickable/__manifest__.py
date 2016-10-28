10f1a84805c5e72db6a0a10878bd1357ca734a01
Antonio Espinosa <antespi@gmail.com>
2016-10-28 20:33:48 +0200

    web_tree_many2one_clickable

--- web_tree_many2one_clickable/__manifest__.py
+++ web_tree_many2one_clickable/__manifest__.py
@@ -1,38 +1,26 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright
-#      (C) 2013 Therp BV (<http://therp.nl>).
-#      (c) 2015 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
-#               Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2013 Therp BV (<http://therp.nl>).
+# Copyright 2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
     "name": "Clickable many2one fields for tree views",
-    "version": "8.0.1.0.0",
+    "summary": "Open the linked resource when clicking on their name",
+    "version": "9.0.1.0.0",
+    "category": "Hidden",
+    "website": "https://odoo-community.org/",
     "author": "Therp BV, "
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "Tecnativa, "
               "Odoo Community Association (OCA)",
-    "category": "Dependency",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
     "depends": [
         'web',
     ],
     "data": [
+        'data/ir_config_parameter.xml',
         'views/asset.xml',
     ],
-    'installable': False,
 }
