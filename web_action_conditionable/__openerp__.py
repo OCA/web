6c4d4840cd9b0076661c4f0d296970a4967bbc91
Cristian Salamea <ovnicraft@gmail.com>
2016-10-31 14:13:05 -0500

    migrate to 9.0

--- web_action_conditionable/__openerp__.py
+++ web_action_conditionable/__openerp__.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
+# © 2016 Cristian Salamea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
-    "name": 'web_action_conditionable',
-    "version": "8.0.0.1.0",
-    "depends": [
-        'base',
+    'name': 'Conditionable states',
+    'version': '9.0.0.1.0',
+    'license': 'AGPL-3',
+    'category': 'Web',
+    'depends': [
         'web',
     ],
     'data': ['views/view.xml'],
-    "author": "Cristian Salamea,Odoo Community Association (OCA)",
-    'installable': False,
+    'author': 'Cristian Salamea,Odoo Community Association (OCA)',
+    'installable': True,
 }
