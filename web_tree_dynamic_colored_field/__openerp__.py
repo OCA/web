cf70d8590bb79b46a6239f1dfcf1414c336af3a6
Holger Brunn <hbrunn@therp.nl>
2016-11-08 16:01:42 +0100

    [ADD] demo view

--- web_tree_dynamic_colored_field/__openerp__.py
+++ web_tree_dynamic_colored_field/__openerp__.py
@@ -10,6 +10,9 @@
     'author': "Camptocamp,Therp BV,Odoo Community Association (OCA)",
     'license': 'AGPL-3',
     'website': 'https://github.com/OCA/web',
+    'demo': [
+        "demo/res_users.xml",
+    ],
     'data': [
         'views/web_tree_dynamic_colored_field.xml',
     ],
