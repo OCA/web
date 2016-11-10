09aafa665be58d2dff338578d3d40d7bb1a39db6
tarteo <dennissluijk@outlook.com>
2016-11-10 21:11:50 +0100

    [ADD] web_x2many_delete_all

---
+++ web_x2many_delete_all/static/src/js/web_x2many_delete_all.js
@@ -0,0 +1,39 @@
+/* Copyright 2016 Onestein
+ * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
+
+odoo.define('web_x2many_delete_all', function (require) {
+"use strict";
+    var core = require('web.core'),
+        _t = core._t;
+
+    var X2ManyListDeleteAllMixin = {
+        events: {
+            'click th.o_list_record_delete': 'btn_delete_all_clicked'
+        },
+        reload_current_view: function() {
+            var self = this;
+            var res = this._super.apply(this, arguments);
+            res.then(function() {
+                self.toggle_btn_delete_all();
+            });
+            return res
+        },
+        toggle_btn_delete_all: function() {
+            if(this.get('effective_readonly')) {
+                this.$('th.o_list_record_delete > .fa-trash-o').addClass('hidden');
+            } else {
+                this.$('th.o_list_record_delete > .fa-trash-o').removeClass('hidden');
+            }
+        },
+        btn_delete_all_clicked: function() {
+            if(this.get('effective_readonly')) return;
+            this.delete_all();
+        },
+        delete_all: function() {
+            this.viewmanager.views.list.controller.do_delete(this.dataset.ids);
+        }
+    }
+
+    core.form_widget_registry.get('many2many').include(X2ManyListDeleteAllMixin);
+    core.form_widget_registry.get('one2many').include(X2ManyListDeleteAllMixin);
+});
