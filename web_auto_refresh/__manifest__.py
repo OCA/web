cc2db5cd884a904c41431c71b06f7767f8b9dbbd
szufisher <szufisher@gmail.com>
2016-11-18 15:47:01 +0800

    Create __manifest__.py

---
+++ web_auto_refresh/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Auto Refresh',
+    'version': '10.0.1.0.0',
+    'author': 'Fisher Yu, Odoo Community Association (OCA)',
+    'category': 'Base',
+    'website': 'https://github.com/szufisher',
+    'depends': ['web','bus', 'mail'],
+    'data': [
+        'views.xml',
+        ],
+    'qweb': [        
+    ],
+    'installable': True
+}
