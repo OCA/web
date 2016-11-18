ed582f0682decdc59874ff14eee2d76b5b0417b3
szufisher <szufisher@gmail.com>
2016-11-18 15:13:26 +0800

    Create __manifest__.py

---
+++ m2o_multi/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'many2one multi selection',
+    'version': '10.0.1.0.0',
+    'author': 'Fisher Yu, ',
+    'category': 'Base',
+    'website': 'https://github.com/szufisher',
+    'depends': ['web'],
+    'data': [
+        'views.xml',
+        ],
+    'qweb': [        
+    ],
+    'installable': True
+}
