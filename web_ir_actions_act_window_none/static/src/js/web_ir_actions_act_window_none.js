61fdb5cb8b1dabd6a45e0fddfcf2d88c13b58632
susport <richard.pompa84@gmail.com>
2016-11-21 16:25:59 -0300

    change js v9 Odoo
    change js v9 Odoo

--- web_ir_actions_act_window_none/static/src/js/web_ir_actions_act_window_none.js
+++ web_ir_actions_act_window_none/static/src/js/web_ir_actions_act_window_none.js
@@ -2,7 +2,7 @@
 //?? 2016 Therp BV <http://therp.nl>
 //License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-openerp.web_ir_actions_act_window_none = function(instance)
+odoo.web_ir_actions_act_window_none = function(instance)
 {
     instance.web.ActionManager.include({
         ir_actions_act_window_none: function(action)
