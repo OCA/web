173e3063a44e34ca62a8853744f86cf420905c62
Dave Lasley <dave@laslabs.com>
2016-11-21 10:58:17 -0800

    Remove python test - functionality covered in UI test

--- web_session_allow_public/tests/test_ir_http.py
+++ web_session_allow_public/tests/test_ir_http.py
@@ -2,28 +2,7 @@
 # Copyright 2016 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-import mock
-
-from openerp.http import SessionExpiredException
-from openerp.tests.common import HttpCase, TransactionCase
-
-
-MODULE_PATH = 'openerp.addons.web_session_allow_public.models.ir_http'
-
-
-class TestIrHttp(TransactionCase):
-
-    def setUp(self):
-        super(TestIrHttp, self).setUp()
-        self.Model = self.env['ir.http']
-
-    @mock.patch('%s.super' % MODULE_PATH)
-    def test_auth_method_user(self, _super):
-        """ It should return public session on SessionExpiredException """
-        _super.auth_method_user.side_effect = SessionExpiredException
-        with mock.patch.object(self.Model, '_auth_method_public') as public:
-            res = self.Model._auth_method_user()
-            self.assertEqual(res, public())
+from openerp.tests.common import HttpCase
 
 
 class TestPublicSession(HttpCase):
