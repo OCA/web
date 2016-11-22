d83ec0858f0e7dcfad27f6205a4a00502204c966
Dave Lasley <dave@laslabs.com>
2016-11-21 18:17:39 -0800

    Switch to python test in light of mock issue

--- web_session_allow_public/tests/test_ir_http.py
+++ web_session_allow_public/tests/test_ir_http.py
@@ -2,13 +2,24 @@
 # Copyright 2016 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
+import mock
+
 from openerp.tests.common import HttpCase
 
+from openerp.addons.base.ir.ir_http import ir_http
 
-class TestPublicSession(HttpCase):
 
-    def test_ui_web(self):
-        """ It should allow a Model query using public user """
-        self.phantom_js("/web/tests?module=web_session_allow_public",
-                        "",
-                        )
+MODULE_PATH = 'openerp.addons.base.ir.ir_http'
+
+
+class TestIrHttp(HttpCase):
+
+    @mock.patch('%s.request' % MODULE_PATH)
+    def test_public_session(self, request):
+        """ It should allow HTTP authentication on public user """
+        request.session.uid = False
+        with mock.patch.object(
+            ir_http, '_auth_method_public'
+        ) as _auth_method_public:
+            res = self.env['ir.http']._auth_method_user()
+            self.assertEqual(res, _auth_method_public())
