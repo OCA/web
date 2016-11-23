7ff6f9862abb27af30cb66a031aeb93f5f7650a3
susport <richard.pompa84@gmail.com>
2016-11-23 19:12:55 -0300

    mig v9.0 js
    mig v9.0 js

--- web_ir_actions_act_window_none/static/src/js/web_ir_actions_act_window_none.js
+++ web_ir_actions_act_window_none/static/src/js/web_ir_actions_act_window_none.js
@@ -2,18 +2,23 @@
 //?? 2016 Therp BV <http://therp.nl>
 //License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-odoo.web_ir_actions_act_window_none = function(instance)
-{
-    instance.web.ActionManager.include({
-        ir_actions_act_window_none: function(action)
-        {
-            if(action.reload === undefined || action.reload)
+
+odoo.define('web_ir_actions_act_window_none', function(require) {
+
+var ActionManager = require('web.ActionManager');
+    ActionManager.include({
+
+    ir_actions_act_window_none: function(action)
             {
-                if(this.dialog_widget && this.dialog_widget.views.form)
+                if(action.reload === undefined || action.reload)
                 {
-                    return this.dialog_widget.views.form.controller.reload();
+                    if(this.dialog_widget && this.dialog_widget.views.form)
+                    {
+                        return this.dialog_widget.views.form.controller.reload();
+                    }
                 }
-            }
-        },
+            },
+
     });
-}
+
+});
