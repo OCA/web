f9da21f861b2c7b68dfe9e005bf05cb61a947d0d
szufisher <szufisher@gmail.com>
2016-11-28 10:14:21 +0800

    Create __init__.py

---
+++ bi_view_editor/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2015-2016 ONESTEiN BV (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import bve_view
+from . import ir_model
