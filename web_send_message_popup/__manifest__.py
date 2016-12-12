fa31829af5bf26380a6e776bea75841c226311f3
Nicolas JEUDY <nicolas@sudokeys.com>
2016-12-12 23:48:00 +0100

    [MIG] web_send_message_popup 10.0 migration (#504)
    * Intitial manifest changes
    
    * [FIX] small README tricks

--- web_send_message_popup/__manifest__.py
+++ web_send_message_popup/__manifest__.py
@@ -2,7 +2,7 @@
 # © 2014-2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {'name': 'Web Send Message as Popup',
- 'version': '9.0.1.0.0',
+ 'version': '10.0.1.0.0',
  'author': "Camptocamp, Odoo Community Association (OCA)",
  'maintainer': 'Camptocamp',
  'license': 'AGPL-3',
@@ -12,6 +12,6 @@
  'website': 'http://www.camptocamp.com',
  'data': ['views/web_send_message_popup.xml'
           ],
- 'installable': False,
+ 'installable': True,
  'auto_install': False,
  }
