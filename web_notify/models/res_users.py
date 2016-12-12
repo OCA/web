791e491d294ac32cefb706f86c64ea4a46a16d8a
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-12-12 17:01:31 +0100

    [IMP] Add possibility to send notification in new environement.

--- web_notify/models/res_users.py
+++ web_notify/models/res_users.py
@@ -4,6 +4,8 @@
 
 from odoo import api, fields, models, _
 
+import odoo
+
 
 class ResUsers(models.Model):
 
@@ -23,19 +25,21 @@ class ResUsers(models.Model):
         compute='_compute_channel_names')
 
     @api.multi
-    def notify_info(self, message, title=None, sticky=False):
+    def notify_info(self, message, title=None, sticky=False, commit=False):
         title = title or _('Information')
         self._notify_channel(
-            'notify_info_channel_name', message, title, sticky)
+            'notify_info_channel_name', message, title, sticky, commit=commit)
 
     @api.multi
-    def notify_warning(self, message, title=None, sticky=False):
+    def notify_warning(self, message, title=None, sticky=False, commit=False):
         title = title or _('Warning')
         self._notify_channel(
-            'notify_warning_channel_name', message, title, sticky)
+            'notify_warning_channel_name', message, title, sticky,
+            commit=commit)
 
     @api.multi
-    def _notify_channel(self, channel_name_field, message, title, sticky):
+    def _notify_channel(self, channel_name_field, message, title, sticky,
+                        commit=False):
         bus_message = {
             'message': message,
             'title': title,
@@ -43,4 +47,13 @@ class ResUsers(models.Model):
         }
         notifications = [(getattr(record, channel_name_field), bus_message)
                          for record in self]
-        self.env['bus.bus'].sendmany(notifications)
+        if commit:
+            with odoo.api.Environment.manage():
+                with odoo.registry(self.env.cr.dbname).cursor() as new_cr:
+                    new_env = api.Environment(new_cr, self.env.uid,
+                                              self.env.context)
+                    self.env['bus.bus'].with_env(new_env).sendmany(
+                        notifications)
+                    new_env.cr.commit()
+        else:
+            self.env['bus.bus'].sendmany(notifications)
