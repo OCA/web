839262a11bda12c3f87fc6b584323d74e184d0fd
Nicolas Mac Rouillon <nmr@adhoc.com.ar>
2016-12-13 16:02:54 -0300

    FIX remove api.one

--- web_advanced_search_wildcard/static/src/js/search.js
+++
@@ -1,6 +0,0 @@
-openerp.web_advanced_search_wildcard = function(instance){
-    var _lt = instance.web._lt;
-    instance.web.search.ExtendedSearchProposition.Char.prototype.operators.push(
-        {value: '=ilike', text: _lt("matches")}
-    );
-};
