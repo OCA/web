ebb55129fd8b04875f07a575ee7a29885b9c45e3
Andrea <a.stirpe@onestein.nl>
2016-12-27 10:31:18 +0100

    Add module web_disable_export_group

---
+++ web_disable_export_group/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Web Disable Export Group',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Onestein',
+    'website': 'http://www.onestein.eu',
+    'category': 'Web',
+    'depends': ['web'],
+    'data': [
+        'security/groups.xml',
+        'templates/assets.xml',
+    ],
+    'installable': True,
+}
