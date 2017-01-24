987d0a5eebfb37fd0cb275fb8b1d7e0e4fab9644
Dennis Sluijk <dennissluijk@outlook.com>
2017-01-24 16:04:15 +0100

    [FIX] Copyright year

--- web_listview_range_select/__manifest__.py
+++ web_listview_range_select/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Onestein (<http://www.onestein.eu>)
+# © 2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
