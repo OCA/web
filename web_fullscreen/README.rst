7496beb4137d237059c04de28877283e617aba80
Andrea <a.stirpe@onestein.nl>
2017-02-02 11:01:28 +0100

    Updated author + minor code review of web_fullscreen

---
+++ web_fullscreen/README.rst
@@ -0,0 +1,18 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==========
+Fullscreen
+==========
+
+Adds a fullscreen mode button that toggles the primary and secondary menu.
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Dennis Sluijk <d.sluijk@onestein.nl>
--- web_fullscreen/__openerp__.py
+++ web_fullscreen/__openerp__.py
@@ -1,33 +1,14 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2016 ONESTEiN BV (<http://www.onestein.nl>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': "Fullscreen",
-    'summary': """""",
-    'description': """
-Adds a fullscreen mode button that toggles the primary and secondary menu.
-""",
-    'author': "ONESTEiN BV",
-    'website': "http://www.onestein.eu",
+    'name': 'Fullscreen',
+    'summary': 'Adds a fullscreen mode button',
+    'author': 'Onestein',
+    'website': 'http://www.onestein.eu',
     'category': 'Extra Tools',
-    'version': '1.0',
+    'version': '8.0.1.0.0',
     'depends': ['web'],
     'data': [
         'views/web_fullscreen_views.xml'
@@ -35,8 +16,5 @@ Adds a fullscreen mode button that toggles the primary and secondary menu.
     'qweb': [
         'views/web_fullscreen_templates.xml'
     ],
-    'demo': [],
     'installable': True,
-    'auto_install': False,
-    'application': False,
 }
