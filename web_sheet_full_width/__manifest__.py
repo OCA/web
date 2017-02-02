4154cdab0c4f2ba7b8ce7b75daba7bc124be7682
Frédéric Garbely <BT-fgarbely@users.noreply.github.com>
2017-02-02 19:16:24 +0100

    [FIX] Bugfix in web_sheet_full_width for enterprise edition (#530)

--- web_sheet_full_width/__manifest__.py
+++ web_sheet_full_width/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Show sheets with full width",
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.0.1",
     "author": "Therp BV,Sudokeys,GRAP,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "summary": "Use the whole available screen width when displaying sheets",
