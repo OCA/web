45ca9c89b202c2105c74c4d5c28a45984ff0c617
tarteo <dennissluijk@outlook.com>
2017-02-03 12:20:09 +0100

    [ADD] web_chatter_paste
    Initial commit
    
    Initial commit
    
    [ADD] Tests
    
    [FIX] Dependencies
    
    [REM] Controller tests
    
    [FIX] Removed unnecessary incompatibility warning
    
    [FIX] Hyperlink to issues
    
    [ADD] Tests
    
    [FIX] Tests
    
    [?] Testing test problem
    
    [?] Testing test problem
    
    [?] Testing test problem

---
+++ web_chatter_paste/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Chatter Paste',
+    'summary': """
+        Paste images and drop files into the chatter and upload them directly
+    """,
+    'version': '10.0.1.0.0',
+    'category': 'Web',
+    'author': 'Onestein,Odoo Community Association (OCA)',
+    'website': 'http://www.onestein.eu',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'web'
+    ],
+    'data': [
+        'templates/assets.xml'
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ web_chatter_paste/tests/test_web_chatter_paste.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo.http import request
+from odoo.tests.common import TransactionCase
+from odoo.addons.web_chatter_paste.controllers.main \
+    import ChatterPasteController
+
+
+class TestWebChatterPaste(TransactionCase):
+    def test_controller(self):
+        partner_id = self.ref('base.main_partner')
+        attachment_obj = self.env['ir.attachment'].with_env(request.env)
+        attachment_count = attachment_obj.search_count([
+            ('res_model', '=', 'res.partner'),
+            ('res_id', '=', partner_id)
+        ])
+        controller = ChatterPasteController()
+        controller.upload_attachment(
+            '_',
+            'res.partner',
+            partner_id,
+            'test.png',
+            'image/png',
+            'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6'
+            'QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFN'
+            'vZnR3YXJlAHBhaW50Lm5ldCA0LjAuOWwzfk4AAAAMSURBVBhXY/j//z8ABf4C/q'
+            'c1gYQAAAAASUVORK5CYII='
+        )
+        new_attachment_count = attachment_obj.search_count([
+            ('res_model', '=', 'res.partner'),
+            ('res_id', '=', partner_id)
+        ])
+        self.assertEqual(attachment_count + 1, new_attachment_count)
