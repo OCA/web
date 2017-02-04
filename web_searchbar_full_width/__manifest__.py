34feda51c7390c2e992a333bcc01b45b31aa8d7a
Siddharth Bhalgami <siddharth.bhalgami@gmail.com>
2017-02-04 16:17:52 +0530

    [10.0-MIG] web_searchbar_full_width for v10.0

--- web_searchbar_full_width/__manifest__.py
+++ web_searchbar_full_width/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Full width searchbar",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "Therp BV, "
               "Odoo Community Association (OCA), "
               "Siddharth Bhalgami",
