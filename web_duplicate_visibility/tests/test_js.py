8a26438ba9ff06b769c0d234ecb4f99a72f938a1
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2017-02-15 14:33:40 +0100

    [FIX] Migrate qunit tests for web_duplicate_visibility to 10.0

--- web_duplicate_visibility/tests/test_js.py
+++ web_duplicate_visibility/tests/test_js.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import odoo.tests
 
-from openerp.tests import HttpCase
 
-
-class TestJS(HttpCase):
+@odoo.tests.common.at_install(False)
+@odoo.tests.common.post_install(True)
+class TestJS(odoo.tests.HttpCase):
     def test_js(self):
         self.phantom_js(
             "/web/tests?module=web_duplicate_visibility",
