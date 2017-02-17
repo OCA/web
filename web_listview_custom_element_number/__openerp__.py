d3a6adbe092a914405b5aff6866fe6397f9a514f
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-02-17 22:33:24 +0100

    [PORT] 9.0 web_listview_custom_element_number

--- web_listview_custom_element_number/__openerp__.py
+++ web_listview_custom_element_number/__openerp__.py
@@ -1,33 +1,16 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Web - Custom Element Number in ListView module for Odoo
-#    Copyright (C) 2015-Today Akretion (http://www.akretion.com)
-#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015-Today Akretion (http://www.akretion.com)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Web - Custom Element Number in ListView',
     'summary': 'Allow users to set manually a quantity of items to display'
             ' in a tree view',
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'category': 'web',
-    'author': "Akretion,Odoo Community Association (OCA)",
-    'website': 'http://www.akretion.com',
+    'author': "GRAP,Akretion,Odoo Community Association (OCA)",
+    'website': 'https://odoo-community.org/',
     'license': 'AGPL-3',
     'depends': [
         'web',
@@ -35,5 +18,5 @@
     'data': [
         'views/templates.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
