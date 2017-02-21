7300e2e0d29bc4eb46c4b004d32eab6be110ebbf
Jairo Llopis <yajo.sk8@gmail.com>
2017-02-21 10:57:55 +0100

    [FIX] web_advanced_search_x2x: Several things
    * Make it good-looking
    * Update known issues
    * Fix templates.
    * Make other operators work again

--- web_advanced_search/static/src/js/web_advanced_search_x2x.js
+++ web_advanced_search/static/src/js/web_advanced_search_x2x.js
@@ -119,13 +119,8 @@ odoo.define('web_advanced_search_x2x.search_filters', function (require) {
         x2x_autocomplete_open: function()
         {
             var widget = this._x2x_field.$input.autocomplete("widget");
-            widget.on('click', 'li', function(e)
-            {
-                widget.trigger(
-                    'menuselect',
-                    {item: jQuery(e.currentTarget)}
-                );
-                e.stopPropagation();
+            widget.on('click', 'li', function(event) {
+                event.stopPropagation();
             });
         },
         get_domain: function () {
@@ -201,6 +196,8 @@ odoo.define('web_advanced_search_x2x.search_filters', function (require) {
          * @return {jQuery.Deferred}
          */
         value_rerender: function () {
+            this.value._x2x_field && this.value._x2x_field.destroy();
+            delete this.value._x2x_field;
             return this.value.appendTo(
                 this.$(".searchview_extended_prop_value").show().empty()
             );
