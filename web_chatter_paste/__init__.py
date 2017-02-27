16b9608f8461a2e6df338e62ccb50698deb56a28
tarteo <dennissluijk@outlook.com>
2017-02-27 09:53:14 +0100

    [ADD] Tests

--- web_chatter_paste/__init__.py
+++ web_chatter_paste/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2017 Onestein (<http://www.onestein.eu>)
+# Copyright 2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import controllers
--- web_chatter_paste/tests/__init__.py
+++ web_chatter_paste/tests/__init__.py
@@ -2,4 +2,4 @@
 # Copyright 2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import main
+from . import test_web_chatter_paste
