f502b984530e6110fe58c06f7ae094a98bdd908d
tarteo <dennissluijk@outlook.com>
2017-02-27 08:57:09 +0100

    [FIX] Short headers

--- web_shortcut/tests/test_web_shortcut.py
+++ web_shortcut/tests/test_web_shortcut.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Copyright 2017-today Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openerp.tests.common import TransactionCase
 
 
