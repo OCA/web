cebb11140426b039f707548d924830bfb8f80d25
tarteo <dennissluijk@outlook.com>
2017-03-01 11:19:17 +0100

    [FIX] Copyrights, disclaimer, authors

--- web_shortcut/__openerp__.py
+++ web_shortcut/__openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2010-2011 Odoo SA (<http://openerp.com>).
+# Copyright 2010-2011 Odoo SA (<http://odoo.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Shortcut Menu',
