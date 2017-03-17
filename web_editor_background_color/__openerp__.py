4f47b99ad9d6e80ba8c8a3c60be3e6bb05358a77
Jairo Llopis <Yajo@users.noreply.github.com>
2017-03-17 20:29:42 +0100

    [ADD][web_editor_background_color] Custom background color picker (#582)
    * [ADD][web_editor_background_color] Custom background color picker
    
    This addon lets the user choose a preferred background color when editing some web content.
    
    Works out of the box with `website` and `mass_mailing` addons, although requires none of those.
    
    * [FIX] Avoid clicks removing dropdown menu
    
    This happened specifically in Chrome as demonstrated in
    https://github.com/OCA/web/pull/582#issuecomment-287057052.
    
    * [FIX] Use strict equality operator.

---
+++ web_editor_background_color/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Web Editor Background Color Picker",
+    "summary": "Set any background color for web editor snippets",
+    "version": "9.0.1.0.0",
+    "category": "Website",
+    "website": "https://www.tecnativa.com",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "images": [
+        "static/description/mass_mailing_editor.png",
+    ],
+    "depends": [
+        "web_editor",
+    ],
+    "data": [
+        "views/assets.xml",
+        "views/snippets.xml",
+    ],
+}
