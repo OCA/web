03660703e78f3f53845e526bb020d7691980f996
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2017-03-18 17:57:20 +0530

    [MIG] Migrated web_search_alphabetic to v10
    [MIG] Migrated web_search_alphabetic to v10

---
+++ web_search_alphabetic/README.rst
@@ -0,0 +1,60 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License: AGPL-3
+
+=====================
+Web Search Alphabetic
+=====================
+
+Provides search functionality with the help of alphabetic buttons.
+
+After the installation of the module, you will be able to see alphabetic buttons appeared on the top of the Views panel
+in sequence. By clicking on any of these buttons, it will provide you with the records starting with the relevant alphabet.
+
+
+Installation
+============
+
+This module is simple to install as it has no depenency on addons other than core web module.
+
+
+Usage
+=====
+
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/162/10.0
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues <https://github.com/OCA/web/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed feedback.
+
+Credits
+=======
+
+Images
+------
+
+* Odoo Community Association: `Icon <https://github.com/OCA/maintainer-tools/blob/master/template/module/static/description/icon.svg>`_.
+
+Contributors
+------------
+
+* Serpent Consulting Services Pvt. Ltd.<jay.vora@serpentcs.com>
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit https://odoo-community.org.
--- web_search_alphabetic/static/src/js/web_search.js
+++ web_search_alphabetic/static/src/js/web_search.js
@@ -1,32 +1,55 @@
-openerp.web_search_alphabetic = function (instance) {
-	var QWeb = instance.web.qweb;
-    instance.web.SearchView.include({
+odoo.define('web_search_alphabetic',function(require){
+"use strict";
+
+    var core = require('web.core')
+    var QWeb = core.qweb
+    var SearchView = require('web.SearchView');
+    var ViewManager = require('web.ViewManager');
+    var FormView = require('web.FormView');
+    var ControlPanel = require('web.ControlPanel');
+
+    SearchView.include({
         start: function() {
-            var result = this._super();
             var self = this;
-            self.search_on = "name";
-            self.fields_selection = [];
-            this.dataset.call('fields_get', [false, {}]).done(function (fields) {
-                $.each(fields, function (value) {
-                    if(fields[value].type == "char"){
-                        fields[value].id = value;
-                        self.fields_selection.push(fields[value]);
-                    }
-                })
-                if(self.fields_selection.length){
-                    $('.field_selection_column').append((QWeb.render('field-selection', {widget: self})));
-                    $("#field_name_selection").change(function(){
-                        self.search_on =  $(this).val() || false;
-                    })
+            return $.when(this._super()).done(function(){
+                if(self.ViewManager && self.ViewManager.action && self.ViewManager.action.target != 'new'){
+                    self.search_on = "name";
+                    self.fields_selection = [];
+                    self.dataset.call('fields_get', [false, {}]).done(function (fields) {
+                        $.each(fields, function (value) {
+                            if(fields[value].type == "char") {
+                                fields[value].id = value;
+                                self.fields_selection.push(fields[value]);
+                            }
+                        })
+                        if(self.fields_selection.length){
+                            if($("#field_name_selection")){
+                                $("#field_name_selection").remove();
+                            }
+                            if($('.field_selection_column')[0]){
+                                $('.field_selection_column').append((QWeb.render('field-selection', {widget: self})));
+                                self.search_on = $('#field_name_selection').val();
+                                $("#field_name_selection").change(function(){
+                                    self.search_on =  $(this).val() || false;
+                                });
+                                $('.oe_filter_label').unbind('click').bind('click',function (e) {
+                                    $(this).toggleClass('enabled');
+                                    if($(this).hasClass('enabled')){
+                                        $(this).addClass('highlight')
+                                    }
+                                    else{
+                                        $(this).removeClass('highlight')
+                                    }
+                                    self.do_search();
+                                });
+                            }
+                        }
+                    });
                 }
             });
-            $('.oe_filter_label').click(function (e) {
-                $(this).toggleClass('enabled');
-                self.do_search();
-            });
-            return result;
         },
-        search_filter: function(){
+
+        search_filter: function() {
             var filter_domain = [];
             var self = this;
             _.each($('.oe_filter_label'), function(value) {
@@ -36,45 +59,61 @@ openerp.web_search_alphabetic = function (instance) {
             });
             if (filter_domain.length) {
                 var filter_or_domain = [];
-                for (i = 0; i < filter_domain.length-1; i++) {
+                for (var i = 0; i < filter_domain.length-1; i++) {
                     filter_or_domain.push("['|']");
                 }
                 return filter_or_domain.concat(filter_domain || []);
             }
             return false;
         },
+
         build_search_data: function () {
             var result = this._super();
-            filter_domain = this.search_filter();
+            var filter_domain = this.search_filter();
             if (filter_domain)
                 result['domains'] = filter_domain.concat(result.domains || []);
             return result;
         }, 
     });
 
-    instance.web.ViewManager.include({
-        switch_mode: function(view_type, no_store, view_options) {
+    ViewManager.include({
+        switch_mode: function(view_type, view_options) {
+            var self = this;
             var view = this.views[view_type];
-            var result = this._super(view_type, no_store, view_options);
-            if (this.searchview && this.active_view != "form") {
-                if ((view.controller.searchable === false || this.searchview.options.hidden)  || this.active_view == "form"){
-                    $('.search_filter').live().hide();
-                }else
-                    $('.search_filter').show();
-            }
-            else{
-                $('.search_filter').live().hide();
+            var result = this._super(view_type, view_options);
+            if(this.action && this.action.target != 'new'){
+                if (this.searchview) {
+                    if(view_type=="form" || (view.controller.searchable === false || this.searchview.options.hidden) || this.active_view == "form"){
+                            $('.search_filter').on().hide();
+                    }else{
+                        $('.search_filter').show();
+                    }
+                }else{
+                    $('.search_filter').on().hide();
+                }
             }
             return result;
         },
     });
-    instance.web.FormView.include({
+
+    FormView.include({
         load_defaults: function () {
             if($('.search_filter'))
                 $('.search_filter').hide();
             return this._super();
         },
     });
-};
 
-// vim:et fdc=0 fdl=0 foldnestmax=3 fdm=syntax:
+    ControlPanel.include({
+        _toggle_visibility: function(visible) {
+            this._super();
+            this.do_toggle(visible);
+            if (!visible && !this.$content) {
+                this.$content = this.$el.contents().detach();
+            } else if (this.$content) {
+                this.$content.appendTo(this.$el);
+                this.$content = null;
+            }
+        },
+    })
+});
