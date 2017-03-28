2f797d462250e91e1032c2f3048f2b31a187d9c4
zeroheure <xavier@alternatif.org>
2017-03-28 23:17:25 +0200

    Create ribbon.py

---
+++ web_environment_ribbon/models/ribbon.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2017 Xavier Brochard
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+
+import os
+from odoo import models
+
+# brouillon OCA/server-tools/blob/10.0/auto_backup/models/db_backup.py
+class DbName(models.Model):
+    folder = fields.Char(
+        default=lambda self: self._default_name(),
+        help='Text printed on the Ribbon',
+        required=True
+    )
+    @api.model
+    def _default_name(self):
+        """Default to database name."""
+        return self.env.cr.dbname
