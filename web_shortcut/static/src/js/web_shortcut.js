d54732716d975fba844bcd9aa966e416bcd8b970
tarteo <dennissluijk@outlook.com>
2017-04-25 14:49:33 +0200

    [FIX] Refactor to fix issues

--- web_shortcut/static/src/js/web_shortcut.js
+++ web_shortcut/static/src/js/web_shortcut.js
@@ -60,12 +60,12 @@ odoo.define('web.shortcut', function (require) {
         },
         click: function ($link) {
             var self = this,
-                action_id = $link.data('id');
-            new DataModel('ir.ui.menu').query(['action']).filter([['id', '=', action_id]]).context(null).all().then(function (menu) {
+                menu_id = $link.data('id');
+            new DataModel('ir.ui.menu').query(['action']).filter([['id', '=', menu_id]]).context(null).all().then(function (menu) {
                 var action_str = menu[0].action;
                 var action_str_parts = action_str.split(',');
-                action_id = parseInt(action_str_parts[1]);
-                self.trigger('click', action_id);
+                var action_id = parseInt(action_str_parts[1]);
+                self.trigger('click', action_id, menu_id);
             });
         },
         has: function (menu_id) {
@@ -89,10 +89,12 @@ odoo.define('web.shortcut', function (require) {
                 self.shortcut_menu = _.find(self.systray_menu.widgets, function (item) {
                     return item instanceof ShortcutMenu;
                 });
-                self.shortcut_menu.on('click', self, function (action_id) {
+                self.shortcut_menu.on('click', self, function (action_id, menu_id) {
                     self.do_action(action_id, {
                         clear_breadcrumbs: true,
-                        replace_breadcrumb: true
+                        replace_breadcrumb: true,
+                        action_menu_id: menu_id,
+                        shortcut: true
                     });
                 });
             });
@@ -101,71 +103,94 @@ odoo.define('web.shortcut', function (require) {
 
 
     ViewManager.include({
+        init: function() {
+            var self = this;
+            var res = this._super.apply(this, arguments);
+
+            if (this.action_manager) {
+                // Anonymous users don't have user_menu
+                // Check whether it's the 'main' action_manager
+                this.shortcut_menu = this.action_manager.webclient && this.action_manager.webclient.shortcut_menu;
+            }
+            return res;
+        },
         switch_mode: function (view_type, no_store) {
             var self = this;
             return this._super.apply(this, arguments).done(function () {
-                self.shortcut_check(self.views[view_type]);
+                self.$shortcut_toggle = self.action_manager.main_control_panel.$el.find('.oe_shortcut_toggle');
+                self.shortcut_toggle_bind();
+                var visible = self.shortcut_toggle_visibility(self.views[view_type]);
+                if (visible) {
+                    $.when(self.action_manager.webclient.menu.initial_menu_open).then(function() {
+                        self.shortcut_toggle_state();
+                    });
+                }
             });
         },
-        shortcut_check: function (view) {
-            var self = this;
-
-            // Child view managers
-            if (!this.action_manager) {
-                return;
-            }
-
+        shortcut_toggle_visibility: function(view) {
             // display shortcuts if on the first view for the action
-            var $shortcut_toggle = this.action_manager.main_control_panel.$el.find('.oe_shortcut_toggle');
-            if (!this.action.name || !(view.view_type === this.view_stack[0].view_type &&
+            if ((!this.action.name || !(view.view_type === this.view_stack[0].view_type &&
                 view.view_id === this.view_stack[0].view_id)
-            ) {
-                $shortcut_toggle.addClass('hidden');
-                return;
+            ) || !this.action_manager.webclient) {
+                this.$shortcut_toggle.addClass('hidden');
+                return false;
             }
-            $shortcut_toggle.removeClass('hidden');
-
-            // Anonymous users don't have user_menu
-            // Check whether it's the 'main' action_manager
-            var shortcuts_menu = this.action_manager.webclient && this.action_manager.webclient.shortcut_menu;
-            if (shortcuts_menu) {
-                // Toggle on / off
-                $.when(this.action_manager.webclient.menu.initial_menu_open).then(function() {
-                    $shortcut_toggle.toggleClass('oe_shortcut_remove', shortcuts_menu.has(self.session.active_id));
-                    $shortcut_toggle.unbind("click").click(function () {
-                        var menu_id = session.active_id;
-                        // In the case we come from a parent menu, no action is linked to the menu
-                        // We must take the first child menu
-                        if (self.action_manager.webclient.menu_data) {
-                            for (var i = 0; i < self.action_manager.webclient.menu_data.children.length; i++) {
-                                if (self.action_manager.webclient.menu_data.children[i].id === session.active_id) {
-                                    menu_id = self.action_manager.webclient.menu_data.children[i].children[0].id;
-                                    break;
-                                }
-                            }
-                        }
-                        if ($shortcut_toggle.hasClass("oe_shortcut_remove")) {
-                            shortcuts_menu.trigger('remove', menu_id);
-                        }
-                        else {
-                            shortcuts_menu.trigger('add', {
-                                'user_id': session.uid,
-                                'menu_id': menu_id,
-                                'name': session.name
-                            });
-                        }
-                        $shortcut_toggle.toggleClass("oe_shortcut_remove");
-                    });
+            this.$shortcut_toggle.removeClass('hidden');
+            return true;
+        },
+        shortcut_toggle_state: function(menu_id) {
+            this.$shortcut_toggle.toggleClass('oe_shortcut_remove', this.shortcut_menu.has(menu_id || this.session.active_id));
+        },
+        shortcut_toggle_bind: function() {
+            var self = this;
+            this.$shortcut_toggle.unbind('click').click(function () {
+                self.shortcut_toggle_click();
+            });
+        },
+        shortcut_get_menu_id: function() {
+            var menu_id = session.active_id;
+            // In the case we come from a parent menu, no action is linked to the menu
+            // We must take the first child menu
+            if (this.action_manager.webclient.menu_data) {
+                for (var i = 0; i < this.action_manager.webclient.menu_data.children.length; i++) {
+                    if (this.action_manager.webclient.menu_data.children[i].id === session.active_id) {
+                        menu_id = this.action_manager.webclient.menu_data.children[i].children[0].id;
+                        break;
+                    }
+                }
+            }
+            return menu_id;
+        },
+        shortcut_toggle_click: function() {
+            var menu_id = this.shortcut_get_menu_id();
+            //Add / update
+            if (this.$shortcut_toggle.hasClass("oe_shortcut_remove")) {
+                this.shortcut_menu.trigger('remove', menu_id);
+            } else {
+                this.shortcut_menu.trigger('add', {
+                    'user_id': session.uid,
+                    'menu_id': menu_id,
+                    'name': session.name
                 });
             }
+            this.$shortcut_toggle.toggleClass("oe_shortcut_remove");
         }
     });
 
-
     ActionManager.include({
-        do_action: function () {
-            this.main_control_panel.$el.find('.oe_shortcut_toggle').addClass('hidden');
-            return this._super.apply(this, arguments);
+        do_action: function (action, options) {
+            var self = this;
+            return this._super(action, options).done(function(e) {
+                if (!self.inner_widget.shortcut_menu) {
+                    var toggle = self.main_control_panel.$el.find('.oe_shortcut_toggle');
+                    toggle.addClass('hidden');
+                    return;
+                }
+                if (!options.shortcut)
+                    self.inner_widget.shortcut_toggle_state();
+                else
+                    self.inner_widget.shortcut_toggle_state(e.menu_id);
+            });
         }
     });
 
