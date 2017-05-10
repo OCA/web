c83b5091f010c4b04a15a330088cd124d9735108
Denis Roussel <denis.roussel@acsone.eu>
2017-05-10 14:45:43 +0200

    [10.0][MIG] Migrates web_domain_field

--- web_domain_field/__manifest__.py
+++ web_domain_field/__manifest__.py
@@ -6,7 +6,7 @@
     'name': 'Web Domain Field',
     'summary': """
         Use computed field as domain""",
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
     'website': 'https://acsone.eu/',
