ce9b3db87f806f2e83243b75d1b79f9416c28172
Florent de Labarre <florent.mirieu@gmail.com>
2017-05-30 07:55:36 +0200

    [FIX] init

--- web_product_variante_selector/__init__.py
+++ web_product_variante_selector/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-import wizard
+from . import wizard
--- web_product_variante_selector/wizard/__init__.py
+++ web_product_variante_selector/wizard/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-import wizard_product_variante_selector
+from . import wizard_product_variante_selector
