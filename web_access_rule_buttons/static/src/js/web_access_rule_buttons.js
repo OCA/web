f1d0f1fd918ccca923745c02e273ab03d43d1d9a
George Daramouskas <george.daramouskas@gmail.com>
2017-06-03 14:24:32 +0300

    Refactoring javascript code

--- web_access_rule_buttons/static/src/js/web_access_rule_buttons.js
+++ web_access_rule_buttons/static/src/js/web_access_rule_buttons.js
@@ -3,38 +3,29 @@
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
  */
 odoo.define("web_access_rule_buttons.main", function(require) {
-    "use strict";
+	"use strict";
 
-    var core = require("web.core");
+	var FormView = require("web.FormView");
+	FormView.include({
 
-    var FormView = require("web.FormView");
-    var ListView = require("web.ListView");
-    FormView.include({
+		load_record : function() {
+			return this._super.apply(this, arguments).then($.proxy(this.show_hide_buttons, this));
+		},
 
-	load_record() {
-	    var self = this;
-	    return this._super.apply(this, arguments).then(function() {
-		self.show_hide_buttons()
-	    });
-	},
+		show_hide_buttons : function() {
+			var self = this;
+			this.dataset.call("check_access_rule_all", [ [ this.datarecord.id ], [ "write" ] ]).then(function(accesses) {
+				self.show_hide_edit_button(accesses.write);
+			});
+		},
 
-	show_hide_buttons() {
-	    var self = this;
-	    this.dataset.call("check_access_rule_all", [ [ this.datarecord.id ], [ "write" ] ]).then(function(accesses) {
-		console.log(accesses)
-		self.show_hide_edit_button(accesses.write);
-	    });
-	},
-
-	show_hide_edit_button(access) {
-	    if (this.$buttons) {
-		var button = this.$buttons.find(".o_form_button_edit");
-		if (button) {
-		    button.prop("disabled", !access);
+		show_hide_edit_button : function(access) {
+			if (this.$buttons) {
+				var button = this.$buttons.find(".o_form_button_edit");
+				if (button) {
+					button.prop("disabled", !access);
+				}
+			}
 		}
-	    }
-	}
-
-    });
-
+	});
 });
