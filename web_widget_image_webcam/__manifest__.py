ea20fd8c2dfe5846f36b2e1c463893671e6af167
smurf-U <kbprajapati@live.com>
2017-06-04 21:27:43 +0530

    [10.0][MIG] Migrate web_widget_image_webcam v10

--- web_widget_image_webcam/__manifest__.py
+++ web_widget_image_webcam/__manifest__.py
@@ -4,11 +4,12 @@
 {
     "name": "Web Widget - Image WebCam",
     "summary": "Allows to take image with WebCam",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "web",
     "website": "https://www.techreceptives.com",
     "author": "Tech Receptives, "
-              "Odoo Community Association (OCA)",
+              "Odoo Community Association (OCA), "
+              "Kaushal Prajapati",
     "license": "LGPL-3",
     "data": [
         "views/assets.xml",
