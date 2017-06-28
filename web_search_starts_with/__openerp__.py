22c001b564175784f89f7214107f75de5b23bcf4
Raph <raph@poir.es>
2017-06-28 17:21:12 +0200

    Add module web_search_starts_with
    adds a "starts with" operator in advanced search

---
+++ web_search_starts_with/__openerp__.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+# © 2015 Akretion Mourad EL HADJ MIMOUNE, David BEAL, Raphaël REVERDY
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'web search starts with',
+    'summary': 'Recherche avancée commence par ',
+    'version': '0.3',
+    'author': 'Akretion',
+    'maintainer': 'Akretion',
+    'depends': [
+        'web',
+    ],
+    'website': 'http://www.akretion.com/',
+    'data': [
+        'static/src/xml/search.xml',
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
+}
