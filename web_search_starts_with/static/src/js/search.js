b2a655e7144a10b6129860420cdabb3f76f2a69f
Raph <raph@poir.es>
2017-06-28 17:37:35 +0200

    Refine object

--- web_search_starts_with/static/src/js/search.js
+++ web_search_starts_with/static/src/js/search.js
@@ -3,10 +3,10 @@
 (function() {
 
 var instance = openerp;
-var _t =  instance.web._t;
+var _t = instance.web._t;
 var _lt = instance.web._lt;
 
-instance.web.search.ExtendedSearchProposition.Field.include({
+instance.web.search.ExtendedSearchProposition.Char.include({
     get_domain: function (field, operator) {
     	if (operator.value == '=ilike') 
     		return this.make_domain(field.name, operator.value, this.get_value() + '%');
