f4aa2f10055dea566d7ad62beb04a2dec1613c2e
Antonio Esposito <espo-tony@users.noreply.github.com>
2017-07-25 18:03:35 +0200

    [10.0][ADD] Module web_listview_invert_selection (#538)
    * [10.0][ADD] Module web_listview_invert_selection
    
    * [FIX] Multiple code reviews
    
    * [FIX] Changed single-quotes to double-quotes for improving codacy tests;

---
+++ web_listview_invert_selection/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'List Invert Selection',
+    'summary': 'Invert current selection of list of records',
+    'version': '10.0.1.0.0',
+    'category': 'Web',
+    'author': 'Onestein,Odoo Community Association (OCA)',
+    'website': 'http://www.onestein.eu',
+    'license': 'AGPL-3',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'templates/assets.xml'
+    ],
+    'qweb': ['static/src/xml/web_listview_invert_selection.xml'],
+    'installable': True,
+}
