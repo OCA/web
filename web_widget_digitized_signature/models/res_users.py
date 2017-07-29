d2a15456d3a82c2af90512baba4d48ff43485d36
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-07-29 13:30:43 +0200

    [IMP] web_widget_digitized_signature: Add tests

--- web_widget_digitized_signature/models/res_users.py
+++ web_widget_digitized_signature/models/res_users.py
@@ -4,13 +4,24 @@
 # Copyright 2017 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from openerp import api, fields, models
 
 
-class Users(models.Model):
+class ResUsers(models.Model):
     _name = 'res.users'
-    _inherit = 'res.users'
+    _inherit = ['res.users', 'mail.thread']
 
     signature_image = fields.Binary(
         string='Signature',
     )
+
+    @api.model
+    def create(self, vals):
+        res = super(ResUsers, self).create(vals)
+        res._track_signature(vals, 'signature')
+        return res
+
+    @api.multi
+    def write(self, vals):
+        self._track_signature(vals, 'signature')
+        return super(ResUsers, self).write(vals)
