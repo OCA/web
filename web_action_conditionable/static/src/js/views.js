be440c24c19358f27a5d5f6a1b7d3ed598c0c6e0
George Daramouskas <george.daramouskas@gmail.com>
2017-08-07 21:08:27 +0200

    [FIX] Further formatting changes in order to confirm to OCA's standards

--- web_action_conditionable/static/src/js/views.js
+++ web_action_conditionable/static/src/js/views.js
@@ -2,20 +2,20 @@ odoo.define("web_action_conditionable.view", function(require) {
     "use strict";
     var view = require("web.View");
     view.include({
-	is_action_enabled : function(action) {
-	    var attrs = this.fields_view.arch.attrs;
-	    if (action in attrs) {
-		try {
-		    return this._super(action);
-		} catch (error) {
-		    var expr = attrs[action];
-		    var expression = py.parse(py.tokenize(expr));
-		    var result = py.evaluate(expression).toJSON();
-		    return result;
-		}
-	    } else {
-		return true;
-	    }
-	}
+        is_action_enabled : function(action) {
+            var attrs = this.fields_view.arch.attrs;
+            if (action in attrs) {
+            try {
+                return this._super(action);
+             } catch (error) {
+                var expr = attrs[action];
+                var expression = py.parse(py.tokenize(expr));
+                var result = py.evaluate(expression).toJSON();
+                return result;
+                }
+            } else {
+                return true;
+            }
+        }
     });
 });
