0722eda28c3b63956d537feaf03aaba1b231b7d9
lreficent <lois.rilo@eficent.com>
2017-09-06 12:30:51 +0200

    [9.0][ADD] web_widget_bokeh_chart

---
+++ web_widget_bokeh_chart/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).#
+
+{
+    "name": "Web Widget Bokeh Chart",
+    "category": "Hidden",
+    "summary": "This widget allows to display charts using Bokeh library.",
+    "author": "Eficent, "
+              "Odoo Community Association (OCA)",
+    "version": "9.0.1.0.0",
+    "website": "https://github.com/OCA/web",
+    "depends": ["web"],
+    "data": [
+        "views/web_widget_bokeh_chart.xml",
+    ],
+    "external_dependencies": {
+        "python": ['bokeh'],
+    },
+    "auto_install": False,
+    "license": "AGPL-3",
+}
