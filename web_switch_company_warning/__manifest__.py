b1f108c1ec1461f2d242d7f49203ee6a14bbdd88
hparfr <raphael.reverdy@akretion.com>
2017-09-08 15:17:08 +0000

    Migrate switch_company_warning to 10
    Lint
    
    Switch to old name
    
    [UPD] Update web_switch_company_warning.pot
    
    [ADD] icon.png

--- web_switch_company_warning/__manifest__.py
+++ web_switch_company_warning/__manifest__.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-# © <2015> <Akretion, OCA>
+# Copyright 2017 Akretion <raphael.reverdy@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Multicompany - Switch Company Warning",
     "summary": "Show a warning if current company has been switched"
-    " in another tab or window.",
-    "version": "8.0.0.1.0",
+               " in another tab or window.",
+    "version": "10.0.0.1.0",
     "category": "web",
     "website": "http://akretion.com",
     "license": "AGPL-3",
-    "author": "Akretion / Odoo Community Association (OCA)",
+    "author": "Akretion, "
+              "Odoo Community Association (OCA)",
     "depends": [
         'web',
     ],
@@ -19,6 +20,6 @@
     "qweb": [
         "static/src/xml/switch_company_warning.xml",
     ],
-    'installable': False,
+    'installable': True,
     "application": False,
 }
