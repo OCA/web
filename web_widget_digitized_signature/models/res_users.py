d8c21594c06c948bf0f75db3f7063042af07a54f
eLBati <lorenzo.battistini@agilebg.com>
2017-09-11 17:15:29 +0200

    porting  web_widget_digitized_signature to 10.0

--- web_widget_digitized_signature/models/res_users.py
+++ web_widget_digitized_signature/models/res_users.py
@@ -4,7 +4,7 @@
 # Copyright 2017 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class ResUsers(models.Model):
