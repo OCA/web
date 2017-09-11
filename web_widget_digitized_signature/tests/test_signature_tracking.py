5d6181ddb047cab723b11d6cafa4edf57e89e9c6
eLBati <lorenzo.battistini@agilebg.com>
2017-09-11 17:15:29 +0200

    porting  web_widget_digitized_signature to 10.0

--- web_widget_digitized_signature/tests/test_signature_tracking.py
+++ web_widget_digitized_signature/tests/test_signature_tracking.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests import common
+from odoo.tests import common
 
 
 class TestSignatureTracking(common.SavepointCase):
