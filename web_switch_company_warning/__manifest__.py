048943447931d3126cf4b7fff24ca39789cafa2c
hparfr <raphael.reverdy@akretion.com>
2017-09-11 11:49:24 +0000

    Lint part 2

--- web_switch_company_warning/__manifest__.py
+++ web_switch_company_warning/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Multicompany - Switch Company Warning",
     "summary": "Show a warning if current company has been switched"
-        " in another tab or window.",
+               " in another tab or window.",
     "version": "10.0.0.1.0",
     "category": "web",
     "website": "http://akretion.com",
--- web_switch_company_warning/static/src/js/switch_company_warning_worker.js
+++ web_switch_company_warning/static/src/js/switch_company_warning_worker.js
@@ -9,11 +9,11 @@ addEventListener("connect", function(ee) {
     var port = ee.ports[0];
     con.push(port);
 
-    port.onmessage = function (e) { //addEventListener doesnt seams to work well 
+    port.onmessage = function (e) {
         var newCtx = e.data;
 
         if (lastCtx && newCtx !== lastCtx) {
-            con.map(function (eport) {
+            con.forEach(function (eport) {
                 eport.postMessage({type: "newCtx", "newCtx": newCtx, "lastCtx": lastCtx});
             });
         }
