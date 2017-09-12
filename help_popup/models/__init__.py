be49e5ca8033436b0c86f976f0b18feac25578c9
George Daramouskas <gdaramouskas@therp.nl>
2017-09-12 14:20:30 +0200

    [ADD] Adds CSS classes, changes demo file, renames model

--- help_popup/models/__init__.py
+++ help_popup/models/__init__.py
@@ -2,4 +2,4 @@
 # Copyright (C) 2015-TODAY Akretion (<http://www.akretion.com>).
 # © 2017 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import model
+from . import ir_actions_act_window
