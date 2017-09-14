dc27ed0470d8571ff605e5a699c430d71749d5c3
Stefan Rijnhart <stefan@opener.am>
2017-09-14 13:59:04 +0200

    [MIG][10.0] web_ir_actions_act_window_page

--- web_ir_actions_act_window_page/__manifest__.py
+++ web_ir_actions_act_window_page/__manifest__.py
@@ -1,31 +1,20 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2013-2015 Therp BV (<http://therp.nl>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# coding: utf-8
+# © 2013-2015 Therp BV (<http://therp.nl>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 {
     "name": "Window actions for client side paging",
     "summary": ("Allows a developer to trigger a pager to show the previous "
                 "or next next record in the form view"),
-    "version": "8.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
-    "category": "Dependency",
-    "depends": ['web'],
-    'data': ['view/qweb.xml'],
-    'installable': False,
+    "version": "10.0.1.0.0",
+    "category": "Technical",
+    "depends": [
+        'web',
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
+    'url': 'https://github.com/oca/web',
 }
