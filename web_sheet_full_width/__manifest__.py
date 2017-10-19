d9b72a2d61e850c03a36668c8292d744f855f56f
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-10-19 16:25:55 +0200

    [FIX] web_sheet_full_width: PEP8

--- web_sheet_full_width/__manifest__.py
+++ web_sheet_full_width/__manifest__.py
@@ -3,7 +3,8 @@
 {
     "name": "Show sheets with full width",
     "version": "11.0.1.0.1",
-    "author": "Therp BV, Sudokeys, GRAP, Métal Sartigan, Odoo Community Association (OCA)",
+    "author": "Therp BV, Sudokeys, GRAP, Métal Sartigan, "
+              "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "summary": "Use the whole available screen width when displaying sheets",
     "category": "Tools",
