15be76862efb3afb3856a54e94593141d288c955
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-10-19 18:13:00 +0200

    [FIX] web_widget_domain_v11: Bump version
    [skip ci]

--- web_widget_domain_v11/__manifest__.py
+++ web_widget_domain_v11/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Odoo 11.0 Domain Widget",
     "summary": "Updated domain widget",
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.0.1",
     "category": "Extra Tools",
     "website": "https://www.tecnativa.com/",
     "author": "Tecnativa, Odoo S.A., Odoo Community Association (OCA)",
