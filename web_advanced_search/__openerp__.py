1229788b26f138b12cbfd2f09174eeaf9268a1a9
Benjamin Willig <benjamin.willig@acsone.eu>
2017-10-20 12:41:59 +0200

    [FIX] web_advanced_search: 2 fixes
    * Ensure that domain field can be evaluated before creating x2x field
    * Fixed navigation error after using x2x advanced search

--- web_advanced_search/__openerp__.py
+++ web_advanced_search/__openerp__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Search x2x fields",
-    "version": "10.0.2.0.2",
+    "version": "10.0.2.0.3",
     "author": "Therp BV, "
               "Tecnativa, "
               "Odoo Community Association (OCA)",
