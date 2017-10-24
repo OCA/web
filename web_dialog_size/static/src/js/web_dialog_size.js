a5b11ca375b96f386e0732a9d7d22c7ba90af288
David Vidal <david.vidal@tecnativa.com>
2017-10-24 12:15:53 +0200

    [10.0][FIX] web_dialog_size: export modal (#746)
    * [10.0][FIX] web_dialog_size: export modal
    
    - PR #733 introduced draggable dialogs wich broke some of them

--- web_dialog_size/static/src/js/web_dialog_size.js
+++ web_dialog_size/static/src/js/web_dialog_size.js
@@ -14,7 +14,7 @@ Dialog.include({
 
         new Model('ir.config_parameter').query(['key', 'value']).
         filter([['key', '=', 'web_dialog_size.default_maximize']]).all().then(function(default_maximize) {
-            if (default_maximize.length && default_maximize[0]['value'] == 1) {
+            if (default_maximize.length && default_maximize[0].value === 1) {
                 self._extending();
             } else {
                 self._restore();
@@ -25,15 +25,17 @@ Dialog.include({
     open: function() {
         var res = this._super.apply(this, arguments);
         this.$modal.draggable({
-            handle: "div.modal-header",
+            handle: '.modal-header',
+            helper: false
         });
         return res;
     },
 
     close: function() {
         var draggable = this.$modal.draggable( "instance" );
-        if (draggable)
+        if (draggable) {
             this.$modal.draggable("destroy");
+        }
         var res = this._super.apply(this, arguments);
         return res;
     },
