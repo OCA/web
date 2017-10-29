08e18cdf62d485aa5842734d5dbb6c123a6ac259
Damien Bouvy <dbo@odoo.com>
2017-10-29 16:08:20 +0100

    [MIG] web_notify: Migration to 11.0
    - Use the 'session' class of the JS Framework (session no lounger bound
    to web client)
    - Test change: compare emitted & received messages based on content, not
    order. Using string comparison raises false positives.

--- web_notify/__manifest__.py
+++ web_notify/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,7 +5,7 @@
     'name': 'Web Notify',
     'summary': """
         Send notification messages to user""",
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'description': 'Web Notify',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
