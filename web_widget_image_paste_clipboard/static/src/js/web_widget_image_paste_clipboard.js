0ce13411a12aefe4666cc058c43368410a907d6d
Florent de Labarre <florent.mirieu@gmail.com>
2017-11-09 22:50:08 +0100

    FIX

--- web_widget_image_paste_clipboard/static/src/js/web_widget_image_paste_clipboard.js
+++ web_widget_image_paste_clipboard/static/src/js/web_widget_image_paste_clipboard.js
@@ -11,22 +11,24 @@ odoo.define('web_paste_image.FieldBinaryImage', function(require) {
 
             function handlePaste(event) {
                 var items = (event.clipboardData || event.originalEvent.clipboardData).items;
-                var blob = null;
-                for (var i = 0; i < items.length; i++) {
-                    if (items[i].type.indexOf("image") === 0) {
-                        blob = items[i].getAsFile();
-                        break;
-                    }
-                }
-                if (blob !== null) {
-                    var filereader = new FileReader();
-                    filereader.readAsDataURL(blob);
-                    filereader.onloadend = function(upload) {
-                        var data = upload.target.result;
-                        data = data.split(',')[1];
-                        self.on_file_uploaded(blob.size, 'paste_image', blob.type, data);
+                if items {
+                    var blob = null;
+                    for (var i = 0; i < items.length; i++) {
+                        if (items[i].type.indexOf("image") === 0) {
+                            blob = items[i].getAsFile();
+                            break;
+                        };
                     };
-                }
+                    if (blob !== null) {
+                        var filereader = new FileReader();
+                        filereader.readAsDataURL(blob);
+                        filereader.onloadend = function(upload) {
+                            var data = upload.target.result;
+                            data = data.split(',')[1];
+                            self.on_file_uploaded(blob.size, 'paste_image', blob.type, data);
+                        };
+                    };
+                };
             }
             this.$('.o_paste_image').on("paste", handlePaste);
         }
