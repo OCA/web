b36386e8874e2c1f0ca1d72a13dcedef006fb761
JBF91 <josemaria.bernet@guadaltech.es>
2017-11-14 13:42:26 +0100

    [MIG] web_hide_db_manager_link: Migration to 11.0

--- web_hide_db_manager_link/__manifest__.py
+++ web_hide_db_manager_link/__manifest__.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright Anubía, soluciones en la nube,SL (http://www.anubia.es)
 # Alejandro Santana <alejandrosantana@anubia.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Hide link to database manager in login screen',
-    'version': "10.0.1.0.0",
+    'version': "11.0.1.0.0",
     'category': 'Web',
     'license': 'AGPL-3',
     'author': 'Alejandro Santana, Odoo Community Association (OCA)',
-    'website': 'http://anubia.es',
+    'website': 'https://github.com/OCA/web',
     'summary': 'Hide link to database manager in login screen',
     'depends': ['web'],
     'data': ['views/webclient_templates.xml'],
