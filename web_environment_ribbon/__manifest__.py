1c89fafa97b380856c8fe572983f5805f7fc00d1
Xavier Jiménez <xavier.jimenez@qubiq.es>
2017-11-21 09:47:16 +0100

    [FIX][11.0] web_environment_ribbon (#806)
    * [FIX] web_environment_ribbon: Fix to show the ribbon to every non-admin user
    
    * [FIX] web_environment_ribbon: Version number

--- web_environment_ribbon/__manifest__.py
+++ web_environment_ribbon/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': "Web Environment Ribbon",
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     'category': 'Web',
     'author': 'Francesco OpenCode Apruzzese, '
               'Tecnativa, '
