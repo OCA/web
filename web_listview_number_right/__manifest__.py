302b8b469575c8741f24dd13c0af884c8a2a1c32
Achref Mhadhbi <mhadhbi.achref@gmail.com>
2017-11-29 18:02:25 +0100

    [10.0] Add module web_listview_number_right

---
+++ web_listview_number_right/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Mhadhbi Achraf(https://github.com/AMhadhbi).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'List View Number right',
+    'version': '10.0.1.0.0',
+    'category': 'web',
+    'summary': "Set alignment right of number type of fields",
+    'author': "Mhadhbi Achraf,Odoo Community Association (OCA)",
+    'website': 'https://github.com/AMhadhbi',
+    'license': 'AGPL-3',
+    'depends': ['web'],
+    'data': [
+        'views/asset.xml',
+    ],
+    'installable': True,
+}
