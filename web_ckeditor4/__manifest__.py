94bc55c982fef3d748b67acb5effc98ed442cc13
George Daramouskas <george.daramouskas@gmail.com>
2017-12-04 23:43:26 +0100

    [PORT] Port web_ckeditor4 to 10.0

--- web_ckeditor4/__manifest__.py
+++ web_ckeditor4/__manifest__.py
@@ -1,28 +1,9 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2013-2015 Therp BV (<http://therp.nl>)
-#    All Rights Reserved
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# -*- coding: utf-8 -*-
+# Copyright 2018 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'CKEditor 4.x widget',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'author': "Therp BV,Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/web',
     'summary': 'Provides a widget for editing HTML fields using CKEditor 4.x',
@@ -37,7 +18,6 @@
     'css': [
         'static/src/css/web_ckeditor4.css',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
-    'certificate': '',
 }
