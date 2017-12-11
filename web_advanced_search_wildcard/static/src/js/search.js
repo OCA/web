add3daf605cbec3749355e2187586d5d658fbc30
Alex Comba <alex.comba@agilebg.com>
2017-12-11 17:10:16 +0100

    [MIG] web_advanced_search_wildcard: Migration to 10.0

--- web_advanced_search_wildcard/static/src/js/search.js
+++ web_advanced_search_wildcard/static/src/js/search.js
@@ -1,6 +1,12 @@
-openerp.web_advanced_search_wildcard = function(instance){
-    var _lt = instance.web._lt;
-    instance.web.search.ExtendedSearchProposition.Char.prototype.operators.push(
+odoo.define('web_advanced_search_wildcard', function (require) {
+    "use strict";
+
+    var core = require('web.core');
+    var search_filters = require('web.search_filters');
+
+    var _lt = core._lt;
+
+    search_filters.ExtendedSearchProposition.Char.prototype.operators.push(
         {value: '=ilike', text: _lt("matches")}
     );
-};
+});
