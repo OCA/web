17ddbdb0a655385c667c6bb7fc1dec4de560709d
Alex Comba <alex.comba@agilebg.com>
2017-12-11 17:10:16 +0100

    [MIG] web_advanced_search_wildcard: Migration to 10.0

---
+++ web_advanced_search_wildcard/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 initOS GmbH & Co. KG (<http://www.initos.com>).
+# Copyright 2016 Therp BV <http://therp.nl>.
+# Copyright 2017 Alex Comba - Agile Business Group
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Wildcard in advanced search",
+    "summary": "Webmodule to add wildcard operators in advanced search field",
+    "version": "10.0.1.0.0",
+    "category": "web",
+    "license": "AGPL-3",
+    "author": "initOS GmbH & Co. KG, Therp BV, "
+              "Odoo Community Association (OCA)",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "web"
+    ],
+    "data": [
+        "views/template.xml",
+    ],
+}
