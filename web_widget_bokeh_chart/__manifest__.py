545d5aab9aa3d5c1be1344c239099d3e82b559fd
lreficent <lois.rilo@eficent.com>
2017-12-27 13:02:22 +0100

    [10.0][MIG] web_widget_bokeh_chart

--- web_widget_bokeh_chart/__manifest__.py
+++ web_widget_bokeh_chart/__manifest__.py
@@ -8,7 +8,7 @@
     "summary": "This widget allows to display charts using Bokeh library.",
     "author": "Eficent, "
               "Odoo Community Association (OCA)",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "website": "https://github.com/OCA/web",
     "depends": ["web"],
     "data": [
