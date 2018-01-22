23353294612b6a6918c83fd51df76727ef340f05
andreparames <github@andreparames.com>
2018-01-22 18:17:04 +0100

    [MIG] web_action_conditionable: Migration to 11.0

--- web_action_conditionable/__manifest__.py
+++ web_action_conditionable/__manifest__.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
-
 {
     "name": 'web_action_conditionable',
-    "version": "8.0.0.1.0",
+    "version": "11.0.1.0.0",
     "depends": [
         'base',
         'web',
     ],
     'data': ['views/view.xml'],
     "author": "Cristian Salamea,Odoo Community Association (OCA)",
-    'installable': False,
+    "website": "https://github.com/OCA/web",
+    "license": "AGPL-3",
+    'installable': True,
 }
