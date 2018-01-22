1a89ac7e2146b71fe14ca4187fb187f75d8247cb
andreparames <github@andreparames.com>
2018-01-22 18:17:04 +0100

    [MIG] web_action_conditionable: Migration to 11.0
    (cherry picked from commit 1cff9b12ba74cbd8039cc9e5020a62d304555cb4)

--- web_action_conditionable/static/src/js/views.js
+++ web_action_conditionable/static/src/js/views.js
@@ -1,22 +1,24 @@
-/*global openerp, _, $ */
+odoo.define('web.web_action_conditionable', function (require) {
+    "use strict";
 
-openerp.web_action_conditionable = function (instance) {
-  instance.web.View.include({
-    is_action_enabled: function(action) {
-      var attrs = this.fields_view.arch.attrs;
-      if (action in attrs) {
-	try {
-	  return this._super(action);
-	} catch(error) {
-	  var expr = attrs[action];
-	  var expression = py.parse(py.tokenize(expr));
-	  var cxt = this.dataset.get_context().__eval_context.__contexts[1];
-	  var result = py.evaluate(expression, cxt).toJSON();
-	  return result
-	}
-      } else {
-	return true;
-      }
-    }    
-  });
-}
+    var View = require('web.View');
+
+    View.include({
+        is_action_enabled: function(action) {
+            var attrs = this.fields_view.arch.attrs;
+            if (action in attrs) {
+                try {
+                      return this._super(action);
+                } catch(error) {
+                      var expr = attrs[action];
+                      var expression = py.parse(py.tokenize(expr));
+                      var cxt = this.dataset.get_context().__eval_context.__contexts[1];
+                      var result = py.evaluate(expression, cxt).toJSON();
+                      return result
+                }
+            } else {
+                return true;
+            }
+        }
+    });
+});
