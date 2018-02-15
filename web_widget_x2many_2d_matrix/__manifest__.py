d14334ff2d9435d4776bda579170b4ce16bbbc24
Artem Kostyuk <a.kostyuk@mobilunity.com>
2018-02-15 09:45:16 +0200

    [11][MIG] web_widget_x2many_2d_matrix WIP

--- web_widget_x2many_2d_matrix/__manifest__.py
+++ web_widget_x2many_2d_matrix/__manifest__.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Holger Brunn <hbrunn@therp.nl>
 # Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 {
     "name": "2D matrix for x2many fields",
-    "version": "10.0.1.0.1",
+    "version": "11.0.1.0.0",
     "author": "Therp BV, "
-              "Tecnativa,"
+              "Tecnativa, "
               "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
     "category": "Hidden/Dependency",
     "summary": "Show list fields as a matrix",
