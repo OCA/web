acadad7e61ca1c83e84d656b23e3206656f35a02
David <david.vidal@tecnativa.com>
2018-02-16 14:19:54 +0100

    [FIX] web_decimal_numpad: extend FieldMonetary

--- web_decimal_numpad_dot/static/src/js/numpad_dot.js
+++ web_decimal_numpad_dot/static/src/js/numpad_dot.js
@@ -40,4 +40,38 @@ odoo.define("web_decimal_numpad_dot.FieldFloat", function (require) {
             this.$input.prop("selectionStart", to).prop("selectionEnd", to);
         },
     });
+    basic_fields.FieldMonetary.include({
+        init: function () {
+            this.events = $.extend({}, this.events, {
+                "keydown": "numpad_dot_replace",
+            });
+            return this._super.apply(this, arguments);
+        },
+
+        l10n_decimal_point: function () {
+            return this.widget == "float_time"
+                ? ":" : translation._t.database.parameters.decimal_point;
+        },
+
+        numpad_dot_replace: function (event) {
+            // Only act on numpad dot key
+            if (event.keyCode != 110) {
+                return;
+            }
+            event.preventDefault();
+            var from = this.$input.prop("selectionStart"),
+                to = this.$input.prop("selectionEnd"),
+                cur_val = this.$input.val(),
+                point = this.l10n_decimal_point();
+            // Replace selected text by proper character
+            this.$input.val(
+                cur_val.substring(0, from) +
+                point +
+                cur_val.substring(to)
+            );
+            // Put user caret in place
+            to = from + point.length
+            this.$input.prop("selectionStart", to).prop("selectionEnd", to);
+        },
+    });
 });
