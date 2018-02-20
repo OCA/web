c5f86b1c232706146adaa46ea6d3cc65f6f80db6
Artem Kostyuk <a.kostyuk@mobilunity.com>
2018-02-20 18:16:37 +0200

    [MIG] web_tree_dynamic_colored_field: Migration to 11.0

--- web_tree_dynamic_colored_field/__init__.py
+++ web_tree_dynamic_colored_field/__init__.py
@@ -1,3 +1,2 @@
-# -*- coding: utf-8 -*-
-# © 2015 Camptocamp SA, Damien Crier
+# Copyright 2015-2018 Camptocamp SA, Damien Crier
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
