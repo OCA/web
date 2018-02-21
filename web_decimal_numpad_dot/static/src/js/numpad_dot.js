0ba4d05c952889502f2ab8c86d5a99cac95ad9e9
David <david.vidal@tecnativa.com>
2018-02-21 11:07:15 +0100

    fixup! [FIX] web_decimal_numpad: extend FieldMonetary

--- web_decimal_numpad_dot/static/src/js/numpad_dot.js
+++ web_decimal_numpad_dot/static/src/js/numpad_dot.js
@@ -6,7 +6,7 @@ odoo.define("web_decimal_numpad_dot.FieldFloat", function (require) {
     var basic_fields = require("web.basic_fields");
     var translation = require("web.translation");
 
-    basic_fields.FieldFloat.include({
+    var NumpadDotReplaceMixin = {
         init: function () {
             this.events = $.extend({}, this.events, {
                 "keydown": "numpad_dot_replace",
@@ -39,39 +39,12 @@ odoo.define("web_decimal_numpad_dot.FieldFloat", function (require) {
             to = from + point.length
             this.$input.prop("selectionStart", to).prop("selectionEnd", to);
         },
-    });
-    basic_fields.FieldMonetary.include({
-        init: function () {
-            this.events = $.extend({}, this.events, {
-                "keydown": "numpad_dot_replace",
-            });
-            return this._super.apply(this, arguments);
-        },
+    }
 
-        l10n_decimal_point: function () {
-            return this.widget == "float_time"
-                ? ":" : translation._t.database.parameters.decimal_point;
-        },
+    basic_fields.FieldFloat.include(NumpadDotReplaceMixin);
+    basic_fields.FieldMonetary.include(NumpadDotReplaceMixin);
 
-        numpad_dot_replace: function (event) {
-            // Only act on numpad dot key
-            if (event.keyCode != 110) {
-                return;
-            }
-            event.preventDefault();
-            var from = this.$input.prop("selectionStart"),
-                to = this.$input.prop("selectionEnd"),
-                cur_val = this.$input.val(),
-                point = this.l10n_decimal_point();
-            // Replace selected text by proper character
-            this.$input.val(
-                cur_val.substring(0, from) +
-                point +
-                cur_val.substring(to)
-            );
-            // Put user caret in place
-            to = from + point.length
-            this.$input.prop("selectionStart", to).prop("selectionEnd", to);
-        },
-    });
+    return {
+        NumpadDotReplaceMixin: NumpadDotReplaceMixin,
+    }
 });
