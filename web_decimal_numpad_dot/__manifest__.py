5a5cedce0161d00148ca890ce8bd1665fef7ff32
David Vidal <david.vidal@tecnativa.com>
2018-02-23 00:54:35 +0100

    [FIX] web_decimal_numpad: extend FieldMonetary (#867)

--- web_decimal_numpad_dot/__manifest__.py
+++ web_decimal_numpad_dot/__manifest__.py
@@ -3,12 +3,12 @@
 # Copyright 2015 Tecnativa - Pedro M. Baeza
 # Copyright 2015 Comunitea - Omar Castiñeira Saavedra
 # Copyright 2016 Oliver Dony
-# Copyright 2017 Tecnativa - David Vidal
+# Copyright 2017-18 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Web - Numpad Dot as decimal separator",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "license": "AGPL-3",
     "summary": "Allows using numpad dot to enter period decimal separator",
     "depends": [
