461a37ade62a3373a5536e7cabb3b12f3cef1403
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2018-03-07 09:34:33 +0100

    web_tree_dynamic_colored_field: correct options examples in readme

--- web_tree_dynamic_colored_field/__manifest__.py
+++ web_tree_dynamic_colored_field/__manifest__.py
@@ -4,7 +4,7 @@
     'name': 'Colorize field in tree views',
     'summary': 'Allows you to dynamically color fields on tree views',
     'category': 'Hidden/Dependency',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     'depends': ['web'],
     'author': "Camptocamp, Therp BV, Odoo Community Association (OCA)",
     'license': 'AGPL-3',
