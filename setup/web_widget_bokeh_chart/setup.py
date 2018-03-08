61c66a27c5436b978f9e6f6891dbd6836429e027
Lois Rilo <lois.rilo@eficent.com>
2018-03-08 12:36:34 +0100

    [10.0][FIX] web_widget_bokeh_chart: force the version of bokeh when using pip

--- setup/web_widget_bokeh_chart/setup.py
+++ setup/web_widget_bokeh_chart/setup.py
@@ -2,5 +2,11 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon=True,
+    odoo_addon={
+        'external_dependencies_override': {
+            'python': {
+                'Bokeh': 'bokeh==0.12.7',
+            },
+        },
+    },
 )
