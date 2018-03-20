528c9b4557e6553cf7eb4df6db6c1f0b01272289
Francesco Apruzzese <cescoap@gmail.com>
2018-03-20 17:08:45 +0100

    [MIG] web_search_with_and: Migration to 11.0

--- web_search_with_and/__manifest__.py
+++ web_search_with_and/__manifest__.py
@@ -1,20 +1,17 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Andrius Preimantas <andrius@versada.lt>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': "Use AND conditions on omnibar search",
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'author': 'Versada UAB, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'category': 'web',
-    'website': 'http://www.versada.lt',
+    'website': 'https://github.com/OCA/web',
     'depends': [
         'web',
     ],
     'data': [
         'data/data.xml',
     ],
-    'installable': True,
-    'application': False,
 }
