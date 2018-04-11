dcd920cd481d8f2586da488aec36bd94934a2af8
Aldo_Soares <soares_aldo@hotmail.com>
2018-04-11 16:50:08 -0300

    [MIG] web_listview_range_select: Migration to 11.0

--- web_listview_range_select/static/src/js/web_listview_range_select.js
+++ web_listview_range_select/static/src/js/web_listview_range_select.js
@@ -3,25 +3,21 @@
 
 odoo.define('web_listview_range_select', function (require) {
 "use strict";
-    var core = require('web.core'),
-        _t = core._t,
-        listview = require('web.ListView');
 
-    listview.List.include({
+    var ListRenderer = require('web.ListRenderer');
+
+    ListRenderer.include({
         _range_history: [],
+
         _get_range_selection: function() {
             var self = this;
             var result = {ids: [], records: []};
-            if (!this.options.selectable) {
-                return result;
-            }
-            var records = this.records;
 
             //Get start and end
             var start = null,
                 end = null;
 
-            this.$current.find('td.o_list_record_selector input').each(function (i, el) {
+            this.$el.find('td.o_list_record_selector input').each(function (i, el) {
                 var id = $(el).closest('tr').data('id');
                 var checked = self._range_history.indexOf(id) != -1;
                 if (checked && $(el).prop('checked')) {
@@ -33,11 +29,11 @@ odoo.define('web_listview_range_select', function (require) {
             });
 
             //Preserve already checked
-            this.$current.find('td.o_list_record_selector input:checked').closest('tr').each(function (i, el) {
+            this.$el.find('td.o_list_record_selector input:checked').closest('tr').each(function (i, el) {
                 if (i == start || i == end) return;
-                var record = records.get($(el).data('id'));
-                result.ids.push(record.get('id'));
-                result.records.push(record.attributes);
+                var record_id = $(el).data('id')
+                result.ids.push(record_id);
+                result.records.push(el.attributes);
             });
 
             var new_range = this.get_range_selection(start, end);
@@ -46,48 +42,48 @@ odoo.define('web_listview_range_select', function (require) {
 
             return result;
         },
+
         get_range_selection: function(start, end) {
-            var records = this.records;
             var result = {ids: [], records: []};
-            this.$current.find('td.o_list_record_selector input').closest('tr').each(function (i, el) {
-                var record = records.get($(el).data('id'));
+            this.$el.find('td.o_list_record_selector input').closest('tr').each(function (i, el) {
+                var record_id = $(el).data('id');
                 if (start != null && end != null && i >= start && i <= end) {
-                    result.ids.push(record.get('id'));
-                    result.records.push(record.attributes);
+                    result.ids.push(record_id);
+                    result.records.push(el.attributes);
                 } else if(start != null && end == null && start == i) {
-                    result.ids.push(record.get('id'));
-                    result.records.push(record.attributes);
+                    result.ids.push(record_id);
+                    result.records.push(el.attributes);
                 }
             });
             return result;
         },
+
         push_range_history: function(id) {
             this._range_history.push(id);
             if (this._range_history.length == 3)
                 this._range_history.shift();
         },
-        init: function() {
-            var self = this;
-            var res = this._super.apply(this, arguments);
-            this.$current.delegate('td.o_list_record_selector', 'click', function (e) {
-                //Update history
-                if ($(this).find('input').prop('checked'))
-                    self.push_range_history($(this).closest('tr').data('id'));
 
-                if (e.shiftKey) {
-                    //Get selection
-                    var selection = self._get_range_selection();
-                    self.$current.find('td.o_list_record_selector input').closest('tr').each(function () {
-                        //Check input visual
-                        var record = self.records.get($(this).data('id'));
-                        if (selection.ids.indexOf(record.get('id')) != -1)
-                            $(this).find('td.o_list_record_selector input').prop('checked', true);
-                    });
-                    //Check input internal
-                    $(self).trigger(
-                        'selected', [selection.ids, selection.records, true]);
-                }
-            });
+        _onSelectRecord: function(event) {
+            var res = this._super(event);
+            var self = this;
+            var el = $(event.currentTarget);
+            if (el.find('input').prop('checked'))
+                self.push_range_history(el.closest('tr').data('id'));
+
+            if (event.shiftKey) {
+                //Get selection
+                var selection = self._get_range_selection();
+                this.$el.find('td.o_list_record_selector input').closest('tr').each(function () {
+                    //Check input visual
+                    var record_id = $(this).data('id');
+                    if (selection.ids.indexOf(record_id) != -1)
+                        $(this).find('td.o_list_record_selector input').prop('checked', true);
+                });
+                //Check input internal
+                $(self).trigger(
+                    'selected', [selection.ids, selection.records, true]);
+            }
             return res;
         }
     });
