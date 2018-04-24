01739f73e99ec76bd699f678e83cf9235f8f5b43
Jean-Paul Ladage <j.ladage@zestsoftware.nl>
2018-04-24 09:05:48 +0200

    web_environment_ribbon: import from odoo instead of openerp.

--- web_environment_ribbon/models/web_environment_ribbon_backend.py
+++ web_environment_ribbon/models/web_environment_ribbon_backend.py
@@ -1,7 +1,7 @@
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, models
+from odoo import api, models
 
 
 class WebEnvironmentRibbonBackend(models.AbstractModel):
