8b95e9c9e6a63787c67e44adb67dda6d0269ca24
kseniya-tkopen <kseniya.bout@tkobr.com>
2018-05-09 14:10:05 +0000

    added web_widget_field_selector

---
+++ web_widget_field_selector/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
---
+++ web_widget_field_selector/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Field Selector Widget",
+    "summary": "Select fields",
+    "version": "10.0.1.0.1",
+    "category": "Extra Tools",
+    "website": "https://www.tko-br.com/",
+    "author": "TKO, Odoo S.A., Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "templates/assets.xml",
+    ],
+    "qweb": [
+        "static/src/xml/templates.xml",
+    ],
+}
