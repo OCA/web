c330da920d4826758cc84b9cad82707f688ecf9a
ernesto <ernesto.tejeda87@gmail.com>
2018-05-28 00:01:05 -0400

    [MIG] web_widget_image_download: Migration to 11.0

--- web_widget_image_download/__manifest__.py
+++ web_widget_image_download/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Flavio Corpa <flavio.corpa@tecnativa.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Web Widget - Image Download",
     "summary": "Allows to download any image from its widget",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "web",
     "website": "https://www.tecnativa.com",
     "author": "Tecnativa, Odoo Community Association (OCA), Kaushal Prajapati",
