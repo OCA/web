ef252546e4ef5cb072f88e104564c753e184ac81
Michele Zaccheddu <michelezak93@gmail.com>
2018-06-04 17:16:17 +0200

    [10.0] web_relational_enhancer: Update manifest
    - Add OCA as author
    - Set correct repo URL
    - Set proper version number
    - Add 'web' dependency

--- web_relational_enhancer/__manifest__.py
+++ web_relational_enhancer/__manifest__.py
@@ -2,8 +2,8 @@
 {
     'name': "Web Relational Field Enhancer",
     'summary': """
-        This module allow to add information to the 'display_name' on
-        many2one dropdowns and many2many tags.
+        This module allow to display additional fields to
+        the many2one widget dropdown and the many2many tags.
     """,
     'description': """
         Usage for O2m:
@@ -26,10 +26,10 @@
                        "display_fields": "name,state,description"
                    }'/>
     """,
-    'author': "Michele Zaccheddu - LevelPrime Srl",
-    'website': "http://www.levelprime.it",
+    'author': "Michele Zaccheddu - LevelPrime Srl, Odoo Community Association (OCA)",
+    'website': "https://github.com/OCA/web",
     'category': 'Web',
-    'version': '1.0',
-    'depends': ['base'],
+    'version': '10.0.1.0.0',
+    'depends': ['base', 'web'],
     'data': ['static/src/xml/assets.xml']
 }
