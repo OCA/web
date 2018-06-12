a33ba5007d1582f793fe68fd9b2925b1c0f9e409
Jairo Llopis <yajo.sk8@gmail.com>
2018-06-12 10:49:11 +0200

    fixup! [FIX] web_dialog_size: Fix usage for non-admins and reduce calls

--- web_dialog_size/models/ir_config_parameter.py
+++ web_dialog_size/models/ir_config_parameter.py
@@ -1,13 +1,14 @@
 # Copyright 2018 Tecnativa - Jairo Llopis
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.models import Model
+from odoo.models import api, Model
 from odoo.tools.safe_eval import const_eval
 
 
 class IrConfigParameter(Model):
     _inherit = "ir.config_parameter"
 
+    @api.model
     def get_web_dialog_size_config(self):
         return {
             key: const_eval(
