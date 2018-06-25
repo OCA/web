ae8d7e7ff46d826a07dcc5e147a8fe7290b87a38
QS5ELkMu <dev@redneboa.es>
2018-06-25 15:29:29 +0200

    [CLEAN] Comments

--- web_responsive/models/inherited_res_users.py
+++ web_responsive/models/inherited_res_users.py
@@ -8,8 +8,8 @@ class ResUsers(models.Model):
     _inherit = 'res.users'
 
     def __init__(self, pool, cr):
-        """ Override of __init__ to add access rights on notification_email_send
-            and alias fields. Access rights are disabled by default, but allowed
+        """ Override of __init__ to add access rights.
+        Access rights are disabled by default, but allowed
             on some specific fields defined in self.SELF_{READ/WRITE}ABLE_FIELDS.
         """
         init_res = super(ResUsers, self).__init__(pool, cr)
