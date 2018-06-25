aa0d93206ab397d9a27a8f0c563327b8d89a39d9
QS5ELkMu <dev@redneboa.es>
2018-06-25 17:25:13 +0200

    [IMP] Code

--- web_responsive/models/inherited_res_users.py
+++ web_responsive/models/inherited_res_users.py
@@ -7,21 +7,21 @@ from odoo import models, fields
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
+    chatter_position = fields.Selection([
+        ('normal', 'Normal'),
+        ('sided', 'Sided'),
+    ], string="Chatter Position", default='normal')
+
+
     def __init__(self, pool, cr):
         """ Override of __init__ to add access rights.
-        Access rights are disabled by default, but allowed
-            on some specific fields defined in self.SELF_{READ/WRITE}ABLE_FIELDS.
+        Access rights are disabled by default, but allowed on some specific
+        fields defined in self.SELF_{READ/WRITE}ABLE_FIELDS.
         """
-        init_res = super(ResUsers, self).__init__(pool, cr)
+        super(ResUsers, self).__init__(pool, cr)
         # duplicate list to avoid modifying the original reference
         type(self).SELF_WRITEABLE_FIELDS = list(self.SELF_WRITEABLE_FIELDS)
         type(self).SELF_WRITEABLE_FIELDS.extend(['chatter_position'])
         # duplicate list to avoid modifying the original reference
         type(self).SELF_READABLE_FIELDS = list(self.SELF_READABLE_FIELDS)
         type(self).SELF_READABLE_FIELDS.extend(['chatter_position'])
-        return init_res
-
-    chatter_position = fields.Selection([
-        ('normal', 'Normal'),
-        ('sided', 'Sided'),
-    ], string="Chatter Position", default='normal')
