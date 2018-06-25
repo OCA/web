de23e674448f18478c5c8a55340fc25649e4f1b3
QS5ELkMu <dev@redneboa.es>
2018-06-25 15:26:20 +0200

    [FIX] Read/Write permissions

--- web_responsive/models/inherited_res_users.py
+++ web_responsive/models/inherited_res_users.py
@@ -7,6 +7,20 @@ from odoo import models, fields
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
+    def __init__(self, pool, cr):
+        """ Override of __init__ to add access rights on notification_email_send
+            and alias fields. Access rights are disabled by default, but allowed
+            on some specific fields defined in self.SELF_{READ/WRITE}ABLE_FIELDS.
+        """
+        init_res = super(ResUsers, self).__init__(pool, cr)
+        # duplicate list to avoid modifying the original reference
+        type(self).SELF_WRITEABLE_FIELDS = list(self.SELF_WRITEABLE_FIELDS)
+        type(self).SELF_WRITEABLE_FIELDS.extend(['chatter_position'])
+        # duplicate list to avoid modifying the original reference
+        type(self).SELF_READABLE_FIELDS = list(self.SELF_READABLE_FIELDS)
+        type(self).SELF_READABLE_FIELDS.extend(['chatter_position'])
+        return init_res
+
     chatter_position = fields.Selection([
         ('normal', 'Normal'),
         ('sided', 'Sided'),
