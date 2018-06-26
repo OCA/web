8928f5a3cb2d800b9bbca1fe51d123c09de81f20
Nedas Zilinskas <nedas.zilinskas@gmail.com>
2018-06-26 18:25:01 +0300

    [ADD] [8.0] - web_no_crawler (#963)

---
+++ web_no_crawler/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018 Ventor, Xpansa Group (<https://ventor.tech/>)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import controllers
