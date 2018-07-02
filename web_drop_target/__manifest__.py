99b2f8eee9eef223ec30db327eec98140e551902
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-07-02 12:49:01 +0200

    [MIG] web_drop_target : Migration to 11.0

--- web_drop_target/__manifest__.py
+++ web_drop_target/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Drop target support",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
---
+++ web_drop_target/readme/CONTRIBUTORS.rst
@@ -0,0 +1,2 @@
+* Holger Brunn <hbrunn@therp.nl>
+* Akim Juillerat <akim.juillerat@camptocamp.com>
