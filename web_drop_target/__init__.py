9c91d8d3f2d81c42b122ce20a08370526946b0ca
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-07-02 12:49:01 +0200

    [MIG] web_drop_target : Migration to 11.0

--- web_drop_target/__init__.py
+++ web_drop_target/__init__.py
@@ -1,3 +1,2 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
---
+++ web_drop_target/readme/DEVELOP.rst
@@ -0,0 +1,4 @@
+Libraries
+---------
+
+* `base64js <https://raw.githubusercontent.com/beatgammit/base64-js>`_.
