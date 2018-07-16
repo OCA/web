257a24a40e0d673fa1dcd90065a1c6aed1240f4d
David <david.vidal@tecnativa.com>
2018-07-16 12:07:52 +0200

    [MIG] web_disable_group_export: Migration to 11.0

--- web_disable_export_group/__init__.py
+++ web_disable_export_group/__init__.py
@@ -1,3 +1,1 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Onestein (<http://www.onestein.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import models
--- web_disable_export_group/__manifest__.py
+++ web_disable_export_group/__manifest__.py
@@ -1,15 +1,18 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Onestein (<http://www.onestein.eu>)
+# Copyright 2018 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 {
     'name': 'Web Disable Export Group',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'Onestein',
-    'website': 'http://www.onestein.eu',
+    'author': 'Onestein, '
+              'Tecnativa, '
+              'Odoo Community Association (OCA)',
+    'website': 'http://www.github.com/OCA/web',
     'category': 'Web',
-    'depends': ['web'],
+    'depends': [
+        'web',
+    ],
     'data': [
         'security/groups.xml',
         'templates/assets.xml',
