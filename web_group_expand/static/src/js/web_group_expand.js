ae9dd533e87a790c5f0d825052635ac00cd4772b
QS5ELkMu <dev@redneboa.es>
2018-07-17 18:59:14 +0200

    [11.0][FIX] web_group_expand: Views without searchview

--- web_group_expand/static/src/js/web_group_expand.js
+++ web_group_expand/static/src/js/web_group_expand.js
@@ -28,22 +28,23 @@ odoo.define('web_group_expand.web_group_expand', function (require) {
     ViewManager.include({
         init: function(parent, dataset, views, flags, options) {
             this._super.apply(this, arguments);
-            this._show_group_expand_collapse_buttons = false;
+            this._has_groupby_search = false;
         },
         _process_search_data: function () {
             var res = this._super.apply(this, arguments);
-            if (this.searchview && this.searchview.groupby_expand_menu) {
-                this._show_group_expand_collapse_buttons = (res.groupBy.length > 0);
-                if (this.active_view && this.active_view.type === 'list') {
-                    this.searchview.groupby_expand_menu.do_toggle_visibility(this._show_group_expand_collapse_buttons);
-                }
-            }
+            this._has_groupby_search = (res.groupBy.length > 0);
+            this._set_groupby_expand_menu_visible(this._has_groupby_search && this.active_view && this.active_view.type === 'list');
             return res;
         },
         switch_mode: function(view_type, view_options) {
-            return this._super.apply(this, arguments).then(function(){
-                this.searchview.groupby_expand_menu.do_toggle_visibility(view_type === 'list' && this.searchview && this.searchview.groupby_expand_menu && this._show_group_expand_collapse_buttons);
+            return this._super.apply(this, arguments).then(function() {
+                this._set_groupby_expand_menu_visible(this._has_groupby_search && view_type === 'list');
             }.bind(this));
+        },
+        _set_groupby_expand_menu_visible: function(is_visible) {
+            if (this.searchview && this.searchview.groupby_expand_menu) {
+                this.searchview.groupby_expand_menu.do_toggle_visibility(is_visible);
+            }
         },
     });
 });
