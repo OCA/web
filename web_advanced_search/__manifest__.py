e1366747425c59a6f5efc2a7a99e06323ad54901
Jairo Llopis <yajo.sk8@gmail.com>
2018-07-20 10:35:14 +0100

    [MIG][REF] web_advanced_search: Rename, refactor, migrate
    * Complete migration to v11
    * Refactor to use the new v11 decoupled widgets system
    * Advanced search is now a high-level feature from the filters menu; it simplifies code a lot, and the UX is even better
    * Split README system
    * Add fun to ROADMAP
    * Addon is renamed to web_advanced_search, since it enhaces the searching experience for all kind of fields now

--- web_advanced_search/__manifest__.py
+++ web_advanced_search/__manifest__.py
@@ -1,24 +1,27 @@
 # Copyright 2015 Therp BV <http://therp.nl>
 # Copyright 2017 Tecnativa - Vicent Cubells
+# Copyright 2018 Tecnativa - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Search x2x fields",
+    "name": "Advanced search",
     "version": "11.0.1.0.0",
     "author": "Therp BV, "
               "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Usability",
-    "summary": "Use a search widget in advanced search for x2x fields",
-    "depends": [],
+    "summary": "Easier and more powerful searching tools",
+    "website": "https://github.com/OCA/web",
+    "depends": [
+        'web',
+    ],
     "data": [
         'views/templates.xml',
     ],
     "qweb": [
-        'static/src/xml/web_advanced_search_x2x.xml',
+        'static/src/xml/web_advanced_search.xml',
     ],
-    "auto_install": False,
     "installable": True,
     "application": False,
 }
