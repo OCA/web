752687d1466ee5cf446b466f6b881c0adeb6a839
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-08-07 18:18:37 +0200

    web_widget_x2many_matrix: fix issue with limit of records

--- web_widget_x2many_2d_matrix/__manifest__.py
+++ web_widget_x2many_2d_matrix/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "2D matrix for x2many fields",
-    "version": "11.0.1.1.0",
+    "version": "11.0.1.1.1",
     "author": "Therp BV, "
               "Tecnativa, "
               "Camptocamp, "
