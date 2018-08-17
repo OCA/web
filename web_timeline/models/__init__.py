c9f14e3217480044117d530a4514124c8fe51d81
tarteo <d.sluijk@onestein.nl>
2018-08-17 11:53:44 +0200

    [ADD] web_timeline: New dependency_arrow attribute
    Update README.rst
    
    [FIX] Remove console.log
    
    [ADD] Make timeline.fit optional
    
    [FIX] Use stringified points
    
    [IMP] Reversed the arrow head and fixed lint issues
    
    [IMP] Use options parameter for line color and width
    
    [FIX] Version number
    
    [IMP] Minor improvements

--- web_timeline/models/__init__.py
+++ web_timeline/models/__init__.py
@@ -1,4 +1,4 @@
-# © 2016 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import ir_view
