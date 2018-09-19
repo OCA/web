906062481d137e8e0c6e922d89aa0846fe66972e
Enric Tobella <etobella@creublanca.es>
2018-09-19 14:56:15 +0200

    [ADD] web_fontawesome5

---
+++ web_fontawesome5/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module upgrades the fontawesome font to 5.3.1.
+
+It allows to use new icons.
---
+++ web_fontawesome5/static/src/js/list_renderer.js
@@ -0,0 +1,34 @@
+odoo.define('web_fontawesome5.ListRenderer', function (require) {
+"use strict";
+
+var ListRenderer = require('web.ListRenderer');
+
+
+ListRenderer.include({
+
+    _renderButton: function (record, node) {
+        var $button = this._super.apply(this, arguments);
+
+        if (node.attrs.brand_icon) {
+            $('<div>')
+                .addClass('fab fa-fw o_button_icon')
+                .addClass(node.attrs.brand_icon)
+                .prependTo($button);
+        }
+        if (node.attrs.solid_icon) {
+            $('<div>')
+                .addClass('fas fa-fw o_button_icon')
+                .addClass(node.attrs.solid_icon)
+                .prependTo($button);
+        }
+        if (node.attrs.regular_icon) {
+            $('<div>')
+                .addClass('far fa-fw o_button_icon')
+                .addClass(node.attrs.regular_icon)
+                .prependTo($button);
+        }
+        return $button;
+    }
+});
+
+});
