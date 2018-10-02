f033a27c9e17a8ed873717e13e920b2e01b05add
Wolfgang Pichler <wpichler@callino.at>
2018-10-02 12:19:06 +0200

    [ADD] Added web_gesture module for swipe support on mobile devices

---
+++ web_gesture/static/src/js/gestures.js
@@ -0,0 +1,32 @@
+/* Copyright 2013, 2018 Fisher Yu, Pichler Wolfgang
+ * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
+
+odoo.define('web_gestures', function (require) {
+    "use strict";
+
+    var core = require('web.core');
+    var QWeb = core.qweb;
+    var _t = core._t;
+    var Pager = require('web.Pager');
+
+    Pager.include({
+        start: function(){
+            var res = this._super.apply(this);
+            if ((!(/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase())))) {
+                // Do not initialize on desktop browsers
+                return;
+            }
+            var self = this;
+            var content = $(".o_content");
+            this.hammertime = new Hammer(content.get(0));
+            this.hammertime.on('swipeleft', function(ev) {
+                self.next();
+            });
+            this.hammertime.on('swiperight', function(ev) {
+                self.previous();
+            });
+            return res;
+        }
+    });
+
+});
