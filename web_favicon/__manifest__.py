bca9c9c7500c8a0cad6f1914361e9675f6d1f18a
tarteo <d.sluijk@onestein.nl>
2018-10-02 15:32:25 +0200

    [MIG] web_favicon: Migration to 12.0

--- web_favicon/__manifest__.py
+++ web_favicon/__manifest__.py
@@ -1,17 +1,17 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Therp BV <http://therp.nl>
 # Copyright 2016 Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Custom shortcut icon",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Therp BV, "
               "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Website",
     "summary": "Allows to set a custom shortcut icon (aka favicon)",
+    "website": "https://github.com/OCA/web",
     "depends": [
         "web",
     ],
