2cc3ba4bff97940d64d69b6efb7fb2a94ac78942
Cristian Salamea <ovnicraft@gmail.com>
2018-10-07 10:25:04 -0500

    [10.0] migrated to new API module web_action_conditionable (#547)
    * [IMP] migrated to new API
    
    * Update view.xml

--- web_action_conditionable/__manifest__.py
+++ web_action_conditionable/__manifest__.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 {
     "name": 'web_action_conditionable',
-    "version": "10.0.1.0.0",
+    "version": "10.0.0.1.0",
     "depends": [
-        'base',
-        'web',
+        'web'
     ],
     'data': ['views/view.xml'],
     "author": "Cristian Salamea,Odoo Community Association (OCA)",
