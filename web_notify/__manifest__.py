ae8e4ec59deea6063bd912d6be0f62e4e5a8bc3b
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2018-10-08 10:04:36 +0200

    Prevent to send web notifications to other users
    Only the admin user (sudo) is allowed to send notifications to other
    users. The normal users can only send notifications to themselves.
    
    This is to prevent attackers to craft malicious notifications and send
    them to other users using RPC.
    
    Correction based on the idea of @hbrunn

--- web_notify/__manifest__.py
+++ web_notify/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Web Notify',
     'summary': """
         Send notification messages to user""",
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.1.0',
     'description': 'Web Notify',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
