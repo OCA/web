265744c2e11e06a17221b28c39b6cccb0e44d8a0
Gusti Tammam <gustitammam@gmail.com>
2018-10-19 08:00:16 +0700

    [MIG] web_m2x_options: Migration to 12.0

--- web_m2x_options/__manifest__.py
+++ web_m2x_options/__manifest__.py
@@ -1,7 +1,11 @@
+# Copyright 2015 0k.io
+# Copyright 2016 ACSONE SA/NV
+# Copyright 2017 Tecnativa
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": 'web_m2x_options',
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     'category': 'Web',
     "author": "ACSONE SA/NV, "
               "0k.io, "
@@ -10,7 +14,6 @@
     'website': 'https://github.com/OCA/web',
     'license': 'AGPL-3',
     "depends": [
-        'base',
         'web',
     ],
     'data': [
