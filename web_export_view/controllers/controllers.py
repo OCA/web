1f1bd0239d3a1b672171748213937718d3afd129
Jérémie Payet <jeremie.payet@hotmail.fr>
2018-11-14 10:42:59 -1000

    Update controllers.py
    Add filters in the 1st line of the generated file if they exist

--- web_export_view/controllers/controllers.py
+++ web_export_view/controllers/controllers.py
@@ -1,12 +1,34 @@
+# Copyright 2018 Jérémie Payet Github: jpcweb
 # Copyright 2016 Henry Zhou (http://www.maxodoo.com)
 # Copyright 2016 Rodney (http://clearcorp.cr/)
 # Copyright 2012 Agile Business Group
 # Copyright 2012 Therp BV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import json
 import odoo.http as http
+import io
+import babel.messages.pofile
+import base64
+import datetime
+import functools
+import glob
+import hashlib
+import imghdr
+import io
+import itertools
+import jinja2
+import json
+import logging
+import operator
+import os
+import re
+import sys
+import tempfile
+import time
+import zlib
 from odoo.http import request
+from odoo.tools import crop_image, topological_sort, html_escape, pycompat
+from odoo.tools.misc import str2bool, xlwt, file_open
 from odoo.addons.web.controllers.main import ExcelExport
 
 
@@ -16,15 +38,72 @@ class ExcelExportView(ExcelExport):
             raise AttributeError()
         return super(ExcelExportView, self).__getattribute__(name)
 
+    def from_data(self,fields, rows, searchs = []):
+        if len(rows) > 65535:
+            raise UserError(_('There are too many rows (%s rows, limit: 65535) to export as Excel 97-2003 (.xls) format. Consider splitting the export.') % len(rows))
+
+        workbook = xlwt.Workbook()
+        worksheet = workbook.add_sheet('Sheet 1')
+
+        pos = 0
+
+        if len(searchs) > 0 :
+            worksheet.write(pos, 0, "Les filtres: ")
+            for i, search in enumerate(searchs):
+                worksheet.write(pos, i+1, search)
+
+            pos=pos+1
+
+        for i, fieldname in enumerate(fields):
+            worksheet.write(pos, i, fieldname)
+            worksheet.col(i).width = 8000 # around 220 pixels
+
+        base_style = xlwt.easyxf('align: wrap yes')
+        date_style = xlwt.easyxf('align: wrap yes', num_format_str='YYYY-MM-DD')
+        datetime_style = xlwt.easyxf('align: wrap yes', num_format_str='YYYY-MM-DD HH:mm:SS')
+
+        for row_index, row in enumerate(rows):
+            for cell_index, cell_value in enumerate(row):
+                cell_style = base_style
+
+                if isinstance(cell_value, bytes) and not isinstance(cell_value, pycompat.string_types):
+                    # because xls uses raw export, we can get a bytes object
+                    # here. xlwt does not support bytes values in Python 3 ->
+                    # assume this is base64 and decode to a string, if this
+                    # fails note that you can't export
+                    try:
+                        cell_value = pycompat.to_text(cell_value)
+                    except UnicodeDecodeError:
+                        raise UserError(_("Binary fields can not be exported to Excel unless their content is base64-encoded. That does not seem to be the case for %s.") % fields[cell_index])
+
+                if isinstance(cell_value, pycompat.string_types):
+                    cell_value = re.sub("\r", " ", pycompat.to_text(cell_value))
+                    # Excel supports a maximum of 32767 characters in each cell:
+                    cell_value = cell_value[:32767]
+                elif isinstance(cell_value, datetime.datetime):
+                    cell_style = datetime_style
+                elif isinstance(cell_value, datetime.date):
+                    cell_style = date_style
+                worksheet.write(row_index + pos + 1, cell_index, cell_value, cell_style)
+
+        fp = io.BytesIO()
+        workbook.save(fp)
+        fp.seek(0)
+        data = fp.read()
+        fp.close()
+        return data
+
+
     @http.route('/web/export/xls_view', type='http', auth='user')
     def export_xls_view(self, data, token):
         data = json.loads(data)
         model = data.get('model', [])
+        columns_searchs = data.get('searchs', [])
         columns_headers = data.get('headers', [])
         rows = data.get('rows', [])
 
         return request.make_response(
-            self.from_data(columns_headers, rows),
+            self.from_data(columns_headers, rows, columns_searchs),
             headers=[
                 ('Content-Disposition', 'attachment; filename="%s"'
                  % self.filename(model)),
