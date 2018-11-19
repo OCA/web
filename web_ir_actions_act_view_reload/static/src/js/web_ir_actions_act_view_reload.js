633e0c8a2d3e8e2a7d9cda18b1b2ee5c98481450
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-11-19 18:30:15 +0200

    [MIG] web_ir_actions_act_view_reload: Migration to 12.0

--- web_ir_actions_act_view_reload/static/src/js/web_ir_actions_act_view_reload.js
+++ web_ir_actions_act_view_reload/static/src/js/web_ir_actions_act_view_reload.js
@@ -1,16 +1,38 @@
 // Copyright 2017 - 2018 Modoolar <info@modoolar.com>
+// Copyright 2018 Modoolar <info@modoolar.com>
 // License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 odoo.define('web_ir_actions_act_view_reload.ir_actions_act_view_reload', function (require) {
-"use strict";
+    "use strict";
 
     var ActionManager = require('web.ActionManager');
 
     ActionManager.include({
-        ir_actions_act_view_reload: function (action, options) {
-            if (this.inner_widget && this.inner_widget.active_view && this.inner_widget.active_view.controller)
-                this.inner_widget.active_view.controller.reload();
+
+        /**
+         * Intersept action handling to detect extra action type
+         * @override
+         */
+        _handleAction: function (action, options) {
+            if (action.type === 'ir.actions.act_view_reload') {
+                return this._executeReloadAction(action, options);
+            }
+
+            return this._super.apply(this, arguments);
+        },
+
+        /**
+         * Handle 'ir.actions.act_view_reload' action
+         * @returns {$.Promise}
+         */
+        _executeReloadAction: function () {
+            var controller = this.getCurrentController();
+            if (controller && controller.widget) {
+                controller.widget.reload();
+            }
+
             return $.when();
-        }
+        },
+
     });
 
 });
