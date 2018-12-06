38017035977c921cab509a4408b95205e73196ac
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-12-06 14:25:47 +0200

    [FIX] web_widget_x2many_2d_matrix: _renderBodyCell + _renderAggregateColCells

--- web_widget_x2many_2d_matrix/__manifest__.py
+++ web_widget_x2many_2d_matrix/__manifest__.py
@@ -4,11 +4,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': '2D matrix for x2many fields',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'author': (
         'Therp BV, '
         'Tecnativa, '
         'Camptocamp, '
+        'Brainbean Apps, '
         'Odoo Community Association (OCA)'
     ),
     'website': 'https://github.com/OCA/web',
