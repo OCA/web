548b14773deff1d308fb778f9ae67bd80ec3a4d4
Mark Robinson <me@markdrobinson.net>
2018-12-20 15:49:40 -0800

    Add web_sheet_widescreen.

---
+++ web_sheet_widescreen/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Web - Sheets for Widescreen",
+    "version": "10.0.1.0.1",
+    "author": "Mark Robinson",
+    "license": "AGPL-3",
+    "summary": "Clone of web_sheet_full_width, but instead of full screen sheets, "
+               "adds an additional media query to make sheets a bit wider on widescreen displays.",
+    "category": "Tools",
+    "depends": [
+        'web',
+    ],
+    "data": [
+        "view/qweb.xml",
+    ],
+    "installable": True,
+}
