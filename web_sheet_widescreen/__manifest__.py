d81ea71f26c7a325fc8aca55a12ffd5088ae0afd
Mark Robinson <me@markdrobinson.net>
2018-12-21 11:43:15 -0800

    Update author/website

--- web_sheet_widescreen/__manifest__.py
+++ web_sheet_widescreen/__manifest__.py
@@ -3,7 +3,8 @@
 {
     "name": "Web - Sheets for Widescreen",
     "version": "10.0.1.0.1",
-    "author": "Mark Robinson",
+    "author": "Mark Robinson, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
     "summary": "Clone of web_sheet_full_width, but instead of full screen sheets, "
                "adds an additional media query to make sheets a bit wider on widescreen displays.",
