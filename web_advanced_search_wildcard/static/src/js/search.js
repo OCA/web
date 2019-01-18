5cbc05a44932db319f0ebcdd90dfa362c550e8f5
Alexis de Lattre <alexis@via.ecp.fr>
2019-01-18 16:45:12 +0100

    [10.0] web_advanced_search_wildcard: adds 'starts with' and 'ends with' (#1142)
    * We keep the historic feature *matches* provided by
      web_advanced_search_wildcard, but we add *starts with* and *ends with*
      with are super-easy and intuitive to use and cover the majority of use cases.
    
    * FR translation

--- web_advanced_search_wildcard/static/src/js/search.js
+++ web_advanced_search_wildcard/static/src/js/search.js
@@ -7,6 +7,20 @@ odoo.define('web_advanced_search_wildcard', function (require) {
     var _lt = core._lt;
 
     search_filters.ExtendedSearchProposition.Char.prototype.operators.push(
+        {value: "startswith", text: _lt("starts with")},
+        {value: "endswith", text: _lt("ends with")},
         {value: '=ilike', text: _lt("matches")}
     );
+
+    search_filters.ExtendedSearchProposition.Char.include({
+        get_domain: function (field, operator) {
+            switch (operator.value) {
+            case '∃': return [[field.name, '!=', false]];
+            case '∄': return [[field.name, '=', false]];
+            case 'startswith': return [[field.name, '=ilike', this.get_value() + '%']];
+            case 'endswith': return [[field.name, '=ilike', '%' + this.get_value()]];
+            default: return [[field.name, operator.value, this.get_value()]];
+            }
+        },
+    });
 });
