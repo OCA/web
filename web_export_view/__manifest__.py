4377d62d839d518ac33ae95f62537d3868b041b9
David <david.vidal@tecnativa.com>
2019-01-21 12:25:19 +0100

    [11.0] web_export_view: add security group

--- web_export_view/__manifest__.py
+++ web_export_view/__manifest__.py
@@ -1,13 +1,14 @@
-# Copyright 2016 Henry Zhou (http://www.maxodoo.com)
-# Copyright 2016 Rodney (http://clearcorp.cr/)
 # Copyright 2012 Agile Business Group
 # Copyright 2012 Domsense srl (<http://www.domsense.com>)
 # Copyright 2012 Therp BV
+# Copyright 2016 Henry Zhou (http://www.maxodoo.com)
+# Copyright 2016 Rodney (http://clearcorp.cr/)
+# Copyright 2019 Tecnativa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Web Export Current View',
-    'version': '11.0.1.0.0',
+    'version': '11.0.2.0.0',
     'category': 'Web',
     'author': 'Henry Zhou, Agile Business Group, \
             Odoo Community Association (OCA)',
@@ -17,11 +18,13 @@
         'web',
     ],
     "data": [
+        'security/groups.xml',
         'views/web_export_view_view.xml',
     ],
     'qweb': [
         "static/src/xml/web_export_view_template.xml",
     ],
+
     'installable': True,
     'auto_install': False,
 }
