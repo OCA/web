e265779409b6efa07613bbf40a5e80b5a24fa191
tarteo <d.sluijk@onestein.nl>
2019-03-01 11:56:28 +0100

    [FIX] web_edit_user_filter: Custom filters

--- web_edit_user_filter/__manifest__.py
+++ web_edit_user_filter/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Edit User Filters',
     'category': 'Extra Tools',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'development_status': 'Production/Stable',
     'author': 'Onestein,Odoo Community Association (OCA)',
     'license': 'AGPL-3',
