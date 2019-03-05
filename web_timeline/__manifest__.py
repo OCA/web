fe773e36c928793b9333fd2fa815b26ecaee865f
InfernalLV <karlisdreizis@gmail.com>
2019-03-05 09:31:09 +0200

    Fix version number
    Comply with OCA. Address the breaking change in the version number.

--- web_timeline/__manifest__.py
+++ web_timeline/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': "Web timeline",
     'summary': "Interactive visualization chart to show events in time",
-    "version": "11.1.0.0.0",
+    "version": "11.0.2.0.0",
     'author': 'ACSONE SA/NV, '
               'Tecnativa, '
               'Monk Software, '
