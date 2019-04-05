9b905c8e95a70ef0cb017c306549a6142732c187
Shepilov Vladislav <shepilov-vladislav@users.noreply.github.com>
2019-04-05 13:39:14 +0300

    [12.0] web_notify: improve popup UI (#1231)
    * [ADD]: all available bootstrap notifications (success/danger/warning/info/default)
    * [IMP] use black color for text for default notification.
    * [FIX] reverted require string for `bus.Longpolling` and rename `on_message_received` to `on_message` to prevent collisions.

--- web_notify/__manifest__.py
+++ web_notify/__manifest__.py
@@ -1,3 +1,4 @@
+# pylint: disable=missing-docstring
 # Copyright 2016 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,7 +7,6 @@
     'summary': """
         Send notification messages to user""",
     'version': '12.0.1.0.0',
-    'description': 'Web Notify',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV,'
               'AdaptiveCity,'
