f93e5310388a518c46ff94e3fc14a9e19188d780
cubells <vicent@vcubells.net>
2019-04-19 19:25:21 +0200

    [MIG] web_widget_digitized_signature: Migration to 11.0

--- web_widget_digitized_signature/__init__.py
+++ web_widget_digitized_signature/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2004-2010 OpenERP SA (<http://www.openerp.com>)
 # Copyright 2011-2015 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
