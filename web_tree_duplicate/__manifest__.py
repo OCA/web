b23804cbd082372c96e378eef4a1e271757cc9fc
Dennis Sluijk <d.sluijk@onestein.nl>
2019-04-19 13:41:27 +0200

    [12.0][ADD] web_tree_duplicate: Duplicate records from the tree view (#1247)
    * [ADD] web_tree_duplicate
    
    [ADD] Opt in attribute
    
    [FIX] Lint
    
    * [IMP] Show duplicated records

---
+++ web_tree_duplicate/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2019 Onestein
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Tree View Duplicate Records',
+    'summary': 'Duplicate records directly from the tree view.',
+    'development_status': 'Beta',
+    'category': 'Extra Tools',
+    'version': '12.0.1.0.0',
+    'author': 'Onestein, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/OCA/web',
+    'depends': [
+        'web'
+    ],
+    'data': [
+        'templates/assets.xml'
+    ],
+    'demo': [
+        'demo/res_users_view.xml'
+    ],
+    'maintainers': ['tarteo']
+}
