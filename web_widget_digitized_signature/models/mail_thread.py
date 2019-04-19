101c8075e78550dd9664a323874208a15f9cade1
cubells <vicent@vcubells.net>
2019-04-19 19:25:21 +0200

    [MIG] web_widget_digitized_signature: Migration to 11.0

--- web_widget_digitized_signature/models/mail_thread.py
+++ web_widget_digitized_signature/models/mail_thread.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2004-2010 OpenERP SA (<http://www.openerp.com>)
 # Copyright 2011-2015 Serpent Consulting Services Pvt. Ltd.
 # Copyright 2017 Tecnativa - Vicent Cubells
