acc46258751767dc300f6872951e947147c9f818
tarteo <d.sluijk@onestein.nl>
2019-04-24 15:40:03 +0200

    [FIX] web_widget_x2many_2d_matrix: Use widget for formatting aggregates

--- web_widget_x2many_2d_matrix/__manifest__.py
+++ web_widget_x2many_2d_matrix/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': '2D matrix for x2many fields',
-    'version': '12.0.1.0.3',
+    'version': '12.0.1.0.4',
     'author': (
         'Therp BV, '
         'Tecnativa, '
