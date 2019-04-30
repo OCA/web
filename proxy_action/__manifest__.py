b36f70496eba5e770fa39b8435729f9d906d980e
Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
2019-04-30 10:25:03 +0200

    [FIX] rebase conflict on proxy_action/models/helper.py & black formating

--- proxy_action/__manifest__.py
+++ proxy_action/__manifest__.py
@@ -4,21 +4,16 @@
 # Copyright 2017 Raphael Reverdy <raphael.reverdy@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Proxy Action',
-    'version': '10.0.0.0.1',
-    'author': 'Akretion',
-    'website': 'www.akretion.com',
-    'license': 'AGPL-3',
-    'category': 'Generic Modules',
-    'description': """
+    "name": "Proxy Action",
+    "version": "10.0.0.0.1",
+    "author": "Akretion",
+    "website": "www.akretion.com",
+    "license": "AGPL-3",
+    "category": "Generic Modules",
+    "description": """
     Forward to pywebdriver odoo's actions.
     """,
-    'depends': [
-        'base',
-        'web',
-    ],
-    'data': [
-        'static/src/xml/view.xml',
-    ],
-    'installable': True,
+    "depends": ["base", "web"],
+    "data": ["static/src/xml/view.xml"],
+    "installable": True,
 }
--- proxy_action/models/helper.py
+++ proxy_action/models/helper.py
@@ -4,8 +4,9 @@
 # Copyright 2017 Raphaël Reverdy <raphael.reverdy@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, models
 import base64
+
+from openerp import api, models
 
 
 class ProxyActionHelper(models.AbstractModel):
@@ -14,61 +15,43 @@ class ProxyActionHelper(models.AbstractModel):
 
     @api.model
     def get_print_data_action(
-            self, data,
-            printer_name=None,
-            raw=False,
-            to_encode64=False,
-            copies=1,
-            host='https://localhost'):
+        self,
+        data,
+        printer_name=None,
+        raw=False,
+        to_encode64=False,
+        copies=1,
+        host="https://localhost",
+    ):
         """ Prepare a PyWebdriver.print action """
         if to_encode64:
             data = base64.b64encode(data)
-        kwargs = {'options': {}}
+        kwargs = {"options": {}}
         if copies > 1:
-            kwargs['options']['copies'] = copies
+            kwargs["options"]["copies"] = copies
         if raw:
-            kwargs['options']['raw'] = True
+            kwargs["options"]["raw"] = True
         return {
-            'url': '%s/cups/printData' % host,
-            'params': {
-                'args': [printer_name, data],
-                'kwargs': kwargs,
-            }
+            "url": "%s/cups/printData" % host,
+            "params": {"args": [printer_name, data], "kwargs": kwargs},
         }
 
-<<<<<<< HEAD
     @api.model
-    def get_print_xml_receipt_action(
-            self, receipt,
-            host='https://localhost'):
+    def get_print_xml_receipt_action(self, receipt, host="https://localhost"):
         """ Prepare a PyWebdriver.print action """
 
         return {
-            'url': '%s/hw_proxy/print_xml_receipt' % host,
-<<<<<<< HEAD
-            'params' : {'params': {'receipt': receipt}},
-            }
-=======
-    def get_print_report_action(self, records, report_name, **kwargs):
-        data = self.env['report'].get_pdf(records.ids, report_name)
-        data = base64.b64encode(data)
-	return self.get_print_data_action(data, **kwargs)
->>>>>>> [IMP] migrate module to version 8
-=======
-            'params': {'params': {'receipt': receipt}},
+            "url": "%s/hw_proxy/print_xml_receipt" % host,
+            "params": {"params": {"receipt": receipt}},
         }
 
     def get_print_report_action(self, records, report_name, **kwargs):
-        data = self.env['report'].get_pdf(records.ids, report_name)
+        data = self.env["report"].get_pdf(records.ids, report_name)
         data = base64.b64encode(data)
         return self.get_print_data_action(data, **kwargs)
->>>>>>> pylint
 
     def send_proxy(self, todo):
         """ @param todo: list of requests
                          (printings, webservices)
         """
-        return {
-            'type': 'ir.actions.act_proxy',
-            'action_list': todo,
-        }
+        return {"type": "ir.actions.act_proxy", "action_list": todo}
