0d0f21315ab624b880a2bfc6543a791c30b2dba5
JonathanOsAlc <jonathan@vauxoo.com>
2019-05-04 11:43:18 -0500

    [FIX] web_widget_timepicker: fix show current time when choose 0:00

--- web_widget_timepicker/static/src/js/web_widget_timepicker.js
+++ web_widget_timepicker/static/src/js/web_widget_timepicker.js
@@ -25,7 +25,7 @@ odoo.define('web_widget_timepicker', function (require) {
 
         setValue: function (value) {
             this.set({'value': value});
-            var formatted_value = value ? this._formatClient(value) : null;
+            var formatted_value = this._formatClient(value) || value;
             this.$input.val(formatted_value);
             if (this.picker) {
                 var fdate = new moment(formatted_value, this.options.format);
@@ -36,7 +36,7 @@ odoo.define('web_widget_timepicker', function (require) {
 
         getValue: function () {
             var value = this.get('value');
-            return value ? this._formatClient(value) : null;
+            return this._formatClient(value) || value;
         },
 
         changeDatetime: function () {
