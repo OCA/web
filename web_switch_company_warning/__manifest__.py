febb5cfdd0e211ae7b820cf3deac265eae63f2cf
Benoît Fontaine <benoitfontaine.ba@gmail.com>
2019-06-07 13:37:41 +0200

    Add module web_switch_company_warning

---
+++ web_switch_company_warning/__manifest__.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+# Copyright 2017 Akretion <raphael.reverdy@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Multicompany - Switch Company Warning",
+    "summary": "Show a warning if current company has been switched"
+    " in another tab or window.",
+    "version": "1.0",
+    "category": "web",
+    "website": "http://akretion.com",
+    "license": "AGPL-3",
+    "author": "Akretion, " "Odoo Community Association (OCA)",
+    "depends": ["web"],
+    "data": ["view/view.xml"],
+    "qweb": ["static/src/xml/switch_company_warning.xml"],
+    "installable": True,
+    "application": False,
+}
---
+++ web_switch_company_warning/static/src/js/switch_company_warning.js
@@ -0,0 +1,46 @@
+odoo.define('web_switch_company_warning.widget', function(require) {
+    'use strict';
+    var Widget = require('web.Widget');
+    var UserMenu = require('web.UserMenu');
+    //Show a big banner in the top of the page if the company has been
+    //changed in another tab or window (in the same browser)
+
+    if (!window.SharedWorker) {
+        //not supported
+        return;
+    }
+    var SwitchCompanyWarningWidget = Widget.extend({
+        template: 'web_switch_company_warning.warningWidget',
+        init: function() {
+            this._super();
+            var self = this;
+            var w = new SharedWorker('/web_switch_company_warning/static/src/js/switch_company_warning_worker.js');
+            w.port.addEventListener('message', function(msg) {
+                if (msg.data.type !== 'newCtx') {
+                    return;
+                }
+                if (msg.data.newCtx === self.generateSignature()) {
+                    self.$el.hide();
+                } else {
+                    self.$el.show();
+                }
+            });
+            w.port.start();
+            w.port.postMessage(this.generateSignature());
+        },
+        generateSignature: function() {
+            return [odoo.session_info.company_id, odoo.session_info.db].join();
+        }
+    });
+
+    UserMenu.include({
+        init: function(parent) {
+            this._super(parent);
+            var switchCompanyWarning = new SwitchCompanyWarningWidget();
+            switchCompanyWarning.appendTo('#oe_main_menu_navbar');
+        }
+
+    });
+
+    return SwitchCompanyWarningWidget;
+});
