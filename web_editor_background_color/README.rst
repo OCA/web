e6717bf02437be74aaf4578dee75595145717de1
Jairo Llopis <yajo.sk8@gmail.com>
2019-07-09 12:06:25 +0100

    [MIG] web_editor_background_color: Migrate to v12
    - Remove bootstrap-colorpicker library, which is deprecated.
    - Use Odoo's internal color picker dialog.
    
    Co-Authored-By: Alexandre Díaz <alexandre.diaz@tecnativa.com>

--- web_editor_background_color/README.rst
+++ web_editor_background_color/README.rst
@@ -14,13 +14,13 @@ Web Editor Background Color Picker
     :target: http://www.gnu.org/licenses/lgpl-3.0-standalone.html
     :alt: License: LGPL-3
 .. |badge3| image:: https://img.shields.io/badge/github-OCA%2Fweb-lightgray.png?logo=github
-    :target: https://github.com/OCA/web/tree/11.0/web_editor_background_color
+    :target: https://github.com/OCA/web/tree/12.0/web_editor_background_color
     :alt: OCA/web
 .. |badge4| image:: https://img.shields.io/badge/weblate-Translate%20me-F47D42.png
-    :target: https://translation.odoo-community.org/projects/web-11-0/web-11-0-web_editor_background_color
+    :target: https://translation.odoo-community.org/projects/web-12-0/web-12-0-web_editor_background_color
     :alt: Translate me on Weblate
 .. |badge5| image:: https://img.shields.io/badge/runbot-Try%20me-875A7B.png
-    :target: https://runbot.odoo-community.org/runbot/162/11.0
+    :target: https://runbot.odoo-community.org/runbot/162/12.0
     :alt: Try me on Runbot
 
 |badge1| |badge2| |badge3| |badge4| |badge5| 
@@ -50,13 +50,22 @@ To use this module, you need to:
    * Writing any valid HTML color code in the text input.
    * Selecting a color from the color picker.
 
+Known issues / Roadmap
+======================
+
+* Preview when user plays with colors.
+  See https://github.com/OCA/web/pull/1327#issuecomment-509621572 to understand
+  the (co/i)mplications.
+* When (& if) Odoo merges https://github.com/odoo/odoo/pull/34687, this
+  module will no longer be needed.
+
 Bug Tracker
 ===========
 
 Bugs are tracked on `GitHub Issues <https://github.com/OCA/web/issues>`_.
 In case of trouble, please check there if your issue has already been reported.
 If you spotted it first, help us smashing it by providing a detailed and welcomed
-`feedback <https://github.com/OCA/web/issues/new?body=module:%20web_editor_background_color%0Aversion:%2011.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+`feedback <https://github.com/OCA/web/issues/new?body=module:%20web_editor_background_color%0Aversion:%2012.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
 
 Do not contact contributors directly about support or help with technical issues.
 
@@ -93,6 +102,6 @@ OCA, or the Odoo Community Association, is a nonprofit organization whose
 mission is to support the collaborative development of Odoo features and
 promote its widespread use.
 
-This module is part of the `OCA/web <https://github.com/OCA/web/tree/11.0/web_editor_background_color>`_ project on GitHub.
+This module is part of the `OCA/web <https://github.com/OCA/web/tree/12.0/web_editor_background_color>`_ project on GitHub.
 
 You are welcome to contribute. To learn how please visit https://odoo-community.org/page/Contribute.
--- web_editor_background_color/static/src/js/background_color.js
+++ web_editor_background_color/static/src/js/background_color.js
@@ -1,103 +1,49 @@
-/* Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+/* Copyright 2016-2019 Jairo Llopis <jairo.llopis@tecnativa.com>
  * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl). */
 
 odoo.define("web_editor_background_color.colorpicker", function (require) {
     "use strict";
+    var ColorpickerDialog = require('web.colorpicker');
     var options = require("web_editor.snippets.options");
     var colorpicker = options.registry.colorpicker;
 
     colorpicker.include({
-        events: _.extend({}, colorpicker.prototype.events, {
-            "changeColor [data-name=custom_color]":
-                "set_inline_background_color",
-            // Remove inline background-color for normal class-based buttons
-            "click .o_colorpicker_section button[data-color]":
-                "remove_inline_background_color",
-            "click [data-name=custom_color] input": "input_select",
-            "click [data-name=custom_color]": "custom_abort_event",
-            "keydown [data-name=custom_color]": "custom_abort_event",
-            "keypress [data-name=custom_color]": "custom_abort_event",
-            "keyup [data-name=custom_color]": "custom_abort_event",
+        custom_events: _.extend({}, colorpicker.prototype.custom_events, {
+            "colorpicker:saved": "_onCustomColorSave",
         }),
-        xmlDependencies: colorpicker.prototype.xmlDependencies.concat([
-            "/web_editor_background_color/static/src/xml/colorpicker.xml",
-        ]),
+        events: _.extend({}, colorpicker.prototype.events, {
+            "click .o_colorpicker_section[data-name=custom]>.text-muted":
+                "_onCustomColorAsk",
+        }),
 
         /**
-         * @override
+         * Called when the user clicks on "Custom color" section header
          */
-        start: function () {
-            this._super();
-            // Enable custom color picker
-            this.$custom = this.$el.find('[data-name="custom_color"]');
-            this.$custom.colorpicker({
-                color: this.$target.css("background-color"),
-                container: true,
-                inline: true,
-                sliders: {
-                    saturation: {
-                        maxLeft: 118,
-                        maxTop: 118,
-                    },
-                    hue: {
-                        maxTop: 118,
-                    },
-                    alpha: {
-                        maxTop: 118,
-                    },
+        _onCustomColorAsk: function () {
+            var dialog = new ColorpickerDialog(this, {
+                defaultColor: this.$target.css("background-color"),
+            });
+            dialog.open();
+        },
+
+        /**
+         * Called when the user saves a custom color
+         *
+         * @param {Event} event
+         */
+        _onCustomColorSave: function (event) {
+            // Add a button to remind recent choices
+            var $button = $("<button/>", {
+                class: "o_custom_color",
+                css: {
+                    "background-color": event.data.cssColor,
                 },
             });
-            // Activate border color changes if it matches background's
-            var style = this.$target.prop("style");
-            this.change_border =
-                style["border-color"] &&
-                style["background-color"] === style["border-color"];
-        },
-
-        /**
-         * A HACK to avoid dropdown disappearing when picking colors
-         *
-         * @param {Event} event
-         */
-        custom_abort_event: function (event) {
-            event.stopPropagation();
-        },
-
-        /**
-         * Select the color picker input
-         *
-         * @param {Event} event
-         */
-        input_select: function (event) {
-            $(event.target).focus().select();
-        },
-
-        /**
-         * Undo the inline background color, besides upstream color classes
-         *
-         * @override
-         */
-        _onColorResetButtonClick: function (event) {
-            this._super.apply(this, arguments);
-            this.$target.css("background-color", "");
-            if (this.change_border) {
-                this.$target.css("border-color", "");
-            }
-            this.$target.trigger("background-color-event", event.type);
-        },
-
-        /**
-         * Apply the chosen color as an inline style
-         *
-         * @param {Event} event
-         */
-        set_inline_background_color: function (event) {
-            var color = String(event.color);
-            this.$target.css("background-color", color);
-            if (this.change_border) {
-                this.$target.css("border-color", color);
-            }
-            this.$target.trigger("background-color-event", event.type);
+            var $custom = this.$el.find(
+                ".o_colorpicker_section[data-name=custom]");
+            $custom.append($button);
+            // Emulate a hover & click on that new button
+            $button.mouseenter().click();
         },
     });
 });
