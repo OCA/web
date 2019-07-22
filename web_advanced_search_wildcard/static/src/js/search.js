17643593764a9dc46bb7d71ca437afcc965fc378
sbejaoui <souheil.bejaoui@acsone.eu>
2019-07-22 10:10:50 +0200

    [IMP] - Use super to get default case

--- web_advanced_search_wildcard/static/src/js/search.js
+++ web_advanced_search_wildcard/static/src/js/search.js
@@ -15,14 +15,13 @@ odoo.define('web_advanced_search_wildcard', function (require) {
 
     Char.include({
         get_domain: function (field, operator) {
+            var res = this._super(field, operator);
             switch (operator.value) {
-            case '∃': return [[field.name, '!=', false]];
-            case '∄': return [[field.name, '=', false]];
             case 'startswith': return [[field.name, '=ilike', this.get_value() + '%']];
             case 'not_startswith': return ['!', [field.name, '=ilike', this.get_value() + '%']];
             case 'endswith': return [[field.name, '=ilike', '%' + this.get_value()]];
             case 'not_endswith': return ['!', [field.name, '=ilike', '%' + this.get_value()]];
-            default: return [[field.name, operator.value, this.get_value()]];
+            default: return res;
             }
         },
     });
