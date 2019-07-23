23a0715cd429ac7488cad5a7c36b027065e18875
Larbi Gharib <43585585+lgharib@users.noreply.github.com>
2019-07-23 11:21:57 -0400

    Update web_calendar_custom/static/src/js/web_calendar_custom.js
    Co-Authored-By: Alexandre Díaz <alexandre.diaz@tecnativa.com>

--- web_calendar_custom/static/src/js/web_calendar_custom.js
+++ web_calendar_custom/static/src/js/web_calendar_custom.js
@@ -1,5 +1,5 @@
 /*
-    © 2018 Savoir-faire Linux <https://savoirfairelinux.com>
+    Copyright 2018 Savoir-faire Linux <https://savoirfairelinux.com>
     License LGPL-3.0 or later (http://www.gnu.org/licenses/LGPL.html).
 */
 
