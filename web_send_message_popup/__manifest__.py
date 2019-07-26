18e7ea06a64aed357739eea67f91aed4caa7f229
Iryna Vyshnevska <i.vyshnevska@mobilunity.com>
2019-07-26 15:26:10 +0300

    [12.0][MIG] web_send_message_popup: migrate

--- web_send_message_popup/__manifest__.py
+++ web_send_message_popup/__manifest__.py
@@ -1,8 +1,8 @@
-# Copyright 2014-2018 Camptocamp SA
+# Copyright 2014-2019 Camptocamp SA
 # License AGPL-3.0 or later (http://gnu.org/licenses/agpl).
 {
     'name': 'Web Send Message as Popup',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': "Camptocamp, Odoo Community Association (OCA)",
     'maintainer': 'Camptocamp',
     'license': 'AGPL-3',
