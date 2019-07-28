7029d9396b3d4989b998d7a64d291932b1b949e8
Iryna Vushnevska <i.vyshnevska@mobilunity.com>
2019-07-28 20:51:49 +0300

    [IMP] rid off delay for deffered obj

--- web_send_message_popup/static/src/js/chatter.js
+++ web_send_message_popup/static/src/js/chatter.js
@@ -1,4 +1,4 @@
-/* Copyright 2018 Camptocamp SA
+/* Copyright 2019 Camptocamp SA
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
 odoo.define('web_send_message_popup.Chatter', function (require) {
     "use strict";
@@ -7,22 +7,11 @@ odoo.define('web_send_message_popup.Chatter', function (require) {
 
     Chatter.include({
         _onOpenComposerMessage: function () {
-            // wait for composer input to be initialized
-            // taken from http://stackoverflow.com/questions/7307983/while-variable-is-not-defined-wait
-            var self = this;
-            $.when(this._super.apply(this, arguments)).then(function () {
-                function checkVariable() {
-                    if (typeof self.composer !== 'undefined' && typeof self.composer.$input !== 'undefined') {
-                        self.composer.on_open_full_composer();
-                    }
-                    else {
-                        setTimeout(function () {
-                            checkVariable();
-                        }, 50);
-                    }
-                }
-                checkVariable();
-            });
-        }
+            this._super.apply(this, arguments);
+            this.suggested_partners_def.done($.proxy(function () {
+                this._closeComposer(true);
+                this.composer.on_open_full_composer();
+            }, this));
+        },
     });
 });
