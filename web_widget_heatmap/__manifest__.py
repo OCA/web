710b364c79150ce7ecc1aa47939541ca36a72287
Dinar <gabbasov@it-projects.info>
2019-08-02 18:46:24 +0500

    Update web_widget_heatmap/__manifest__.py
    Co-Authored-By: Alexandre Díaz <alexandre.diaz@tecnativa.com>

--- web_widget_heatmap/__manifest__.py
+++ web_widget_heatmap/__manifest__.py
@@ -6,7 +6,7 @@
     "version": "11.0.1.0.0",
     "development_status": "Stable",
     "category": "Extra Tools",
-    "website": "https://github.com/OCA/web/tree/11.0/web_widget_heatmap",
+    "website": "https://github.com/OCA/web",
     "author": "Dinar Gabbasov, Odoo Community Association (OCA)",
     "maintainers": ["GabbasovDinar"],
     "license": "AGPL-3",
