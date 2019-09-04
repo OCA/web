969da3faeb2b96700ac89d8a476e502cf814f080
Quentin Dupont <perso@quentindupont.fr>
2019-09-04 17:51:56 +0200

    [ADD] Widget Numeric step

--- web_widget_numeric_step/__manifest__.py
+++ web_widget_numeric_step/__manifest__.py
@@ -1,8 +1,8 @@
-# Odoo, Open Source Web Widget Integer Choice
+# Odoo, Open Source Web Widget Numeric Step
 #
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).#
 {
-    'name': "Web Widget UX Number Choice",
+    'name': "Web Widget Numeric Step",
     'category': "web",
     'version': "12.0.1.0.0",
     'author': "GRAP, "
@@ -11,10 +11,10 @@
     'website': 'https://github.com/OCA/web',
     'depends': ['web'],
     'data': [
-        'view/web_widget_number_ux_choice.xml'
+        'view/web_widget_numeric_step.xml'
     ],
     'qweb': [
-        'static/src/xml/number_ux_choice.xml',
+        'static/src/xml/numeric_step.xml',
     ],
     'auto_install': False,
     'installable': True,
--- web_widget_numeric_step/readme/DESCRIPTION.rst
+++ web_widget_numeric_step/readme/DESCRIPTION.rst
@@ -1,2 +1,2 @@
 This widget changes input number field and make it easier to incremente the number thanks to 2 buttons (+ and -).
-We can be parametred to choose the iteration step (0.1 for more precision etc.)
+Use JS native logic for input number, so you can use the options ``min``, ``max``, ``step``, ``placeholder``.
