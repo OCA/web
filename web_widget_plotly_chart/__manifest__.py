3e77cd1a0a8be02550c1ed83c1704bc96fa028fd
Roberto Fichera <robyf@tekno-soft.it>
2019-09-10 11:40:22 +0200

    [12.0][NEW] web_widget_plotly_chart

---
+++ web_widget_plotly_chart/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Web Widget Plotly",
+    'summary': """Allow to draw plotly charts.""",
+    'description': """Allow to draw plotly charts.""",
+    'author': "LevelPrime srl, "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
+    'category': 'Web',
+    'version': '12.0.1.0.0',
+    'depends': ['web'],
+    'data': [
+        'views/web_widget_plotly_chart.xml',
+    ],
+    "external_dependencies": {
+        "python": ['plotly'],
+    },
+    "auto_install": False,
+    "license": "LGPL-3",
+}
