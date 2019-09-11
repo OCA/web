8efe13ebec13391d5aca58785a652ab615936b58
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-09-11 14:04:03 +0200

    [ADD] web_widget_dropdown_dynamic
    [UPD] Update web_widget_dropdown_dynamic.pot
    
    [UPD] Update web_widget_dropdown_dynamic_example.pot
    
    [UPD] README.rst
    
    [ADD] icon.png
    
    [UPD] README.rst

---
+++ web_widget_dropdown_dynamic/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Dynamic Dropdown Widget',
+    'summary': 'This module adds support for dynamic dropdown widget',
+    'category': 'Web',
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author':
+        'Brainbean Apps OU, '
+        'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/web/',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'templates/assets.xml',
+    ],
+    'installable': True,
+}
