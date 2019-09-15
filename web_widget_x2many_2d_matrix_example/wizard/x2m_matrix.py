09dd19078020f0e9e992d7b11a8080df430f6458
Holger Brunn <hbrunn@therp.nl>
2019-09-15 06:12:55 +0200

    [ADD] extend the demo module to showcase more possibilities

--- web_widget_x2many_2d_matrix_example/wizard/x2m_matrix.py
+++ web_widget_x2many_2d_matrix_example/wizard/x2m_matrix.py
@@ -10,19 +10,19 @@ class X2mMatrixDemoWiz(models.TransientModel):
         'x2m.demo.line', default=lambda self: self._default_line_ids())
 
     def _default_line_ids(self):
+        """take care that the widget gets records passed for every combination
+        of x2m.demo and res.users involved"""
         recs = self.env['x2m.demo'].search([])
-        # same with users
         users = self.env['x2m.demo.line'].search([]).mapped('user_id')
         return [
             (0, 0, {
                 'name': "{}'s task on {}".format(usr.name, rec.name),
                 'demo_id': rec.id,
                 'user_id': usr.id,
-                'value': 0,
             })
-            # if the project doesn't have a task for the user, create a new one
+            # if there isn't a demo line record for the user, create a new one
             if not rec.line_ids.filtered(lambda x: x.user_id == usr) else
-            # otherwise, return the task
+            # otherwise, return the line
             (4, rec.line_ids.filtered(lambda x: x.user_id == usr)[0].id)
             for rec in recs
             for usr in users
