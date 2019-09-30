fcabe42b740531ffb62a6238e4df38a190bcf6d8
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-09-30 17:32:35 +0200

    [MIG] web_responsive: Migration to 13.0

--- web_responsive/__manifest__.py
+++ web_responsive/__manifest__.py
@@ -1,14 +1,18 @@
 # Copyright 2016-2017 LasLabs Inc.
-# Copyright 2018 Alexandre Díaz
+# Copyright 2018-2019 Alexandre Díaz
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Web Responsive",
     "summary": "Responsive web client, community-supported",
+<<<<<<< HEAD
     "version": "12.0.1.3.2",
+=======
+    "version": "13.0.1.0.0",
+>>>>>>> 14ddc06b... [MIG] web_responsive: Migration to 13.0
     "category": "Website",
     "website": "https://github.com/OCA/web",
-    "author": "LasLabs, Tecnativa, Alexandre Díaz, "
+    "author": "LasLabs, Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "installable": True,
