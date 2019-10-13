e7291b9bb2c3a5ca8c4055e41b86cbcce15f5025
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-10-13 23:41:25 +0200

    black, isort

--- web_responsive/__manifest__.py
+++ web_responsive/__manifest__.py
@@ -8,22 +8,14 @@
     "version": "13.0.1.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/web",
-    "author": "LasLabs, Tecnativa, "
-              "Odoo Community Association (OCA)",
+    "author": "LasLabs, Tecnativa, " "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "installable": True,
-    "depends": [
-        'web',
-        'mail',
-    ],
-    "data": [
-        'views/assets.xml',
-        'views/res_users.xml',
-        'views/web.xml',
-    ],
-    'qweb': [
-        'static/src/xml/apps.xml',
-        'static/src/xml/form_view.xml',
-        'static/src/xml/navbar.xml',
+    "depends": ["web", "mail"],
+    "data": ["views/assets.xml", "views/res_users.xml", "views/web.xml"],
+    "qweb": [
+        "static/src/xml/apps.xml",
+        "static/src/xml/form_view.xml",
+        "static/src/xml/navbar.xml",
     ],
 }
