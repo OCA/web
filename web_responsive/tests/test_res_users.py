6a722d815b35f80c5752c9a0c02002ee4ca71131
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-10-13 23:41:25 +0200

    black, isort

--- web_responsive/tests/test_res_users.py
+++ web_responsive/tests/test_res_users.py
@@ -5,12 +5,9 @@ from odoo.tests import common
 
 
 class TestResUsers(common.TransactionCase):
-
     def test_chatter_position_wr(self):
-        user_public = self.env.ref('base.public_user')
+        user_public = self.env.ref("base.public_user")
 
-        self.assertEqual(user_public.chatter_position, 'sided')
-        user_public.sudo(user_public).write({
-            'chatter_position': 'normal',
-        })
-        self.assertEqual(user_public.chatter_position, 'normal')
+        self.assertEqual(user_public.chatter_position, "sided")
+        user_public.sudo(user_public).write({"chatter_position": "normal"})
+        self.assertEqual(user_public.chatter_position, "normal")
