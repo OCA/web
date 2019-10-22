3b028b4866ac7bdc106a1f6000dbda65406d793a
Jan Verbeek <jverbeek@therp.nl>
2019-10-22 18:02:46 +0200

    [MIG] Migrate web_widget_mermaid to 12.0

--- web_widget_mermaid/static/src/js/web_widget_mermaid.js
+++ web_widget_mermaid/static/src/js/web_widget_mermaid.js
@@ -2,7 +2,8 @@ odoo.define('web.web_widget_mermaid', function(require) {
     "use strict";
 
     var core = require('web.core');
-    var form_widgets = require('web.form_widgets');
+    var basic_fields = require('web.basic_fields');
+    var field_registry = require('web.field_registry');
 
     // Calling mermaid.initialize() multiple times is ok.
     // But there's a catch: it will keep the config of previous calls unless
@@ -53,35 +54,31 @@ odoo.define('web.web_widget_mermaid', function(require) {
             fontFamily: '"Lucida Grande", Helvetica, Verdana, Arial, '
                         + 'sans-serif',  // Match Odoo's font choices
             numberSectionStyles: 4,
+            // Match configured date format
             axisFormat: core._t.database.parameters.date_format,
         }
     };
 
-    var MermaidField = form_widgets.FieldChar.extend({
+    var MermaidField = basic_fields.FieldText.extend({
         init: function() {
             this._super.apply(this, arguments);
             this.chartId = _.uniqueId('mermaid_chart_');
         },
-        template: "MermaidField",
-        widget_class: 'o_form_field_mermaid',
-        render_value: function() {
-            if (!this.get('effective_readonly')) {
-                return this._super();
-            }
-            var value = this.get('value');
-            if (!value) {
+        className: 'o_form_field_mermaid',
+        _renderReadonly: function() {
+            if (!this.value) {
                 return;
             }
             var config = _.extend(
                 {},
                 defaultConfig,
-                this.options
+                this.attrs.options
             );
             mermaid.mermaidAPI.initialize(config);
             try {
                 mermaid.mermaidAPI.render(
                     this.chartId,
-                    value,
+                    this.value,
                     this.$el.html.bind(this.$el)
                 );
             } catch (e) {
@@ -92,9 +89,9 @@ odoo.define('web.web_widget_mermaid', function(require) {
             // rid of it if it's still around. The id is based on the chartId.
             $('#d' + this.chartId).remove();
         }
-    })
+    });
 
-    core.form_widget_registry.add('mermaid', MermaidField);
+    field_registry.add('mermaid', MermaidField);
 
     return {
         MermaidField: MermaidField,
