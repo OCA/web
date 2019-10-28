326ded35082c4b1508342503ccd8819f460042fd
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2019-10-28 16:20:44 +0100

    [12.0][ADD] web_widget_url_translatable
    This module extends the URL widget, so it works the same than other
    widget with translatable fields.
    In edition mode the button that allows to open the tranlation view
    is displayed.

---
+++ web_widget_url_translatable/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2019 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    'name': 'Translatable URL widget',
+    'version': '12.0.1.0.0',
+    'author': 'Camptocamp, Odoo Community Association (OCA)',
+    'category': 'Web',
+    'depends': ['web'],
+    'summary': """Enable to set translation on fields using URL widget""",
+    'website': 'https://github.com/OCA/web',
+    'installable': True,
+    'license': 'LGPL-3',
+    'data': ['views/assets.xml'],
+}
