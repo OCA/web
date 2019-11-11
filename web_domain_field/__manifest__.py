a900ed8e14efae5e1690ef81549fc054637bc379
laurent.corron <laurent.corron@acsone.eu>
2019-11-11 20:24:52 +0100

    [REF] web_domain_field: Black python code

--- web_domain_field/__manifest__.py
+++ web_domain_field/__manifest__.py
@@ -3,19 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Web Domain Field',
-    'summary': """
+    "name": "Web Domain Field",
+    "summary": """
         Use computed field as domain""",
-    'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://acsone.eu/',
-    'depends': [
-        'web'
-    ],
-    'data': [
-        'views/web_domain_field.xml',
-    ],
-    'demo': [
-    ],
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://acsone.eu/",
+    "depends": ["web"],
+    "data": ["views/web_domain_field.xml",],
+    "demo": [],
 }
