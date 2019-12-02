82386cdd93cf54f9bec826997794e4c88c3c85c3
Cyril Favre <17401810+josueBulle@users.noreply.github.com>
2019-12-02 10:04:16 +0100

    readme

--- web_refresher/__manifest__.py
+++ web_refresher/__manifest__.py
@@ -1,36 +1,10 @@
-##############################################################################
-#
-#       ______ Releasing children from poverty      _
-#      / ____/___  ____ ___  ____  ____ ___________(_)___  ____
-#     / /   / __ \/ __ `__ \/ __ \/ __ `/ ___/ ___/ / __ \/ __ \
-#    / /___/ /_/ / / / / / / /_/ / /_/ (__  |__  ) / /_/ / / / /
-#    \____/\____/_/ /_/ /_/ .___/\__,_/____/____/_/\____/_/ /_/
-#                        /_/
-#                            in Jesus' name
-#
-#    Copyright (C) 2019 Compassion CH (http://www.compassion.ch)
-#    @author: Samuel Fringeli <samuel.fringeli@me.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 # pylint: disable=C8101
 
 {
     'name': 'Web Refresher',
     'version': '11.0.1.0.0',
-    'author': 'Samuel Fringeli',
+    'author': 'Compassion Switzerland, '
+              'Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'website': 'https://github.com/OCA/web',
     'data': [
