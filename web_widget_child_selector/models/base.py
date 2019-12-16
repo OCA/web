521afb5864e8cf0130c79c71595f46457249c152
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-12-16 11:35:41 +0100

    [11.0][IMP] child_selection_clear

--- web_widget_child_selector/models/base.py
+++ web_widget_child_selector/models/base.py
@@ -1,38 +1,31 @@
 # Copyright 2019 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, _
+from odoo import models
 
 
 class Base(models.AbstractModel):
     _inherit = 'base'
 
-    def get_record_parent(self):
-        self.ensure_one()
-        if not hasattr(self, self._parent_name):
-            return False
-        parent = getattr(self, self._parent_name)
-        if not parent:
-            return False
-        return parent.name_get()[0]
-
     def _get_record_parents(self, field):
         if not self:
-            return [(False, _('Root'))]
+            return []
         return getattr(
             self, self._parent_name
         )._get_record_parents(field) + [(self.id, str(getattr(self, field)))]
 
-    def _get_record_direct_childs(self, field):
+    def _get_record_direct_childs(self, field, domain):
         if not hasattr(self, self._parent_name):
             return []
         return [(r.id, str(getattr(r, field))) for r in self.search([(
             self._parent_name, '=', self.id or False
-        )])]
+        )] + domain)]
 
-    def get_record_direct_childs_parents(self, options):
+    def get_record_direct_childs_parents(self, options, domain=False):
+        if not domain:
+            domain = []
         field = options.get('child_selection_field', 'display_name')
         return {
-            'childs': self._get_record_direct_childs(field),
+            'childs': self._get_record_direct_childs(field, domain),
             'parents': self._get_record_parents(field)
         }
