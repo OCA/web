d199153b97283fad59aa18bffac7a96100889310
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2019-12-17 16:00:14 +0100

    [13.0][MIG] web_widget_bokeh_chart

--- web_widget_bokeh_chart/__manifest__.py
+++ web_widget_bokeh_chart/__manifest__.py
@@ -1,23 +1,18 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 ForgeFlow S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Web Widget Bokeh Chart",
     "category": "Hidden",
     "summary": "This widget allows to display charts using Bokeh library.",
-    "author": "Eficent, "
-              "Odoo Community Association (OCA)",
-    "version": "12.0.1.0.1",
-    "maintainers": ["lreficent"],
+    "author": "ForgeFlow, " "Odoo Community Association (OCA)",
+    "version": "13.0.1.0.0",
+    "maintainers": ["LoisRForgeFlow"],
     "development_status": "Production/Stable",
     "website": "https://github.com/OCA/web",
     "depends": ["web"],
-    "data": [
-        "views/web_widget_bokeh_chart.xml",
-    ],
-    "external_dependencies": {
-        "python": ['bokeh'],
-    },
+    "data": ["views/web_widget_bokeh_chart.xml"],
+    "external_dependencies": {"python": ["bokeh"]},
     "auto_install": False,
     "license": "LGPL-3",
 }
