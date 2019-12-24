cc5e5f29c97e10966e2d5a31e9d566c632db41f6
Enric Tobella <etobella@creublanca.es>
2019-12-24 13:41:17 +0100

    [11.0][ADD] web_search_date

---
+++ web_search_date/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Web Search Date',
+    'summary': """
+        Add more options for search on dates""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Creu Blanca,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/web',
+    'depends': [
+        'web'
+    ],
+    'data': [
+        'views/web_widget_image_url.xml',
+    ],
+}
---
+++ web_search_date/readme/USAGE.rst
@@ -0,0 +1,3 @@
+Write the date and the options `before`, `at` and `after` will be added.
+You need to write at least the year and the month, for example: 2020-01 for
+january 2020.
