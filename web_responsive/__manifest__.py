2d2f079fa91a8a126f8de96c039bb155bd6b5a83
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-12-24 10:42:20 +0100

    [IMP] web-responsive: Document Viewer

--- web_responsive/__manifest__.py
+++ web_responsive/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Web Responsive",
     "summary": "Responsive web client, community-supported",
-    "version": "13.0.1.0.2",
+    "version": "13.0.2.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/web",
     "author": "LasLabs, Tecnativa, " "Odoo Community Association (OCA)",
@@ -20,5 +20,6 @@
         "static/src/xml/apps.xml",
         "static/src/xml/form_view.xml",
         "static/src/xml/navbar.xml",
+        "static/src/xml/document_viewer.xml",
     ],
 }
