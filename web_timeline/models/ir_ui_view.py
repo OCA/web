35ddb1ba47b66286cddd4914ad0fdf756b645eef
Thong Nguyen Van <thoongnv@gmail.com>
2019-12-30 21:21:46 +0700

    [MIG] web_timeline: Migration to 13.0

--- web_timeline/models/ir_ui_view.py
+++ web_timeline/models/ir_ui_view.py
@@ -1,7 +1,7 @@
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from odoo import fields, models
 
 TIMELINE_VIEW = ("timeline", "Timeline")
 
--- web_timeline/readme/DESCRIPTION.rst
+++ web_timeline/readme/DESCRIPTION.rst
@@ -1,4 +1,4 @@
 Define a new view displaying events in an interactive visualization chart.
 
 The widget is based on the external library
-http://visjs.org/timeline_examples.html
+https://visjs.github.io/vis-timeline/examples/timeline
