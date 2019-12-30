6b2d18e834b8119f27232232a21b92d9218187c7
Thong Nguyen Van <thoongnv@gmail.com>
2019-12-30 18:12:23 +0700

    [IMP] web_timeline: black, isort

--- web_timeline/models/ir_view.py
+++ web_timeline/models/ir_view.py
@@ -3,11 +3,10 @@
 
 from openerp import fields, models
 
-
-TIMELINE_VIEW = ('timeline', 'Timeline')
+TIMELINE_VIEW = ("timeline", "Timeline")
 
 
 class IrUIView(models.Model):
-    _inherit = 'ir.ui.view'
+    _inherit = "ir.ui.view"
 
     type = fields.Selection(selection_add=[TIMELINE_VIEW])
