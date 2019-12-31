f9110fcb6a2dcad0eab947c1af7780ca4f044fc0
Thong Nguyen Van <thoongnv@gmail.com>
2019-12-31 09:34:05 +0700

    [REF] web_timeline: Refactor source code

--- web_timeline/models/ir_ui_view.py
+++ web_timeline/models/ir_ui_view.py
@@ -3,10 +3,10 @@
 
 from odoo import fields, models
 
-TIMELINE_VIEW = ('timeline', 'Timeline')
+TIMELINE_VIEW = ("timeline", "Timeline")
 
 
 class IrUIView(models.Model):
-    _inherit = 'ir.ui.view'
+    _inherit = "ir.ui.view"
 
     type = fields.Selection(selection_add=[TIMELINE_VIEW])
