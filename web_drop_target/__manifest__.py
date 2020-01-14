6e52441201061847f4093037cf7c2288b0f7b96e
Núria Martín Xifré <nuria.martin@eficent.com>
2020-01-14 11:30:23 +0100

    [13.0][MIG] web_drop_target

--- web_drop_target/__manifest__.py
+++ web_drop_target/__manifest__.py
@@ -2,13 +2,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Drop target support",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
     "category": "Usability",
     "summary": "Allows to drag files into Odoo",
-    "depends": ["web", "document"],
+    "depends": ["web"],
     "data": ["views/templates.xml"],
     "qweb": ["static/src/xml/widgets.xml"],
 }
