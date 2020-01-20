9d3fd089cdc7db7be30aba6ade7268797628e7e9
BT-nstuder <nicola.studer@braintec-group.com>
2020-01-20 12:44:57 +0100

    [MIG] web_no_bubble: Migration to 13.0

--- web_no_bubble/__manifest__.py
+++ web_no_bubble/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Web No Bubble",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Savoir-faire Linux, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
