434c81f6a66b177c1d1535f6aacbf743d74f49ee
Jairo Llopis <jairo.llopis@tecnativa.com>
2020-01-24 13:45:39 +0000

    [IMP] pre-commit run after update
    Includes some manual fixes to silent ESLint warnings.

--- web_environment_ribbon/static/src/js/ribbon.js
+++ web_environment_ribbon/static/src/js/ribbon.js
@@ -5,14 +5,14 @@
    Copyright 2017 Xavier Jiménez <xavier.jimenez@qubiq.es>
    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
 
-odoo.define('web_environment_ribbon.ribbon', function (require) {
+odoo.define("web_environment_ribbon.ribbon", function(require) {
     "use strict";
 
-    var rpc = require('web.rpc');
-    var core = require('web.core');
+    var rpc = require("web.rpc");
+    var core = require("web.core");
 
     // Code from: http://jsfiddle.net/WK_of_Angmar/xgA5C/
-    function validStrColour (strToTest) {
+    function validStrColour(strToTest) {
         if (strToTest === "") {
             return false;
         }
@@ -33,31 +33,31 @@ odoo.define('web_environment_ribbon.ribbon', function (require) {
         return image.style.color !== "rgb(255, 255, 255)";
     }
 
-    core.bus.on('web_client_ready', null, function () {
+    core.bus.on("web_client_ready", null, function() {
         var ribbon = $('<div class="test-ribbon hidden"/>');
-        $('body').append(ribbon);
+        $("body").append(ribbon);
         ribbon.hide();
         // Get ribbon data from backend
         rpc.query({
-            model: 'web.environment.ribbon.backend',
-            method: 'get_environment_ribbon',
-        }).then(
-            function (ribbon_data) {
-                // Ribbon name
-                if (ribbon_data.name && ribbon_data.name !== 'False') {
-                    ribbon.html(ribbon_data.name);
-                    ribbon.show();
-                }
-                // Ribbon color
-                if (ribbon_data.color && validStrColour(ribbon_data.color)) {
-                    ribbon.css('color', ribbon_data.color);
-                }
-                // Ribbon background color
-                if (ribbon_data.background_color &&
-                    validStrColour(ribbon_data.background_color)) {
-                    ribbon.css('background-color', ribbon_data.background_color);
-                }
+            model: "web.environment.ribbon.backend",
+            method: "get_environment_ribbon",
+        }).then(function(ribbon_data) {
+            // Ribbon name
+            if (ribbon_data.name && ribbon_data.name !== "False") {
+                ribbon.html(ribbon_data.name);
+                ribbon.show();
             }
-        );
+            // Ribbon color
+            if (ribbon_data.color && validStrColour(ribbon_data.color)) {
+                ribbon.css("color", ribbon_data.color);
+            }
+            // Ribbon background color
+            if (
+                ribbon_data.background_color &&
+                validStrColour(ribbon_data.background_color)
+            ) {
+                ribbon.css("background-color", ribbon_data.background_color);
+            }
+        });
     });
 });
