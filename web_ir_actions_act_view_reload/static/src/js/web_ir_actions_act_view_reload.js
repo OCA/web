95de4624bd8bdc0b38f4413da9acfb45f02a6c9e
Jairo Llopis <jairo.llopis@tecnativa.com>
2020-01-24 13:45:39 +0000

    [IMP] pre-commit run after update
    Includes some manual fixes to silent ESLint warnings.
    
    [UPD] README.rst
    
    [UPD] README.rst

--- web_ir_actions_act_view_reload/static/src/js/web_ir_actions_act_view_reload.js
+++ web_ir_actions_act_view_reload/static/src/js/web_ir_actions_act_view_reload.js
@@ -1,19 +1,20 @@
 // Copyright 2017 - 2018 Modoolar <info@modoolar.com>
 // Copyright 2018 Modoolar <info@modoolar.com>
 // License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-odoo.define('web_ir_actions_act_view_reload.ir_actions_act_view_reload', function (require) {
+odoo.define("web_ir_actions_act_view_reload.ir_actions_act_view_reload", function(
+    require
+) {
     "use strict";
 
-    var ActionManager = require('web.ActionManager');
+    var ActionManager = require("web.ActionManager");
 
     ActionManager.include({
-
         /**
          * Intercept action handling to detect extra action type
          * @override
          */
-        _handleAction: function (action, options) {
-            if (action.type === 'ir.actions.act_view_reload') {
+        _handleAction: function(action, options) {
+            if (action.type === "ir.actions.act_view_reload") {
                 return this._executeReloadAction(action, options);
             }
 
@@ -24,7 +25,7 @@ odoo.define('web_ir_actions_act_view_reload.ir_actions_act_view_reload', functio
          * Handle 'ir.actions.act_view_reload' action
          * @returns {Promise} Resolved promise
          */
-        _executeReloadAction: function () {
+        _executeReloadAction: function() {
             var controller = this.getCurrentController();
             if (controller && controller.widget) {
                 controller.widget.reload();
@@ -32,7 +33,5 @@ odoo.define('web_ir_actions_act_view_reload.ir_actions_act_view_reload', functio
 
             return Promise.resolve();
         },
-
     });
-
 });
