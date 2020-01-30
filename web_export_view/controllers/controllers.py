925aa252b40b85baa1567e96cdccda8b16298711
sdurgut <sinan.durgut@tai.com.tr>
2020-01-30 08:33:23 +0300

    [13.0][Migrate] web_export_view migrated 13.0

---
+++ web_export_view/controllers/controllers.py
@@ -0,0 +1,34 @@
+# Copyright 2016 Henry Zhou (http://www.maxodoo.com)
+# Copyright 2016 Rodney (http://clearcorp.cr/)
+# Copyright 2012 Agile Business Group
+# Copyright 2012 Therp BV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import json
+import odoo.http as http
+from odoo.http import request
+from odoo.addons.web.controllers.main import ExcelExport
+
+
+class ExcelExportView(ExcelExport):
+    def __getattribute__(self, name):
+        if name == 'fmt':
+            raise AttributeError()
+        return super(ExcelExportView, self).__getattribute__(name)
+
+    @http.route('/web/export/xls_view', type='http', auth='user')
+    def export_xls_view(self, data, token):
+        data = json.loads(data)
+        model = data.get('model', [])
+        columns_headers = data.get('headers', [])
+        rows = data.get('rows', [])
+
+        return request.make_response(
+            self.from_data(columns_headers, rows),
+            headers=[
+                ('Content-Disposition', 'attachment; filename="%s"'
+                 % self.filename(model)),
+                ('Content-Type', self.content_type)
+            ],
+            cookies={'fileToken': token}
+        )
