abeef9b89fa0e4382e7be7b4a3583db6668e4a0c
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-02-07 02:43:49 +0100

    [IMP] web_widget_numeric_step: Better CSS and supports mobile layouts

--- web_widget_numeric_step/__manifest__.py
+++ web_widget_numeric_step/__manifest__.py
@@ -1,20 +1,24 @@
-# Odoo, Open Source Web Widget Numeric Step
-#
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).#
+# Copyright 2019 GRAP - Quentin DUPONT
+# Copyright 2020 Tecnativa - Alexandre Díaz
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
 {
     'name': "Web Widget Numeric Step",
     'category': "web",
     'version': "12.0.1.0.0",
-    'author': "GRAP, "
+    'author': "GRAP, Tecnativa, "
               "Odoo Community Association (OCA)",
     'license': 'AGPL-3',
     'website': 'https://github.com/OCA/web',
     'depends': ['web'],
     'data': [
-        'view/web_widget_numeric_step.xml'
+        'view/assets.xml'
     ],
     'qweb': [
         'static/src/xml/numeric_step.xml',
+    ],
+    'demo': [
+        'demo/res_users_view.xml'
     ],
     'auto_install': False,
     'installable': True,
