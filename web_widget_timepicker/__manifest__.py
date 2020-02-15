5acefa7f2a58a1bd724ce6b7436ac9aeb6aac408
David Alonso <david.alonso@solvos.es>
2020-02-15 14:11:56 +0100

    [MIG] web_widget_timepicker: migration to 12.0

--- web_widget_timepicker/__manifest__.py
+++ web_widget_timepicker/__manifest__.py
@@ -1,8 +1,8 @@
 # Copyright 2016 Vividlab (<http://www.vividlab.de>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Web Timepicker Widget",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "VividLab, "
               "Kaushal Prajapati, "
               "Alexandre Díaz, "
