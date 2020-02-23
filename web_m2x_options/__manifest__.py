b6da423fc3378513859cc9604a5131b8aff0db57
Andrés Rodríguez <androdriguez14@gmail.com>
2020-02-23 14:38:03 -0400

    [IMP] web_m2x_options: balck and eslint fix

--- web_m2x_options/__manifest__.py
+++ web_m2x_options/__manifest__.py
@@ -4,23 +4,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": 'web_m2x_options',
+    "name": "web_m2x_options",
     "version": "13.0.1.0.0",
-    'category': 'Web',
+    "category": "Web",
     "author": "ACSONE SA/NV, "
-              "0k.io, "
-              "Tecnativa, "
-              "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/web',
-    'license': 'AGPL-3',
-    "depends": [
-        'web',
-    ],
-    'data': [
-        'views/view.xml'
-    ],
-    'qweb': [
-        'static/src/xml/base.xml',
-    ],
-    'installable': True,
+    "0k.io, "
+    "Tecnativa, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
+    "license": "AGPL-3",
+    "depends": ["web"],
+    "data": ["views/view.xml"],
+    "qweb": ["static/src/xml/base.xml"],
+    "installable": True,
 }
