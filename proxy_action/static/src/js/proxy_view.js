649ff40c6c5df1afc2c2c267b80ab81b36573a71
Kevin Khao <kevinkhao@gmail.com>
2020-02-27 15:18:51 +0100

    [IMP][10.0] Add UI notification for AJAX request status

--- proxy_action/static/src/js/proxy_view.js
+++ proxy_action/static/src/js/proxy_view.js
@@ -1,11 +1,12 @@
 'use strict';
 odoo.define('proxy_action.proxy_view', function (require) {
     var ActionManager = require('web.ActionManager');
-
+    var core = require('web.core')
+    var _t = core._t
     ActionManager.include({
         ir_actions_act_proxy: function (action, options) {
             var self = this;
-
+            self.do_notify(_t('Proxy action executing'), _t('Your action is being executed'));
             action.action_list.forEach(function (task) {
                 $.ajax({
                     url: task['url'],
@@ -13,12 +14,12 @@ odoo.define('proxy_action.proxy_view', function (require) {
                     data: JSON.stringify(task['params']),
                     contentType: 'application/json',
                 }).done(function (result) {
-                    console.log("Proxy action have been done with sucess", result);
-                    //TODO add an UI feedback
+                    console.log("Proxy action has been successfully sent: ", result);
+                    self.do_notify(_t('Success'), _t('Proxy action successfully sent'));
                     self.ir_actions_act_window_close(action, options);
                 }).fail(function (result) {
-                    console.log('Proxy action have failed', result);
-                    //TODO add an UI feedback
+                    console.log('Proxy action has failed: ', result);
+                    self.do_warn(_t("Failure"), _t("Proxy action failure. Please check logs."));
                     self.ir_actions_act_window_close(action, options);
                 })
             })
