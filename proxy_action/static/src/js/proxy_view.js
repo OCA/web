08926329feb1ccfa1865e3a5e46ac43af194dd71
Kevin Khao <kevinkhao@gmail.com>
2020-02-28 11:52:14 +0100

    [IMP] Concise proxy action status notification instead of 1 per action

--- proxy_action/static/src/js/proxy_view.js
+++ proxy_action/static/src/js/proxy_view.js
@@ -16,20 +16,27 @@ odoo.define('proxy_action.proxy_view', function (require) {
         _executeProxyAction: function(action, options){
             var self = this;
             self.do_notify(_t('Proxy action executing'), _t('Your action is being executed'));
+            var action_success = true;
             action.action_list.forEach(function (task) {
                 $.ajax({
                     url: task['url'],
                     type: 'POST',
                     data: JSON.stringify(task['params']),
                     contentType: 'application/json',
+                    async: false,
                 }).done(function (result) {
                     console.log("Proxy action has been successfully sent: ", result);
-                    self.do_notify(_t('Success'), _t('Proxy action successfully sent'));
                 }).fail(function (result) {
                     console.log('Proxy action has failed: ', result);
-                    self.do_warn(_t("Failure"), _t("Proxy action failure. Please check logs."));
+                    action_success = false;
                 })
             })
+            if (action_success == true){
+                self.do_notify(_t('Success'), _t('Proxy action successfully sent'));
+            }
+            else{
+                self.do_warn(_t("Failure"), _t("Proxy action failure. Please check logs."));
+            }
             action = { type: 'ir.actions.act_window_close' };
             return self.doAction(action, []);
         },
