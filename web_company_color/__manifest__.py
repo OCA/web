4d48b3fb8c16db12a78e1ee1ae7819bffe258259
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-03-19 15:04:49 +0100

    [MIG] web_widget_color: Migration to 13.0

--- web_company_color/__manifest__.py
+++ web_company_color/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "13.0.1.0.0",
     "author": "Alexandre Díaz, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
-    "depends": ["web", "base_sparse_field", "web_widget_color"],
+    "depends": ["web", "base_sparse_field"],
     "data": ["view/assets.xml", "view/res_company.xml"],
     "uninstall_hook": "uninstall_hook",
     "post_init_hook": "post_init_hook",
--- web_company_color/hooks.py
+++ web_company_color/hooks.py
@@ -7,7 +7,7 @@ from .models.res_company import URL_BASE
 
 def uninstall_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    env["ir.attachment"].search([("url", "like", "%s%%" % URL_BASE)]).unlink()
+    env["ir.attachment"].search([("url", "=like", "%s%%" % URL_BASE)]).unlink()
 
 
 def post_init_hook(cr, registry):
