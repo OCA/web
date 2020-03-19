f13b0a6e0615bd0b3140c464f42e758e2a4e00e1
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-19 12:04:11 +0100

    [IMP] web_company_color: black, isort

--- web_company_color/models/res_company.py
+++ web_company_color/models/res_company.py
@@ -2,17 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import base64
 import time
-from colorsys import rgb_to_hls, hls_to_rgb
-from odoo import models, fields, api
-from ..utils import image_to_rgb, convert_to_image, n_rgb_to_hex
+from colorsys import hls_to_rgb, rgb_to_hls
 
+from odoo import api, fields, models
 
-URL_BASE = '/web_company_color/static/src/scss/'
-URL_SCSS_GEN_TEMPLATE = URL_BASE + 'custom_colors.%d.%s.gen.scss'
+from ..utils import convert_to_image, image_to_rgb, n_rgb_to_hex
+
+URL_BASE = "/web_company_color/static/src/scss/"
+URL_SCSS_GEN_TEMPLATE = URL_BASE + "custom_colors.%d.%s.gen.scss"
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
     SCSS_TEMPLATE = """
         .o_main_navbar {
@@ -47,13 +48,12 @@ class ResCompany(models.Model):
     """
 
     company_colors = fields.Serialized()
-    color_navbar_bg = fields.Char('Navbar Background Color',
-                                  sparse='company_colors')
+    color_navbar_bg = fields.Char("Navbar Background Color", sparse="company_colors")
     color_navbar_bg_hover = fields.Char(
-        'Navbar Background Color Hover', sparse='company_colors')
-    color_navbar_text = fields.Char('Navbar Text Color',
-                                    sparse='company_colors')
-    scss_modif_timestamp = fields.Char('SCSS Modif. Timestamp')
+        "Navbar Background Color Hover", sparse="company_colors"
+    )
+    color_navbar_text = fields.Char("Navbar Text Color", sparse="company_colors")
+    scss_modif_timestamp = fields.Char("SCSS Modif. Timestamp")
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -64,22 +64,24 @@ class ResCompany(models.Model):
     @api.multi
     def unlink(self):
         result = super().unlink()
-        IrAttachmentObj = self.env['ir.attachment']
+        IrAttachmentObj = self.env["ir.attachment"]
         for record in self:
-            IrAttachmentObj.sudo().search([
-                ('url', 'like', '%s%%' % record._scss_get_url_simplified()),
-            ]).sudo().unlink()
+            IrAttachmentObj.sudo().search(
+                [("url", "like", "%s%%" % record._scss_get_url_simplified()),]
+            ).sudo().unlink()
         return result
 
     @api.multi
     def write(self, values):
-        if not self.env.context.get('ignore_company_color', False):
-            fields_to_check = ('color_navbar_bg',
-                               'color_navbar_bg_hover',
-                               'color_navbar_text')
-            if 'logo' in values:
-                if values['logo']:
-                    _r, _g, _b = image_to_rgb(convert_to_image(values['logo']))
+        if not self.env.context.get("ignore_company_color", False):
+            fields_to_check = (
+                "color_navbar_bg",
+                "color_navbar_bg_hover",
+                "color_navbar_text",
+            )
+            if "logo" in values:
+                if values["logo"]:
+                    _r, _g, _b = image_to_rgb(convert_to_image(values["logo"]))
                     # Make color 10% darker
                     _h, _l, _s = rgb_to_hls(_r, _g, _b)
                     _l = max(0, _l - 0.1)
@@ -87,11 +89,13 @@ class ResCompany(models.Model):
                     # Calc. optimal text color (b/w)
                     # Grayscale human vision perception (Rec. 709 values)
                     _a = 1 - (0.2126 * _r + 0.7152 * _g + 0.0722 * _b)
-                    values.update({
-                        'color_navbar_bg': n_rgb_to_hex(_r, _g, _b),
-                        'color_navbar_bg_hover': n_rgb_to_hex(_rd, _gd, _bd),
-                        'color_navbar_text': '#000' if _a < 0.5 else '#fff',
-                    })
+                    values.update(
+                        {
+                            "color_navbar_bg": n_rgb_to_hex(_r, _g, _b),
+                            "color_navbar_bg_hover": n_rgb_to_hex(_rd, _gd, _bd),
+                            "color_navbar_text": "#000" if _a < 0.5 else "#fff",
+                        }
+                    )
                 else:
                     values.update(self.default_get(fields_to_check))
 
@@ -110,14 +114,16 @@ class ResCompany(models.Model):
         # This allow extend company_colors and only sanitize selected fields
         # or add custom values
         values = dict(self.company_colors or {})
-        values.update({
-            'color_navbar_bg': (values.get('color_navbar_bg')
-                                or '$o-brand-odoo'),
-            'color_navbar_bg_hover': (
-                values.get('color_navbar_bg_hover')
-                or '$o-navbar-inverse-link-hover-bg'),
-            'color_navbar_text': (values.get('color_navbar_text') or '#FFF'),
-        })
+        values.update(
+            {
+                "color_navbar_bg": (values.get("color_navbar_bg") or "$o-brand-odoo"),
+                "color_navbar_bg_hover": (
+                    values.get("color_navbar_bg_hover")
+                    or "$o-navbar-inverse-link-hover-bg"
+                ),
+                "color_navbar_text": (values.get("color_navbar_text") or "#FFF"),
+            }
+        )
         return values
 
     @api.multi
@@ -132,42 +138,39 @@ class ResCompany(models.Model):
     # /web_company_color/static/src/scss/custom_colors.<company_id>
     def _scss_get_url_simplified(self):
         self.ensure_one()
-        NTEMPLATE = '.'.join(URL_SCSS_GEN_TEMPLATE.split('.')[:2])
+        NTEMPLATE = ".".join(URL_SCSS_GEN_TEMPLATE.split(".")[:2])
         return NTEMPLATE % self.id
 
     @api.multi
     def scss_get_url(self, timestamp=None):
         self.ensure_one()
-        return URL_SCSS_GEN_TEMPLATE % (self.id,
-                                        timestamp or self.scss_modif_timestamp)
+        return URL_SCSS_GEN_TEMPLATE % (self.id, timestamp or self.scss_modif_timestamp)
 
     @api.multi
     def scss_create_or_update_attachment(self):
-        IrAttachmentObj = self.env['ir.attachment']
+        IrAttachmentObj = self.env["ir.attachment"]
         # The time window is 1 second
         # This mean that all modifications realized in that second will
         # have the same timestamp
         modif_timestamp = str(int(time.time()))
         for record in self:
-            datas = base64.b64encode(
-                record._scss_generate_content().encode('utf-8'))
-            custom_attachment = IrAttachmentObj.sudo().search([
-                ('url', 'like', '%s%%' % record._scss_get_url_simplified())
-            ])
+            datas = base64.b64encode(record._scss_generate_content().encode("utf-8"))
+            custom_attachment = IrAttachmentObj.sudo().search(
+                [("url", "like", "%s%%" % record._scss_get_url_simplified())]
+            )
             custom_url = record.scss_get_url(timestamp=modif_timestamp)
             values = {
-                'datas': datas,
-                'url': custom_url,
-                'name': custom_url,
-                'datas_fname': custom_url.split("/")[-1],
+                "datas": datas,
+                "url": custom_url,
+                "name": custom_url,
+                "datas_fname": custom_url.split("/")[-1],
             }
             if custom_attachment:
                 custom_attachment.sudo().write(values)
             else:
-                values.update({
-                    'type': 'binary',
-                    'mimetype': 'text/scss',
-                })
+                values.update(
+                    {"type": "binary", "mimetype": "text/scss",}
+                )
                 IrAttachmentObj.sudo().create(values)
-        self.write({'scss_modif_timestamp': modif_timestamp})
-        self.env['ir.qweb'].sudo().clear_caches()
+        self.write({"scss_modif_timestamp": modif_timestamp})
+        self.env["ir.qweb"].sudo().clear_caches()
