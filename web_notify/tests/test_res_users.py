5315215a9a24af85fa3c8468e2e115d43527720b
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-03-23 02:39:25 +0100

    [MIG] web_timeline: Finish migration to 13.0

--- web_notify/tests/test_res_users.py
+++ web_notify/tests/test_res_users.py
@@ -2,8 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import json
-
-import mock
+from unittest import mock
 
 from odoo import exceptions
 from odoo.tests import common
