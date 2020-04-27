75b9541531726ef9a7ef3a56956ee68e57216df2
ahenriquez <ahenriquez@eficent.com>
2020-04-27 18:47:21 +0200

    [IMP] : black, isort, prettier

--- web_action_conditionable/__manifest__.py
+++ web_action_conditionable/__manifest__.py
@@ -1,14 +1,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": 'web_action_conditionable',
+    "name": "web_action_conditionable",
     "version": "12.0.1.0.0",
-    "depends": [
-        'base',
-        'web',
-    ],
-    'data': ['views/view.xml'],
+    "depends": ["base", "web",],
+    "data": ["views/view.xml"],
     "author": "Cristian Salamea,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
