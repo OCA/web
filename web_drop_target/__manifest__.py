97fa4c5a9343eb20872004f3704742316bac150c
Enric Tobella <etobella@creublanca.es>
2020-05-27 15:19:57 +0200

    [IMP][13.0] web_drop_target: Change less files for scss
    web_drop_target 13.0.1.1.0
    
    [UPD] README.rst
    
    [UPD] README.rst

--- web_drop_target/__manifest__.py
+++ web_drop_target/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Drop target support",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
