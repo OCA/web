2230526149154fa5ece9050b35ab8fc0768b5a4f
eLBati <lb@takobi.online>
2020-06-05 08:12:22 +0200

    Rename web_pwa -> web_pwa_oca

---
+++ web_pwa_oca/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright 2020 Lorenzo Battistini @ TAKOBI
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Progressive web application",
+    "summary": "Make Odoo a PWA",
+    "version": "12.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Website",
+    "website": "https://github.com/OCA/web",
+    "author": "TAKOBI, Odoo Community Association (OCA)",
+    "maintainers": ["eLBati"],
+    "license": "LGPL-3",
+    "application": True,
+    "installable": True,
+    "depends": [
+        'web',
+        'mail',
+    ],
+    "data": [
+        "views/webclient_templates.xml",
+    ],
+    'qweb': [
+        'static/src/xml/pwa_install.xml',
+    ],
+    'images': ['static/description/pwa.png'],
+}
