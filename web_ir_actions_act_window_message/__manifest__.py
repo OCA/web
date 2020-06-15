f8e5658566d8005f34e97a257d59997149d93d95
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2020-06-15 15:20:17 +0200

    [MIG] web_ir_actions_act_window_message

--- web_ir_actions_act_window_message/__manifest__.py
+++ web_ir_actions_act_window_message/__manifest__.py
@@ -2,20 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Client side message boxes",
-    "version": "12.0.1.0.0",
-    "author": "Therp BV, "
-              "ACSONE SA/NV, "
-              "Odoo Community Association (OCA)",
+    "version": "13.0.1.0.0",
+    "author": "Therp BV, " "ACSONE SA/NV, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Hidden/Dependency",
     "summary": "Show a message box to users",
-    "depends": [
-        'web',
-    ],
-    "data": [
-        'views/templates.xml',
-    ],
-    "qweb": [
-        'static/src/xml/web_ir_actions_act_window_message.xml',
-    ],
+    "depends": ["web"],
+    "data": ["views/templates.xml"],
+    "qweb": ["static/src/xml/web_ir_actions_act_window_message.xml"],
 }
