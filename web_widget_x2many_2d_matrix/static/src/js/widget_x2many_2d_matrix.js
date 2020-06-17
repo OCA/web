OCA/web

commit a5e6acc3a64626cf99b3d31f2f295be16f41b27b

[ADD] detect if used field type can be aggregated
fixes #1365

web_widget_x2many_2d_matrix/static/src/js/widget_x2many_2d_matrix.js
@@ -56,10 +56,23 @@ odoo.define('web_widget_x2many_2d_matrix.widget', function (require) {
                         node[property];
                 }
             }
-            this.show_row_totals =
-                this.parse_boolean(node.show_row_totals || '1');
-            this.show_column_totals =
-                this.parse_boolean(node.show_column_totals || '1');
+            var field_defs = this.recordData[this.name].fields;
+            // TODO: raise when any of the fields above don't exist with a
+            // helpful error message
+            if (!field_defs[this.field_value]) {
+                throw new Error(_.str.sprintf(
+                    'You need to include %s in your view definition',
+                    this.field_value
+                ));
+            }
+            this.show_row_totals = this.parse_boolean(
+                node.show_row_totals ||
+                this.is_aggregatable(field_defs[this.field_value]) ? '1' : ''
+            );
+            this.show_column_totals = this.parse_boolean(
+                node.show_column_totals ||
+                this.is_aggregatable(field_defs[this.field_value]) ? '1' : ''
+            );
         },
 
         /**
@@ -147,6 +160,13 @@ odoo.define('web_widget_x2many_2d_matrix.widget', function (require) {
                 row.data.push(self.by_y_axis[y][x]);
             });
             return row;
+        },
+
+        /**
+         * Determine if a field represented by field_def can be aggregated
+         */
+        is_aggregatable: function (field_def) {
+            return field_def.type in {float: 1, monetary: 1, integer: 1};
         },
 
         /**

commit 74559098b4a1dcd6d0bf62549dd998f15cd4fd24

web_widget_x2many_2d_matrix: Fix show_column_totals option
Commit a5e6acc3 introduced a bug, where show_column_totals="0" wouldn't work.
This is because JS ternary operator took node's attribute value as part of the condition.

As an example simplified JS code (before change):
`'0' || true ? '1' : ''` -> `'1'`
And this is what was probably meant originally (after change)
`'0' || (true ? '1' : '')` -> `'0'`

web_widget_x2many_2d_matrix/static/src/js/widget_x2many_2d_matrix.js
@@ -68,11 +68,11 @@ odoo.define('web_widget_x2many_2d_matrix.widget', function (require) {
             }
             this.show_row_totals = this.parse_boolean(
                 node.show_row_totals ||
-                this.is_aggregatable(field_defs[this.field_value]) ? '1' : ''
+                (this.is_aggregatable(field_defs[this.field_value]) ? '1' : '')
             );
             this.show_column_totals = this.parse_boolean(
                 node.show_column_totals ||
-                this.is_aggregatable(field_defs[this.field_value]) ? '1' : ''
+                (this.is_aggregatable(field_defs[this.field_value]) ? '1' : '')
             );
         },
 