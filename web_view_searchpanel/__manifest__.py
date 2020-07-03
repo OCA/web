f38540257349f612a7424a33b9747572bef6f7fb
Enric Tobella <etobella@creublanca.es>
2020-07-03 12:39:41 +0200

    [MIG] web_view_searchpanel: from muk_web to OCA

--- web_view_searchpanel/__manifest__.py
+++ web_view_searchpanel/__manifest__.py
@@ -1,54 +1,27 @@
-###################################################################################
-#
-#    Copyright (c) 2017-2019 MuK IT GmbH.
-#
-#    This file is part of MuK Search Panel 
-#    (see https://mukit.at).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-###################################################################################
+# Copyright 2017-2019 MuK IT GmbH.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'MuK Search Panel',
+    'name': 'Search Panel',
     'summary': 'Kanban Search Panel',
-    'version': '12.0.2.0.1',
+    'version': '12.0.1.0.0',
     'category': 'Extra Tools',
     'license': 'LGPL-3',
-    'author': 'MuK IT',
-    'website': 'https://www.mukit.at',
-    'live_test_url': 'https://mukit.at/r/SgN',
-    'contributors': [
-        'Mathias Markl <mathias.markl@mukit.at>',
-    ],
+    'author': 'MuK IT,  Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/web',
     'depends': [
-        'muk_web_utils',
+        'web',
     ],
     'data': [
-        "template/assets.xml",
+        'template/assets.xml',
     ],
     'qweb': [
-        'static/src/xml/*.xml',
+        'static/src/xml/kanban.xml',
     ],
     'images': [
         'static/description/banner.png'
     ],
-    'external_dependencies': {
-        'python': [],
-        'bin': [],
-    },
-    'application': False,
-    'installable': True,
-    'auto_install': False,
+    'demo': [
+        'demo/res_users_views.xml',
+    ]
 }
---
+++ web_view_searchpanel/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+With Odoo version 13 a new feature is added which allows kanban views to be
+extended by a search panel. This can be defined via XML and is then automatically
+added to the view. With this module the function is ported back to version 12.
