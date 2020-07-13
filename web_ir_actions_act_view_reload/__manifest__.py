f826b96d9196c377b37fa8fb1a292d9f00a2a6e4
KENLYLST <9990448+KENLYLST@users.noreply.github.com>
2020-07-13 07:54:53 +0200

    web_ir_actions_act_view_reload: fix README (#1633)
    Unresolved reference self and module odoo.api has no attribute multi
    
    [UPD] README.rst
    
    [UPD] Brainbean Apps => CorporateHub
    
    [UPD] README.rst

--- web_ir_actions_act_view_reload/__manifest__.py
+++ web_ir_actions_act_view_reload/__manifest__.py
@@ -1,5 +1,6 @@
 # Copyright 2017 - 2018 Modoolar <info@modoolar.com>
 # Copyright 2018 Brainbean Apps
+# Copyright 2020 CorporateHub (https://corporatehub.eu)
 # License LGPLv3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
 
 {
@@ -8,7 +9,7 @@
     "category": "Web",
     "version": "13.0.1.0.0",
     "license": "LGPL-3",
-    "author": "Modoolar, Brainbean Apps, Odoo Community Association (OCA)",
+    "author": "Modoolar, CorporateHub, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web/",
     "depends": ["web"],
     "data": ["views/web_ir_actions_act_view_reload.xml"],
