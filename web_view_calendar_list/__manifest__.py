232f4f4fe4c9a15b250621bbaee728cebdff75b6
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-07-14 11:42:07 +0200

    [12.0][MIG] web_view_calendar_list

--- web_view_calendar_list/__manifest__.py
+++ web_view_calendar_list/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Web View Calendar List',
     'summary': """
         Show calendars as a List""",
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Creu Blanca,Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/web',
