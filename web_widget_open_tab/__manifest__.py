95e946a12101311b5c73cb367857a7b92ba77769
Raf Ven <raf.ven@dynapps.be>
2020-09-29 13:42:50 +0200

    [IMP] web_widget_open_tab: black, isort

--- web_widget_open_tab/__manifest__.py
+++ web_widget_open_tab/__manifest__.py
@@ -2,14 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Widget Open on new Tab',
-    'summary': """
+    "name": "Widget Open on new Tab",
+    "summary": """
         Allow to open record from trees on new tab from tree views""",
-    'version': '12.0.1.0.1',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/web',
-    'depends': ['web'],
-    'data': ['templates/assets.xml'],
-    'demo': ['demo/res_users_view.xml'],
+    "version": "12.0.1.0.1",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
+    "depends": ["web"],
+    "data": ["templates/assets.xml"],
+    "demo": ["demo/res_users_view.xml"],
 }
