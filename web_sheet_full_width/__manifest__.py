63174ec3bf7f6d0c96a8c8f5eaf7a058aaca48e4
Lois Rilo <lois.rilo@forgeflow.com>
2020-10-06 13:47:20 +0200

    [13.0][MIG] web_sheet_full_width: * less -> scss * fragment readme + add a pointer to web_responsive

--- web_sheet_full_width/__manifest__.py
+++ web_sheet_full_width/__manifest__.py
@@ -1,9 +1,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Show sheets with full width",
+    "name": "Web Sheet Full Width",
     "version": "13.0.1.0.0",
     "author": "Therp BV, Sudokeys, GRAP, Métal Sartigan, "
     "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
     "summary": "Use the whole available screen width when displaying sheets",
     "category": "Tools",
