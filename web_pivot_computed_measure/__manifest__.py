19adc598d5ea9e3add152439c88e163b512463fd
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-10-09 09:58:57 -0400

    [IMP] web_pivot_computed_measure: black, isort, prettier

--- web_pivot_computed_measure/__manifest__.py
+++ web_pivot_computed_measure/__manifest__.py
@@ -2,20 +2,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
-    'name': "Web Pivot Computed Measure",
-    'category': "web",
-    'version': "12.0.1.0.2",
-    'author': "Tecnativa, "
-              "Odoo Community Association (OCA)",
-    'license': 'AGPL-3',
-    'website': 'https://github.com/OCA/web',
-    'depends': ['web'],
-    'data': [
-        'view/assets.xml'
-    ],
-    'qweb': [
-        'static/src/xml/web_pivot_computed_measure.xml',
-    ],
-    'auto_install': False,
-    'installable': True,
+    "name": "Web Pivot Computed Measure",
+    "category": "web",
+    "version": "12.0.1.0.2",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/web",
+    "depends": ["web"],
+    "data": ["view/assets.xml"],
+    "qweb": ["static/src/xml/web_pivot_computed_measure.xml",],
+    "auto_install": False,
+    "installable": True,
 }
