325f55c9ed6470fbb3954d924835c4068f146098
Ivàn Todorovich <ivan.todorovich@gmail.com>
2020-10-13 10:40:43 -0300

    [ADD] web_calendar_color_field

---
+++ web_calendar_color_field/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2019 Iván Todorovich
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Calendar Color Field",
+    "version": "14.0.1.0.0",
+    "category": "Web",
+    "website": "https://github.com/OCA/web",
+    "author": "Iván Todorovich, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "views/assets.xml",
+    ],
+}
