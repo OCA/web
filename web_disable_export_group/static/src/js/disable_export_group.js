1afdc79e56cbd18a077ffc54c2e1db59753ea1d8
João Marques <joao.marques@tecnativa.com>
2020-10-20 08:53:33 +0100

    [IMP] web_disable_export_group: black, isort, prettier

--- web_disable_export_group/static/src/js/disable_export_group.js
+++ web_disable_export_group/static/src/js/disable_export_group.js
@@ -2,7 +2,7 @@
    Copyright 2018 Tecnativa - David Vidal
    License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl). */
 odoo.define("web_disable_export_group", function(require) {
-"use strict";
+    "use strict";
 
     var core = require("web.core");
     var Sidebar = require("web.Sidebar");
@@ -10,10 +10,15 @@ odoo.define("web_disable_export_group", function(require) {
     var _t = core._t;
 
     Sidebar.include({
-        _addItems: function (sectionCode, items) {
+        _addItems: function(sectionCode, items) {
             var _items = items;
-            if (!session.is_superuser && sectionCode === 'other' && items.length && !session.group_export_data) {
-                _items = _.reject(_items, {label:_t("Export")});
+            if (
+                !session.is_superuser &&
+                sectionCode === "other" &&
+                items.length &&
+                !session.group_export_data
+            ) {
+                _items = _.reject(_items, {label: _t("Export")});
             }
             this._super(sectionCode, _items);
         },
