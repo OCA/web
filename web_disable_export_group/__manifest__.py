e6f34839ebd86b8f0ba52c0f75fbc6b09b622e47
João Marques <joao.marques@tecnativa.com>
2020-10-20 08:53:33 +0100

    [IMP] web_disable_export_group: black, isort, prettier

--- web_disable_export_group/__manifest__.py
+++ web_disable_export_group/__manifest__.py
@@ -2,20 +2,13 @@
 # Copyright 2018 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Web Disable Export Group',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Onestein, '
-              'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/web',
-    'category': 'Web',
-    'depends': [
-        'web',
-    ],
-    'data': [
-        'security/groups.xml',
-        'templates/assets.xml',
-    ],
-    'installable': True,
+    "name": "Web Disable Export Group",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Onestein, " "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
+    "category": "Web",
+    "depends": ["web",],
+    "data": ["security/groups.xml", "templates/assets.xml",],
+    "installable": True,
 }
