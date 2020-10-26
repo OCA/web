c7940715da797fbf6f25b26620bd84be4457b355
dsolanki <dhara.solanki@initos.com>
2020-10-26 16:54:42 +0530

    [MIG] Migrate the module web_m2x_options to v14.

--- web_m2x_options/__manifest__.py
+++ web_m2x_options/__manifest__.py
@@ -1,13 +1,15 @@
 # Copyright 2015 0k.io
 # Copyright 2016 ACSONE SA/NV
 # Copyright 2017 Tecnativa
+# Copyright 2020 initOS GmbH.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "web_m2x_options",
-    "version": "13.0.1.0.2",
+    "version": "14.0.1.0.0",
     "category": "Web",
-    "author": "ACSONE SA/NV, "
+    "author": "initOS GmbH,"
+    "ACSONE SA/NV, "
     "0k.io, "
     "Tecnativa, "
     "Odoo Community Association (OCA)",
