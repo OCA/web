db76a5d51b15a3b2b8bd5d6825f8afcff210e0bc
Carlos Roca <carlos.roca@tecnativa.com>
2020-11-02 11:56:55 +0100

    [IMP] web_export_view: black, isort, prettier

--- web_export_view/controllers/controllers.py
+++ web_export_view/controllers/controllers.py
@@ -5,30 +5,34 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import json
+
 import odoo.http as http
 from odoo.http import request
+
 from odoo.addons.web.controllers.main import ExcelExport
 
 
 class ExcelExportView(ExcelExport):
     def __getattribute__(self, name):
-        if name == 'fmt':
+        if name == "fmt":
             raise AttributeError()
         return super(ExcelExportView, self).__getattribute__(name)
 
-    @http.route('/web/export/xls_view', type='http', auth='user')
+    @http.route("/web/export/xls_view", type="http", auth="user")
     def export_xls_view(self, data, token):
         data = json.loads(data)
-        model = data.get('model', [])
-        columns_headers = data.get('headers', [])
-        rows = data.get('rows', [])
+        model = data.get("model", [])
+        columns_headers = data.get("headers", [])
+        rows = data.get("rows", [])
 
         return request.make_response(
             self.from_data(columns_headers, rows),
             headers=[
-                ('Content-Disposition', 'attachment; filename="%s"'
-                 % self.filename(model)),
-                ('Content-Type', self.content_type)
+                (
+                    "Content-Disposition",
+                    'attachment; filename="%s"' % self.filename(model),
+                ),
+                ("Content-Type", self.content_type),
             ],
-            cookies={'fileToken': token}
+            cookies={"fileToken": token},
         )
