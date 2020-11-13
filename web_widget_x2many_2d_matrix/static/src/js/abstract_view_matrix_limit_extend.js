b5fa0cb4f3b9e2dcbc4a908f5b950108dda2aa5d
Saran440 <saranl@ecosoft.co.th>
2020-11-13 10:26:26 +0700

    [IMP] web_widget_x2many_2d_matrix: black, isort, prettier

--- web_widget_x2many_2d_matrix/static/src/js/abstract_view_matrix_limit_extend.js
+++ web_widget_x2many_2d_matrix/static/src/js/abstract_view_matrix_limit_extend.js
@@ -1,4 +1,4 @@
-odoo.define("web_widget_x2many_2d_matrix.matrix_limit_extend", function(require) {
+odoo.define("web_widget_x2many_2d_matrix.matrix_limit_extend", function (require) {
     "use strict";
 
     var FormView = require("web.FormView");
@@ -6,7 +6,7 @@ odoo.define("web_widget_x2many_2d_matrix.matrix_limit_extend", function(require)
     FormView.include({
         // We extend this method so that the view is not limited to
         // just 40 cells when the 'x2many_2d_matrix' widget is used.
-        _setSubViewLimit: function(attrs) {
+        _setSubViewLimit: function (attrs) {
             this._super(attrs);
             if (attrs.widget === "x2many_2d_matrix") {
                 attrs.limit = Infinity;
