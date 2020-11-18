f273ac74598056eb3e13a1d3ff1f45a7da65041c
Ronald Portier <ronald@therp.nl>
2020-11-18 15:45:48 +0100

    [MIG] web_domain_field. Migration from 11.0 to 12.0

--- web_domain_field/__manifest__.py
+++ web_domain_field/__manifest__.py
@@ -1,14 +1,13 @@
 # Copyright 2017 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Web Domain Field',
-    'summary': """
-        Use computed field as domain""",
-    'version': '11.0.1.0.0',
+    'summary': "Use computed field as domain",
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://acsone.eu/',
+    'website': 'https://github.com/OCA/web',
     'depends': [
         'web'
     ],
