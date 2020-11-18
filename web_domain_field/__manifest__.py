b2ee0ad3923bad62c4e09a42f78a923f9906e7f7
Ronald Portier <ronald@therp.nl>
2020-11-18 16:08:40 +0100

    [MIG] web_domain_field. Blacked to ease backports.

--- web_domain_field/__manifest__.py
+++ web_domain_field/__manifest__.py
@@ -2,16 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Web Domain Field',
-    'summary': "Use computed field as domain",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/web',
-    'depends': [
-        'web'
-    ],
-    'data': [
-        'views/web_domain_field.xml',
+    "name": "Web Domain Field",
+    "summary": "Use computed field as domain",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
+    "depends": ["web"],
+    "data": [
+        "views/web_domain_field.xml",
     ],
 }
