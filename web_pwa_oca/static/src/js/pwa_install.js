7bf04cd9bf4b5ad3a408f07cfc771e199412c6fe
Tom Blauwendraat <tom@sunflowerweb.nl>
2020-11-25 17:02:30 +0100

    fixup! [WIP][BCK][10.0] web_pwa_oca

--- web_pwa_oca/static/src/js/pwa_install.js
+++ web_pwa_oca/static/src/js/pwa_install.js
@@ -5,6 +5,8 @@ var core = require('web.core');
 var session = require('web.session');
 var UserMenu = require('web.UserMenu');
 
+var deferredInstallPrompt = null;
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker.register('/service-worker.js')
@@ -12,26 +14,37 @@ if ('serviceWorker' in navigator) {
           console.log('Service worker registered.', reg);
         });
   });
+  window.addEventListener(
+      'beforeinstallprompt',
+      function(event) {
+          deferredInstallPrompt = event;
+      },
+      false
+  );
 }
 
-var deferredInstallPrompt = null;
+function setButtonClass(className) {
+    var install_button = document.getElementById("pwa_install_button");
+    if (install_button) {
+        install_button.className = className;
+    }
+}
 
 UserMenu.include({
 
-    start: function () {
-        window.addEventListener('beforeinstallprompt', this.saveBeforeInstallPromptEvent, false);
-        return this._super.apply(this, arguments);
-    },
-
-   saveBeforeInstallPromptEvent: function(event) {
-        deferredInstallPrompt = event;
+    start: function() {
+        var ret = this._super();
+        if (deferredInstallPrompt) {
+            setButtonClass('');
+        }
+        return ret;
     },
 
     on_menu_installpwa: function () {
-
-        deferredInstallPrompt.prompt();
         // Hide the install button, it can't be called twice.
-        this.$('a[data-menu="installpwa"]').addClass('hidden');
+        setButtonClass('hide');
+        // Show the prompt
+        deferredInstallPrompt.prompt();
         // Log user response to prompt.
         deferredInstallPrompt.userChoice
             .then(function (choice) {
