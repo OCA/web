76d239299b65159eb1789b3733e5e8381c8238b6
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-12-13 13:19:54 -0500

    [IMP] web_widget_numeric_step: better visualization in list view cells

--- web_widget_numeric_step/__manifest__.py
+++ web_widget_numeric_step/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': "Web Widget Numeric Step",
     'category': "web",
-    'version': "12.0.1.1.3",
+    'version': "12.0.1.1.4",
     'author': "GRAP, Tecnativa, "
               "Odoo Community Association (OCA)",
     'license': 'AGPL-3',
