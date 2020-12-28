575daf2e68abaaa8f4cf984c5a71aadfa4115167
Alexandre D. Díaz <alexandre.diaz@tecnativa.com>
2020-12-28 19:28:28 +0100

    [ADD] web_widget_many2one_simple

---
+++ web_widget_many2one_simple/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Tecnativa - Alexandre D. Díaz
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Simple many2one widget",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
+    "depends": ["web"],
+    "data": ["views/assets.xml"],
+    "qweb": ["static/src/xml/many2one_simple.xml"],
+    "installable": True,
+    "maintainers": ["Tardo"],
+}
---
+++ web_widget_many2one_simple/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* `Tecnativa <https://www.tecnativa.com>`_:
+
+  * Alexandre D. Díaz
