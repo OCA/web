8189cda2056789e87b7296c0acf7ecadb14a2a59
Robin Conjour <robin@conjour.nl>
2021-01-04 17:13:35 +0100

    [MIG] web_copy_confirm: Migration to 14.0

--- web_copy_confirm/__manifest__.py
+++ web_copy_confirm/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Show confirmation dialogue before copying records",
-    "version": "8.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Dynapps,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
