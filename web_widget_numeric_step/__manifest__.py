973fd6dde2abcbd0f6c6b578294e53c46e40d9a0
Carlos Roca <carlos.roca@tecnativa.com>
2021-01-25 08:57:03 +0100

    [IMP] web_widget_numeric_step: black, isort, prettier

--- web_widget_numeric_step/__manifest__.py
+++ web_widget_numeric_step/__manifest__.py
@@ -3,23 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
-    'name': "Web Widget Numeric Step",
-    'category': "web",
-    'version': "12.0.1.2.0",
-    'author': "GRAP, Tecnativa, "
-              "Odoo Community Association (OCA)",
-    'license': 'AGPL-3',
-    'website': 'https://github.com/OCA/web',
-    'depends': ['web'],
-    'data': [
-        'view/assets.xml'
-    ],
-    'qweb': [
-        'static/src/xml/numeric_step.xml',
-    ],
-    'demo': [
-        'demo/res_users_view.xml'
-    ],
-    'auto_install': False,
-    'installable': True,
+    "name": "Web Widget Numeric Step",
+    "category": "web",
+    "version": "12.0.1.2.0",
+    "author": "GRAP, Tecnativa, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/web",
+    "depends": ["web"],
+    "data": ["view/assets.xml"],
+    "qweb": ["static/src/xml/numeric_step.xml",],
+    "demo": ["demo/res_users_view.xml"],
+    "auto_install": False,
+    "installable": True,
 }
