65182c84f451d129f0429e52a00cfd575d98df9c
bastian.guenther@ametras.com <Gaertner17!>
2021-02-03 15:22:11 +0100

    [IMP] web_ir_actions_close_wizard_refresh_view: black, isort, prettier

--- web_ir_actions_close_wizard_refresh_view/static/src/js/web_ir_actions_close_wizard_refresh_view.js
+++ web_ir_actions_close_wizard_refresh_view/static/src/js/web_ir_actions_close_wizard_refresh_view.js
@@ -1,21 +1,20 @@
 /* Copyright 2019 LevelPrime
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
 
-odoo.define('web_ir_actions_close_wizard_refresh_view', function (require) {
-    'use strict';
+odoo.define("web_ir_actions_close_wizard_refresh_view", function(require) {
+    "use strict";
 
-    require('web.ActionManager').include({
-        _handleAction: function (action, options) {
-            if (action.type === 'ir.actions.close_wizard_refresh_view') {
+    require("web.ActionManager").include({
+        _handleAction: function(action, options) {
+            if (action.type === "ir.actions.close_wizard_refresh_view") {
                 return this._executeCloseWizardRefreshViewAction();
             }
             return this._super(action, options);
         },
-        _executeCloseWizardRefreshViewAction: function () {
+        _executeCloseWizardRefreshViewAction: function() {
             this._closeDialog();
 
-            var state = this._getControllerState(
-                this.getCurrentController().jsID);
+            var state = this._getControllerState(this.getCurrentController().jsID);
 
             return $.when(this.loadState(state));
         },
