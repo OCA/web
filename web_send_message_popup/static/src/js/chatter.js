f92cfee2c89534e873220ccc3e64a85263976108
Stephane Mangin <stephane.mangin@camptocamp.com>
2021-02-04 12:33:08 +0100

    [MIG] web_send_message_popup: Migration to 13.0

--- web_send_message_popup/static/src/js/chatter.js
+++ web_send_message_popup/static/src/js/chatter.js
@@ -1,4 +1,4 @@
-/* Copyright 2019 Camptocamp SA
+/* Copyright 2019-2021 Camptocamp SA
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
 odoo.define("web_send_message_popup.Chatter", function(require) {
     "use strict";
@@ -8,12 +8,10 @@ odoo.define("web_send_message_popup.Chatter", function(require) {
     Chatter.include({
         _onOpenComposerMessage: function() {
             this._super.apply(this, arguments);
-            this.suggested_partners_def.done(
-                $.proxy(function() {
-                    this._closeComposer(true);
-                    this._composer._onOpenFullComposer();
-                }, this)
-            );
+            var self = this;
+            this._suggestedPartnersProm.then(function() {
+                self._composer._onOpenFullComposer();
+            });
         },
     });
 });
