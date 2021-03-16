23ff1a737ef363db8802fc6521bffb46cdaa2b87
SimoRubi <simone.rubino@agilebg.com>
2021-03-16 18:01:33 +0100

    [MIG] web_sheet_full_width: Migration to 14.0

--- web_sheet_full_width/__manifest__.py
+++ web_sheet_full_width/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Web Sheet Full Width",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Therp BV, Sudokeys, GRAP, Métal Sartigan, "
     "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
