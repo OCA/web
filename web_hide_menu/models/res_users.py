2b0e530cff7519581f962f48eeb9996cd8a16013
AmamrOSI <aofficewala@opensourceintegrators.com>
2021-04-05 19:38:33 +0530

    [ADD] Added a web Hide menu.

---
+++ web_hide_menu/models/res_users.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2021 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    hide_menu_ids = fields.One2many("hide.menu", "user_id", string="Hide Menu")
