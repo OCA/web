0f1781b20cdd0e8386ea11ce7aae03fc5fd50f58
João Marques <joao.marques@tecnativa.com>
2021-04-12 11:33:59 +0100

    [MIG] web_refresher: Migration to 13.0
    TT28809

--- web_refresher/__manifest__.py
+++ web_refresher/__manifest__.py
@@ -1,8 +1,6 @@
-# pylint: disable=C8101
-
 {
     "name": "Web Refresher",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Compassion Switzerland, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/web",
