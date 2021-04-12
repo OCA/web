a9192d2c7a5e38758fdcb9d1e6687ed7ced15e5e
João Marques <joao.marques@tecnativa.com>
2021-04-12 11:08:24 +0100

    [IMP] web_refresher: black, isort, prettier

--- web_refresher/static/src/js/refresher.js
+++ web_refresher/static/src/js/refresher.js
@@ -1,25 +1,25 @@
-// initial pager is located in source/addons/web/static/src/js/chrome/pager.js
+// Initial pager is located in source/addons/web/static/src/js/chrome/pager.js
 
-odoo.define('refresher.pager', function(require) {
-    'use strict';
+odoo.define("refresher.pager", function(require) {
+    "use strict";
 
-    var pager = require('web.Pager');
+    var pager = require("web.Pager");
     pager.include({
         start: function() {
             var self = this;
             var res = self._super();
 
-            var $button = $('<span>', {
+            var $button = $("<span>", {
                 class: "fa fa-refresh btn btn-icon o_pager_refresh",
                 css: {"margin-right": "8px"},
-                "aria-label": "Refresh"
+                "aria-label": "Refresh",
             });
-            $button.on('click', function(){
+            $button.on("click", function() {
                 self._changeSelection(0);
             });
 
             self.$el.prepend($button);
             return res;
-        }
+        },
     });
 });
