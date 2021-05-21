5ad2cf1d43fd0b375a0f6c18ef8a9d18bf15a9f8
Alexandre D. Díaz <alexandre.diaz@tecnativa.com>
2021-05-21 14:37:09 +0200

    [FIX] web_widget_one2many_product_picker_sale_secondary_unit: Secondary unit management

--- web_widget_one2many_product_picker_sale_secondary_unit/static/src/js/views/One2ManyProductPicker/renderer.js
+++ web_widget_one2many_product_picker_sale_secondary_unit/static/src/js/views/One2ManyProductPicker/renderer.js
@@ -11,9 +11,11 @@ odoo.define(
             _isEqualState: function(state_a, state_b) {
                 const res = this._super.apply(this, arguments);
                 const secondary_uom_id_a =
-                    state_a.data.secondary_uom_id && state_a.data.secondary_uom_id.data.id;
+                    state_a.data.secondary_uom_id &&
+                    state_a.data.secondary_uom_id.data.id;
                 const secondary_uom_id_b =
-                    state_b.data.secondary_uom_id && state_b.data.secondary_uom_id.data.id;
+                    state_b.data.secondary_uom_id &&
+                    state_b.data.secondary_uom_id.data.id;
                 return res && secondary_uom_id_a === secondary_uom_id_b;
             },
         });
