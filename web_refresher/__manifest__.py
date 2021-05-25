a841bd47d612bbf808aaf605e6711a900d4660bc
João Marques <joao.marques@tecnativa.com>
2021-05-25 07:57:28 +0100

    [MIG] web_refresher: Migration to 14.0

--- web_refresher/__manifest__.py
+++ web_refresher/__manifest__.py
@@ -1,10 +1,10 @@
 {
     "name": "Web Refresher",
-    "version": "13.0.1.0.0",
-    "author": "Compassion Switzerland, " "Odoo Community Association (OCA)",
+    "version": "14.0.1.0.0",
+    "author": "Compassion Switzerland, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/web",
-    "data": ["templates/assets.xml"],
+    "qweb": ["templates/pager_button.xml"],
     "depends": ["web"],
     "installable": True,
     "auto_install": False,
