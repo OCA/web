02e7b9bb4754fc5b4a95a13039901bf3dde01eb3
João Marques <joao.marques@tecnativa.com>
2021-05-25 06:50:23 +0100

    [IMP] web_refresher: black, isort, prettier

--- web_refresher/static/src/js/refresher.js
+++ web_refresher/static/src/js/refresher.js
@@ -1,11 +1,11 @@
 // Initial pager is located in source/addons/web/static/src/js/chrome/pager.js
 
-odoo.define("refresher.pager", function(require) {
+odoo.define("refresher.pager", function (require) {
     "use strict";
 
     var pager = require("web.Pager");
     pager.include({
-        start: function() {
+        start: function () {
             var self = this;
             var res = self._super();
 
@@ -14,7 +14,7 @@ odoo.define("refresher.pager", function(require) {
                 css: {"margin-right": "8px"},
                 "aria-label": "Refresh",
             });
-            $button.on("click", function() {
+            $button.on("click", function () {
                 self._changeSelection(0);
             });
 
