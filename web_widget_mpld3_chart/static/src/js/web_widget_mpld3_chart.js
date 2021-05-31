04b78d8e26f79ff9042d23d2ff118a1cb101129d
Genís Ponce <genis.ponce@forgeflow.com>
2021-05-31 09:58:56 +0200

    [IMP] web_widget_mpld3_chart: black, isort, prettier

--- web_widget_mpld3_chart/static/src/js/web_widget_mpld3_chart.js
+++ web_widget_mpld3_chart/static/src/js/web_widget_mpld3_chart.js
@@ -1,11 +1,11 @@
-odoo.define("web_widget_mpld3_chart", function(require) {
+odoo.define("web_widget_mpld3_chart", function (require) {
     "use strict";
 
     var fieldRegistry = require("web.field_registry");
     var AbstractField = require("web.AbstractField");
 
     var Mpld3ChartWidget = AbstractField.extend({
-        start: function() {
+        start: function () {
             var val = this.value;
             this.$el.html(val);
         },
