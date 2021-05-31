ea6e7350fca7a244042c1ae7a89080846fa8821c
Genís Ponce <genis.ponce@forgeflow.com>
2021-05-31 10:56:26 +0200

    [MIG] web_widget_mpld3_chart: Migration to 14.0

--- web_widget_mpld3_chart/__manifest__.py
+++ web_widget_mpld3_chart/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "Hidden",
     "summary": "This widget allows to display charts using MPLD3 library.",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "website": "https://github.com/OCA/web",
     "depends": ["web"],
     "data": ["views/web_widget_mpld3_chart.xml"],
