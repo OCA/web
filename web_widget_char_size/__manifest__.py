2ab92a6f58e4d26efc406fbf57e40a691010738a
SimoRubi <simone.rubino@agilebg.com>
2021-06-10 11:13:33 +0200

    [ADD] web_widget_char_size

---
+++ web_widget_char_size/__manifest__.py
@@ -0,0 +1,17 @@
+#  Copyright 2021 Simone Rubino - Agile Business Group
+#  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Widget Char size",
+    "summary": "Add size option to Char widget",
+    "version": "14.0.1.0.0",
+    "category": "Hidden",
+    "website": "https://github.com/OCA/web",
+    "author": "Agile Business Group, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "views/assets.xml",
+    ],
+}
