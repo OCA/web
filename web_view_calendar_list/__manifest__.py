0a77dc4a812a481ac1cdbd228875c363374750c9
Enric Tobella <etobella@creublanca.es>
2021-06-21 16:09:56 +0200

    [MIG] web_view_calendar_list: Migration to 13.0

--- web_view_calendar_list/__manifest__.py
+++ web_view_calendar_list/__manifest__.py
@@ -5,10 +5,10 @@
     "name": "Web View Calendar List",
     "summary": """
         Show calendars as a List""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
-    "depends": ["web",],
-    "data": ["templates/assets.xml",],
+    "depends": ["web"],
+    "data": ["templates/assets.xml"],
 }
