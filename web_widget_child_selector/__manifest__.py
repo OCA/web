41da8d2114bea9271fe8a27ec2afe8104a60847b
Enric Tobella <etobella@creublanca.es>
2021-06-21 16:41:50 +0200

    [MIG] web_widget_child_selector: Migration to 13.0

--- web_widget_child_selector/__manifest__.py
+++ web_widget_child_selector/__manifest__.py
@@ -4,11 +4,11 @@
 {
     "name": "Web Widget Child Selector",
     "summary": "Widget used for navigation on hierarchy fields",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
-    "depends": ["web",],
-    "data": ["views/assets.xml",],
-    "qweb": ["static/src/xml/widget_child_selector.xml",],
+    "depends": ["web"],
+    "data": ["views/assets.xml"],
+    "qweb": ["static/src/xml/widget_child_selector.xml"],
 }
