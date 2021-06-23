a74dd7ab54f7c84e1cbb2bf3a98414a84355def3
KKamaa <kevkamaa96@gmail.com>
2021-06-24 00:57:17 +0300

    [FIX] pep8 clean up

--- web_widget_text_markdown/static/src/js/web_widget_text_markdown.js
+++ web_widget_text_markdown/static/src/js/web_widget_text_markdown.js
@@ -3,80 +3,85 @@
  * Copyright 2017 Komit - <http:///komit-consulting.com>
  * Copyright 2019 Alexandre Díaz - <dev@redneboa.es>
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
-odoo.define("web_widget_text_markdown.FieldTextMarkDown", function (require) {
-    'use strict';
+odoo.define("web_widget_text_markdown.FieldTextMarkDown", function(require) {
+    "use strict";
 
-    var basic_fields = require('web.basic_fields');
-    var field_registry = require('web.field_registry');
-    var core = require('web.core');
+    var basic_fields = require("web.basic_fields");
+    var field_registry = require("web.field_registry");
+    var core = require("web.core");
 
     var _t = core._t;
-    var LIBS_PATH = '/web_widget_text_markdown/static/src/lib/';
+    var LIBS_PATH = "/web_widget_text_markdown/static/src/lib/";
 
     var FieldTextMarkDown = basic_fields.FieldText.extend({
-        className: [basic_fields.FieldText.prototype.className,
-            'o_field_text_markdown',].join(' '),
-        jsLibs: [
-            LIBS_PATH + 'marked.js',
-            LIBS_PATH + 'bootstrap-markdown.js',
-        ],
-        cssLibs: [
-            LIBS_PATH + 'bootstrap-markdown.min.css',
-        ],
+        className: [
+            basic_fields.FieldText.prototype.className,
+            "o_field_text_markdown",
+        ].join(" "),
+        jsLibs: [LIBS_PATH + "marked.js", LIBS_PATH + "bootstrap-markdown.js"],
+        cssLibs: [LIBS_PATH + "bootstrap-markdown.min.css"],
 
-        _getValue: function () {
+        _getValue: function() {
             return this.$markdown.getContent();
         },
 
-        _prepareInput: function () {
+        _prepareInput: function() {
             var $input = this._super.apply(this, arguments);
-            _.defer(function ($elm) {
-                $input.removeClass(this.className);
-                $input.wrap(
-                    _.str.sprintf("<div class='%s'></div>", this.className));
-                $elm.markdown(this._getMarkdownOptions());
-                this.$markdown = $elm.data("markdown");
-                this.$markdown.setContent(this.value || "");
-            }.bind(this), $input);
+            _.defer(
+                function($elm) {
+                    $input.removeClass(this.className);
+                    $input.wrap(
+                        _.str.sprintf("<div class='%s'></div>", this.className)
+                    );
+                    $elm.markdown(this._getMarkdownOptions());
+                    this.$markdown = $elm.data("markdown");
+                    this.$markdown.setContent(this.value || "");
+                }.bind(this),
+                $input
+            );
             return $input;
         },
 
-        _renderReadonly: function () {
+        _renderReadonly: function() {
             this.$el.html(marked(this._formatValue(this.value)));
         },
 
-        _getMarkdownOptions: function () {
+        _getMarkdownOptions: function() {
             var markdownOpts = {
-                iconlibrary: 'fa',
+                iconlibrary: "fa",
                 autofocus: false,
-                width: 'o_field_text_markdown',
+                width: "o_field_text_markdown",
                 savable: false,
                 language: this.getSession().user_context.lang,
             };
 
             if (_t.database.multi_lang && this.field.translate) {
                 markdownOpts.additionalButtons = [
-                    [{
-                    name: 'oTranslate',
-                        data: [{
-                        name: 'cmdTranslate',
-                            title: _t('Translate'),
-                            icon: {fa: 'fa fa-flag'},
-                            callback: this._markdownTranslate.bind(self),
-                        }],
-                    }],
+                    [
+                        {
+                            name: "oTranslate",
+                            data: [
+                                {
+                                    name: "cmdTranslate",
+                                    title: _t("Translate"),
+                                    icon: {fa: "fa fa-flag"},
+                                    callback: this._markdownTranslate.bind(self),
+                                },
+                            ],
+                        },
+                    ],
                 ];
             }
 
             return markdownOpts;
         },
 
-        _markdownTranslate: function () {
-            // event is the click event from callback
+        _markdownTranslate: function() {
+            // Event is the click event from callback
             this._onTranslate(event);
         },
     });
 
-    field_registry.add('bootstrap_markdown', FieldTextMarkDown);
+    field_registry.add("bootstrap_markdown", FieldTextMarkDown);
     return FieldTextMarkDown;
 });
