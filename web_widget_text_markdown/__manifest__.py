dc5404c9cd7fecc1b008f45b55cc6216d41039ec
Tom Blauwendraat <tom@sunflowerweb.nl>
2021-06-26 09:27:23 +0200

    [UPD] black, isort, prettier

--- web_widget_text_markdown/__manifest__.py
+++ web_widget_text_markdown/__manifest__.py
@@ -3,29 +3,21 @@
 #
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Web Widget Text Markdown',
-    'version': '11.0.1.0.0',
+    "name": "Web Widget Text Markdown",
+    "version": "11.0.1.0.0",
     "author": "Alexandre Díaz, "
-              "Komit, "
-              "Sudokeys, "
-              "Odoo Community Association (OCA)",
-    'category': 'Web',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/OCA/web',
-    'summary': 'Widget to text fields that adds markdown support',
-    'depends': [
-        'web'
-    ],
-    'demo': [
-        "demo/bootstrap_markdown.xml",
-    ],
-    'data': [
-        'views/assets.xml',
-    ],
-    "qweb": [
-        "static/src/xml/bootstrap_markdown.xml",
-    ],
-    'installable': True,
-    'auto_install': False,
-    'application': False
+    "Komit, "
+    "Sudokeys, "
+    "Odoo Community Association (OCA)",
+    "category": "Web",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/web",
+    "summary": "Widget to text fields that adds markdown support",
+    "depends": ["web"],
+    "demo": ["demo/bootstrap_markdown.xml"],
+    "data": ["views/assets.xml"],
+    "qweb": ["static/src/xml/bootstrap_markdown.xml"],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
 }
