34f1bca55b62d364813748260593e70ed2b91f62
KKamaa <kevkamaa96@gmail.com>
2021-07-08 05:10:18 +0300

    [MIG] support_branding: Migration to 14.0

--- support_branding/__manifest__.py
+++ support_branding/__manifest__.py
@@ -1,25 +1,24 @@
 # Copyright 2012-2015 Therp BV (<http://therp.nl>)
 # Copyright 2016 - Tecnativa - Angel Moya <odoo@tecnativa.com>
 # Copyright 2017 - redO2oo   - Robert Rottermann <robert@redO2oo.ch>
+# Copyright 2021 Sunflower IT (<https://www.sunflowerweb.nl>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Support branding",
+    "name": "Support Branding",
     "summary": "Adds your branding to an Odoo instance",
-    "category": "Dependecy/Hidden",
-    "version": "10.0.1.0.0",
+    "category": "Hidden/Tools",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Therp BV,Odoo Community Association (OCA)",
+    "author": "Therp BV,Sunflower IT,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
-    "depends": [
-        "web",
-    ],
+    "depends": ["web", "base_setup"],
     "qweb": [
         "static/src/xml/base.xml",
     ],
-    "data": [
-        "data/ir_config_parameter.xml",
-        "views/qweb.xml",
+    "data": ["views/asset.xml", "views/res_config_settings.xml"],
+    "demo": [
+        "demo/ir_config_parameter_data.xml",
     ],
     "installable": True,
 }
--- support_branding/static/src/js/support_branding.js
+++ support_branding/static/src/js/support_branding.js
@@ -2,105 +2,169 @@
  * Copyright 2016 - Tecnativa - Angel Moya <odoo@tecnativa.com>
  * Copyright 2017 - redO2oo   - Robert Rottermann <robert@redO2oo.ch>
  * Copyright 2018 - Therp BV
+ * Copyright 2021 - Sunflower IT
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
 
-odoo.define("web.support_branding", function (require) {
-    var CrashManager = require("web.CrashManager");
-    var core = require("web.core");
-    var Model = require("web.Model");
+odoo.define("support_branding.CrashManager", function (require) {
+    "use strict";
+    var CrashManager = require("web.CrashManager").CrashManager;
     var session = require("web.session");
+    var core = require("web.core");
+
     var _t = core._t;
+
     CrashManager.include({
         init: function () {
-            var self = this,
-                ir_config_parameter = new Model("ir.config_parameter");
-            ir_config_parameter
-                .call("get_param", ["support_branding.support_email"])
-                .then(function (email) {
-                    self.support_branding_support_email = email;
+            var self = this;
+            $.when(this._super.apply(this, arguments)).then(function () {
+                self._rpc({
+                    model: "ir.config_parameter",
+                    method: "get_param",
+                    args: ["support_company"],
+                }).then(function (name) {
+                    self.support_cp_name = name;
                 });
-            ir_config_parameter
-                .call("get_param", ["support_branding.company_name"])
-                .then(function (name) {
-                    self.support_branding_company_name = name;
+
+                self._rpc({
+                    model: "ir.config_parameter",
+                    method: "get_param",
+                    args: ["support_company_url"],
+                }).then(function (url) {
+                    self.support_cp_url = url;
                 });
-            return this._super(this, arguments);
+
+                self._rpc({
+                    model: "ir.config_parameter",
+                    method: "get_param",
+                    args: ["support_email"],
+                }).then(function (email) {
+                    self.support_cp_email = email;
+                });
+
+                self._rpc({
+                    model: "ir.config_parameter",
+                    method: "get_param",
+                    args: ["support_release"],
+                }).then(function (release) {
+                    self.support_cp_release = release;
+                });
+
+                self._rpc({
+                    model: "ir.config_parameter",
+                    method: "get_param",
+                    args: ["support_branding_color"],
+                }).then(function (color) {
+                    self.support_cp_color = color;
+                });
+            });
         },
         show_error: function (error) {
             var self = this;
-            error._session = session;
-            this._super.apply(this, arguments);
-            jQuery(".support-branding-submit-form").each(function () {
-                var $form = jQuery(this),
-                    $button = $form.find("button"),
-                    $description = $form.find('textarea[name="description"]'),
-                    $subject = $form.find('input[name="subject"]'),
-                    $body = $form.find('input[name="body"]');
-                if (self.support_branding_support_email) {
-                    $form.attr(
-                        "action",
-                        "mailto:" + self.support_branding_support_email
-                    );
+            var dialog = this._super.apply(this, arguments);
+            var subject =
+                session.username +
+                "@" +
+                session.db +
+                "[" +
+                session.server +
+                "]:" +
+                error.message;
+            var body = error.data.debug;
+            var inputs =
+                "" +
+                '<input type="hidden" name="subject" value=' +
+                subject +
+                "/>\n" +
+                '<input type="hidden" class="sp-body" name="body" value=\'' +
+                body +
+                "'/>";
+            dialog.opened(function () {
+                var $form = $(".support-branding-submit-form");
+                var $statement = $(".support-statement");
+                var $description = $(".support-desc");
+                var $button = $(".support-btn");
+                var $body = $(".sp-body");
+                var $header = $form.parents(".modal-dialog").find(".modal-header");
+                var $footer = $form.parents(".modal-dialog").find(".modal-footer");
+
+                $statement.prepend(inputs);
+                if (self.support_cp_email) {
+                    if (self.support_cp_name) {
+                        var title = "Support By " + self.support_cp_name;
+                        $('<h3 class="text-primary">' + title + "</h3>").insertBefore(
+                            ".support-branding-submit-form"
+                        );
+                        $button.text(
+                            _.str.sprintf(_t("Email to %s"), self.support_cp_name)
+                        );
+                    }
+                    $form.attr("action", "mailto:" + self.support_cp_email);
                     $form
                         .parents(".modal")
                         .find(".modal-body")
                         .css("max-height", "70vh");
-                    $button.click(function (ev) {
-                        var mail_mail = new Model("mail.mail");
+                    $button.on("click", function (ev) {
+                        var $btn = $(this);
                         if (!$description.val()) {
                             $description.parent().addClass("oe_form_invalid");
                             ev.preventDefault();
                             return;
                         }
-                        mail_mail
-                            .call("create", [
-                                {
-                                    state: "outgoing",
-                                    auto_delete: true,
-                                    email_to: self.support_branding_support_email,
-                                    subject: $subject.val(),
-                                    body_html: jQuery("<div/>")
-                                        .append(
-                                            jQuery("<div/>").text($description.val()),
-                                            jQuery("<pre/>").text($body.val())
-                                        )
-                                        .html(),
-                                },
-                            ])
-                            .then(
-                                function (mail_id) {
-                                    return mail_mail.call("send", [[mail_id]]);
-                                },
-                                function () {
-                                    // If the call failed, fire the mailto link
-                                    // hoping there is a properly configured email
-                                    // client
-                                    $body.val($description.val() + "\n" + $body.val());
-                                    $button.unbind("click");
-                                    $button.click();
-                                }
-                            )
-                            .then(function () {
-                                $form.parents(".modal").modal("hide");
-                            });
                         ev.preventDefault();
+                        var desc = $description.val();
+                        var params = {
+                            state: "outgoing",
+                            auto_delete: true,
+                            email_to: self.support_cp_email,
+                            subject: subject,
+                            body_html: jQuery("<div/>")
+                                .append(
+                                    jQuery("<div/>").text(desc),
+                                    jQuery("<pre/>").text(body)
+                                )
+                                .html(),
+                        };
+                        self._rpc({
+                            model: "mail.mail",
+                            method: "create",
+                            args: [params],
+                        }).then(
+                            function (mail_id) {
+                                if (mail_id) {
+                                    self._rpc({
+                                        model: "mail.mail",
+                                        method: "send",
+                                        args: [mail_id],
+                                    }).then(function (res) {
+                                        if (res) {
+                                            self.do_notify(
+                                                "Success",
+                                                "Support mail created!"
+                                            );
+                                        }
+                                    });
+                                }
+                            },
+                            function () {
+                                $body.val(desc + "\n" + $body.val());
+                                $btn.unbind("click");
+                                $btn.click();
+                            }
+                        );
                     });
                 } else {
-                    $description.hide();
-                    $button.hide();
+                    $description.css({display: "none"});
+                    $button.css({display: "none"});
                 }
-                if (self.support_branding_company_name) {
-                    $button.text(
-                        _.str.sprintf(
-                            _t("Email to %s"),
-                            self.support_branding_company_name
-                        )
-                    );
+                $form.prependTo($footer);
+                if (self.support_cp_color) {
+                    $header.css({background: self.support_cp_color});
+                    $footer.css({background: self.support_cp_color});
+                } else {
+                    $header.css({background: ""});
+                    $footer.css({background: ""});
                 }
-                $form.prependTo($form.parents(".modal-dialog").find(".modal-footer"));
             });
         },
     });
-    // This is already instantiated, so we need to call init manually
-    require("web.crash_manager").init();
 });
