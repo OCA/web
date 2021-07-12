36d3413df3b01c5c56c8cfe2f7cb81c22421f166
hkapatel <helly.kapatel@initos.com>
2021-07-12 13:04:11 +0530

    [IMP] web_widget_text_markdown: black, isort, prettier

--- web_widget_text_markdown/static/tests/js/web_widget_text_markdown.js
+++ web_widget_text_markdown/static/tests/js/web_widget_text_markdown.js
@@ -2,7 +2,7 @@
 /* Copyright 2019 Alexandre Díaz - <dev@redneboa.es>
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
 
-odoo.define("web_widget_text_markdown.test", function(require) {
+odoo.define("web_widget_text_markdown.test", function (require) {
     "use strict";
 
     var FormView = require("web.FormView");
@@ -13,7 +13,7 @@ odoo.define("web_widget_text_markdown.test", function(require) {
     QUnit.module(
         "web_widget_text_markdown",
         {
-            beforeEach: function() {
+            beforeEach: function () {
                 this.data = {
                     partner: {
                         fields: {comment: {string: "Comment", type: "text"}},
@@ -27,11 +27,11 @@ odoo.define("web_widget_text_markdown.test", function(require) {
                     "</sheet></form>";
             },
         },
-        function() {
+        function () {
             QUnit.module("FieldTextMarkDown");
             QUnit.test(
                 "bootstrap markdown widget are correctly rendered (preview)",
-                function(assert) {
+                function (assert) {
                     assert.expect(1);
 
                     var done = assert.async();
@@ -42,8 +42,8 @@ odoo.define("web_widget_text_markdown.test", function(require) {
                         data: this.data,
                         arch: this.arch,
                         res_id: 1,
-                    }).then(function(form) {
-                        _.defer(function() {
+                    }).then(function (form) {
+                        _.defer(function () {
                             assert.strictEqual(form.$(".md-editor").length, 0);
                             form.destroy();
                             done();
@@ -53,7 +53,7 @@ odoo.define("web_widget_text_markdown.test", function(require) {
             );
             QUnit.test(
                 "bootstrap markdown widget are correctly rendered (edit)",
-                function(assert) {
+                function (assert) {
                     assert.expect(1);
 
                     var done = assert.async();
@@ -65,8 +65,8 @@ odoo.define("web_widget_text_markdown.test", function(require) {
                         arch: this.arch,
                         res_id: 1,
                         viewOptions: {mode: "edit"},
-                    }).then(function(form) {
-                        _.defer(function() {
+                    }).then(function (form) {
+                        _.defer(function () {
                             assert.strictEqual(form.$(".md-editor").length, 1);
                             form.destroy();
                             done();
