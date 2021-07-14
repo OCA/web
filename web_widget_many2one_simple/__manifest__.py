65016845252c5b461d50ce907b352661d4958648
fshah <foram.shah@initos.com>
2021-07-14 10:16:15 +0530

    [MIG] Migrate module web_widget_many2one_simple to v14.

--- web_widget_many2one_simple/__manifest__.py
+++ web_widget_many2one_simple/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Simple many2one widget",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
