462ba83a7bf5b1b2d879756d500bfa80ced5edc0
Peerapong-Supasompob <emporiolaser@gmail.com>
2021-07-14 10:46:55 +0700

    [IMP] web_widget_image_download: black, isort, prettier

--- web_widget_image_download/static/src/js/web_widget_image_download.js
+++ web_widget_image_download/static/src/js/web_widget_image_download.js
@@ -1,13 +1,13 @@
 /* Copyright 2016 Flavio Corpa <flavio.corpa@tecnativa.com>
  * Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
  * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl). */
-odoo.define("web_widget_image_download.widget", function(require) {
+odoo.define("web_widget_image_download.widget", function (require) {
     "use strict";
 
     var FieldBinaryImage = require("web.basic_fields").FieldBinaryImage;
 
     FieldBinaryImage.include({
-        _render: function() {
+        _render: function () {
             this._super();
             var $widget = this.$el.find(".o_form_binary_file_download");
             this.imgSrc = this.$el.find('img[name="' + this.name + '"]').attr("src");
@@ -16,7 +16,7 @@ odoo.define("web_widget_image_download.widget", function(require) {
                 // Avoid downloading full image, just headers
                 type: "HEAD",
                 url: this.imgSrc,
-                complete: function(xhr) {
+                complete: function (xhr) {
                     $widget.attr(
                         "download",
                         xhr.getResponseHeader("Content-Type").replace("/", ".")
