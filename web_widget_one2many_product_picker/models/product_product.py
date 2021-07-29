OCA/web-addons

[MIG] web_widget_one2many_product_picker: Migration to 13.0

web_widget_one2many_product_picker/models/product_product.py
@@ -1,37 +1,21 @@
 # Copyright 2020 Tecnativa - Alexandre D. Díaz
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
-from odoo import api, fields, models, tools
+from odoo import fields, models
 
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
     image_variant_medium = fields.Binary(
-        "Variant Image Medium (Computed)",
-        compute="_compute_variant_image",
+        "Variant Image Medium (Related)",
+        related="image_variant_512",
         help="This field holds the image used as image for the product variant"
         "or product image medium, limited to 512x512px.",
     )
 
     image_variant_big = fields.Binary(
-        "Variant Image Big (Computed)",
-        compute="_compute_variant_image",
+        "Variant Image Big (Related)",
+        related="image_variant_1920",
         help="This field holds the image used as image for the product variant"
         "or product image, limited to 1024x1024px.",
     )
-
-    @api.depends("image_variant", "product_tmpl_id.image")
-    def _compute_variant_image(self):
-        for record in self:
-            if record.image_variant:
-                resized_images = tools.image_get_resized_images(
-                    record.image_variant,
-                    return_big=False,
-                    return_small=False,
-                    avoid_resize_medium=True,
-                )
-                record.image_variant_medium = resized_images["image_medium"]
-                record.image_variant_big = record.image_variant
-            else:
-                record.image_variant_medium = record.product_tmpl_id.image_medium
-                record.image_variant_big = record.product_tmpl_id.image

OCA/web

web_widget_one2many_product_picker/models/product_product.py
@@ -1,37 +1,21 @@
 # Copyright 2020 Tecnativa - Alexandre D. Díaz
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
-from odoo import api, fields, models, tools
+from odoo import fields, models
 
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
     image_variant_medium = fields.Binary(
-        "Variant Image Medium (Computed)",
-        compute="_compute_variant_image",
+        "Variant Image Medium (Related)",
+        related="image_512",
         help="This field holds the image used as image for the product variant"
         "or product image medium, limited to 512x512px.",
     )
 
     image_variant_big = fields.Binary(
-        "Variant Image Big (Computed)",
-        compute="_compute_variant_image",
+        "Variant Image Big (Related)",
+        related="image_1024",
         help="This field holds the image used as image for the product variant"
         "or product image, limited to 1024x1024px.",
     )
-
-    @api.depends("image_variant", "product_tmpl_id.image")
-    def _compute_variant_image(self):
-        for record in self:
-            if record.image_variant:
-                resized_images = tools.image_get_resized_images(
-                    record.image_variant,
-                    return_big=False,
-                    return_small=False,
-                    avoid_resize_medium=True,
-                )
-                record.image_variant_medium = resized_images["image_medium"]
-                record.image_variant_big = record.image_variant
-            else:
-                record.image_variant_medium = record.product_tmpl_id.image_medium
-                record.image_variant_big = record.product_tmpl_id.image