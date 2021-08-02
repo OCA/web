b72df34261636d83e648cbe88e4852a045abc3a5
SilvioC2C <silvio.gregorini@camptocamp.com>
2021-08-02 16:27:15 +0200

    [14.0] web_m2x_options_manager: New module to manage create/edit options from interface

---
+++ web_m2x_options_manager/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2021 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import ir_model
+from . import ir_ui_view
+from . import m2x_create_edit_option
