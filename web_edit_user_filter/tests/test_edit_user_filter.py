8c969107e679b41a4921079008eb521472135b4e
Roberto Fichera <robyf@tekno-soft.it>
2021-09-05 12:49:58 +0200

    [IMP] web_edit_user_filter: black, isort, prettier

--- web_edit_user_filter/tests/test_edit_user_filter.py
+++ web_edit_user_filter/tests/test_edit_user_filter.py
@@ -7,26 +7,14 @@ from odoo.tests.common import SingleTransactionCase, post_install
 @post_install(True)
 class TestEditUserFilter(SingleTransactionCase):
     def test_filter_facet_inclusion(self):
-        self.env['ir.filters'].create({
-            'name': 'any2',
-            'model_id': 'ir.filters',
-            'domain': '[]',
-            'facet': 'test2'
-        })
-        new_filter = self.env['ir.filters'].create({
-            'name': 'any',
-            'model_id': 'ir.filters',
-            'domain': '[]',
-            'facet': 'test'
-        })
-        res = self.env['ir.filters'].get_filters('ir.filters')
-        self.assertTrue('facet' in res[0])
+        self.env["ir.filters"].create(
+            {"name": "any2", "model_id": "ir.filters", "domain": "[]", "facet": "test2"}
+        )
+        new_filter = self.env["ir.filters"].create(
+            {"name": "any", "model_id": "ir.filters", "domain": "[]", "facet": "test"}
+        )
+        res = self.env["ir.filters"].get_filters("ir.filters")
+        self.assertTrue("facet" in res[0])
         self.assertEqual(
-            list(
-                filter(
-                    lambda f: f['id'] == new_filter.id,
-                    res
-                )
-            )[0]['facet'],
-            'test'
+            list(filter(lambda f: f["id"] == new_filter.id, res))[0]["facet"], "test"
         )
