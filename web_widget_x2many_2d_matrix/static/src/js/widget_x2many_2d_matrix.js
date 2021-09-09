8ee8d00560b63f65a9fee75bd3d9478eadc2251e
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-09-09 19:52:17 +0200

    [FIX] web_widget_x2many_2d_matrix: remove console.log

--- web_widget_x2many_2d_matrix/static/src/js/widget_x2many_2d_matrix.js
+++ web_widget_x2many_2d_matrix/static/src/js/widget_x2many_2d_matrix.js
@@ -132,7 +132,6 @@ odoo.define("web_widget_x2many_2d_matrix.widget", function (require) {
                 show_row_totals: this.show_row_totals,
                 show_column_totals: this.show_column_totals,
             };
-            console.log(this.matrix_data);
         },
 
         /**
