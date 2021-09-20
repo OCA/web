23b5186b556b5402045b34e102add388994ff5d2
Alexandre D. Díaz <alexandre.diaz@tecnativa.com>
2021-09-20 13:04:50 +0200

    [ADD] web_time_range_menu_custom

---
+++ web_time_range_menu_custom/static/src/js/control_panel_controller.js
@@ -0,0 +1,26 @@
+odoo.define("web_time_range_menu_custom.ControlPanelController", function(require) {
+    "use strict";
+
+    const ControlPanelController = require("web.ControlPanelController");
+
+    ControlPanelController.include({
+        custom_events: _.extend({}, ControlPanelController.prototype.custom_events, {
+            activate_custom_time_range: "_onActivateCustomTimeRange",
+        }),
+
+        /**
+         * @override
+         */
+        _onActivateCustomTimeRange: function(ev) {
+            ev.stopPropagation();
+            this.model.activateTimeRangeCustom(
+                ev.data.id,
+                ev.data.timeRangeId,
+                ev.data.comparisonTimeRangeId,
+                ev.data.timeRangeCustom,
+                ev.data.comparisonTimeRangeCustom
+            );
+            this._reportNewQueryAndRender();
+        },
+    });
+});
---
+++ web_time_range_menu_custom/static/src/js/control_panel_view_parameters.js
@@ -0,0 +1,20 @@
+/* Copyright 2021 Tecnativa - Alexandre D. Díaz
+ * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl). */
+odoo.define("web_time_range_menu_custom.controlPanelViewParameters", function(require) {
+    "use strict";
+
+    const controlPanelViewParameters = require("web.controlPanelViewParameters");
+    const core = require("web.core");
+
+    const _lt = core._lt;
+
+    controlPanelViewParameters.PERIOD_OPTIONS = controlPanelViewParameters.PERIOD_OPTIONS.concat(
+        [{description: _lt("Custom Period"), optionId: "custom_period", groupId: 4}]
+    );
+    controlPanelViewParameters.TIME_RANGE_OPTIONS =
+        controlPanelViewParameters.PERIOD_OPTIONS;
+
+    controlPanelViewParameters.COMPARISON_TIME_RANGE_OPTIONS = controlPanelViewParameters.COMPARISON_TIME_RANGE_OPTIONS.concat(
+        [{description: _lt("Custom Period"), optionId: "custom_comparison_period"}]
+    );
+});
