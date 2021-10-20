9ef60dec421ca317e1c0ceb2d95d1aab5348f15a
Nabil Ragab <nabil@ragab.net>
2021-10-20 22:08:45 +0200

    [MIG] web_refresher: Migration to 15.0

--- web_refresher/__manifest__.py
+++ web_refresher/__manifest__.py
@@ -1,10 +1,13 @@
 {
     "name": "Web Refresher",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
+    'category': 'Hidden',
     "author": "Compassion Switzerland, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/web",
-    "qweb": ["templates/pager_button.xml"],
+    'web.assets_qweb': [
+        'web_refresher/static/src/legacy/xml/web_refresher.xml',
+    ],
     "depends": ["web"],
     "installable": True,
     "auto_install": False,
