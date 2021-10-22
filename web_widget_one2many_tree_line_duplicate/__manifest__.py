e582d38b3e06f11758fb904d6ba98e4ee1ab5244
Alexandre D. Díaz <alexandre.diaz@tecnativa.com>
2021-10-22 19:08:09 +0200

    [ADD] web_widget_one2many_tree_line_duplicate

---
+++ web_widget_one2many_tree_line_duplicate/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Tecnativa - Alexandre Díaz
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Web Widget One2many Tree Line Duplicate",
+    "category": "web",
+    "version": "13.0.1.0.0",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/web",
+    "depends": ["web"],
+    "data": ["view/assets.xml"],
+    "auto_install": False,
+    "installable": True,
+}
