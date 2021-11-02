bd2e501225c1a05120f928665f0544145575dfd3
AyoubZahid <azahid@agilorg.com>
2021-11-02 10:11:50 +0100

    [MIG] web_widget_x2many_2d_matrix: Migration to 15.0

---
+++ web_widget_x2many_2d_matrix/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2015 Holger Brunn <hbrunn@therp.nl>
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2018 Simone Orsi <simone.orsi@camptocamp.com>
+# Copyright 2020 CorporateHub (https://corporatehub.eu)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "2D matrix for x2many fields",
+    "version": "15.0.1.0.1",
+    "author": (
+        "Therp BV, "
+        "Tecnativa, "
+        "Camptocamp, "
+        "CorporateHub, "
+        "Odoo Community Association (OCA)"
+    ),
+    "website": "https://github.com/OCA/web",
+    "license": "AGPL-3",
+    "category": "Hidden/Dependency",
+    "summary": "Show list fields as a matrix",
+    "depends": ["web"],
+    'assets': {
+        "web.assets_backend": [
+            "web_widget_x2many_2d_matrix/static/src/js/*.js",
+            "web_widget_x2many_2d_matrix/static/src/scss/web_widget_x2many_2d_matrix.scss"
+        ],
+    },
+    "installable": True,
+}
---
+++ web_widget_x2many_2d_matrix/static/src/js/abstract_view_matrix_limit_extend.js
@@ -0,0 +1,16 @@
+odoo.define("web_widget_x2many_2d_matrix.matrix_limit_extend", function (require) {
+    "use strict";
+
+    var FormView = require("web.FormView");
+
+    FormView.include({
+        // We extend this method so that the view is not limited to
+        // just 40 cells when the 'x2many_2d_matrix' widget is used.
+        _setSubViewLimit: function (attrs) {
+            this._super(attrs);
+            if (attrs.widget === "x2many_2d_matrix") {
+                attrs.limit = Infinity;
+            }
+        },
+    });
+});
