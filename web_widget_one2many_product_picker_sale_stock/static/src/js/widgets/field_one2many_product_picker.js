7237e9a569fd60701999cccb7513993418dbfb95
Alexandre D. Díaz <alexandre.diaz@tecnativa.com>
2021-11-10 12:52:56 +0100

    [IMP] web_widget_one2many_product_picker_sale_stock: Adapt to work with product_picker

--- web_widget_one2many_product_picker_sale_stock/static/src/js/widgets/field_one2many_product_picker.js
+++ web_widget_one2many_product_picker_sale_stock/static/src/js/widgets/field_one2many_product_picker.js
@@ -1,20 +1,21 @@
 // Copyright 2020 Tecnativa - Alexandre Díaz
 // License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-odoo.define("web_widget_one2many_product_picker_sale_stock.FieldOne2ManyProductPicker", function (
-    require
-) {
-    "use strict";
+odoo.define(
+    "web_widget_one2many_product_picker_sale_stock.FieldOne2ManyProductPicker",
+    function(require) {
+        "use strict";
 
-    var FieldOne2ManyProductPicker = require("web_widget_one2many_product_picker.FieldOne2ManyProductPicker");
+        var FieldOne2ManyProductPicker = require("web_widget_one2many_product_picker.FieldOne2ManyProductPicker");
 
-    FieldOne2ManyProductPicker.include({
-        /**
-         * @override
-         */
-        _getDefaultOptions: function () {
-            var defaults = this._super.apply(this, arguments);
-            defaults["show_sale_stock"] = true;
-            return defaults;
-        },
-    });
-});
+        FieldOne2ManyProductPicker.include({
+            /**
+             * @override
+             */
+            _getDefaultOptions: function() {
+                var defaults = this._super.apply(this, arguments);
+                defaults.show_sale_stock = false;
+                return defaults;
+            },
+        });
+    }
+);
