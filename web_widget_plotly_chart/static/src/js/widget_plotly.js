81e636f7837fb0e5754ed520241db78c75e9e7b8
Roberto Fichera <robyf@tekno-soft.it>
2021-12-06 23:02:39 +0100

    [IMP] web_widget_plotly: black, isort, prettier

--- web_widget_plotly_chart/static/src/js/widget_plotly.js
+++ web_widget_plotly_chart/static/src/js/widget_plotly.js
@@ -1,17 +1,17 @@
-odoo.define('web_widget_plotly_chart', function(require) {
-'use strict'
+odoo.define("web_widget_plotly_chart", function (require) {
+    "use strict";
 
-    var fieldRegistry = require('web.field_registry');
-    var AbstractField = require('web.AbstractField');
+    var fieldRegistry = require("web.field_registry");
+    var AbstractField = require("web.AbstractField");
 
     var PlotlyChartWidget = AbstractField.extend({
-        _renderReadonly: function() {
+        _renderReadonly: function () {
             var val = this.value;
             this.$el.html(val);
-        }
+        },
     });
-    fieldRegistry.add('plotly_chart', PlotlyChartWidget);
+    fieldRegistry.add("plotly_chart", PlotlyChartWidget);
     return {
-        PlotlyChartWidget: PlotlyChartWidget
+        PlotlyChartWidget: PlotlyChartWidget,
     };
 });
