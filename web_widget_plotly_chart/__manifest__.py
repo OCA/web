2bc689d86a92ab1d4961b343142c9193e7edf6fd
Roberto Fichera <robyf@tekno-soft.it>
2021-12-06 23:02:39 +0100

    [IMP] web_widget_plotly: black, isort, prettier

--- web_widget_plotly_chart/__manifest__.py
+++ web_widget_plotly_chart/__manifest__.py
@@ -1,17 +1,16 @@
 {
-    'name': "Web Widget Plotly",
-    'summary': """Allow to draw plotly charts.""",
-    'author': "LevelPrime srl, "
-              "Odoo Community Association (OCA)",
+    "name": "Web Widget Plotly",
+    "summary": """Allow to draw plotly charts.""",
+    "author": "LevelPrime srl, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
-    'category': 'Web',
-    'version': '12.0.1.0.0',
-    'depends': ['web'],
-    'data': [
-        'views/web_widget_plotly_chart.xml',
+    "category": "Web",
+    "version": "14.0.1.0.0",
+    "depends": ["web"],
+    "data": [
+        "views/web_widget_plotly_chart.xml",
     ],
     "external_dependencies": {
-        "python": ['plotly'],
+        "python": ["plotly"],
     },
     "auto_install": False,
     "license": "LGPL-3",
