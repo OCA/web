b67336cbf1c96b3ab003f859d4dd67e1d378e626
Christopher Ormaza <chris.ormaza@forgeflow.com>
2021-12-10 11:44:07 -0500

    [15.0][MIG] web_widget_bokeh_chart

--- web_widget_bokeh_chart/__manifest__.py
+++ web_widget_bokeh_chart/__manifest__.py
@@ -6,13 +6,18 @@
     "category": "Hidden",
     "summary": "This widget allows to display charts using Bokeh library.",
     "author": "ForgeFlow, " "Odoo Community Association (OCA)",
-    "version": "14.0.2.3.1",
-    "maintainers": ["LoisRForgeFlow"],
+    "version": "15.0.1.0.0",
+    "maintainers": ["LoisRForgeFlow", "ChrisOForgeFlow"],
     "development_status": "Production/Stable",
     "website": "https://github.com/OCA/web",
     "depends": ["web"],
-    "data": ["views/web_widget_bokeh_chart.xml"],
-    "external_dependencies": {"python": ["bokeh==2.3.1"]},
+    "data": [],
+    "external_dependencies": {"python": ["bokeh==2.4.2"]},
     "auto_install": False,
     "license": "LGPL-3",
+    "assets": {
+        "web.assets_backend": [
+            "web_widget_bokeh_chart/static/src/js/web_widget_bokeh_chart.esm.js",
+        ],
+    },
 }
