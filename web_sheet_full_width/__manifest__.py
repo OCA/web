6ab2b4d59190abafa5580b47d50b786161d53178
Joan Mateu Jordi <joan.mateu@forgeflow.com>
2021-12-22 11:03:27 +0100

    [MIG] web_sheet_full_width: Migration to 15.0

--- web_sheet_full_width/__manifest__.py
+++ web_sheet_full_width/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Web Sheet Full Width",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "author": "Therp BV, Sudokeys, GRAP, Métal Sartigan, "
     "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
@@ -9,6 +9,10 @@
     "summary": "Use the whole available screen width when displaying sheets",
     "category": "Tools",
     "depends": ["web"],
-    "data": ["templates/assets.xml"],
+    "assets": {
+        "web.assets_common": [
+            "web_sheet_full_width/static/src/scss/web_sheet_full_width.scss",
+        ],
+    },
     "installable": True,
 }
