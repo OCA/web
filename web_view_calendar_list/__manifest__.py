0f783d4b23697c80c6cea721a0714914dd687436
Olga Marco <olga.marco@creublanca.es>
2022-01-07 10:07:37 +0100

    [MIG] web_view_calendar_list: Migration to 14.0

--- web_view_calendar_list/__manifest__.py
+++ web_view_calendar_list/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Web View Calendar List",
     "summary": """
         Show calendars as a List""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
--- web_view_calendar_list/static/src/js/calendar_list_controller.js
+++ web_view_calendar_list/static/src/js/calendar_list_controller.js
@@ -1,9 +1,5 @@
 odoo.define("web_view_calendar_list.CalendarListController", function (require) {
     "use strict";
-
     var CalendarController = require("web.CalendarController");
-
-    var CalendarListController = CalendarController.extend({});
-
-    return CalendarListController;
+    return CalendarController.extend({});
 });
