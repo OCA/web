a75112624cefdd3c83d512485e17516106b5bada
Alexandre D. Díaz <alexandre.diaz@tecnativa.com>
2022-01-10 16:36:34 +0100

    [FIX] web_widget_one2many_product_picker_sale_stock: Destroy popover when the widget is detached

---
+++ web_widget_one2many_product_picker_sale_stock/static/src/js/widgets/qty_at_date_widget.js
@@ -0,0 +1,15 @@
+// Copyright 2022 Tecnativa - Alexandre Díaz
+// License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+odoo.define("web_widget_one2many_product_picker_sale_stock.QtyAtDateWidget", function (
+    require
+) {
+    "use strict";
+
+    var QtyAtDateWidget = require('sale_stock.QtyAtDateWidget');
+
+    QtyAtDateWidget.include({
+        on_detach_callback: function() {
+            this.$el.popover("dispose");
+        },
+    });
+});
