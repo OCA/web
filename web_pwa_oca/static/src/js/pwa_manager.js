5a345daed70e243feae37079e7e813a48bd441aa
Alexandre D. Díaz <alexandre.diaz@tecnativa.com>
2022-01-19 12:50:45 +0100

    [FIX] web_pwa_oca: Add 'isPWAStandalone'

--- web_pwa_oca/static/src/js/pwa_manager.js
+++ web_pwa_oca/static/src/js/pwa_manager.js
@@ -5,9 +5,25 @@ odoo.define("web_pwa_oca.PWAManager", function(require) {
     "use strict";
 
     var core = require("web.core");
+    var config = require("web.config");
     var Widget = require("web.Widget");
 
     var _t = core._t;
+
+    /**
+     * @returns {Boolean}
+     */
+    function isPWAStandalone() {
+        return (
+            window.navigator.standalone ||
+            document.referrer.includes("android-app://") ||
+            window.matchMedia("(display-mode: standalone)").matches
+        );
+    }
+
+    if (isPWAStandalone()) {
+        config.device.isMobile = true;
+    }
 
     var PWAManager = Widget.extend({
         /**
@@ -41,6 +57,13 @@ odoo.define("web_pwa_oca.PWAManager", function(require) {
                 .catch(function(error) {
                     console.log(_t("[ServiceWorker] Registration failed: "), error);
                 });
+        },
+
+        /**
+         * @returns {Boolean}
+         */
+        isPWAStandalone: function() {
+            return isPWAStandalone();
         },
 
         /**
