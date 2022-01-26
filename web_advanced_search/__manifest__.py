47b3aa6140f25931bb2a476926a25bb07d7b89a9
Maksym Yankin <yankinmk@gmail.com>
2022-01-26 14:38:14 +0200

    [MIG] web_advanced_search: Migration to 15.0

--- web_advanced_search/__manifest__.py
+++ web_advanced_search/__manifest__.py
@@ -5,17 +5,25 @@
 
 {
     "name": "Advanced search",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "author": "Therp BV, " "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Usability",
     "summary": "Easier and more powerful searching tools",
     "website": "https://github.com/OCA/web",
     "depends": ["web"],
-    "data": ["views/templates.xml"],
-    "qweb": [
-        "static/src/xml/web_advanced_search.xml",
-    ],
+    "assets": {
+        "web.assets_backend": [
+            "web_advanced_search/static/src/js/control_panel/advanced_filter_item.js",
+            "web_advanced_search/static/src/js/control_panel/filter_menu.js",
+            "web_advanced_search/static/src/js/control_panel/custom_filter_item.js",
+            "web_advanced_search/static/src/js/human_domain.js",
+            "web_advanced_search/static/src/js/relational.js",
+        ],
+        "web.assets_qweb": [
+            "web_advanced_search/static/src/xml/web_advanced_search.xml",
+        ],
+    },
     "installable": True,
     "application": False,
 }
