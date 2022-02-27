1bb8d7abd04530a89c383ae8cbbf487cd5b3eb9f
Bluis Knot <bluisknot@gmail.com>
2022-02-27 10:50:24 -0500

    Enable to notify via Odoo action option and to suppress any undesirable notification.
    In order to enable the last, a specific context and checks for ignored
    and computed fields were introduced, likewise decorators and context managers.

--- model_basic_ops_notifications/models/mixin.py
+++ model_basic_ops_notifications/models/mixin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import logging
+from contextlib import contextmanager
+from functools import wraps
 
 from odoo import _, api, models
 
@@ -19,8 +21,10 @@ class BasicModelOpsNotification(models.AbstractModel):
     _model_successfull_single_selected_repr = "The selected record"
     _model_successfull_plural_repr = "The records"
     _model_successfull_plural_selected_repr = "The selected records"
-    _default_notification_type = "success"
+    _success_notification_type = "success"
+    _failure_notification_type = "danger"
     _sticky_notification = False
+    _non_reportable_fields = []
 
     @api.model
     def _get_operations_repr_map(self):
@@ -112,22 +116,28 @@ class BasicModelOpsNotification(models.AbstractModel):
     @api.model
     def create(self, vals_list):
         result = super().create(vals_list)
-        if result and not self._context.get("do_not_notify", None):
+        if result and not self._context.get("suppress_notifications", None):
             result._perform_notification()
         return result
 
     def write(self, vals):
         result = super().write(vals)
-        if result and not self._context.get("do_not_notify", None):
-            # TODO: Find a way to differentiate `unlink` requests made from the list and form view.
+
+        if result and not self._context.get("suppress_notifications", None):
+            # TODO: Find a way to differentiate `write` requests made from the list and form view.
             # In the context Odoo always put `'view_type': 'list'`...
             # So we can also use `refer_to_selected` in that moment.
-            self._perform_notification(operation="write", modified_vals=vals)
+            non_computed = [
+                fname for fname in vals if not self._fields[fname].compute and not self._fields[fname].related
+            ]
+            non_ignored = [fname for fname in non_computed if fname not in self._non_reportable_fields]
+            if non_ignored:
+                self._perform_notification(operation="write", modified_vals=non_computed)
         return result
 
     def unlink(self):
         result = super().unlink()
-        if result and not self._context.get("do_not_notify", None):
+        if result and not self._context.get("suppress_notifications", None):
             # TODO: Find a way to differentiate `unlink` requests made from the list and form view.
             # In the context Odoo always put `'view_type': 'list'`...
             # So we can also use `refer_to_selected` in that moment.
@@ -140,24 +150,141 @@ class BasicModelOpsNotification(models.AbstractModel):
         operation="create",
         successfull=True,
         refer_to_selected=False,
-        alternate_message=None,
-        alternate_notification_type="warning",
+        message=None,
         modified_vals=None,
+        as_action=False,
+        title=None,
+        notification_type="success",
+        sticky=False,
+        next_action=None,
     ):
-        message = alternate_message or self._get_notification_message(
+        message = message or self._get_notification_message(
             operation, successfull=successfull, refer_to_selected=refer_to_selected
         )
         if message:
-            self.env[BUS_MODEL_NAME]._sendone(
-                self.env.user.partner_id,
-                f"notify-record-operation",
-                {
-                    "message": message,
-                    "type": self._default_notification_type if successfull else alternate_notification_type,
-                    "sticky": self._sticky_notification,
-                },
-            )
-            _logger_message = f'Notification performed for model "{self._name}" on "{operation}" operation.'
-            if modified_vals:
-                _logger_message = f'{_logger_message}\nSince a "write" operation was trigger here are the modified values: {modified_vals}'
-            _logger.info(_logger_message)
+            if not as_action:
+                self.env[BUS_MODEL_NAME]._sendone(
+                    self.env.user.partner_id,
+                    "notify-record-operation",
+                    {
+                        "message": message,
+                        "type": self._success_notification_type if successfull else self._failure_notification_type,
+                        "sticky": self._sticky_notification,
+                    },
+                )
+                _logger_message = f'Notification performed for model "{self._name}" on "{operation}" operation.'
+                if modified_vals:
+                    _logger_message_extension = '\nSince a "write" operation was trigger here are the modified values:'
+                    _logger_message = f"{_logger_message}{_logger_message_extension} {modified_vals}"
+                _logger.info(_logger_message)
+            else:
+                next_action = next_action or {"type": "ir.actions.act_window_close"}
+                notify_action = {
+                    "type": "ir.actions.client",
+                    "tag": "display_notification",
+                    "params": {"message": message, "type": notification_type, "sticky": sticky, "next": next_action},
+                }
+                if title:
+                    notify_action["params"]["title"] = title
+                return notify_action
+
+
+def with_specific_context(context):
+    """Decorates a Odoo method in order to set a specific context."""
+
+    def _with_specific_context(method):
+        @wraps(method)
+        def _wrapper(model_instance, *args, **kwargs):
+            return method(model_instance.with_context(context), *args, **kwargs)
+
+        return _wrapper
+
+    return _with_specific_context
+
+
+def suppress_notifications():
+    """
+    Decorates a Odoo method in order to suppress notifications.
+
+    We can't do a context manager that work both as context manager and a decorator in this case.
+
+    From the docs (https://docs.python.org/3/library/contextlib.html):
+    Note that there is one additional limitation when using context managers as function decorators:
+    there’s no way to access the return value of __enter__().
+    If that value is needed, then it is still necessary to use an explicit with statement.
+
+    Examples::
+    >>> from odoo.addons.model_basic_ops_notifications.models.mixin import suppress_notifications
+        ...
+        @suppress_notifications()
+        @api.depends("field_name")
+        def _compute_method(self):
+            # Here `self` will have its context already updated with the `{"suppress_notifications": True}` value.
+            for i in self:
+            ...
+    """
+
+    return with_specific_context({"suppress_notifications": True})
+
+
+@contextmanager
+def notifications_suppressor(model_instance):
+    """
+    Context manager to suppress notifications for a Odoo model instance.
+
+    Examples::
+    >>> from odoo.addons.model_basic_ops_notifications.models.mixin import notification_suppressor
+        ...
+        @api.depends("field_name")
+        def _compute_method(self):
+            with notifications_suppressor(self) as new_self:
+                # Here `new_self` will have its context already updated with the `{"suppress_notifications": True}` value.
+                for i in new_self:
+                ...
+    """
+
+    yield model_instance.with_context({"suppress_notifications": True})
+
+
+def depends_and_suppress_notifications(*args):
+    """
+    Decorates a Odoo method in order to specify its dependencies and to suppress notifications.
+
+    Examples::
+    >>> from odoo.addons.model_basic_ops_notifications.models.mixin import depends_and_suppress_notifications
+        ...
+        @depends_and_suppress_notifications("field_name")
+        def _compute_method(self):
+            # Here `self` will have its context already updated with the `{"suppress_notifications": True}` value.
+            for i in self:
+            ...
+    """
+
+    def _depends_and_suppress_notifications(method):
+        depends = api.depends(*args)(method)
+        return suppress_notifications()(depends)
+
+    return _depends_and_suppress_notifications
+
+
+def onchange_and_suppress_notifications(*args):
+    """
+    Decorates a Odoo method in order to specify how must behave when a field change and to suppress notifications.
+    THIS WILL BE ONLY NECESSARY if you use `write` sentences within you `onchange` definitions to specify certain
+    values.
+
+    Examples::
+    >>> from odoo.addons.model_basic_ops_notifications.models.mixin import onchange_and_suppress_notifications
+        ...
+        @onchange_and_suppress_notifications("field_name")
+        def _onchange_method(self):
+            # Here `self` will have its context already updated with the `{"suppress_notifications": True}` value.
+            self.write({'attr': value})
+            ...
+    """
+
+    def _onchange_and_suppress_notifications(method):
+        onchanges = api.onchange(*args)(method)
+        return suppress_notifications()(onchanges)
+
+    return _onchange_and_suppress_notifications
