a6013943cafe888b3aa9c6bfd2c16652c3cb397c
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2022-03-23 08:38:29 +0100

    web_widget_mpld3_chart: fix mpld3 dependency
    This is an attempt to fix an installation issue on runboat.
    The reason for the pin is unknown at the moment, unfortunately.

--- setup/web_widget_mpld3_chart/setup.py
+++ setup/web_widget_mpld3_chart/setup.py
@@ -2,5 +2,11 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon=True,
+    odoo_addon={
+        "external_dependencies_override": {
+            "python": {
+                "mpld3": "mpld3==0.3",
+            },
+        },
+    },
 )
