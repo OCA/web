78ed28b0e7ad9f82bb15ae1408701b2d4a8eb8fd
yibudak <yibudak@gmail.com>
2022-04-04 16:05:35 +0300

    fixed flake8 warnings

--- web_widget_uom_view_precision/models/uom_uom.py
+++ web_widget_uom_view_precision/models/uom_uom.py
@@ -1,11 +1,13 @@
 from odoo import models, fields
 
+
 class UomUom(models.Model):
     _inherit = 'uom.uom'
 
     view_precision = fields.Integer('View Precision',
                                     default=3,
-                                    help="Number of digits after the decimal separator when displaying the value"
+                                    help="Number of digits after the decimal"
+                                         " separator when displaying the value"
                                          " for this unit of measure. Default is 3.")
 
     _sql_constraints = [
