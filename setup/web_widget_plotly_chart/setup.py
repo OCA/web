fa53d50da2b932547cc2b6715f397f9b99b7ee44
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2022-05-04 15:40:48 +0200

    web_widget_plotly_chart: pin plotly version
    As indicated in the addon README.

--- setup/web_widget_plotly_chart/setup.py
+++ setup/web_widget_plotly_chart/setup.py
@@ -2,5 +2,11 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon=True,
+    odoo_addon={
+        "external_dependencies_override": {
+            "python": {
+                "plotly": "plotly==4.1.0",
+            }
+        }
+    },
 )
