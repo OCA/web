37003a4fee7891166046a2021a5e6e58042f219d
Olga Marco <olga.marco@creublanca.es>
2022-05-11 11:38:33 +0200

    [MIG] web_drop_target: Migration to 15.0

--- web_drop_target/__manifest__.py
+++ web_drop_target/__manifest__.py
@@ -2,13 +2,19 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Drop target support",
-    "version": "14.0.1.1.0",
+    "version": "15.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "license": "LGPL-3",
     "category": "Usability",
     "summary": "Allows to drag files into Odoo",
     "depends": ["web"],
-    "data": ["views/templates.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "web_drop_target/static/src/js/**/*",
+            "web_drop_target/static/src/scss/**/*",
+        ],
+        "web.assets_qweb": ["web_drop_target/static/src/xml/**/*"],
+    },
     "qweb": ["static/src/xml/widgets.xml"],
 }
