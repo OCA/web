fba2708e69b2b36324b62c4493338ce66b273fa6
Olga Marco <olga.marco@creublanca.es>
2022-05-11 11:10:10 +0200

    [MIG] web_view_calendar_list: Migration to 15.0

--- web_view_calendar_list/__manifest__.py
+++ web_view_calendar_list/__manifest__.py
@@ -5,10 +5,15 @@
     "name": "Web View Calendar List",
     "summary": """
         Show calendars as a List""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "depends": ["web"],
-    "data": ["templates/assets.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "web_view_calendar_list/static/src/js/**/*",
+            "web_view_calendar_list/static/src/scss/**/*",
+        ],
+    },
 }
