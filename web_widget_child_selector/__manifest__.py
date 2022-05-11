62fa7353ec8bac8e50e2cd59db939d82f781c5fd
Olga Marco <olga.marco@creublanca.es>
2022-05-11 11:23:55 +0200

    [MIG] web_widget_child_selector: Migration to 15.0

--- web_widget_child_selector/__manifest__.py
+++ web_widget_child_selector/__manifest__.py
@@ -4,11 +4,17 @@
 {
     "name": "Web Widget Child Selector",
     "summary": "Widget used for navigation on hierarchy fields",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "depends": ["web"],
-    "data": ["views/assets.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "web_widget_child_selector/static/src/js/**/*",
+            "web_widget_child_selector/static/src/scss/**/*",
+        ],
+        "web.assets_qweb": ["web_widget_child_selector/static/src/xml/**/*"],
+    },
     "qweb": ["static/src/xml/widget_child_selector.xml"],
 }
