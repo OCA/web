37a61917b2481b6c3bb37e87be622ea0eb551541
Simone Rubino <sir@takobi.online>
2022-07-15 14:22:24 +0200

    [ADD] web_button_no_save

---
+++ web_button_no_save/static/src/js/web_button_no_save.js
@@ -0,0 +1,19 @@
+odoo.define("web_button_no_save.FormController", function (require) {
+    "use strict";
+
+    var FormController = require("web.FormController");
+
+    FormController.include({
+        _onButtonClicked: function (event) {
+            var attrs = event.data.attrs;
+            if (attrs.no_save) {
+                event.stopPropagation();
+                this._disableButtons();
+                var def = this._callButtonAction(attrs, event.data.record);
+                def.always(this._enableButtons.bind(this));
+            } else {
+                return this._super.apply(this, arguments);
+            }
+        },
+    });
+});
