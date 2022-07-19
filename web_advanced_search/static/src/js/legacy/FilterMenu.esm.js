40021cf98525df5d4c72fe45ccead8d20227b006
Ivàn Todorovich <ivan.todorovich@gmail.com>
2022-07-19 14:39:40 -0300

    [FIX] web_advanced_search: Implement 'Add Custom Filter' for owl widgets (pivot, graph)
    Steps to reproduce:
    
    1. Go to any Pivot or Graph view.
    2. Add a custom filter, for example, Company.
    3. Use the "is equal to" operator.
    
    Result:
    
    Uncaught Promise > Cannot find the definition of component "RecordPicker"

--- web_advanced_search/static/src/js/legacy/FilterMenu.esm.js
+++ web_advanced_search/static/src/js/legacy/FilterMenu.esm.js
@@ -4,7 +4,12 @@ import {patch} from "@web/core/utils/patch";
 import FilterMenu from "web.FilterMenu";
 import AdvancedFilterItem from "../AdvancedFilterItem.esm";
 
-
+/**
+ * Patches the FilterMenu for legacy widgets.
+ *
+ * Tree views still use this old legacy widget, so we need to patch it.
+ * This is likely to dissapear in 16.0
+ */
 patch(FilterMenu, "web_advanced_search.legacy.FilterMenu", {
     components: {
         ...FilterMenu.components,
