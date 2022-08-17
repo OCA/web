5e4886628348726799eef674c0e999481dab774f
Francisco Javier Luna Vazquez <fluna@vauxoo.com>
2022-08-17 13:41:17 -0500

    [ADD] web_tree_column_width_remember: Remember the tree view columns' widths
    Remember the tree columns' widths across sessions, and after filtering, grouping, or reordering.

---
+++ web_remember_tree_column_width/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name": "Web Remember Tree Column Width",
+    "summary": "Remember the tree columns' widths across sessions.",
+    "author": "Vauxoo, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
+    "license": "LGPL-3",
+    "category": "Extra Tools",
+    "version": "15.0.1.0.0",
+    "maintainers": [
+        "frahikLV",
+        "luisg123v",
+    ],
+    "depends": [
+        "web",
+    ],
+    "data": [],
+    "assets": {
+        "web.assets_backend": [
+            "remember_tree_column_width/static/src/js/list_renderer.js",
+            "remember_tree_column_width/static/src/scss/main.scss",
+        ],
+    },
+    "installable": True,
+}
