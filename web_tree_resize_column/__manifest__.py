3deb44461245f69234363ace4cb2b60d5149fce3
KBui <vancouver2990@gmail.com>
2022-08-17 13:22:48 +0200

    [IMP] web_tree_resize_column: black, isort, prettier

--- web_tree_resize_column/__manifest__.py
+++ web_tree_resize_column/__manifest__.py
@@ -2,18 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Resize Columns',
-    'summary': 'Resize columns in tree views',
-    'category': 'Extra Tools',
-    'version': '12.0.1.0.0',
-    'development_status': 'Production/Stable',
-    'author': 'Onestein, Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/OCA/web',
-    'depends': [
-        'web'
-    ],
-    'data': [
-        'templates/assets.xml',
-    ],
+    "name": "Resize Columns",
+    "summary": "Resize columns in tree views",
+    "category": "Extra Tools",
+    "version": "12.0.1.0.0",
+    "development_status": "Production/Stable",
+    "author": "Onestein, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/web",
+    "depends": ["web"],
+    "data": ["templates/assets.xml",],
 }
--- web_tree_resize_column/static/src/js/backend.js
+++ web_tree_resize_column/static/src/js/backend.js
@@ -1,31 +1,30 @@
 /* Copyright 2019 Onestein
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
 
-odoo.define('web_tree_resize_column.backend', function (require) {
+odoo.define("web_tree_resize_column.backend", function(require) {
     "use strict";
 
-    var ListRenderer = require('web.ListRenderer');
+    var ListRenderer = require("web.ListRenderer");
 
     ListRenderer.include({
-
         /**
          * @override
          */
         _renderView: function() {
             // Preserve width of columns
             var styles = [];
-            this.$el.find('thead th').each(function () {
-                styles.push($(this).attr('style'));
+            this.$el.find("thead th").each(function() {
+                styles.push($(this).attr("style"));
             });
 
             var res = this._super.apply(this, arguments);
 
             // Initialize jQuery plugin
-            this.$el.find('table').resizableColumns();
+            this.$el.find("table").resizableColumns();
 
             // Restore width of columns
-            this.$el.find('thead th').each(function (index, th) {
-                $(th).attr('style', styles[index]);
+            this.$el.find("thead th").each(function(index, th) {
+                $(th).attr("style", styles[index]);
             });
 
             return res;
@@ -36,8 +35,8 @@ odoo.define('web_tree_resize_column.backend', function (require) {
          *
          * @override
          */
-        _onSortColumn: function (event) {
-            if ($(event.target).is('.resizer')) {
+        _onSortColumn: function(event) {
+            if ($(event.target).is(".resizer")) {
                 return;
             }
 
