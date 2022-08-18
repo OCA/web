7505a5206f5d4960a10080222c39bcf1a17c60df
KBui <vancouver2990@gmail.com>
2022-08-18 14:51:22 +0200

    [IMP] web_export_view: black, isort, prettier

--- web_export_view/__manifest__.py
+++ web_export_view/__manifest__.py
@@ -7,24 +7,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Web Export Current View',
-    'version': '12.0.1.0.1',
-    'category': 'Web',
-    'author': 'Henry Zhou, Agile Business Group, \
-            Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/web',
-    'license': 'AGPL-3',
-    'depends': [
-        'web',
-    ],
-    "data": [
-        'security/groups.xml',
-        'views/web_export_view_view.xml',
-    ],
-    'qweb': [
-        "static/src/xml/web_export_view_template.xml",
-    ],
-
-    'installable': True,
-    'auto_install': False,
+    "name": "Web Export Current View",
+    "version": "12.0.1.0.1",
+    "category": "Web",
+    "author": "Henry Zhou, Agile Business Group, \
+            Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
+    "license": "AGPL-3",
+    "depends": ["web",],
+    "data": ["security/groups.xml", "views/web_export_view_view.xml",],
+    "qweb": ["static/src/xml/web_export_view_template.xml",],
+    "installable": True,
+    "auto_install": False,
 }
