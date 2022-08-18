91eb16c0b72b18dad71d9c8a61ae5863c97181de
KBui <vancouver2990@gmail.com>
2022-08-18 10:04:28 +0200

    [IMP] web_widget_color: black, isort, prettier

--- web_widget_color/static/src/js/widget.js
+++ web_widget_color/static/src/js/widget.js
@@ -1,28 +1,28 @@
 /* global jscolor */
-odoo.define('web.web_widget_color', function (require) {
+odoo.define("web.web_widget_color", function(require) {
     "use strict";
 
-    var basic_fields = require('web.basic_fields');
-    var field_registry = require('web.field_registry');
-    var ListRenderer = require('web.ListRenderer');
-    var pyUtils = require('web.py_utils');
+    var basic_fields = require("web.basic_fields");
+    var field_registry = require("web.field_registry");
+    var ListRenderer = require("web.ListRenderer");
+    var pyUtils = require("web.py_utils");
 
     var FieldColor = basic_fields.FieldChar.extend({
-        template: 'FieldColor',
-        widget_class: 'oe_form_field_color',
+        template: "FieldColor",
+        widget_class: "oe_form_field_color",
 
-        _renderReadonly: function () {
+        _renderReadonly: function() {
             // Do Nothing
         },
 
-        _renderEdit: function () {
+        _renderEdit: function() {
             var isRequired = false;
-            if ('required' in this.attrs) {
+            if ("required" in this.attrs) {
                 isRequired = pyUtils.py_eval(this.attrs.required);
             } else {
                 isRequired = this.field.required;
             }
-            this.$input = this.$el.find('input');
+            this.$input = this.$el.find("input");
             this.jscolor = new jscolor(this.$input[0], {
                 hash: true,
                 zIndex: 2000,
@@ -30,19 +30,18 @@ odoo.define('web.web_widget_color', function (require) {
             });
         },
     });
-    field_registry.add('color', FieldColor);
+    field_registry.add("color", FieldColor);
 
     // Deny unselect row if jscolor actived
     ListRenderer.include({
-        unselectRow: function () {
+        unselectRow: function() {
             var canUnselect = true;
             if (this.currentRow !== null) {
                 var record = this.state.data[this.currentRow];
                 var recordWidgets = this.allFieldWidgets[record.id];
-                canUnselect = !_.some(recordWidgets, function (widget) {
+                canUnselect = !_.some(recordWidgets, function(widget) {
                     var $el = widget.getFocusableElement();
-                    return $el instanceof jQuery &&
-                        $el.hasClass('jscolor-active');
+                    return $el instanceof jQuery && $el.hasClass("jscolor-active");
                 });
             }
 
