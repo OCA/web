f03dae6abb6c0486176897d08ea0bca8c0d3afb0
Lois Rilo <lois.rilo@forgeflow.com>
2022-08-24 15:10:59 +0200

    [IMP] black, isort, prettier

--- web_confirm_duplicate/static/src/js/basic_view.js
+++ web_confirm_duplicate/static/src/js/basic_view.js
@@ -1,12 +1,12 @@
 /* Copyright 2022 ForgeFlow S.L.
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
-odoo.define("web_confirm_duplicate.basic_view", function(require) {
+odoo.define("web_confirm_duplicate.basic_view", function (require) {
     "use strict";
 
     var BasicView = require("web.BasicView");
 
     BasicView.include({
-        init: function() {
+        init: function () {
             this._super.apply(this, arguments);
             this.controllerParams.confirmOnDuplicate = true;
         },
--- web_confirm_duplicate/static/src/js/form_controller.js
+++ web_confirm_duplicate/static/src/js/form_controller.js
@@ -1,6 +1,6 @@
 /* Copyright 2022 ForgeFlow S.L.
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
-odoo.define("web_confirm_duplicate", function(require) {
+odoo.define("web_confirm_duplicate", function (require) {
     "use strict";
 
     var FormController = require("web.FormController");
@@ -8,15 +8,15 @@ odoo.define("web_confirm_duplicate", function(require) {
     var _t = require("web.core")._t;
 
     FormController.include({
-        init: function(parent, model, renderer, params) {
+        init: function (parent, model, renderer, params) {
             this._super.apply(this, arguments);
             this.confirmOnDuplicate = params.confirmOnDuplicate;
         },
 
-        _onDuplicateRecord: function() {
+        _onDuplicateRecord: function () {
             var self = this;
             function doIt() {
-                self.model.duplicateRecord(self.handle).then(function(handle) {
+                self.model.duplicateRecord(self.handle).then(function (handle) {
                     self.handle = handle;
                     self._updateEnv();
                     self._setMode("edit");
