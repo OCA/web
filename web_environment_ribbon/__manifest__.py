1ce575096e3b0a245ea80862c7d55e9f8a9d954b
Benoit Aimont <benoit.aimont@acsone.eu>
2022-08-26 09:23:44 +0200

    [MIG] web_environment_ribbon: Migration to 16.0

--- web_environment_ribbon/__manifest__.py
+++ web_environment_ribbon/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "Web Environment Ribbon",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Web",
     "author": "Francesco OpenCode Apruzzese, "
     "Tecnativa, "
