5d27c9daaa3a67f45a33e4cfdffdc4d90716dead
yiğit budak <yibudak@gmail.com>
2022-09-12 21:30:43 +0300

    [12.0][FIX] Fix the manifest website key

--- web_field_monetary_view_precision/__manifest__.py
+++ web_field_monetary_view_precision/__manifest__.py
@@ -2,21 +2,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': "Monetary Field View Precision",
-
     'summary': """
         This module allows to set the view precision on monetary fields.
     """,
-
     "author": "Yiğit Budak, "
               "Odoo Community Association (OCA)",
-    'website': "https://github.com/oca/web/",
-
+    'website': 'https://github.com/OCA/web',
     "license": "AGPL-3",
     "category": "Web",
     'version': '12.0.1.0.0',
-
     'depends': ['base', 'account'],
-
     'data': [
         'views/res_currency_view.xml',
     ]
