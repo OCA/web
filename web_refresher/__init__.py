13e21c405ce7b8c513fcda4476d51979cf69b66c
Hugo Santos <hugo.santos@factorlibre.com>
2022-10-18 10:21:08 +0200

    [MIG] web_refresher: Migration to 16.0

--- web_refresher/__init__.py
+++
@@ -1,9 +0,0 @@
-##############################################################################
-#
-#    Copyright (C) 2019 Compassion CH (http://www.compassion.ch)
-#    Releasing children from poverty in Jesus' name
-#    @author: Samuel Fringeli <samuel.fringeli@me.com>
-#
-#    The licence is in the file __manifest__.py
-#
-##############################################################################
