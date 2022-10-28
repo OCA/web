f27baaf510ee424ff11a5a45f5a8e43b15171814
Hugo Santos <hugo.santos@factorlibre.com>
2022-10-28 11:23:02 +0200

    [MIG] web_sheet_full_width: Migration to 16.0

--- web_sheet_full_width/__manifest__.py
+++ web_sheet_full_width/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Web Sheet Full Width",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "author": "Therp BV, Sudokeys, GRAP, Métal Sartigan, "
     "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
