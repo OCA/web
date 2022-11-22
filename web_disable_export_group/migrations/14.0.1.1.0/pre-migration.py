20d432963d930faf837c4fb42a7534aa5d58f946
Enric Tobella <etobella@creublanca.es>
2022-11-22 16:37:35 +0100

    [OU] web_disable_export_group: Fix when migration happened on 13.0

--- web_disable_export_group/migrations/14.0.1.1.0/pre-migration.py
+++ web_disable_export_group/migrations/14.0.1.1.0/pre-migration.py
@@ -3,12 +3,14 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.rename_xmlids(
-        env.cr,
-        [
-            (
-                "web_disable_export_group.group_export_data",
-                "web_disable_export_group.group_export_xlsx_data",
-            )
-        ],
-    )
+    # Otherwise, the migration already happened on 13.0
+    if not env.ref("web_disable_export_group.group_export_xlsx_data", False):
+        openupgrade.rename_xmlids(
+            env.cr,
+            [
+                (
+                    "web_disable_export_group.group_export_data",
+                    "web_disable_export_group.group_export_xlsx_data",
+                )
+            ],
+        )
