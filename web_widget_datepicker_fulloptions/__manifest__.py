760512a0b0b5db6eab42bc6c29255f4530b33e5a
Alessandro Uffreduzzi <alessandro.uffreduzzi@pytech.it>
2022-12-28 11:56:44 +0100

    [MIG] web_widget_datepicker_fulloptions: Migration to 14.0

--- web_widget_datepicker_fulloptions/__manifest__.py
+++ web_widget_datepicker_fulloptions/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Web Widget DatePicker Full Options",
-    "version": "12.0.1.0.1",
+    "version": "14.0.1.0.0",
     "author": "GRAP, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Web",
