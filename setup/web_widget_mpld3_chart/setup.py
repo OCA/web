c19fba1c732ce7ce239f6c0405b8e0b6d6507a2e
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2023-01-09 17:44:26 +0100

    Preserve requirements.txt
    Such kind of hard pins should be avoided, but I'll leave that to
    maintainers of web_widget_mpld3.

--- setup/web_widget_mpld3_chart/setup.py
+++ setup/web_widget_mpld3_chart/setup.py
@@ -2,5 +2,15 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon=True,
+    odoo_addon={
+        "external_dependencies_override": {
+            "python": {
+                "mpld3": [
+                    "matplotlib==3.0.3; python_version < '3.7'",
+                    "matplotlib==3.4.1; python_version >= '3.7'",
+                    "mpld3==0.3",
+                ],
+            },
+        },
+    },
 )
