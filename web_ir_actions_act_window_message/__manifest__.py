accddc0f8617b064dd14942a54966b072afb6db4
miguels73 <55379877+miguels73@users.noreply.github.com>
2023-01-16 09:28:50 +0100

    [MIG] web_ir_actions_act_window_message: Migration to 16.0

--- web_ir_actions_act_window_message/__manifest__.py
+++ web_ir_actions_act_window_message/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Client side message boxes",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Therp BV, " "ACSONE SA/NV, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
