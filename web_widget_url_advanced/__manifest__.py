d63c28b152d783b37b2c1568d8c79848ca8dbeb3
dsolanki <dhara.solanki@initos.com>
2023-03-07 15:16:40 +0530

    [IMP] web_widget_url_advanced: pre-commit stuff

--- web_widget_url_advanced/__manifest__.py
+++ web_widget_url_advanced/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": "This module extends URL widget "
     "for displaying anchors with custom labels.",
     "category": "Web",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "LGPL-3",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
