3716b69a1b40c6130545f3fd9513eaca074403b6
dsolanki <dhara.solanki@initos.com>
2023-03-15 14:34:41 +0530

    [IMP] web_widget_numeric_step: pre-commit stuff

--- web_widget_numeric_step/__manifest__.py
+++ web_widget_numeric_step/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Web Widget Numeric Step",
     "category": "web",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "GRAP, Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/web",
