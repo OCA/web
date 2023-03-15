c43d9ff18c19e48131fa03aae802003ede59c8a9
ferran-73 <ferran@studio73.es>
2023-03-15 08:32:46 +0100

    [14.0][MIG] web_remember_tree_column_width: backport from 15.0

--- web_remember_tree_column_width/__manifest__.py
+++ web_remember_tree_column_width/__manifest__.py
@@ -5,20 +5,12 @@
     "website": "https://github.com/OCA/web",
     "license": "LGPL-3",
     "category": "Extra Tools",
-    "version": "15.0.1.0.1",
+    "version": "14.0.1.0.0",
     "maintainers": [
         "frahikLV",
         "luisg123v",
     ],
-    "depends": [
-        "web",
-    ],
-    "data": [],
-    "assets": {
-        "web.assets_backend": [
-            "web_remember_tree_column_width/static/src/js/list_renderer.js",
-            "web_remember_tree_column_width/static/src/scss/main.scss",
-        ],
-    },
+    "depends": ["web"],
+    "data": ["views/assets.xml"],
     "installable": True,
 }
