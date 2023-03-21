65b6571404e6a70d7d701947c1d8d325d60e3852
Andrea Stirpe <a.stirpe@onestein.nl>
2023-03-21 14:25:18 +0100

    [16.0][MIG] web_no_bubble

--- web_no_bubble/__manifest__.py
+++ web_no_bubble/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Web No Bubble",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Savoir-faire Linux, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
