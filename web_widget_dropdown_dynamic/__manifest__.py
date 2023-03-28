92236838c501379acf603ab7a959dabd4978ed1e
Goncalo Brito <gbrito@erpopen.nl>
2023-03-28 10:51:04 +0200

    [16.0][MIG] web_widget_dropdown_dynamic

--- web_widget_dropdown_dynamic/__manifest__.py
+++ web_widget_dropdown_dynamic/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Dynamic Dropdown Widget",
     "summary": "This module adds support for dynamic dropdown widget",
     "category": "Web",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "CorporateHub, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
