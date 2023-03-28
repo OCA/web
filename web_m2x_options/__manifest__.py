4c380a3224c2b0b14fef66b3a6dbf9aa1f2db8e6
Diep Huu Hoang <hoang@trobz.com>
2023-03-28 12:48:05 +0700

    [16.0][MIG] web_m2x_options: Migration to 16.0

--- web_m2x_options/__manifest__.py
+++ web_m2x_options/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "web_m2x_options",
-    "version": "15.0.1.1.0",
+    "version": "16.0.1.1.0",
     "category": "Web",
     "author": "initOS GmbH,"
     "ACSONE SA/NV, "
@@ -16,12 +16,6 @@
     "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
     "depends": ["web"],
-    "assets": {
-        "web.assets_backend": [
-            "web_m2x_options/static/src/js/form.js",
-            "web_m2x_options/static/src/js/ir_options.js",
-        ],
-        "web.assets_qweb": ["web_m2x_options/static/src/xml/base.xml"],
-    },
+    "assets": {"web.assets_backend": ["web_m2x_options/static/src/components/*"]},
     "installable": True,
 }
