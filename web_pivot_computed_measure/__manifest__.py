c743df7d01f8494000ae92231065b65ea5e5beef
Carlos Roca <carlos.roca@tecnativa.com>
2023-05-09 08:31:42 +0200

    [FIX] web_pivot_computed_measure: Error when using filter created with favorites

--- web_pivot_computed_measure/__manifest__.py
+++ web_pivot_computed_measure/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Web Pivot Computed Measure",
     "category": "web",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/web",
