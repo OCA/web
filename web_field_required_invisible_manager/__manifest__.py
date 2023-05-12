5f0d00a2cc2482709c7d6fac2d917a6798a80620
Ilyas <irazor147@gmail.com>
2023-05-12 13:48:02 +0200

    [ADD] web_field_required_invisible_manager

---
+++ web_field_required_invisible_manager/__manifest__.py
@@ -0,0 +1,15 @@
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+{
+    "name": "Web Field Required Invisible Manager",
+    "category": "Web",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Ilyas, ooops404, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
+    "maintainers": ["ilyasProgrammer"],
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/views.xml",
+    ],
+}
