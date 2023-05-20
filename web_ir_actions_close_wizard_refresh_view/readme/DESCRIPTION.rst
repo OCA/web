58ce0077f03d978b862bcd41768f1ace6bd2f5f8
bobslee <bob@novacode.nl>
2023-05-20 13:47:49 +0200

    [16.0][MIG] web_ir_actions_close_wizard_refresh_view

---
+++ web_ir_actions_close_wizard_refresh_view/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Let reload the data in a view, while closing a wizard, without reloading the page.
---
+++ web_ir_actions_close_wizard_refresh_view/static/src/js/web_ir_actions_close_wizard_refresh_view.js
@@ -0,0 +1,18 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+
+async function executeCloseAndRefreshView({ env, action, options }) {
+    // env.services.ui.block();
+    const actionService = env.services['action'];
+    const originalAction = action._originalAction;
+    return actionService.doAction(
+        {'type': "ir.actions.act_window_close"},
+        {'onClose': function() {actionService.doAction(originalAction);}}
+    );
+}
+
+registry
+    .category("action_handlers")
+    .add("ir.actions.close_wizard_refresh_view", executeCloseAndRefreshView);
