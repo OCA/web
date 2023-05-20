64f8d3b522efb18aa436752f8e82b1bb088ac908
bobslee <bob@novacode.nl>
2023-05-20 14:21:37 +0200

    [IMP] eslint

--- web_ir_actions_close_wizard_refresh_view/static/src/js/web_ir_actions_close_wizard_refresh_view.esm.js
+++ web_ir_actions_close_wizard_refresh_view/static/src/js/web_ir_actions_close_wizard_refresh_view.esm.js
@@ -4,8 +4,7 @@ import {registry} from "@web/core/registry";
 import {useService} from "@web/core/utils/hooks";
 
 async function executeCloseAndRefreshView({env, action, options}) {
-    // env.services.ui.block();
-    const actionService = env.services["action"];
+    const actionService = env.services.action;
     const originalAction = action._originalAction;
     return actionService.doAction(
         {type: "ir.actions.act_window_close"},
