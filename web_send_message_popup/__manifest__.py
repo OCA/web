0ce97b7ad4b30836b88f88f55a13f94683f2f18e
Du-ma <domantas@vialaurea.lt>
2023-05-26 11:26:32 +0000

    [MIG] web_send_message_popup: Migration to 15.0

--- web_send_message_popup/__manifest__.py
+++ web_send_message_popup/__manifest__.py
@@ -2,12 +2,14 @@
 # License AGPL-3.0 or later (http://gnu.org/licenses/agpl).
 {
     "name": "Web Send Message as Popup",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainer": "Camptocamp",
     "license": "AGPL-3",
     "category": "Hidden",
     "depends": ["web", "mail"],
     "website": "https://github.com/OCA/web",
-    "data": ["templates/assets.xml"],
+    "assets": {
+        "web.assets_backend": ["web_send_message_popup/static/src/models/**/*.js"]
+    },
 }
