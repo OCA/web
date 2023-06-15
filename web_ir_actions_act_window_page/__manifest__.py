47d3098a455950afa5c44d692f6a74c8dd54fdd5
Holger Brunn <mail@hunki-enterprises.com>
2023-06-15 13:58:45 +0200

    [MIG] web_ir_actions_act_window_page

--- web_ir_actions_act_window_page/__manifest__.py
+++ web_ir_actions_act_window_page/__manifest__.py
@@ -1,23 +1,18 @@
-# coding: utf-8
 # © 2013-2015 Therp BV (<http://therp.nl>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 {
     "name": "Window actions for client side paging",
-    "summary": ("Allows a developer to trigger a pager to show the previous "
-                "or next next record in the form view"),
-    "author": "Therp BV,Odoo Community Association (OCA)",
-    "version": "10.0.1.0.0",
+    "summary": (
+        "Allows a developer to trigger a pager to show the previous "
+        "or next next record in the form view"
+    ),
+    "author": "Hunki Enterprises BV, Therp BV,Odoo Community Association (OCA)",
+    "version": "13.0.1.0.0",
     "category": "Technical",
-    "depends": [
-        'web',
-    ],
-    'data': [
-        'views/assets.xml',
-    ],
-    'demo': [
-        'demo/demo_action.xml',
-    ],
-    'installable': True,
-    'license': 'AGPL-3',
-    'url': 'https://github.com/oca/web',
+    "depends": ["web"],
+    "data": ["views/assets.xml"],
+    "demo": ["demo/demo_action.xml"],
+    "installable": True,
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/web",
 }
