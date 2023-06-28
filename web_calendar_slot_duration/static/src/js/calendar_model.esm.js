05d14f56b6e54ae89bb061ddbd5e4f9ebc44ce3d
Nils Coenen <74965194+NICO-SOLUTIONS@users.noreply.github.com>
2023-06-28 17:54:47 +0200

    [MIG] resource_booking: Migration to 16.0

--- web_calendar_slot_duration/static/src/js/calendar_model.esm.js
+++ web_calendar_slot_duration/static/src/js/calendar_model.esm.js
@@ -1,20 +1,23 @@
 /* Copyright 2021 Tecnativa - Jairo Llopis
  * License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl). */
 
-odoo.define("web_calendar_slot_duration.CalendarModel", function (require) {
-    var CalendarModel = require("web.CalendarModel");
+odoo.define("web_calendar_slot_duration.CalendarController", function (require) {
+    var CalendarController = require("web.CalendarController");
 
-    CalendarModel.include({
+    CalendarController.include({
         /**
          * @override
          */
         _getFullCalendarOptions: function () {
-            var result = this._super.call(this, arguments);
+            var result = this._super.apply(this, arguments);
             result.slotDuration =
-                this.data.context.calendar_slot_duration ||
+                this.model.get(this.handle, {raw: true}).context
+                    .calendar_slot_duration ||
                 result.slotDuration ||
                 "00:30:00";
             return result;
         },
     });
+
+    return CalendarController;
 });
