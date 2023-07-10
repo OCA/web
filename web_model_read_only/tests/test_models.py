e542d6d62fba19b9836c1c43e20004e8a8ab3f91
Andrey <andrejsolodovnikov699@gmail.com>
2023-07-10 21:32:14 +0300

    [14.0][IMP] web_model_read_only
    Add msg for each assertEqual

--- web_model_read_only/tests/test_models.py
+++ web_model_read_only/tests/test_models.py
@@ -33,10 +33,22 @@ class TestIrModel(SavepointCase):
         """Tests check_access_rights function for readonly group"""
 
         model = self.partner_model.with_user(self.user_readonly)
-        self.assertTrue(model.check_access_rights("read", False))
-        self.assertFalse(model.check_access_rights("write", False))
-        self.assertFalse(model.check_access_rights("create", False))
-        self.assertFalse(model.check_access_rights("unlink", False))
+        self.assertTrue(
+            model.check_access_rights("read", False),
+            msg="Read access right should be True",
+        )
+        self.assertFalse(
+            model.check_access_rights("write", False),
+            msg="Write access right should be False",
+        )
+        self.assertFalse(
+            model.check_access_rights("create", False),
+            msg="Create access right should be False",
+        )
+        self.assertFalse(
+            model.check_access_rights("unlink", False),
+            msg="Unlink access right should be False",
+        )
 
     def test_check_access_rights_manager(self):
         """Tests check_access_rights function for manager group
@@ -49,29 +61,63 @@ class TestIrModel(SavepointCase):
         test_record = self.env["res.partner"].create({"name": "Test Record"})
         test_record = test_record.with_user(self.manager)
         # group_manager have access to all operations by default
-        self.assertNotIn(self.group_manager, self.partner_model.readonly_group_ids)
         self.assertNotIn(
-            self.group_partner_manager, self.partner_model.readonly_group_ids
+            self.group_manager,
+            self.partner_model.readonly_group_ids,
+            msg="Group Manager should not be in readonly_group_ids",
         )
-        self.assertTrue(test_record.check_access_rights("read", False))
-        self.assertTrue(test_record.check_access_rights("write", False))
-        self.assertTrue(test_record.check_access_rights("create", False))
-        self.assertTrue(test_record.check_access_rights("unlink", False))
+        self.assertNotIn(
+            self.group_partner_manager,
+            self.partner_model.readonly_group_ids,
+            msg="Group Partner Manager should not be in readonly_group_ids",
+        )
+        self.assertTrue(
+            test_record.check_access_rights("read", False),
+            msg="Read access right should be True",
+        )
+        self.assertTrue(
+            test_record.check_access_rights("write", False),
+            msg="Write access right should be False",
+        )
+        self.assertTrue(
+            test_record.check_access_rights("create", False),
+            msg="Create access right should be False",
+        )
+        self.assertTrue(
+            test_record.check_access_rights("unlink", False),
+            msg="Unlink access right should be False",
+        )
 
         # Add group_manager to readonly_group_ids
         self.partner_model.write({"readonly_group_ids": [(4, self.group_manager.id)]})
-        self.assertIn(self.group_manager, self.partner_model.readonly_group_ids)
-        self.assertTrue(test_record.check_access_rights("read", False))
+        self.assertIn(
+            self.group_manager,
+            self.partner_model.readonly_group_ids,
+            msg="Group Manager must be in readonly_group_ids",
+        )
+        self.assertTrue(
+            test_record.check_access_rights("read", False),
+            msg="Read access right should be True",
+        )
 
         # If raise_exception is not expected, function will return False
-        self.assertFalse(test_record.check_access_rights("write", False))
-        self.assertFalse(test_record.check_access_rights("create", False))
-        self.assertFalse(test_record.check_access_rights("unlink", False))
+        self.assertFalse(
+            test_record.check_access_rights("write", False),
+            msg="Write access right should be False",
+        )
+        self.assertFalse(
+            test_record.check_access_rights("create", False),
+            msg="Create access right should be False",
+        )
+        self.assertFalse(
+            test_record.check_access_rights("unlink", False),
+            msg="Unlink access right should be False",
+        )
 
         # If raise_exception is expected, function will return AccessError
-        with self.assertRaises(AccessError):
+        with self.assertRaises(AccessError, msg="Must Raise Access Error"):
             test_record.check_access_rights("write", raise_exception=True)
-        with self.assertRaises(AccessError):
+        with self.assertRaises(AccessError, msg="Must Raise Access Error"):
             test_record.check_access_rights("create", raise_exception=True)
-        with self.assertRaises(AccessError):
+        with self.assertRaises(AccessError, msg="Must Raise Access Error"):
             test_record.check_access_rights("unlink", raise_exception=True)
