8c067155aef9c67ff1f8d499a97cae9f8ea1a10f
SergiCForgeFlow <sergi.casau@forgeflow.com>
2023-07-18 13:53:28 +0200

    [MIG] web_chatter_position: Migration to 13.0

--- web_chatter_position/__manifest__.py
+++ web_chatter_position/__manifest__.py
@@ -4,22 +4,15 @@
 {
     "name": "Chatter Position",
     "summary": "Add an option to change the chatter position",
-    "version": "15.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Hynsys Technologies, Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "license": "LGPL-3",
     "category": "Extra Tools",
     "images": ["static/description/images/web_chatter_position.png"],
     "depends": ["web", "mail"],
-    "data": ["views/res_users.xml", "views/web.xml"],
-    "assets": {
-        "web.assets_backend": [
-            "/web_chatter_position/static/src/scss/chatter_position.scss",
-            "/web_chatter_position/static/src/scss/attachment_viewer.scss",
-            "/web_chatter_position/static/src/js/form_chatter_position.js",
-        ],
-        "web.assets_qweb": ["/web_chatter_position/static/src/xml/form_buttons.xml"],
-    },
+    "data": ["views/res_users.xml", "views/web.xml", "views/assets.xml"],
+    "qweb": ["static/src/xml/form_buttons.xml"],
     "installable": True,
     "auto_install": False,
 }
