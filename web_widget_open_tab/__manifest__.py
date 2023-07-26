aebf1b5bf655a43c2ff5825bde1fadabaeea8be5
Aungkokolin1997 <lin@quartile.co>
2023-07-26 12:10:56 +0630

    [IMP] web_widget_open_tab

--- web_widget_open_tab/__manifest__.py
+++ web_widget_open_tab/__manifest__.py
@@ -5,12 +5,15 @@
     "name": "Widget Open on new Tab",
     "summary": """
         Allow to open record from trees on new tab from tree views""",
-    "version": "16.0.1.0.0",
+    "version": "16.0.2.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "depends": ["web"],
     "demo": ["demo/res_users_view.xml"],
+    "data": [
+        "views/ir_model_views.xml",
+    ],
     "assets": {
         "web.assets_backend": [
             "web_widget_open_tab/static/src/xml/open_tab_widget.xml",
