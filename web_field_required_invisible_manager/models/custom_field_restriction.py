245f1f1d8800f0f6bc5056ca3e3be2d8a6ce2e0d
Ilyas <irazor147@gmail.com>
2023-07-26 10:58:34 +0200

    [FIX] web_field_required_invisible_manager: reset on change

--- web_field_required_invisible_manager/models/custom_field_restriction.py
+++ web_field_required_invisible_manager/models/custom_field_restriction.py
@@ -1,4 +1,4 @@
-# Copyright 2020 ooops404
+# Copyright 2023 ooops404
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 from odoo import api, fields, models
 
@@ -13,13 +13,11 @@ class CustomFieldRestriction(models.Model):
         required=True,
         string="Field",
     )
-
     field_name = fields.Char(
         related="field_id.name",
         store=True,
         string="Field Name",
     )
-
     required_model_id = fields.Many2one(
         "ir.model",
         ondelete="cascade",
@@ -32,7 +30,12 @@ class CustomFieldRestriction(models.Model):
         string="invisible_model_id",
         index=True,
     )
-
+    readonly_model_id = fields.Many2one(
+        "ir.model",
+        ondelete="cascade",
+        string="readonly_model_id",
+        index=True,
+    )
     model_name = fields.Char(
         compute="_compute_model_name",
         store=True,
@@ -41,18 +44,77 @@ class CustomFieldRestriction(models.Model):
     )
     condition_domain = fields.Char()
     group_ids = fields.Many2many("res.groups", required=True)
-    required = fields.Boolean()
     default_required = fields.Boolean(related="field_id.required")
+    required = fields.Boolean()
     field_invisible = fields.Boolean()
+    field_readonly = fields.Boolean()
+    # generated technical fields used in form attrs:
+    visibility_field_id = fields.Many2one("ir.model.fields", ondelete="cascade")
+    readonly_field_id = fields.Many2one("ir.model.fields", ondelete="cascade")
+    required_field_id = fields.Many2one("ir.model.fields", ondelete="cascade")
 
     @api.onchange("field_id")
     def onchange_field_id(self):
-        self.required = self.field_id.required
+        self.update(
+            {
+                "required": self.field_id.required,
+                "field_invisible": False,
+                "field_readonly": self.field_id.readonly,
+            }
+        )
 
-    @api.depends("required_model_id", "invisible_model_id")
+    @api.depends("required_model_id", "invisible_model_id", "readonly_model_id")
     def _compute_model_name(self):
         for rec in self:
             if rec.required_model_id:
                 rec.model_name = rec.required_model_id.model
             elif rec.invisible_model_id:
                 rec.model_name = rec.invisible_model_id.model
+            elif rec.readonly_model_id:
+                rec.model_name = rec.readonly_model_id.model
+
+    @api.model
+    def create(self, vals):
+        rec = super().create(vals)
+        if rec.invisible_model_id and rec.field_invisible:
+            rec.create_restriction_field("visibility")
+        elif rec.readonly_model_id and rec.field_readonly:
+            rec.create_restriction_field("readonly")
+        elif rec.required_model_id and rec.required:
+            rec.create_restriction_field("required")
+        return rec
+
+    def create_restriction_field(self, f_type):
+        field_name = self.get_field_name(f_type)
+        field_id = self.env["ir.model.fields"].search([("name", "=", field_name)])
+        if f_type == "required":
+            rec_model_id = self.required_model_id.id
+            rec_field_name = "required_field_id"
+        elif f_type == "readonly":
+            rec_model_id = self.readonly_model_id.id
+            rec_field_name = "readonly_field_id"
+        elif f_type == "visibility":
+            rec_model_id = self.invisible_model_id.id
+            rec_field_name = "visibility_field_id"
+        if not field_id:
+            field_id = self.env["ir.model.fields"].create(
+                {
+                    "name": field_name,
+                    "model_id": rec_model_id,
+                    "state": "manual",
+                    "field_description": "%s %s field" % (self.field_id.name, f_type),
+                    "store": False,
+                    "ttype": "boolean",
+                    "compute": "for r in self: r._compute_restrictions_fields()",
+                }
+            )
+        self[rec_field_name] = field_id
+
+    def get_field_name(self, f_type):
+        # e.g. x_computed_res_partner_name_readonly
+        res = "x_computed_%s_%s_%s" % (
+            self.field_id.model.replace(".", "_"),
+            self.field_id.name,
+            f_type,
+        )
+        return res
--- web_field_required_invisible_manager/models/models.py
+++ web_field_required_invisible_manager/models/models.py
@@ -1,4 +1,4 @@
-# Copyright 2020 ooops404
+# Copyright 2023 ooops404
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 import json as simplejson
 
@@ -19,6 +19,10 @@ class IrModel(models.Model):
         "custom.field.restriction",
         "invisible_model_id",
     )
+    custom_readonly_restriction_ids = fields.One2many(
+        "custom.field.restriction",
+        "readonly_model_id",
+    )
 
 
 class Base(models.AbstractModel):
@@ -28,52 +32,98 @@ class Base(models.AbstractModel):
     def fields_view_get(
         self, view_id=None, view_type=False, toolbar=False, submenu=False
     ):
-        res = super().fields_view_get(
+        arch = super().fields_view_get(
             view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu
         )
-        if view_type not in ["form", "tree", "kanban"]:
-            return res
+        if view_type not in ["form", "tree"]:
+            return arch
         # TODO speed up somehow
         restrictions = self.env["custom.field.restriction"].search(
             [
-                "|",
                 ("model_name", "=", self._name),
                 ("group_ids", "in", self.env.user.groups_id.ids),
             ]
         )
         if not restrictions:
-            return res
-        doc = etree.XML(res["arch"])
+            return arch
+        else:
+            return self.create_restrictions_fields(restrictions, view_type, arch)
+        return arch
+
+    def create_restrictions_fields(self, restrictions, view_type, arch):
+        doc = etree.XML(arch["arch"])
         for node in doc.xpath("//field"):
             name = node.attrib.get("name")
             restrictions_filtered = restrictions.filtered(
                 lambda x: x.field_id.name == name
             )
+            if not restrictions_filtered:
+                continue
             for r in restrictions_filtered:
-                if (
-                    view_type == "form"
-                    and self.env.context.get("params")
-                    and self.env.context["params"].get("id")
-                ):
-                    rec_id = self.env[r.model_name].browse(
-                        self.env.context["params"]["id"]
+                field_node_str = "<field name='%s' invisible='1'/>"
+                field_node_mod = bytes(
+                    '{"invisible": true,"column_invisible": true}', "utf-8"
+                )
+                if view_type == "tree":
+                    field_node_str = (
+                        "<field name='%s' column_invisible='1' optional='hide'/>"
                     )
-                    if r.condition_domain:
-                        filtered_rec_id = rec_id.filtered_domain(
-                            safe_eval(r.condition_domain)
-                        )
-                        if not filtered_rec_id:
-                            continue
-                if r.required:
-                    node.set("required", "1")
+                if r.field_invisible and r.invisible_model_id:
                     modifiers = simplejson.loads(node.get("modifiers"))
-                    modifiers["required"] = True
+                    visibility_field_name = r.get_field_name("visibility")
+                    modifiers["invisible"] = (
+                        "[('%s', '=', True)]" % visibility_field_name
+                    )
                     node.set("modifiers", simplejson.dumps(modifiers))
-                    res["arch"] = etree.tostring(doc)
-                if r.field_invisible:
-                    node.set("invisible", "1")
+                    new_node = etree.fromstring(field_node_str % visibility_field_name)
+                    new_node.set("invisible", "1")
+                    new_node.set("modifiers", field_node_mod)
+                    node.getparent().append(new_node)
+                if r.required_field_id and r.required_model_id:
                     modifiers = simplejson.loads(node.get("modifiers"))
-                    modifiers["invisible"] = True
+                    required_field_name = r.get_field_name("required")
+                    modifiers["required"] = "[('%s', '=', True)]" % required_field_name
                     node.set("modifiers", simplejson.dumps(modifiers))
-                    res["arch"] = etree.tostring(doc)
-        return res
+                    new_node = etree.fromstring(field_node_str % required_field_name)
+                    new_node.set("invisible", "1")
+                    new_node.set("modifiers", field_node_mod)
+                    node.getparent().append(new_node)
+                if r.readonly_field_id and r.readonly_model_id:
+                    modifiers = simplejson.loads(node.get("modifiers"))
+                    readonly_field_name = r.get_field_name("readonly")
+                    modifiers["readonly"] = "[('%s', '=', True)]" % readonly_field_name
+                    node.set("modifiers", simplejson.dumps(modifiers))
+                    new_node = etree.fromstring(field_node_str % readonly_field_name)
+                    new_node.set("invisible", "1")
+                    new_node.set("modifiers", field_node_mod)
+                    node.getparent().append(new_node)
+            arch["arch"] = etree.tostring(doc)
+        return arch
+
+    def _compute_restrictions_fields(self):
+        """Common compute method for all restrictions types"""
+        for record in self:
+            restrictions = self.env["custom.field.restriction"].search(
+                [("model_name", "=", self._name)]
+            )
+            if not restrictions:
+                return
+            for r in restrictions:
+                if r.visibility_field_id:
+                    field_name = r.visibility_field_id.name
+                    record[field_name] = False
+                if r.required_field_id:
+                    field_name = r.required_field_id.name
+                    record[field_name] = False
+                if r.readonly_field_id:
+                    field_name = r.readonly_field_id.name
+                    record[field_name] = False
+                if r.condition_domain:
+                    filtered_rec_id = record.filtered_domain(
+                        safe_eval(r.condition_domain)
+                    )
+                    if filtered_rec_id and r.group_ids & self.env.user.groups_id:
+                        record[field_name] = True
+                elif r.group_ids:
+                    if r.group_ids & self.env.user.groups_id:
+                        record[field_name] = True
