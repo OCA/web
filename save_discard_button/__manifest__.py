404bf4911e0f06951c10fe77342078b1bfb4282e
Sam <sam@synconics.in>
2023-07-29 18:34:50 +0530

    [ADD] save_discard_button module
    [MOD] Index
    
    [MOD] Menifest
    
    [ADD] readme folder
    
    [MOD] readme folder
    
    [RID] unnecessary things

---
+++ save_discard_button/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2023-TODAY Synconics Technologies Pvt. Ltd. (<http://www.synconics.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Save & Discard Buttons",
+    "version": "16.0.1.0.1",
+    "summary": "Save & Discard Buttons",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "author": "Synconics Technologies Pvt. Ltd., Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
+    "maintainers": ["synconics"],
+    "depends": ["web"],
+    "data": [],
+    "images": ["static/description/main_screen.png"],
+    "assets": {
+        "web.assets_backend": [
+            "save_discard_button/static/src/scss/indicator_button.scss"
+        ],
+    },
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
