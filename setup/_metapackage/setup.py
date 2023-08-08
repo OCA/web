0ec7c17208b441f4035ab2fc590635efff3d944a
Sam <sam@synconics.in>
2023-08-08 16:01:49 +0530

    [Rename] Module
    [Resolve] pre-commit
    
    [ADD] Module translation
    
    Added translation using Weblate (Dutch)
    
    Translated using Weblate (Dutch)
    
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: web-16.0/web-16.0-web_advanced_search
    Translate-URL: https://translation.odoo-community.org/projects/web-16-0/web-16-0-web_advanced_search/nl/
    
    Translated using Weblate (Dutch)
    
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: web-16.0/web-16.0-web_company_color
    Translate-URL: https://translation.odoo-community.org/projects/web-16-0/web-16-0-web_company_color/nl/
    
    Translated using Weblate (Dutch)
    
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: web-16.0/web-16.0-web_chatter_position
    Translate-URL: https://translation.odoo-community.org/projects/web-16-0/web-16-0-web_chatter_position/nl/
    
    [IMP] Terminology
    
    move all addons to __unported__, set all addons as not installable
    
    [PRT] Port 8.0 of web_ir_actions_act_window_page
    
    [FIX] Unwellformed RST
    
    [IMP] Shorten summary a bit for cosmetic reasons
    
    [IMP] Description
    
    [IMP] Remove empty line
    
    Add OCA as author of OCA addons
    
    In order to get visibility on https://www.odoo.com/apps the OCA board has
    decided to add the OCA as author of all the addons maintained as part of the
    association.
    
    [UPD] prefix versions with 8.0
    
    [MIG] Make modules uninstallable
    
    [MIG] Rename manifest files
    
    run onclose handler if we got one
    
    [MIG][10.0] web_ir_actions_act_window_page
    
    [ADD] Demo data for testing
    
    OCA Transbot updated translations from Transifex
    
    [UPD] Update web_ir_actions_act_window_page.pot
    
    [13.0][MIG] web_ir_actions_act_window_page
    
    [16.0][MIG] web_ir_actions_act_window_page
    
    [UPD] Update web_ir_actions_act_window_page.pot
    
    [UPD] addons table in README.md
    
    [UPD] README.rst
    
    [ADD] setup.py
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: web-16.0/web-16.0-web_ir_actions_act_window_page
    Translate-URL: https://translation.odoo-community.org/projects/web-16-0/web-16-0-web_ir_actions_act_window_page/

--- setup/_metapackage/setup.py
+++ setup/_metapackage/setup.py
@@ -17,6 +17,7 @@ setuptools.setup(
         'odoo-addon-web_domain_field>=16.0dev,<16.1dev',
         'odoo-addon-web_environment_ribbon>=16.0dev,<16.1dev',
         'odoo-addon-web_help>=16.0dev,<16.1dev',
+        'odoo-addon-web_ir_actions_act_window_page>=16.0dev,<16.1dev',
         'odoo-addon-web_m2x_options>=16.0dev,<16.1dev',
         'odoo-addon-web_no_bubble>=16.0dev,<16.1dev',
         'odoo-addon-web_notify>=16.0dev,<16.1dev',
