de9730de86cfd3d6770dd5c40b086009b2bf24f6
Michael Tietz <mtietz@mt-software.de>
2023-10-04 06:59:40 +0200

    [IMP] web_assets_warmup: Add database lock in post_load_hook
    With dedicated services for odoo web and odoo cron
    we can get a serialize access error on startup

--- web_assets_warmup/hooks.py
+++ web_assets_warmup/hooks.py
@@ -1,8 +1,11 @@
 # Copyright 2020 Camptocamp SA
+# Copyright 2023 Michael Tietz (MT Software) <mtietz@mt-software.de>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 import logging
 import os
+
+import psycopg2
 
 from odoo import fields, registry
 
@@ -32,20 +35,35 @@ def active_cron_assets():
             model, res_id = row
             if model != "ir.cron":
                 return
-            query = """
-                UPDATE ir_cron
-                SET active=true, nextcall=%s, priority=%s
-                WHERE id=%s
-            """
-            nextcall = fields.Datetime.to_string(fields.Datetime.now())
-            args = (nextcall, -99, res_id)
-            cr.execute(query, args)
-            logger.info(
-                "Cron '%s.%s' planned for execution at %s",
-                cron_module,
-                cron_ref,
-                nextcall,
-            )
+            # if there is already someone doing the same or already being executed
+            # we can skip the update of ir_cron
+            try:
+                with cr.savepoint():
+                    cr.execute(
+                        "SELECT * FROM ir_cron WHERE id = %s FOR UPDATE NOWAIT;",
+                        (res_id,),
+                    )
+                    query = """
+                        UPDATE ir_cron
+                        SET active=true, nextcall=%s, priority=%s
+                        WHERE id=%s
+                    """
+                    nextcall = fields.Datetime.to_string(fields.Datetime.now())
+                    args = (nextcall, -99, res_id)
+                    cr.execute(query, args)
+                    logger.info(
+                        "Cron '%s.%s' planned for execution at %s",
+                        cron_module,
+                        cron_ref,
+                        nextcall,
+                    )
+            except psycopg2.OperationalError as e:
+                if e.pgcode == "55P03":
+                    logger.info(
+                        "Cron '%s.%s' is currently being executed or updated",
+                        cron_module,
+                        cron_ref,
+                    )
 
 
 def post_load_hook():
