8f15d040fb777418f9e188644a8158b108d4275e
Guillaume Pothier <gpothier@gmail.com>
2023-11-02 00:59:31 -0300

    fix(web_responsive): only show apps menu on startup if no action is specified in the URL
    Otherwise, direct links to any specific place in Odoo opens the Apps menu instead of directly showing the desired place.

--- web_responsive_enterprise/static/src/components/apps_menu/apps_menu.esm.js
+++ web_responsive_enterprise/static/src/components/apps_menu/apps_menu.esm.js
@@ -3,11 +3,13 @@
  * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl). */
 
 import {NavBar} from "@web/webclient/navbar/navbar";
+import {useService} from "@web/core/utils/hooks";
 import {WebClient} from "@web/webclient/webclient";
 import {patch} from "@web/core/utils/patch";
 import {WebClientEnterprise} from "@web_enterprise/webclient/webclient";
 import {AppsMenu} from "@web_responsive/components/apps_menu/apps_menu.esm";
 
+// Restore original NavBar component
 Object.assign(WebClientEnterprise.components, {NavBar});
 
 // Do not show enterprise's home menu (restoring original WebClient behavior)
@@ -19,7 +21,13 @@ patch(WebClientEnterprise.prototype, "web_responsive_enterprise._loadDefaultApp"
 
 // Show apps menu instead of initial app when page loads
 patch(AppsMenu.prototype, "web_responsive_enterprise._getInitialOpenState", {
+    setup() {
+        this.router = useService("router");
+        this._super();
+    },
     _getInitialOpenState() {
-        return true;
+        // Open apps menu on startup if no action is specified in the URL
+        const { hash } = this.router.current;
+        return hash.action === undefined;
     },
 });
