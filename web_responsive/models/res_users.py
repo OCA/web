8a1dec8e55d43c4fb08c73b8bb9775ced846aa2b
Guillaume Pothier <gpothier@gmail.com>
2023-11-03 22:21:28 -0300

    fix(web_responsive): manually fix lint error

--- web_responsive/models/res_users.py
+++ web_responsive/models/res_users.py
@@ -11,11 +11,13 @@ class ResUsers(models.Model):
             ("always", "Always"),
         ],
         string="Show apps menu on load",
-        help="""Determines whether to show the Apps Menu or the default app when Odoo is initially loaded.
+        help="""Determines whether to show the Apps Menu or the default app when Odoo
+        is initially loaded.
         The default behaviour is to show the initial app.
-        The option to show the Apps Menu when no action is specified usually works quite well, but could
-        be problematic in some cases.
-        The option to always show the Apps Menu is not recommended, but it is included for completeness.
+        The option to show the Apps Menu when no action is specified usually works
+        quite well, but could be problematic in some cases.
+        The option to always show the Apps Menu is not recommended, but it is included
+        for completeness.
         The page must be manually reloaded for this option to take effect.""",
         default="default",
     )
