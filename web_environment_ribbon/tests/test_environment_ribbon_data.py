6ab8325e48c402348c1f67498acb272e16c47eb1
Raf Ven <raf.ven@dynapps.be>
2023-11-14 14:13:24 +0100

    [MIG] web_environment_ribbon: Migration to 17.0

--- web_environment_ribbon/tests/test_environment_ribbon_data.py
+++ web_environment_ribbon/tests/test_environment_ribbon_data.py
@@ -7,7 +7,7 @@ from odoo.tests import common
 class TestEnvironmentRibbonData(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
-        super(TestEnvironmentRibbonData, cls).setUpClass()
+        super().setUpClass()
         cls.env["ir.config_parameter"].set_param("ribbon.name", "Test Ribbon {db_name}")
         cls.env["ir.config_parameter"].set_param("ribbon.color", "#000000")
         cls.env["ir.config_parameter"].set_param("ribbon.background.color", "#FFFFFF")
