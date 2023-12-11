d9c56b1f6f78262be61b493ad8b859fc24e38442
Ilyas <irazor147@gmail.com>
2023-12-11 17:16:22 +0500

    [IMP] web_field_required_invisible_manager: consider rel. fields with own views

--- web_field_required_invisible_manager/models/models.py
+++ web_field_required_invisible_manager/models/models.py
@@ -7,6 +7,8 @@ from lxml import etree
 from odoo import api, fields, models
 from odoo.tools.safe_eval import safe_eval
 
+from odoo.addons.base.models.ir_ui_view import NameManager
+
 
 class IrModel(models.Model):
     _inherit = "ir.model"
@@ -45,7 +47,62 @@ class Base(models.AbstractModel):
             ]
         )
         if restrictions:
-            return self.create_restrictions_fields(restrictions, view_type, arch)
+            arch = self.create_restrictions_fields(restrictions, view_type, arch)
+        arch = self.process_child_fields(arch, view_type)
+        return arch
+
+    def process_child_fields(self, arch, view_type):
+        """Collect all relational fields and update its views"""
+        related_fields = [
+            (k, v.comodel_name) for k, v in self._fields.items() if v.comodel_name
+        ]
+        related_models_names = [r[1] for r in related_fields]
+        restrictions = self.env["custom.field.restriction"].search(
+            [
+                ("model_name", "in", related_models_names),
+                ("group_ids", "in", self.env.user.groups_id.ids),
+            ]
+        )
+        if restrictions and view_type == "form":
+            for restr in restrictions:
+                todo_fields = list(
+                    filter(lambda x: x[1] == restr.model_name, related_fields)
+                )
+                for todo_field in todo_fields:
+                    if not arch["fields"].get(todo_field[0]):
+                        continue
+                    for sub_view_type, sub_view in arch["fields"][todo_field[0]][
+                        "views"
+                    ].items():
+                        if sub_view_type not in ["form", "tree"]:
+                            continue
+                        z_arch = sub_view
+                        z_arch = self.create_restrictions_fields(
+                            restr, view_type, z_arch
+                        )
+                        z_arch["arch"] = z_arch["arch"].decode("utf-8")
+                        name_manager = NameManager(False, self.env[restr.model_name])
+                        if restr.readonly_field_id and restr.readonly_model_id:
+                            model_field_infos = name_manager.fields_get.get(
+                                restr.readonly_field_id.name
+                            )
+                            z_arch["fields"][
+                                restr.readonly_field_id.name
+                            ] = model_field_infos
+                        if restr.visibility_field_id and restr.invisible_model_id:
+                            model_field_infos = name_manager.fields_get.get(
+                                restr.visibility_field_id.name
+                            )
+                            z_arch["fields"][
+                                restr.visibility_field_id.name
+                            ] = model_field_infos
+                        if restr.required_field_id and restr.required_model_id:
+                            model_field_infos = name_manager.fields_get.get(
+                                restr.required_field_id.name
+                            )
+                            z_arch["fields"][
+                                restr.required_field_id.name
+                            ] = model_field_infos
         return arch
 
     def create_restrictions_fields(self, restrictions, view_type, arch):
