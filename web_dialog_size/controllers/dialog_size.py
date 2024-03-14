b8b02385ef3f9d8d4590fd8446cada530824e8f9
jethavadakshal <jethavadakshal25@gmail.com>
2024-03-14 15:20:33 +0530

    [ADD] added web_dialog_size module

---
+++ web_dialog_size/controllers/dialog_size.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+
+from odoo import http
+from odoo.http import request
+from odoo.tools.safe_eval import const_eval
+
+
+class portalcustomer(http.Controller):
+
+    @http.route(['/dialogsize'], type='json', auth="public", website=True)
+    def get_web_dialog_size_config(self):
+        get_param = request.env['ir.config_parameter'].sudo().get_param
+        return {
+            "default_maximize": const_eval(
+                get_param("web_dialog_size.default_maximize", "False")
+            )
+        }
