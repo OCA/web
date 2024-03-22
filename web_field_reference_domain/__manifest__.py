b4e9c40291f71b9f712f7dae2c6072c81b6637c0
Alexandre D. Díaz <dev@redneboa.es>
2024-03-22 12:14:07 +0100

    [ADD] web_field_reference_domain

---
+++ web_field_reference_domain/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Alexandre D. Díaz - Grupo Isonor
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Web Field Reference Domain",
+    "summary": "Add support to define domains on reference fields",
+    "version": "14.0.1.0.0",
+    "category": "Web",
+    "website": "https://github.com/OCA/web",
+    "author": "Grupo Isonor, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["web"],
+    "data": ["templates/assets.xml"],
+    "demo": [],
+}
---
+++ web_field_reference_domain/static/src/js/basic_model.js
@@ -0,0 +1,24 @@
+// Copyright 2024 Alexandre D. Díaz - Grupo Isonor
+// License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+odoo.define("web_field_reference_domain.BasicModel", function (require) {
+    "use strict";
+
+    var BasicModel = require("web.BasicModel");
+
+    BasicModel.include({
+        /**
+         * This is donde in this way to avoid do extra searchs.
+         * FIXME: If someday Odoo changes the implementation of the
+         * '_search' method in the 'Many2One', see if you can avoid
+         * doing this.
+         *
+         * @override
+         */
+        _getDomain: function (element, options) {
+            if (options && options.referenceDomain) {
+                return options.referenceDomain;
+            }
+            return this._super(...arguments);
+        },
+    });
+});
