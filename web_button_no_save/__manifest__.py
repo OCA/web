d894c6dc88ecbece33c06711954627bffc208201
Adam Heinz <amh@metricwise.net>
2024-03-22 08:14:45 -0400

    [IMP] web_button_no_save: black, isort, prettier

--- web_button_no_save/__manifest__.py
+++ web_button_no_save/__manifest__.py
@@ -2,19 +2,18 @@
 #  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Web Button No Save',
-    'summary': 'define buttons that do not require saving the form',
-    'version': '12.0.1.0.0',
-    'development_status': 'Beta',
-    'author': 'TAKOBI, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/web',
-    'license': 'AGPL-3',
-    'category': 'Web',
-    'depends': [
-        'web',
+    "name": "Web Button No Save",
+    "summary": "define buttons that do not require saving the form",
+    "version": "12.0.1.0.0",
+    "development_status": "Beta",
+    "author": "TAKOBI, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
+    "license": "AGPL-3",
+    "category": "Web",
+    "depends": [
+        "web",
     ],
-    'data': [
-        'templates/assets.xml',
+    "data": [
+        "templates/assets.xml",
     ],
 }
