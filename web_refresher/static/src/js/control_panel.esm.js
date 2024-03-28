8ffee9f78ea10f75142857155e1eef5e162b63a6
Carlos Roca <carlos.roca@tecnativa.com>
2024-03-28 17:10:26 +0100

    [IMP] web_refresher: Backport refresh way from v17

--- web_refresher/static/src/js/control_panel.esm.js
+++ web_refresher/static/src/js/control_panel.esm.js
@@ -1,9 +1,27 @@
 /** @odoo-module **/
-/* Copyright 2024 Tecnativa - Carlos Roca
+/* Copyright 2022 Tecnativa - Alexandre D. Díaz
+ * Copyright 2022 Tecnativa - Carlos Roca
+ * Copyright 2023 Taras Shabaranskyi
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
+
 import {ControlPanel} from "@web/search/control_panel/control_panel";
 import {Refresher} from "./refresher.esm";
+import {patch} from "@web/core/utils/patch";
 
 ControlPanel.components = Object.assign({}, ControlPanel.components, {
     Refresher,
+});
+
+patch(ControlPanel.prototype, "web_refresher.ControlPanel", {
+    setup() {
+        this._super(...arguments);
+        const {config, searchModel} = this.env;
+        const forbiddenSubType = ["base_settings"];
+        if (!forbiddenSubType.includes(config.viewSubType)) {
+            this.refresherProps = {
+                searchModel: searchModel,
+                pagerProps: this.pagerProps,
+            };
+        }
+    },
 });
