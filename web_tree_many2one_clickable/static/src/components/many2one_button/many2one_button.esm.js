6334d0a9aa5423e021c682a7f120c3f0c2e752ef
maciej-wichowski <maciek.wichowski@gmail.com>
2024-04-01 12:37:28 +0200

    [17.0][MIG] web_tree_many2one_clickable

--- web_tree_many2one_clickable/static/src/components/many2one_button/many2one_button.esm.js
+++ web_tree_many2one_clickable/static/src/components/many2one_button/many2one_button.esm.js
@@ -5,29 +5,29 @@
  * Copyright 2017 Sodexis <dev@sodexis.com>
  * Copyright 2018 Camptocamp SA
  * Copyright 2019 Alexandre Díaz <alexandre.diaz@tecnativa.com>
+ * Copyright 2024 Versada (https://versada.eu)
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
 
+import {patch} from "@web/core/utils/patch";
 import {ListRenderer} from "@web/views/list/list_renderer";
-import {Component} from "@odoo/owl";
 import {useService} from "@web/core/utils/hooks";
 
-export class TreeMany2oneClickableButton extends Component {
+patch(ListRenderer.prototype, {
     setup() {
         this.actionService = useService("action");
-    }
-
-    async onClick(ev) {
+        super.setup(...arguments);
+    },
+    async onClickM2oButton(record, column, ev) {
         ev.stopPropagation();
+        const field = record.fields[column.name];
+        const value = record.data[column.name];
         return this.actionService.doAction({
             type: "ir.actions.act_window",
-            res_model: this.props.field.relation,
-            res_id: this.props.value[0],
+            res_model: field.relation,
+            res_id: value[0],
             views: [[false, "form"]],
             target: "target",
-            additionalContext: this.props.context || {},
+            additionalContext: column.context || {},
         });
-    }
-}
-TreeMany2oneClickableButton.template = "web_tree_many2one_clickable.Button";
-
-Object.assign(ListRenderer.components, {TreeMany2oneClickableButton});
+    },
+});
