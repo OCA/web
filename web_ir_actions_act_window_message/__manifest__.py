a1afd59eaaab6d83a6ecc381ccf351ed52b88146
Ruchir Shukla <ruchir@bizzappdev.com>
2024-04-25 19:43:48 +0530

    [MIG] web_ir_actions_act_window_message: Migration to 17.0

--- web_ir_actions_act_window_message/__manifest__.py
+++ web_ir_actions_act_window_message/__manifest__.py
@@ -2,20 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Client side message boxes",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.1",
     "author": "Therp BV, " "ACSONE SA/NV, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
     "category": "Hidden/Dependency",
     "summary": "Show a message box to users",
     "depends": ["web"],
-    "data": ["security/ir.model.access.csv"],
+    "data": [
+        "security/ir.model.access.csv",
+    ],
     "assets": {
         "web.assets_backend": [
-            "web_ir_actions_act_window_message/static/src/**/**.esm.js",
-        ],
-        "web.assets_qweb": [
-            "web_ir_actions_act_window_message/static/src/**/**.xml",
+            "web_ir_actions_act_window_message/static/src/**/*",
         ],
     },
 }
