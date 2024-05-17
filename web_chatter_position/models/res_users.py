e7ceb142a21e45d015b4ae077f89b388ba2b25f9
jay <jay@alitec.sg>
2024-05-17 18:06:50 +0530

    [17.0][MIG] web_chatter_position

---
+++ web_chatter_position/models/res_users.py
@@ -0,0 +1,26 @@
+# Copyright 2022 Hynsys Technologies
+# Copyright 2024 Alitec Pte Ltd
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    chatter_position = fields.Selection(
+        [
+            ("auto", "Responsive"),
+            ("bottom", "Bottom"),
+            ("sided", "Sided"),
+        ],
+        default="auto",
+    )
+
+    @property
+    def SELF_READABLE_FIELDS(self):
+        return super().SELF_READABLE_FIELDS + ["chatter_position"]
+
+    @property
+    def SELF_WRITEABLE_FIELDS(self):
+        return super().SELF_WRITEABLE_FIELDS + ["chatter_position"]
---
+++ web_chatter_position/readme/CONTRIBUTORS.rst
@@ -0,0 +1,8 @@
+* Hynsys Technologies <hynsystechnologies@gmail.com>
+* Juan Miguel Sánchez Arce <juan.sanchez@camptocamp.com>
+* `Camptocamp <https://www.camptocamp.com>`_
+
+  * Iván Todorovich <ivan.todorovich@camptocamp.com>
+* `Alitec Pte Ltd <http://www.alitec.sg>`_
+
+  * Jay Patel <jay@alitec.sg>
