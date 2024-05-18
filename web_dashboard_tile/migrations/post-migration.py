b8f4bdfb5ce25a8ef6e8c6e8e173b67106b0d592
Daniel Reis <dreis@opensourceintegrators.com>
2024-05-18 18:50:57 +0100

    [REF] web_dashboard_tile: pre-commit fixes

--- web_dashboard_tile/migrations/post-migration.py
+++ web_dashboard_tile/migrations/post-migration.py
@@ -1,6 +1,8 @@
 # Copyright (C) 2023-Today: GRAP (<http://www.grap.coop/>)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+# pylint: disable=C7902
 import logging
 
 from openupgradelib import openupgrade
