dca65819837e500f1248643d3cba5a50fb94af4d
FrancescoBallerini <francescobl.lavoro@gmail.com>
2024-06-03 22:53:53 +0200

    Immediate url update when clicking on main APP (do not wait for do_push_state)
    Good for methods like renderButtons, that are executed before controller is
    ready. In those methods you wouldn't get the updated menu param from URL
    without this implementation.

--- web_responsive_cache_secondary_menu_id/static/src/js/apps_menu.js
+++ web_responsive_cache_secondary_menu_id/static/src/js/apps_menu.js
@@ -6,6 +6,26 @@ odoo.define("web_responsive_cache_secondary_menu_id.AppsMenu", function (require
     require("web_responsive");
 
     AppsMenu.include({
+        /**
+         * Immediately update URL when opening a main app by
+         * clicking on widget "web_responsive.AppIcon". Without
+         * this override you would only be able to retrieve the
+         * menu ID from URL after do_push_state() execution.
+         *
+         * @override
+         */
+        _onAppsMenuItemClicked: function (ev) {
+            var url = window.location.href;
+            var searchParams = new URLSearchParams(url.split("#")[1]);
+            var eventData = ev.currentTarget.dataset;
+            if (eventData.menuId) {
+                searchParams.set("menu_id", eventData.menuId);
+                var new_url = url.split("#")[0] + "#" + searchParams.toString();
+                window.history.pushState({}, "", new_url);
+            }
+            this._super.apply(this, arguments);
+        },
+
         /*
          * Manage URL parameter "secondary_menu_id" when clicking
          * on widget "web_responsive.MenuSearchResults"
