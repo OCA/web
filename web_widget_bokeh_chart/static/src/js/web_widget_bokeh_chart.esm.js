30d6d507a285b2565ad7967d1093d4fa509c5865
DavidJForgeFlow <david.jimenez@forgeflow.com>
2024-06-13 12:50:18 +0200

    [MIG] web_widget_bokeh_chart: Migration to 17.0
    Update bokeh from 3.1.1 to 3.4.1

--- web_widget_bokeh_chart/static/src/js/web_widget_bokeh_chart.esm.js
+++ web_widget_bokeh_chart/static/src/js/web_widget_bokeh_chart.esm.js
@@ -1,9 +1,9 @@
 /** @odoo-module **/
 
+import {markup, onMounted, onPatched, onWillStart, useRef} from "@odoo/owl";
 import {CharField} from "@web/views/fields/char/char_field";
 import {loadBundle} from "@web/core/assets";
 import {registry} from "@web/core/registry";
-const {onWillStart, markup, onMounted, onPatched, useRef} = owl;
 
 export default class BokehChartWidget extends CharField {
     setup() {
@@ -22,23 +22,32 @@ export default class BokehChartWidget extends CharField {
         onWillStart(() =>
             loadBundle({
                 jsLibs: [
-                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-3.1.1.min.js",
-                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-api-3.1.1.min.js",
-                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-widgets-3.1.1.min.js",
-                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-tables-3.1.1.min.js",
-                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-mathjax-3.1.1.min.js",
-                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-gl-3.1.1.min.js",
+                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-3.4.1.min.js",
+                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-api-3.4.1.min.js",
+                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-widgets-3.4.1.min.js",
+                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-tables-3.4.1.min.js",
+                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-mathjax-3.4.1.min.js",
+                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-gl-3.4.1.min.js",
                 ],
             })
         );
     }
     get json_value() {
-        var value = JSON.parse(this.props.value);
-        if (value) {
-            value.div = markup(value.div.trim());
+        var value = false;
+        if (this.props.record.data[this.props.name]) {
+            value = JSON.parse(this.props.record.data[this.props.name]);
+            if (value) {
+                value.div = markup(value.div.trim());
+            }
         }
         return value;
     }
 }
 BokehChartWidget.template = "web_widget_bokeh_chart.BokehChartField";
-registry.category("fields").add("bokeh_chart", BokehChartWidget);
+
+export const bokehChartWidget = {
+    ...CharField,
+    component: BokehChartWidget,
+};
+
+registry.category("fields").add("bokeh_chart", bokehChartWidget);
