3993720bbd9a48faadc28d659a9a89cd34fcdb54
DavidJForgeFlow <david.jimenez@forgeflow.com>
2024-06-13 12:50:18 +0200

    [MIG] web_widget_bokeh_chart: Migration to 17.0
    Update bokeh from 3.1.1 to 3.4.1

--- web_widget_bokeh_chart/static/src/js/web_widget_bokeh_json_chart.esm.js
+++ web_widget_bokeh_chart/static/src/js/web_widget_bokeh_json_chart.esm.js
@@ -1,31 +1,31 @@
 /** @odoo-module **/
 
+import {Component, markup, onMounted, onPatched, onWillStart, useRef} from "@odoo/owl";
 import {loadBundle} from "@web/core/assets";
 import {registry} from "@web/core/registry";
-const {onWillStart, markup, Component, onMounted, onPatched, useRef} = owl;
 
 export default class BokehChartJsonWidget extends Component {
     setup() {
         this.widget = useRef("widget");
         onPatched(() => {
             var script = document.createElement("script");
-            script.text = this.props.value.script;
+            script.text = this.props.record.data[this.props.name].script;
             this.widget.el.append(script);
         });
         onMounted(() => {
             var script = document.createElement("script");
-            script.text = this.props.value.script;
+            script.text = this.props.record.data[this.props.name].script;
             this.widget.el.append(script);
         });
         onWillStart(() =>
             loadBundle({
                 jsLibs: [
-                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-3.1.1.min.js",
-                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-api-3.1.1.min.js",
-                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-widgets-3.1.1.min.js",
-                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-tables-3.1.1.min.js",
-                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-mathjax-3.1.1.min.js",
-                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-gl-3.1.1.min.js",
+                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-3.4.1.min.js",
+                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-api-3.4.1.min.js",
+                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-widgets-3.4.1.min.js",
+                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-tables-3.4.1.min.js",
+                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-mathjax-3.4.1.min.js",
+                    "/web_widget_bokeh_chart/static/src/lib/bokeh/bokeh-gl-3.4.1.min.js",
                 ],
             })
         );
@@ -37,4 +37,9 @@ export default class BokehChartJsonWidget extends Component {
 }
 
 BokehChartJsonWidget.template = "web_widget_bokeh_chart.BokehChartlJsonField";
-registry.category("fields").add("bokeh_chart_json", BokehChartJsonWidget);
+
+export const bokehChartJsonWidget = {
+    component: BokehChartJsonWidget,
+};
+
+registry.category("fields").add("bokeh_chart_json", bokehChartJsonWidget);
