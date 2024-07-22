396b29ff84326af36f843b593d626685ef3ed6f1
Simon COLLIN <simon.collin@tekfor.cloud>
2024-07-22 11:05:52 +0200

    use css

--- web_company_color/models/res_company.py
+++ web_company_color/models/res_company.py
@@ -96,8 +96,8 @@ class ResCompany(models.Model):
 
     WEB_RESPONSIVE_SCSS_TEMPLATE = """
         .o_menu_apps .dropdown-menu {
-          background: linear-gradient(to bottom, gray("200"), gray("white"));
-          .o-app-name {
+          background: linear-gradient(to bottom, #EEEEEE, #FFFFFF);
+          .o-app-name > span {
             color: black !important;
             text-shadow: 1px 1px 1px white !important;
           }
