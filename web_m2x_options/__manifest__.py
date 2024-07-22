35397320ac2813001bbca6202d0eb1695458b6d2
manu <manuel.regidor@sygel.es>
2024-07-22 17:13:53 +0200

    [MIG]web_m2x_options: Migration to 17.0

--- web_m2x_options/__manifest__.py
+++ web_m2x_options/__manifest__.py
@@ -6,16 +6,26 @@
 
 {
     "name": "web_m2x_options",
-    "version": "16.0.1.1.3",
+    "version": "17.0.1.0.0",
     "category": "Web",
     "author": "initOS GmbH,"
     "ACSONE SA/NV, "
     "0k.io, "
     "Tecnativa, "
+    "Sygel, "
     "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
     "depends": ["web"],
-    "assets": {"web.assets_backend": ["web_m2x_options/static/src/components/*"]},
+    "assets": {
+        "web.assets_backend": [
+            (
+                "before",
+                "web/static/src/views/fields/*",
+                "web_m2x_options/static/src/components/form.esm.js",
+            ),
+            "web_m2x_options/static/src/components/base.xml",
+        ]
+    },
     "installable": True,
 }
