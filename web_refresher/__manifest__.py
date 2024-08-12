cecce5d02101ceba99d8e856d095e3aa9e2e55ed
Taras Shabaranskyi <shabaranskij@gmail.com>
2024-08-12 12:15:49 +0300

    [FIX] web_refresher: bug when clicking report

--- web_refresher/__manifest__.py
+++ web_refresher/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Web Refresher",
-    "version": "17.0.1.1.1",
+    "version": "17.0.1.1.2",
     "author": "Compassion Switzerland, Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/web",
@@ -10,12 +10,15 @@
     "assets": {
         "web.assets_backend": [
             "web_refresher/static/src/scss/refresher.scss",
-            "web_refresher/static/src/js/refresher.esm.js",
-            "web_refresher/static/src/js/control_panel.esm.js",
             "web_refresher/static/src/xml/refresher.xml",
             # Load the modification of the master template just after it,
             # for having the modification in all the primary extensions.
             # Example: the project primary view.
+            (
+                "after",
+                "web/static/src/search/control_panel/control_panel.js",
+                "web_refresher/static/src/js/*.esm.js",
+            ),
             (
                 "after",
                 "web/static/src/search/control_panel/control_panel.xml",
