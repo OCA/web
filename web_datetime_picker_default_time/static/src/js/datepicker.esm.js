bda8836396f9516c52310c437e104614074867c7
Akim Juillerat <akim.juillerat@camptocamp.com>
2024-08-13 17:38:08 +0200

    Apply default time on manual entry of date string

--- web_datetime_picker_default_time/static/src/js/datepicker.esm.js
+++ web_datetime_picker_default_time/static/src/js/datepicker.esm.js
@@ -4,6 +4,7 @@
 
 import {DateTimePicker} from "@web/core/datepicker/datepicker";
 import {patch} from "@web/core/utils/patch";
+import {localization} from "@web/core/l10n/localization";
 
 patch(DateTimePicker.prototype, "DateTimePickerDefaultTime", {
     onMounted() {
@@ -20,6 +21,27 @@ patch(DateTimePicker.prototype, "DateTimePickerDefaultTime", {
                 window.$(this.rootRef.el).datetimepicker("date", date);
             }
         });
+    },
+    isStrDate(input_string) {
+        return input_string.trim().length == localization.dateFormat.length;
+    },
+    customParseValue(input_value, options) {
+        const default_time = this.props.defaultTime;
+        let [res, error] = this.parseValueOriginal(input_value, options);
+        if (default_time && this.isStrDate(input_value)) {
+            const new_value = res.set({
+                hour: default_time.hour,
+                minute: default_time.minute,
+                second: default_time.second,
+            });
+            res = new_value;
+        }
+        return [res, error];
+    },
+    initFormat() {
+        this._super.apply(this, arguments);
+        this.parseValueOriginal = this.parseValue;
+        this.parseValue = this.customParseValue;
     },
 });
 
