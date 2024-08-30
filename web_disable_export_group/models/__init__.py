d051024c3e9d004bb6fa9601d5880f6a29fe6e70
prabakaran <prabakaran@sodexis.com>
2024-08-30 13:23:47 +0530

    [MIG] web_disable_export_group: Migration to 17.0

--- web_disable_export_group/models/__init__.py
+++ web_disable_export_group/models/__init__.py
@@ -1,2 +1,1 @@
-from . import ir_http
 from . import models
--- web_disable_export_group/static/src/js/list_controller.esm.js
+++ web_disable_export_group/static/src/js/list_controller.esm.js
@@ -1,20 +1,23 @@
 /** @odoo-module **/
 /* Copyright 2018 Tecnativa - David Vidal
    License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl). */
-import {ListController} from "@web/views/list/list_controller";
-import {onWillStart} from "@odoo/owl";
+import {archParseBoolean} from "@web/views/utils";
+import {exportAllItem} from "@web/views/list/export_all/export_all";
 import {patch} from "@web/core/utils/patch";
 
-patch(ListController.prototype, "disable_export_group", {
-    setup() {
-        this._super(...arguments);
-        onWillStart(async () => {
-            this.isExportEnable = await this.userService.hasGroup(
-                "base.group_allow_export"
-            );
-            this.isExportXlsEnable = await this.userService.hasGroup(
+patch(exportAllItem, {
+    async isDisplayed(env) {
+        const ExportEnable = super.isDisplayed(...arguments);
+        const ExportXlsEnable =
+            env.config.viewType === "list" &&
+            !env.model.root.selection.length &&
+            (await env.model.user.hasGroup(
                 "web_disable_export_group.group_export_xlsx_data"
-            );
-        });
+            )) &&
+            archParseBoolean(env.config.viewArch.getAttribute("export_xlsx"), true);
+        if (ExportXlsEnable) {
+            return ExportXlsEnable;
+        }
+        return ExportEnable;
     },
 });
