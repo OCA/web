4a7f6862f1ca88f82aa65809b7ba65d303e9e2eb
Felix Daniel Coca Calvimontes <daniel.uremix@gmail.com>
2024-10-10 21:27:41 +0000

    First version of map widget and field

---
+++ web_widget_map/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Map field widget",
+    "summary": "A map field widget for Odoo.",
+    "author": "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
+    "category": "Customizations",
+    "version": "17.0.1.0.0",
+    "depends": ["web"],
+    "assets": {
+        "web.assets_backend": [
+            "map_field/static/src/components/**/*",
+            "map_field/static/src/css/map.scss",
+        ],
+    },
+    "license": "AGPL-3",
+}
---
+++ web_widget_map/fields/map.py
@@ -0,0 +1,61 @@
+from odoo import _, fields
+from odoo.exceptions import ValidationError
+
+
+class MapField(fields.Field):
+    """
+    Custom field to store the location of a record.
+
+    The field stores the location as a string in the format 'lat,lng'.
+    """
+
+    type = "char"
+    column_type = ("varchar", "varchar")
+
+    def convert_to_record(self, value, record, validate=True):
+        """
+        Converts the value to a valid value for the record.
+        """
+        try:
+            float(value.split(",")[0])
+            float(value.split(",")[1])
+        except ValueError as e:
+            raise ValidationError(_("Coords must be numbers.")) from e
+        return value
+
+    def convert_to_export(self, value, record):
+        """
+        Converts a value from the database to a format suitable for export.
+        """
+        return value or None
+
+    def convert_to_cache(self, value, record, validate=True):
+        """
+        Converts a value from the database to a format suitable for the cache.
+        """
+        return value or None
+
+    def convert_to_column(self, value, record, validate=True):
+        """
+        Converts a value to a format suitable for the column.
+        """
+        return value or None
+
+    def _get_attrs(self, model_class, name):
+        """
+        Get the attributes of the field.
+        """
+        res = super()._get_attrs(model_class, name)
+        res["type"] = "text"
+        return res
+
+    def get_location(self, field_name, model, id):
+        """
+        Get the location of the record.
+        """
+        record = self.env[model].browse(id)
+        return record[field_name]
+
+
+# Monkey patch the fields module to add the Map field.
+fields.Map = MapField
