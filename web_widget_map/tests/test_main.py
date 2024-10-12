dcee877b83efc10cd88e93303823a1c2b8b343b2
Felix Daniel Coca Calvimontes <daniel.uremix@gmail.com>
2024-10-12 03:40:10 -0400

    Add tests for Map field widget

--- web_widget_map/tests/test_main.py
+++ web_widget_map/tests/test_main.py
@@ -1,45 +1,65 @@
-from odoo.tests import common
+from lxml import etree
+
+from odoo.tests.common import TransactionCase
 
 
-class Test(common.TransactionCase):
+class Test(TransactionCase):
     """
     Test the Map field.
     """
 
-    def test_add_map_field(self):
+    def test_map_field(self):
         """
         Test adding a Map field to a model.
         """
+        view = self.env["ir.ui.view"].create(
+            {
+                "key": "web_widget_map.test_map",
+                "type": "qweb",
+                "arch": """
+                <t t-name="test_map">
+                    <span t-field="record.x_location" t-options-widget="'map'"/>
+                </t>
+            """,
+            }
+        )
         self.env["ir.model.fields"].create(
             {
                 "model_id": self.env.ref("base.model_res_partner").id,
-                "name": "location",
+                "name": "x_location",
                 "field_description": "Location",
                 "ttype": "char",
-                "state": "manual",
-                "model": "res.partner",
-                "required": False,
             }
         )
-        arch, view = self.env["res.partner"]._get_view(view_id=None, view_type="form")
-        found = arch.xpath("//field[@widget='map']")
-        self.assertEqual(len(found), 1)
+        partner = self.env["res.partner"].create(
+            {"name": "test partner", "x_location": "51.505, -0.09"}
+        )
+        html = view._render_template(view.id, {"record": partner})
+        self.assertIn("51.505", html)
 
-    def test_no_add_map_field(self):
+    def test_map_field_no_location(self):
         """
-        Test not adding a Map field to a model.
+        Test adding a Map field to a model without a location.
         """
+        view = self.env["ir.ui.view"].create(
+            {
+                "key": "web_widget_map.test_map",
+                "type": "qweb",
+                "arch": """
+                <t t-name="test_map">
+                    <span t-field="record.x_location" t-options-widget="'map'"/>
+                </t>
+            """,
+            }
+        )
         self.env["ir.model.fields"].create(
             {
                 "model_id": self.env.ref("base.model_res_partner").id,
-                "name": "location",
+                "name": "x_location",
                 "field_description": "Location",
                 "ttype": "char",
-                "state": "manual",
-                "model": "res.partner",
-                "required": False,
             }
         )
-        arch, view = self.env["res.partner"]._get_view(view_id=None, view_type="form")
-        found = arch.xpath("//field[@widget='map']")
-        self.assertEqual(len(found), 0)
+        partner = self.env["res.partner"].create({"name": "test partner"})
+        html = view._render_template(view.id, {"record": partner})
+        self.assertIsInstance(html, str)
