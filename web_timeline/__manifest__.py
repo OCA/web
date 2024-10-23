966f6273091d1829c1d6d5688a3849b16e942294
Carlos Lopez <carlos.lopez@tecnativa.com>
2024-10-23 07:38:33 -0500

    [IMP] web_timeline: Update vis-timeline-graph2d lib: 7.7.0 -> 7.7.3

--- web_timeline/__manifest__.py
+++ web_timeline/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Web timeline",
     "summary": "Interactive visualization chart to show events in time",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "development_status": "Production/Stable",
     "author": "ACSONE SA/NV, "
     "Tecnativa, "
