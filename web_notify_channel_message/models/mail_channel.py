4d994fd9dc0e57d0c8ee7beac12037c5a0b728df
ThiagoMForgeFlow <thiago.mulero@forgeflow.com>
2024-11-08 09:09:37 +0100

    [IMP] web_notify_channel_message: apply the extract method refactor

--- web_notify_channel_message/models/mail_channel.py
+++ web_notify_channel_message/models/mail_channel.py
@@ -1,4 +1,5 @@
-# Copyright 2023 ForgeFlow
+# pylint: disable=missing-docstring
+# Copyright 2023 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import _, models
@@ -7,8 +8,7 @@ from odoo import _, models
 class MailChannel(models.Model):
     _inherit = "mail.channel"
 
-    def message_post(self, *, message_type="notification", **kwargs):
-        message = super().message_post(message_type=message_type, **kwargs)
+    def _notify_message(self, message):
         for partner in self.channel_partner_ids:
             users = partner.user_ids
             for user in users:
@@ -17,4 +17,8 @@ class MailChannel(models.Model):
                 user.with_context(_notify_channel_message=True).notify_info(
                     message=_("You have a new message in channel %s") % self.name
                 )
+
+    def message_post(self, *, message_type="notification", **kwargs):
+        message = super().message_post(message_type=message_type, **kwargs)
+        self._notify_message(message)
         return message
