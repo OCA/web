a1b322868b0d4ffa57338a0e200b0b44f542b8e7
Florian Mounier <paradoxxx.zero@gmail.com>
2024-12-13 15:30:28 +0100

    [ADD] web_url_default_fields_values

---
+++ web_url_default_fields_values/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2024 Akretion (http://www.akretion.com).
+# @author Florian Mounier <florian.mounier@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Web URL Default Fields Values",
+    "version": "16.0.1.0.0",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "summary": "This module adds support for default fields values in URL parameters.",
+    "category": "Tools",
+    "depends": [],
+    "website": "https://github.com/OCA/web",
+    "data": [],
+    "assets": {
+        "web.assets_backend": [
+            "/web_url_default_fields_values/static/src/components/*",
+        ],
+    },
+    "maintainers": ["paradoxxxzero"],
+    "demo": [],
+    "installable": True,
+    "license": "AGPL-3",
+}
