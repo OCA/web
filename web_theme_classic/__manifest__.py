098baf9d58281aacf37a0853585aab9732583b33
“dedinside1337” <golovachenko.kolya@gmail.com>
2025-01-02 15:28:35 +0100

    [MIG] web_theme_classic: Migration to 18.0

--- web_theme_classic/__manifest__.py
+++ web_theme_classic/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Web Theme Classic",
     "summary": "Contrasted style on fields to improve the UI.",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "GRAP, Odoo Community Association (OCA)",
     "maintainers": ["legalsylvain"],
     "website": "https://github.com/OCA/web",
