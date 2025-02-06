7ae57488fde599350bbd44c8227b616e9ac0c8d7
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-02-06 17:32:02 +0100

    [FIX] web_ir_actions_act_multi: pre-commit stuff for the tests.

--- web_ir_actions_act_multi/tests/test_ir_actions_act_multi.py
+++ web_ir_actions_act_multi/tests/test_ir_actions_act_multi.py
@@ -7,24 +7,28 @@ class TestIrActionsActMulti(TransactionCase):
         self.IrActionsActMulti = self.env["ir.actions.act_multi"]
 
     def test_create_ir_actions_act_multi(self):
-        action = self.IrActionsActMulti.create({
-            "name": "Test Action",
-            "type": "ir.actions.act_multi",
-        })
+        action = self.IrActionsActMulti.create(
+            {
+                "name": "Test Action",
+                "type": "ir.actions.act_multi",
+            }
+        )
         self.assertEqual(
             action.type,
             "ir.actions.act_multi",
-            "Default type should be 'ir.actions.act_multi'."
+            "Default type should be 'ir.actions.act_multi'.",
         )
 
     def test_get_readable_fields(self):
-        action = self.IrActionsActMulti.create({
-            "name": "Test Action",
-            "type": "ir.actions.act_multi",
-        })
+        action = self.IrActionsActMulti.create(
+            {
+                "name": "Test Action",
+                "type": "ir.actions.act_multi",
+            }
+        )
         readable_fields = action._get_readable_fields()
         self.assertIn(
             "actions",
             readable_fields,
-            "The 'actions' should be in the readable fields."
+            "The 'actions' should be in the readable fields.",
         )
