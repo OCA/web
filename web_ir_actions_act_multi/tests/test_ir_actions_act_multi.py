796c54a061d97cd993930db08e2fd2cf2faf57c9
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-02-06 17:23:45 +0100

    [FIX] web_ir_actions_act_multi: Fixed the "line #L15 was not covered by tests" message.

---
+++ web_ir_actions_act_multi/tests/test_ir_actions_act_multi.py
@@ -0,0 +1,30 @@
+from odoo.tests.common import TransactionCase
+
+class TestIrActionsActMulti(TransactionCase):
+
+    def setUp(self):
+        super(TestIrActionsActMulti, self).setUp()
+        self.IrActionsActMulti = self.env["ir.actions.act_multi"]
+
+    def test_create_ir_actions_act_multi(self):
+        action = self.IrActionsActMulti.create({
+            "name": "Test Action",
+            "type": "ir.actions.act_multi",
+        })
+        self.assertEqual(
+            action.type,
+            "ir.actions.act_multi",
+            "Default type should be 'ir.actions.act_multi'."
+        )
+
+    def test_get_readable_fields(self):
+        action = self.IrActionsActMulti.create({
+            "name": "Test Action",
+            "type": "ir.actions.act_multi",
+        })
+        readable_fields = action._get_readable_fields()
+        self.assertIn(
+            "actions",
+            readable_fields,
+            "The 'actions' should be in the readable fields."
+        )
