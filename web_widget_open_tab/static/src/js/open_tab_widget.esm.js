9ac7bbd832012f199b0cd15aead689a8ac71e776
widad elbekkouri <wielb@odoo.com>
2025-02-14 14:44:05 +0100

    [18.0][MIG] web_widget_open_tab: Migration to 18.0

---
+++ web_widget_open_tab/static/src/js/open_tab_widget.esm.js
@@ -0,0 +1,31 @@
+import {Component} from "@odoo/owl";
+import {_t} from "@web/core/l10n/translation";
+import {registry} from "@web/core/registry";
+import {standardFieldProps} from "@web/views/fields/standard_field_props";
+
+export class OpenTabWidget extends Component {
+    openNewTab(ev) {
+        ev.stopPropagation();
+    }
+    _getReference() {
+        const url = window.location.href;
+        return url.split("?")[0] + "/" + this.props.record.data.id;
+    }
+}
+
+OpenTabWidget.template = "web_widget_open_tab.openTab";
+OpenTabWidget.props = {
+    ...standardFieldProps,
+    title: {type: String, optional: true},
+};
+
+export const openTabWidget = {
+    component: OpenTabWidget,
+    displayName: _t("Open Tab"),
+    supportedTypes: ["integer"],
+    extractProps: () => ({
+        title: _t("Click to open on new tab"),
+    }),
+};
+
+registry.category("fields").add("open_tab", openTabWidget);
