4a3ad9829c2b361ae7d8f9df5694cc09966b14bf
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2025-02-19 09:01:37 +0100

    [IMP] web_editor_class_selector: Order classes by sequence

--- web_editor_class_selector/__manifest__.py
+++ web_editor_class_selector/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Web editor class selector",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.1.0",
     "summary": "",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
--- web_editor_class_selector/models/web_editor_class.py
+++ web_editor_class_selector/models/web_editor_class.py
@@ -4,8 +4,10 @@ from odoo import fields, models
 class WebEditorClass(models.Model):
     _name = "web.editor.class"
     _description = "Web editor class selector"
+    _order = "sequence,id"
 
     name = fields.Char(required=True)
+    sequence = fields.Integer(default=10)
     class_name = fields.Char(
         required=True,
         help="The class name to be added to the tag. "
