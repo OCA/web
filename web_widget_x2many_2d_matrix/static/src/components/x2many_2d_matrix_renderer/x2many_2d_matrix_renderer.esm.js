12f817fb7b45c2b38f0e7b1d33e324a5eae7de47
Cuong, Nguyen Minh Tran Manh <cuong.nmtm@komit-consulting.com>
2025-03-09 00:33:15 +0700

    [FIX] web_widget_x2many_2d_matrix: Handle missing domain condition to prevent errors

--- web_widget_x2many_2d_matrix/static/src/components/x2many_2d_matrix_renderer/x2many_2d_matrix_renderer.esm.js
+++ web_widget_x2many_2d_matrix/static/src/components/x2many_2d_matrix_renderer/x2many_2d_matrix_renderer.esm.js
@@ -165,7 +165,7 @@ export class X2Many2DMatrixRenderer extends Component {
             name: this.matrixFields.value,
         };
         const domain = record.fields[this.matrixFields.value].domain;
-        if (domain) {
+        if ((Array.isArray(domain) || typeof domain === "string") && domain.length) {
             result.domain = new Domain(
                 evaluateExpr(domain, record.evalContext)
             ).toList();
