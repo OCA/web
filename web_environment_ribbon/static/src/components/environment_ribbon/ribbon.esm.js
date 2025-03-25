d5bdb75d874c12b48a3a284507a2edd5eca35d63
Stefan Rijnhart <stefan@opener.amsterdam>
2025-03-25 17:58:56 +0100

    [UPD] dot-files; remove obsolete declarations of browser globals

--- web_environment_ribbon/static/src/components/environment_ribbon/ribbon.esm.js
+++ web_environment_ribbon/static/src/components/environment_ribbon/ribbon.esm.js
@@ -2,8 +2,6 @@ import {Component, xml} from "@odoo/owl";
 import {useBus, useService} from "@web/core/utils/hooks";
 import {registry} from "@web/core/registry";
 
-const {document} = globalThis;
-
 export class WebEnvironmentRibbon extends Component {
     setup() {
         this.orm = useService("orm");
