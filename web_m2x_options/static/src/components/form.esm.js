d4cc4f0685a7b41cb0e53f43611ac34816568178
Maarten Van Briel <maarten.vanbriel@dynapps.be>
2025-06-25 10:50:33 +0200

    [17.0][FIX] web_m2x_options: prevent crash on Many2OneReferenceField

--- web_m2x_options/static/src/components/form.esm.js
+++ web_m2x_options/static/src/components/form.esm.js
@@ -176,6 +176,12 @@ patch(many2OneField, {
             {attrs, context, decorations, options, string},
             dynamicInfo
         );
+        // FIXME: field of type === "many2one_reference" does not support m2o_options_props.
+        // This no-op prevents crashes, but proper option support is still missing.
+        // See roadmap note in PR #3205
+        if (!this.m2m_options_props) {
+            return props;
+        }
         const new_props = this.m2o_options_props(props, attrs, options);
         return new_props;
     },
