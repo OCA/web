96f12c44b2fbcbbf484a105db9d5601ddce16fbf
anusrinps96 <aprakhasam@nps100.com>
2025-06-26 11:31:53 +0200

    [MIG] web_responsive_company_color: Migration to 16.0

--- web_responsive_company_color/__init__.py
+++ web_responsive_company_color/__init__.py
@@ -1,2 +1,2 @@
 from . import models
-from .hooks import post_init_hook
+from .hooks import post_init_hook, uninstall_hook
--- web_responsive_company_color/__manifest__.py
+++ web_responsive_company_color/__manifest__.py
@@ -5,13 +5,14 @@
     "author": "Anusri Veerappan Prakasam, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "category": "Web",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "depends": [
         "web_company_color",
         "web_responsive",
     ],
     "data": [],
     "post_init_hook": "post_init_hook",
+    "uninstall_hook": "uninstall_hook",
     "installable": True,
     "application": False,
 }
