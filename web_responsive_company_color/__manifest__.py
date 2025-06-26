1066641700910f55fd659448bcec45b6f08f5387
anusrinps96 <aprakhasam@nps100.com>
2025-06-26 09:30:34 +0200

    [ADD] web_responsive_company_color: Added new module

---
+++ web_responsive_company_color/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Web Responsive Company Color",
+    "summary": "Styling hooks for web_responsive elements",
+    "license": "AGPL-3",
+    "author": "Anusri Veerappan Prakasam, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/web",
+    "category": "Web",
+    "version": "14.0.1.0.0",
+    "depends": [
+        "web_company_color",
+        "web_responsive",
+    ],
+    "data": [],
+    "post_init_hook": "post_init_hook",
+    "installable": True,
+    "application": False,
+}
