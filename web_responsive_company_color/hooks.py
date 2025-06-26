8a4be85649af8c1170548ef23cf6d30805994395
anusrinps96 <aprakhasam@nps100.com>
2025-06-26 11:31:53 +0200

    [MIG] web_responsive_company_color: Migration to 16.0

--- web_responsive_company_color/hooks.py
+++ web_responsive_company_color/hooks.py
@@ -1,6 +1,13 @@
 from odoo import SUPERUSER_ID, api
 
 
+def uninstall_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env["res.company"].with_context(uninstall_scss=True).search(
+        []
+    ).scss_create_or_update_attachment()
+
+
 def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     env["res.company"].search([]).scss_create_or_update_attachment()
--- web_responsive_company_color/models/__init__.py
+++ web_responsive_company_color/models/__init__.py
@@ -1,2 +1,1 @@
 from . import res_company
-from . import ir_module
