ba1960b286dbe3782a0677810c4eac9efc26424b
Christopher Rogos <crogos@gmail.com>
2025-06-27 09:30:32 +0000

    [FIX] web_search_with_and: support add custom filter

--- web_search_with_and/static/src/js/search_bar.esm.js
+++ web_search_with_and/static/src/js/search_bar.esm.js
@@ -3,6 +3,10 @@ import {SearchBar} from "@web/search/search_bar/search_bar";
 
 patch(SearchBar.prototype, {
     selectItem(item) {
+        if (item.isAddCustomFilterButton) {
+            return this.env.searchModel.spawnCustomFilterDialog();
+        }
+
         const searchItem = this.getSearchItem(item.searchItemId);
         if (
             (searchItem.type === "field" && searchItem.fieldType === "properties") ||
@@ -14,14 +18,29 @@ patch(SearchBar.prototype, {
 
         if (!item.unselectable) {
             const {searchItemId, label, operator, value} = item;
-            this.env.searchModel.addAutoCompletionValues(searchItemId, {
+            const autoCompleteValues = {
                 label,
                 operator,
                 value,
                 isShiftKey: this.isShiftKey,
-            });
+            };
+            if (value && value[0] === '"' && value[value.length - 1] === '"') {
+                autoCompleteValues.value = value.slice(1, -1);
+                autoCompleteValues.label = label.slice(1, -1);
+                autoCompleteValues.operator = "=";
+                autoCompleteValues.enforceEqual = true;
+            }
+            this.env.searchModel.addAutoCompletionValues(
+                searchItemId,
+                autoCompleteValues
+            );
         }
-        this.resetState();
+
+        if (item.loadMore) {
+            item.loadMore();
+        } else {
+            this.resetState();
+        }
     },
 
     onSearchKeydown(ev) {
