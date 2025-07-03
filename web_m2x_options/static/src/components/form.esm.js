191cf9f6eb051d750304c0cf3f019a34339d9598
Christopher Rogos <crogos@gmail.com>
2025-07-03 13:46:47 +0000

    [FIX] web_m2x_options: Handle undefined m2m_options_props in many2one_reference

--- web_m2x_options/static/src/components/form.esm.js
+++ web_m2x_options/static/src/components/form.esm.js
@@ -112,6 +112,9 @@ patch(many2OneField, {
             {attrs, context, decorations, options, string},
             dynamicInfo
         );
+        if (!this.m2m_options_props) {
+            return props;
+        }
         return this.m2o_options_props(props, attrs, options);
     },
 });
