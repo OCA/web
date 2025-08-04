0aa004bec4afcef722d5cf16d44486710d0ec3f3
Julien Ortet <julien@le-filament.com>
2025-08-04 16:14:45 +0200

    [MIG] web_save_discard_button: Migration to 18.0

--- web_save_discard_button/__manifest__.py
+++ web_save_discard_button/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Save & Discard Buttons",
-    "version": "17.0.1.0.1",
+    "version": "18.0.1.0.0",
     "summary": "Save & Discard Buttons",
     "license": "AGPL-3",
     "category": "Tools",
