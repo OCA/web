ab4be69f8c553365d9d14fa2dfbc9d31e359b830
Arnau <arnau.cruz@forgeflow.com>
2025-08-06 17:23:02 +0200

    [MIG] web_copy_confirm: Migration to 18.0

--- web_copy_confirm/__manifest__.py
+++ web_copy_confirm/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Show confirmation dialogue before copying records",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Dynapps,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
@@ -15,7 +15,7 @@
         "web.assets_backend": [
             "web_copy_confirm/static/src/js/web_copy_confirm.esm.js",
         ],
-        "web.qunit_suite_tests": [
+        "web.assets_unit_tests": [
             "web_copy_confirm/static/tests/**/*",
         ],
     },
