621a3b3fba375c49894efe783b95752ed4a458b3
Arnau <arnau.cruz@forgeflow.com>
2025-08-06 17:23:02 +0200

    [MIG] web_copy_confirm: Migration to 18.0

--- web_copy_confirm/static/src/js/web_copy_confirm.esm.js
+++ web_copy_confirm/static/src/js/web_copy_confirm.esm.js
@@ -1,18 +1,34 @@
-/** @odoo-module **/
-
-import {patch} from "web.utils";
+import {patch} from "@web/core/utils/patch";
 import {ConfirmationDialog} from "@web/core/confirmation_dialog/confirmation_dialog";
 import {_t} from "@web/core/l10n/translation";
 import {FormController} from "@web/views/form/form_controller";
+import {ListController} from "@web/views/list/list_controller";
 
-patch(FormController.prototype, "DuplicateConfirmation", {
+patch(FormController.prototype, {
     async duplicateRecord() {
-        var _super = this._super;
         await this.dialogService.add(ConfirmationDialog, {
             title: _t("Duplicate"),
             body: _t("Are you sure that you would like to copy this record?"),
             confirm: () => {
-                _super.apply(this, arguments);
+                super.duplicateRecord();
+            },
+            cancel: () => {
+                // `ConfirmationDialog` needs this prop to display the cancel
+                // button but we do nothing on cancel.
+            },
+        });
+    },
+});
+
+patch(ListController.prototype, {
+    async duplicateRecords() {
+        await this.dialogService.add(ConfirmationDialog, {
+            title: _t("Duplicate"),
+            body: _t(
+                "Are you sure that you would like to duplicate the selected records?"
+            ),
+            confirm: () => {
+                super.duplicateRecords();
             },
             cancel: () => {
                 // `ConfirmationDialog` needs this prop to display the cancel
