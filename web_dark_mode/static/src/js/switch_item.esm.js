523b9e350bfbf56fe887bf4c7d29f30c05de4679
len <len@lambdao.dev>
2025-08-30 14:33:29 +0200

    [MIG] web_dark_mode: Migration to 18.0

--- web_dark_mode/static/src/js/switch_item.esm.js
+++ web_dark_mode/static/src/js/switch_item.esm.js
@@ -1,40 +1,41 @@
 /** @odoo-module **/
 
+import {_t} from "@web/core/l10n/translation";
 import {browser} from "@web/core/browser/browser";
+import {cookie} from "@web/core/browser/cookie";
 import {registry} from "@web/core/registry";
+import {user} from "@web/core/user";
 
 export function darkModeSwitchItem(env) {
     return {
         type: "switch",
         id: "color_scheme.switch",
-        description: env._t("Dark Mode"),
+        description: _t("Dark Mode"),
         callback: () => {
             env.services.color_scheme.switchColorScheme();
         },
-        isChecked: env.services.cookie.current.color_scheme === "dark",
+        isChecked: cookie.get("color_scheme") === "dark",
         sequence: 40,
     };
 }
 
 export const colorSchemeService = {
-    dependencies: ["cookie", "orm", "ui", "user"],
+    dependencies: ["orm", "ui"],
 
-    async start(env, {cookie, orm, ui, user}) {
+    async start(env, {orm, ui}) {
         registry.category("user_menuitems").add("darkmode", darkModeSwitchItem);
 
-        if (!cookie.current.color_scheme) {
+        if (!cookie.get("color_scheme")) {
             const match_media = window.matchMedia("(prefers-color-scheme: dark)");
             const dark_mode = match_media.matches;
-            cookie.setCookie("color_scheme", dark_mode ? "dark" : "light");
+            cookie.set("color_scheme", dark_mode ? "dark" : "light");
             if (dark_mode) browser.location.reload();
         }
 
         return {
             async switchColorScheme() {
-                const scheme =
-                    cookie.current.color_scheme === "dark" ? "light" : "dark";
-                cookie.setCookie("color_scheme", scheme);
-
+                const scheme = cookie.get("color_scheme") === "dark" ? "light" : "dark";
+                cookie.set("color_scheme", scheme);
                 await orm.write("res.users", [user.userId], {
                     dark_mode: scheme === "dark",
                 });
