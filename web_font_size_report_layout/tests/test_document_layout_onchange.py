68cf1b358e2b8a6e85c71414a725ef65031e088c
Mario Montes <admin@syci.es>
2025-09-03 11:30:26 +0100

    [FIX] web_font_size_report_layout: tests

--- web_font_size_report_layout/tests/test_document_layout_onchange.py
+++ web_font_size_report_layout/tests/test_document_layout_onchange.py
@@ -4,7 +4,7 @@ from odoo.tests.common import TransactionCase, tagged
 
 
 @tagged("post_install", "-at_install")
-class TestDocumentLayoutWizard(TransactionCase):
+class TestDocumentLayoutOnchange(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -12,53 +12,36 @@ class TestDocumentLayoutWizard(TransactionCase):
         cls.company = cls.Company.create({"name": "WZ Co", "report_font_size": "11"})
         cls.Wizard = cls.env["base.document.layout"]
 
-    def test_wizard_default_inherits_company_value(self):
+    def test_onchange_when_compute_preview_absent_does_nothing(self):
         wiz = self.Wizard.with_company(self.company.id).create(
             {"company_id": self.company.id}
         )
-        self.assertEqual(wiz.report_font_size, "11")
+        prev = getattr(wiz, "preview", None)
+        wiz.report_font_size = "10"
 
-    def test_onchange_updates_wizard_field_without_crash(self):
-        wiz = self.Wizard.with_company(self.company.id).create(
-            {"company_id": self.company.id}
-        )
-        wiz.report_font_size = "14"
-        if hasattr(wiz, "_onchange_report_font_size"):
+        with patch.object(type(wiz), "_compute_preview", new=None, create=True):
             wiz._onchange_report_font_size()
-        self.assertEqual(wiz.report_font_size, "14")
 
-    def test_apply_wizard_persists_on_company_via_fallback(self):
-        wiz = self.Wizard.with_company(self.company.id).create(
-            {"company_id": self.company.id}
-        )
-        wiz.report_font_size = "12"
-        self.company.write({"report_font_size": wiz.report_font_size})
-        self.assertEqual(self.company.report_font_size, "12")
-
-    def test_apply_uses_action_apply_when_present(self):
-        wiz = self.Wizard.with_company(self.company.id).create(
-            {"company_id": self.company.id}
-        )
-        wiz.report_font_size = "13"
-
-        def _fake_action_apply(self):
-            self.company_id.report_font_size = self.report_font_size
-
-        with patch.object(type(wiz), "action_apply", _fake_action_apply, create=True):
-            wiz.action_apply()
-
-        self.assertEqual(self.company.report_font_size, "13")
+        self.assertEqual(getattr(wiz, "preview", None), prev)
 
     def test_onchange_handles_compute_exception_sets_preview_false(self):
         wiz = self.Wizard.with_company(self.company.id).create(
             {"company_id": self.company.id}
         )
         self.assertIn("preview", wiz._fields)
-
         wiz.report_font_size = "14"
+
         with patch.object(
             type(wiz), "_compute_preview", side_effect=Exception("boom"), create=True
         ):
             wiz._onchange_report_font_size()
 
         self.assertFalse(bool(wiz.preview))
+
+    def test_onchange_updates_wizard_field_without_crash(self):
+        wiz = self.Wizard.with_company(self.company.id).create(
+            {"company_id": self.company.id}
+        )
+        wiz.report_font_size = "14"
+        wiz._onchange_report_font_size()
+        self.assertEqual(wiz.report_font_size, "14")
