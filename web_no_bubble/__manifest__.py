8ad3539ed2224d1ca27be082032e940117a183b7
Bhavesh Heliconia <oca@heliconia.in>
2025-10-17 15:15:29 +0530

    [MIG] web_no_bubble: Migration to 19.0

--- web_no_bubble/__manifest__.py
+++ web_no_bubble/__manifest__.py
@@ -3,8 +3,8 @@
 
 {
     "name": "Web No Bubble",
-    "version": "18.0.1.0.0",
-    "author": "Savoir-faire Linux, " "Odoo Community Association (OCA)",
+    "version": "19.0.1.0.0",
+    "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
     "category": "Web",
