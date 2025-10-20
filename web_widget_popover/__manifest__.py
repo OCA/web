987ccf6821fd1e9ed1ec11890c6f0627f6f2fad7
Maksym Yankin <yankinmk@gmail.com>
2025-10-20 15:39:07 +0300

    [18.0][MIG] web_widget_popover: Migration to 18.0

--- web_widget_popover/__manifest__.py
+++ web_widget_popover/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Web Widget Popover",
     "summary": "Render an icon that displays the field content in a popover",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainers": ["ivantodorovich"],
     "website": "https://github.com/OCA/web",
