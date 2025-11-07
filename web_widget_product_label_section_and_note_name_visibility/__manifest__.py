32b386c9b96a58199e196958387228129f6facad
Webbeh <1530837+Webbeh@users.noreply.github.com>
2025-11-07 18:04:12 +0100

    Migrate product label visibility toggle to 19.0

---
+++ web_widget_product_label_section_and_note_name_visibility/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name": "Web widget product label section and note",
+    "version": "19.0.1.0.0",
+    "summary": "Alternate the visibility of the product and description.",
+    "author": "Tecnativa, Odoo Community Association (OCA), Odoo S.A.",
+    "website": "https://github.com/OCA/web",
+    "depends": [
+        "web",
+        "account",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            (
+                "after",
+                "account/static/src/components/**/*",
+                "web_widget_product_label_section_and_note_name_visibility/static/src/components/**/*",
+            )
+        ],
+    },
+    "installable": True,
+    "auto_install": False,
+    "license": "AGPL-3",
+    "maintainers": ["carlos-lopez-tecnativa"],
+}
---
+++ web_widget_product_label_section_and_note_name_visibility/readme/CONTRIBUTORS.md
@@ -0,0 +1,4 @@
+- [Tecnativa](https://www.tecnativa.com):
+
+  > - Pedro M. Baeza
+  > - Carlos Lopez
