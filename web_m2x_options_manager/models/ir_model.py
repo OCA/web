1405611ee98e2adb7d469254297b77007df1fc99
SilvioC2C <silvio.gregorini@camptocamp.com>
2025-11-10 16:15:24 +0100

    [UPD] web_m2x_options_manager: overhaul
    * align M2ODialog option configuration to other options
    * allow configuring models' inverse fields too
    * add proper menu and views
    * refactoring
    * improve tests

--- web_m2x_options_manager/models/ir_model.py
+++ web_m2x_options_manager/models/ir_model.py
@@ -1,52 +1,64 @@
 # Copyright 2021 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class IrModel(models.Model):
     _inherit = "ir.model"
 
-    m2x_create_edit_option_ids = fields.One2many(
+    m2x_option_ids = fields.One2many(
         "m2x.create.edit.option",
         "model_id",
     )
+    m2x_comodels_option_ids = fields.One2many(
+        "m2x.create.edit.option",
+        "comodel_id",
+    )
+    comodel_field_ids = fields.One2many("ir.model.fields", "comodel_id")
 
-    def button_empty(self):
-        for ir_model in self:
-            ir_model._empty_m2x_create_edit_option()
+    def button_empty_m2x_options(self):
+        self._empty_m2x_options(own=True)
 
-    def button_fill(self):
-        for ir_model in self:
-            ir_model._fill_m2x_create_edit_option()
+    def button_fill_m2x_options(self):
+        self._fill_m2x_options(own=True)
 
-    def _empty_m2x_create_edit_option(self):
-        """Removes every option for model ``self``"""
-        self.ensure_one()
-        self.m2x_create_edit_option_ids.unlink()
+    def button_empty_m2x_comodels_options(self):
+        self._empty_m2x_options(comodels=True)
 
-    def _fill_m2x_create_edit_option(self):
-        """Adds every missing field option for model ``self``"""
-        self.ensure_one()
-        existing = self.m2x_create_edit_option_ids.mapped("field_id")
-        valid = self.field_id.filtered(lambda f: f.ttype in ("many2many", "many2one"))
-        vals = [(0, 0, {"field_id": f.id}) for f in valid - existing]
-        self.write({"m2x_create_edit_option_ids": vals})
+    def button_fill_m2x_comodels_options(self):
+        self._fill_m2x_options(comodels=True)
 
+    def _empty_m2x_options(self, own=False, comodels=False):
+        """Removes every option for model ``self``'s fields
 
-class IrModelFields(models.Model):
-    _inherit = "ir.model.fields"
+        :param bool own: if True, deletes options for model's fields
+        :param bool comodels: if True, deletes options for fields where ``self`` is
+            the field's comodel
+        """
+        to_delete = self.env["m2x.create.edit.option"]
+        if own:
+            to_delete += self.m2x_option_ids
+        if comodels:
+            to_delete += self.m2x_comodels_option_ids
+        if to_delete:
+            to_delete.unlink()
 
-    @api.model
-    def name_search(self, name="", args=None, operator="ilike", limit=100):
-        res = super().name_search(name, args, operator, limit)
-        if not (name and self.env.context.get("search_by_technical_name")):
-            return res
-        domain = list(args or []) + [("name", operator, name)]
-        new_fids = self.search(domain, limit=limit).ids
-        for fid in [x[0] for x in res]:
-            if fid not in new_fids:
-                new_fids.append(fid)
-        if limit and limit > 0:
-            new_fids = new_fids[:limit]
-        return self.browse(new_fids).sudo().name_get()
+    def _fill_m2x_options(self, own=False, comodels=False):
+        """Adds every missing field option for model ``self`` (with default values)
+
+        :param bool own: if True, creates options for model's fields
+        :param bool comodels: if True, creates options for fields where ``self`` is
+            the field's comodel
+        """
+        todo = set()
+        if own:
+            exist = self.m2x_option_ids.field_id
+            valid = self.field_id.filtered("can_have_options")
+            todo.update((valid - exist).ids)
+        if comodels:
+            exist = self.m2x_comodels_option_ids.field_id
+            valid = self.comodel_field_ids.filtered("can_have_options")
+            todo.update((valid - exist).ids)
+        if todo:
+            self.env["m2x.create.edit.option"].create([{"field_id": i} for i in todo])
--- web_m2x_options_manager/models/m2x_create_edit_option.py
+++ web_m2x_options_manager/models/m2x_create_edit_option.py
@@ -8,36 +8,49 @@ from odoo.tools.safe_eval import safe_eval
 
 
 class M2xCreateEditOption(models.Model):
-    _name = "m2x.create.edit.option"
-    _description = "Manage Options 'Create/Edit' For Fields"
+    """Technical model to define M2X option at single field level.
 
+    Each record is uniquely defined by its ``field_id``.
+    """
+
+    _name = "m2x.create.edit.option"
+    _description = "Field 'Create & Edit' Options"
+
+    name = fields.Char(compute="_compute_name", store=True)
     field_id = fields.Many2one(
         "ir.model.fields",
-        domain=[("ttype", "in", ("many2many", "many2one"))],
+        domain=[("can_have_options", "=", True)],
         ondelete="cascade",
         required=True,
+        index=True,
         string="Field",
     )
-
     field_name = fields.Char(
         related="field_id.name",
         store=True,
         string="Field Name",
     )
-
     model_id = fields.Many2one(
         "ir.model",
-        ondelete="cascade",
-        required=True,
+        related="field_id.model_id",
+        store=True,
         string="Model",
     )
-
     model_name = fields.Char(
-        compute="_compute_model_name",
-        inverse="_inverse_model_name",
+        related="field_id.model",
         store=True,
     )
-
+    comodel_id = fields.Many2one(
+        "ir.model",
+        related="field_id.comodel_id",
+        store=True,
+        string="Comodel",
+    )
+    comodel_name = fields.Char(
+        related="field_id.relation",
+        store=True,
+        string="Comodel Name",
+    )
     option_create = fields.Selection(
         [
             ("none", "Do nothing"),
@@ -56,7 +69,6 @@ class M2xCreateEditOption(models.Model):
         required=True,
         string="Create Option",
     )
-
     option_create_edit = fields.Selection(
         [
             ("none", "Do nothing"),
@@ -75,86 +87,125 @@ class M2xCreateEditOption(models.Model):
         required=True,
         string="Create & Edit Option",
     )
-
-    option_create_edit_wizard = fields.Boolean(
-        default=True,
+    option_m2o_dialog = fields.Selection(
+        [
+            ("none", "Do nothing"),
+            ("set_true", "Add"),
+            ("force_true", "Force Add"),
+            ("set_false", "Remove"),
+            ("force_false", "Force Remove"),
+        ],
+        default="set_false",
         help="Defines behaviour for 'Create & Edit' Wizard\n"
-        "Set to False to prevent 'Create & Edit' Wizard to pop up",
+        "* Do nothing: nothing is done\n"
+        "* Add/Remove: the 'Create & Edit' Wizard is displayed/hidden only if the"
+        " related attribute is not explicitly set in the view definition\n"
+        "* Force Add/Remove: the 'Create & Edit' Wizard is displayed/hidden overriding"
+        " the related attribute, even if explicitly set in the view definition",
         string="Create & Edit Wizard",
     )
 
     _sql_constraints = [
         (
-            "model_field_uniqueness",
-            "unique(field_id,model_id)",
-            "Options must be unique for each model/field couple!",
+            "field_uniqueness",
+            "unique(field_id)",
+            "Options must be unique for each field!",
         ),
     ]
 
     @api.model_create_multi
     def create(self, vals_list):
-        # Clear cache to avoid misbehavior from cached :meth:`_get()`
-        type(self)._get.clear_cache(self.browse())
+        # Clear cache to avoid misbehavior from cached methods
+        self.clear_caches()
         return super().create(vals_list)
 
     def write(self, vals):
-        # Clear cache to avoid misbehavior from cached :meth:`_get()`
-        type(self)._get.clear_cache(self.browse())
+        # Clear cache to avoid misbehavior from cached methods
+        if set(vals).intersection(["field_id"] + self._get_option_fields()):
+            self.clear_caches()
         return super().write(vals)
 
     def unlink(self):
-        # Clear cache to avoid misbehavior from cached :meth:`_get()`
-        type(self)._get.clear_cache(self.browse())
+        # Clear cache to avoid misbehavior from cached methods
+        self.clear_caches()
         return super().unlink()
 
-    @api.depends("model_id")
-    def _compute_model_name(self):
+    @api.depends("field_id")
+    def _compute_name(self):
         for opt in self:
-            opt.model_name = opt.model_id.model
-
-    def _inverse_model_name(self):
-        getter = self.env["ir.model"]._get
-        for opt in self:
-            # This also works as a constrain: if ``model_name`` is not a
-            # valid model name, then ``model_id`` will be emptied, but it's
-            # a required field!
-            opt.model_id = getter(opt.model_name)
-
-    @api.constrains("model_id", "field_id")
-    def _check_field_in_model(self):
-        for opt in self:
-            if opt.field_id.model_id != opt.model_id:
-                msg = _(
-                    "%(field)s is not a valid field for model %(model)s!",
-                    field=opt.field_name,
-                    model=opt.model_name,
-                )
-                raise ValidationError(msg)
+            try:
+                opt.name = str(self.env[opt.field_id.model]._fields[opt.field_id.name])
+            except KeyError:
+                opt.name = "Invalid field"
 
     @api.constrains("field_id")
-    def _check_field_type(self):
-        ttypes = ("many2many", "many2one")
-        if any(o.field_id.ttype not in ttypes for o in self):
-            msg = _("Only Many2many and Many2one fields can be chosen!")
-            raise ValidationError(msg)
+    def _check_field_can_have_options(self):
+        for opt in self:
+            if opt.field_id and not opt.field_id.can_have_options:
+                raise ValidationError(
+                    _(
+                        "Field %(field)s cannot have M2X options",
+                        field=opt.field_id.display_name,
+                    )
+                )
 
     def _apply_options(self, node):
-        """Applies options ``self`` to ``node``"""
+        """Applies options ``self`` to ``node``
+
+        :param etree._Element node: view ``<field/>`` node to update
+        :rtype: None
+        """
+        self.ensure_one()
+        node_options = self._read_node_options(node)
+        for key, (mode, value) in self._read_own_options().items():
+            if mode == "force" or key not in node_options:
+                node_options[key] = value
+        node.set("options", str(node_options))
+
+    def _read_node_options(self, node):
+        """Helper method to read "options" attribute on ``node``
+
+        :param etree._Element node: view ``<field/>`` node to parse
+        :rtype: dict[str, Any]
+        """
         self.ensure_one()
         options = node.attrib.get("options") or {}
         if isinstance(options, str):
-            options = safe_eval(options, dict(self.env.context or [])) or {}
-        for k in ("create", "create_edit"):
-            opt = self["option_%s" % k]
-            if opt == "none":
-                continue
-            mode, val = opt.split("_")
-            if mode == "force" or k not in options:
-                options[k] = val == "true"
-        node.set("options", str(options))
-        if not self.option_create_edit_wizard:
-            node.set("can_create", "false")
-            node.set("can_write", "false")
+            options = safe_eval(options, self._get_node_options_eval_context()) or {}
+        return dict(options)
+
+    def _get_node_options_eval_context(self):
+        """Helper method to get eval context to read "options" attribute from a node
+
+        :rtype: dict
+        """
+        self.ensure_one()
+        eval_ctx = dict(self.env.context or [])
+        eval_ctx.update({"context": dict(eval_ctx)})
+        return eval_ctx
+
+    def _read_own_options(self):
+        """Helper method to retrieve M2X options from ``self``
+
+        :return: a dictionary mapping each M2X option to its mode and value, eg:
+            {'create': ('force', 'true'), 'create_edit': ('set', 'false')}
+        :rtype: dict[str, tuple[str, Any]]
+        """
+        self.ensure_one()
+        res = {}
+        for fname, fvalue in self.read(self._get_option_fields())[0].items():
+            if fname != "id" and fvalue != "none":
+                mode, value = tuple(fvalue.split("_"))
+                res[fname.replace("option_", "")] = (mode, value == "true")
+        return res
+
+    def _get_option_fields(self):
+        """Helper method to retrieve field names to parse as M2X options
+
+        :return: list of field names to parse as M2X options
+        :rtype: list[str]
+        """
+        return ["option_create", "option_create_edit", "option_m2o_dialog"]
 
     @api.model
     def get(self, model_name, field_name):
@@ -162,22 +213,29 @@ class M2xCreateEditOption(models.Model):
 
         :param str model_name: technical model name (i.e. "sale.order")
         :param str field_name: technical field name (i.e. "partner_id")
+        :return: a ``m2x.create.edit.option`` record
+        :rtype: M2xCreateEditOption
         """
-        return self.browse(self._get(model_name, field_name))
+        return self.browse(self._get_id(model_name, field_name))
 
     @api.model
     @ormcache("model_name", "field_name")
-    def _get(self, model_name, field_name):
+    def _get_id(self, model_name, field_name):
         """Inner implementation of ``get``.
+
         An ID is returned to allow caching (see :class:`ormcache`); :meth:`get`
         will then convert it to a proper record.
 
         :param str model_name: technical model name (i.e. "sale.order")
         :param str field_name: technical field name (i.e. "partner_id")
+        :return: a ``m2x.create.edit.option`` record ID
+        :rtype: int
         """
-        dom = [
-            ("model_name", "=", model_name),
-            ("field_name", "=", field_name),
-        ]
-        # `_check_field_model_uniqueness()` grants uniqueness if existing
-        return self.search(dom, limit=1).id
+        opt_id = 0
+        field = self.env["ir.model.fields"]._get(model_name, field_name)
+        if field:
+            # SQL constraint grants record uniqueness (if existing)
+            opt = self.search([("field_id", "=", field.id)], limit=1)
+            if opt:
+                opt_id = opt.id
+        return opt_id
