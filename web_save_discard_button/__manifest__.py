2704999bc86d6479b4ad8ff5386a73a31676d113
Saran440 <saranl@ecosoft.co.th>
2025-12-12 11:10:55 +0700

    [FIX] web_save_discard_button: readme

--- web_save_discard_button/__manifest__.py
+++ web_save_discard_button/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Save & Discard Buttons",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "summary": "Save & Discard Buttons",
     "license": "AGPL-3",
     "category": "Tools",
