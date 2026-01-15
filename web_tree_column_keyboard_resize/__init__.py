f53f2242711ee52763e490cd6d8acbb1c34235d8
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2026-01-15 10:20:51 +0100

    [ADD] web_tree_column_keyboard_resize

---
+++ web_tree_column_keyboard_resize/__init__.py
@@ -0,0 +1,2 @@
+# Copyright 2026 ForgeFlow (https://www.forgeflow.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
---
+++ web_tree_column_keyboard_resize/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2026 ForgeFlow (https://www.forgeflow.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Web Tree Column Keyboard Resize",
+    "summary": "Allow resizing tree view columns using keyboard shortcuts",
+    "version": "18.0.1.0.0",
+    "category": "Extra Tools",
+    "website": "https://github.com/OCA/web",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["web"],
+    "assets": {
+        "web.assets_backend": [
+            "web_tree_column_keyboard_resize/static/src/js/list_renderer.esm.js",
+        ],
+    },
+}
