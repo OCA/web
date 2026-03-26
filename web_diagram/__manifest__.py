c20cb81f8c71ba8bd1303c9bb7bbdaefa1561cc3
Mathieu Benoit <mathben@technolibre.ca>
2026-03-26 15:19:33 -0400

    [MIG] web_diagram: move from odoo
    - apply OCA script migration with OpenUpgrade

---
+++ web_diagram/__manifest__.py
@@ -0,0 +1,21 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Odoo Web Diagram',
+    'category': 'Hidden',
+    'description': """
+Openerp Web Diagram view.
+=========================
+
+""",
+    'version': "14.0.1.0.0",
+    'depends': ['web'],
+    'data': [
+        'views/web_diagram_templates.xml',
+    ],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
