f46415c56051f793f798cca7228b1d2df0575ea2
Mathieu Benoit <mathben@technolibre.ca>
2026-03-27 00:17:51 -0400

    [MIG] web_diagram support odoo 18

--- web_diagram/__manifest__.py
+++ web_diagram/__manifest__.py
@@ -8,7 +8,7 @@ Openerp Web Diagram view.
 =========================
 
 """,
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'depends': ['web'],
     'assets': {
         'web.assets_backend': [
