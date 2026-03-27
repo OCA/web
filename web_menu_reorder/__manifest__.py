42f89367422066905bcf3d821f1c76f50460a254
Mathieu Benoit <mathben@technolibre.ca>
2026-03-27 18:17:07 -0400

    [IMP] web_menu_reorder

---
+++ web_menu_reorder/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2026 TechnoLibre (https://technolibre.ca)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Web Menu Reorder",
+    "version": "18.0.1.0.0",
+    "category": "Technical",
+    "summary": "Reorder top-level menus alphabetically or by custom label list",
+    "author": "TechnoLibre",
+    "license": "AGPL-3",
+    "website": "https://technolibre.ca",
+    "depends": [
+        "base",
+        "web",
+    ],
+    "data": [
+        "views/res_config_settings_views.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
