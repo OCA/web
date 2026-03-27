cdec4b5905f410a90b18803205f81316241bf14e
Mathieu Benoit <mathben@technolibre.ca>
2026-03-27 00:11:57 -0400

    [MIG] web_diagram migrate to odoo 17

--- web_diagram/__manifest__.py
+++ web_diagram/__manifest__.py
@@ -8,7 +8,7 @@ Openerp Web Diagram view.
 =========================
 
 """,
-    'version': "16.0.1.0.0",
+    'version': "17.0.1.0.0",
     'depends': ['web'],
     'assets': {
         'web.assets_backend': [
