d8031332da40ae1854130c6e9f6270ac79b37bcb
Jordan Andres Pincay Vinces <j.pincayvinces@gmail.com>
2026-04-02 18:10:30 +0000

    [MIG] web_copy_confirm: Migration to 19.0

--- web_copy_confirm/static/src/js/web_copy_confirm.esm.js
+++ web_copy_confirm/static/src/js/web_copy_confirm.esm.js
@@ -2,7 +2,7 @@ import {patch} from "@web/core/utils/patch";
 import {ConfirmationDialog} from "@web/core/confirmation_dialog/confirmation_dialog";
 import {_t} from "@web/core/l10n/translation";
 import {FormController} from "@web/views/form/form_controller";
-import {ListController} from "@web/views/list/list_controller";
+import {DynamicList} from "@web/model/relational_model/dynamic_list";
 
 patch(FormController.prototype, {
     async duplicateRecord() {
@@ -20,20 +20,21 @@ patch(FormController.prototype, {
     },
 });
 
-patch(ListController.prototype, {
-    async duplicateRecords() {
-        await this.dialogService.add(ConfirmationDialog, {
-            title: _t("Duplicate"),
-            body: _t(
-                "Are you sure that you would like to duplicate the selected records?"
-            ),
-            confirm: () => {
-                super.duplicateRecords();
-            },
-            cancel: () => {
-                // `ConfirmationDialog` needs this prop to display the cancel
-                // button but we do nothing on cancel.
-            },
+patch(DynamicList.prototype, {
+    duplicateRecords(records = []) {
+        return new Promise((resolve) => {
+            this.model.dialog.add(ConfirmationDialog, {
+                title: _t("Duplicate"),
+                body: _t(
+                    "Are you sure that you would like to duplicate the selected records?"
+                ),
+                confirm: () => {
+                    resolve(super.duplicateRecords(records));
+                },
+                cancel: () => {
+                    resolve();
+                },
+            });
         });
     },
 });
