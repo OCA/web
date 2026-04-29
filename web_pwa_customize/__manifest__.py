0ba7d6341f4a7b63318d908f76e7cefa94058b9e
Javier Carpio <carpio701021@gmail.com>
2026-04-29 09:45:19 -0600

    Update web_pwa_customize/__manifest__.py
    Change version
    
    Co-authored-by: Víctor Martínez <victoralmau@users.noreply.github.com>

--- web_pwa_customize/__manifest__.py
+++ web_pwa_customize/__manifest__.py
@@ -4,7 +4,6 @@
     "name": "Web Pwa Customize",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
-    "version": "18.0.1.0.1",
     "depends": ["web"],
     "license": "AGPL-3",
     "category": "Website",
