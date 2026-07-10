dd68663de4d358e17c152baaaa18ccbce48575ee
abdulla bashir <abdullagozer@gmail.com>
2026-07-10 03:51:14 +0200

    Update asset inclusion in __manifest__.py

--- web_notify/__manifest__.py
+++ web_notify/__manifest__.py
@@ -14,7 +14,8 @@
     "depends": ["web", "bus", "base", "mail"],
     "assets": {
         "web.assets_backend": [
-            "web_notify/static/src/js/services/*.js",
+            "web_notify/static/src/js/services/notification_services.esm.js",
+            # "web_notify/static/src/js/services/*.js",
         ]
     },
     # "demo": ["views/res_users_demo.xml"],
